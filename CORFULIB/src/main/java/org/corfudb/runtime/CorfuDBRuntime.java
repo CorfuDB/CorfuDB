c732faa2629370f5f178ccd0483599ef6fe0867d
Mahesh Balakrishnan <maheshb@gmail.com>
2015-01-15 15:47:36 -0800

    efficient linearization, support for concurrent clients, tx bookkeeping

--- CORFULIB/src/main/java/org/corfudb/runtime/CorfuDBRuntime.java
+++ CORFULIB/src/main/java/org/corfudb/runtime/CorfuDBRuntime.java
@@ -1,12 +1,24 @@
+/**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.corfudb.runtime;
 
 import java.nio.ByteBuffer;
-import java.util.LinkedList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.Stack;
-import java.util.Iterator;
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.corfudb.sharedlog.ClientLib;
 import org.corfudb.sharedlog.CorfuException;
@@ -45,13 +57,42 @@ public class CorfuDBRuntime
 			throw e;
 		}
 
+
+		int numthreads;
+
+		//turn on to test ClientLib in isolation
+/*		numthreads = 2;
+		for(int i=0;i<numthreads;i++)
+		{
+			Thread T = new Thread(new ClientLibTester(masternode));
+			T.start();
+		}
+		Thread.sleep(10000);
+		if(true) return;
+*/
+
 		List<Long> streams = new LinkedList<Long>();
 		streams.add(new Long(1234)); //hardcoded hack
-		CorfuDBRuntime TR = new CorfuDBRuntime(new StreamBundleImpl(streams, new CorfuStreamingSequencer(crf), new CorfuLogAddressSpace(crf)));
+
+		StreamBundle sb = new StreamBundleImpl(streams, new CorfuStreamingSequencer(crf), new CorfuLogAddressSpace(crf));
+
+		//turn on to test stream bundle in isolation
+/*
+		numthreads = 2;
+		for(int i=0;i<numthreads;i++)
+		{
+			Thread T = new Thread(new StreamBundleTester(sb));
+			T.start();
+		}
+		Thread.sleep(10000);
+		if(true) return;
+*/
+
+		CorfuDBRuntime TR = new CorfuDBRuntime(sb);
 
 		CorfuDBCounter ctr = new CorfuDBCounter(TR, 1234);
-		
-		int numthreads = 1;
+
+		numthreads = 2;
 		for(int i=0;i<numthreads;i++)
 		{
 			Thread T = new Thread(new CorfuDBTester(ctr));
@@ -62,6 +103,52 @@ public class CorfuDBRuntime
 	Map<Long, CorfuDBObject> objectmap;
 	StreamBundle curbundle;
 
+	//used to coordinate between querying threads and the sync thread
+	Lock queuelock;
+	List<Object> curqueue;
+
+	final ThreadLocal<TxInt> curtx = new ThreadLocal<TxInt>();
+
+	class Pair<X, Y>
+	{
+		final X first;
+		final Y second;
+		Pair(X f, Y s)
+		{
+			if(f==null || s==null) throw new RuntimeException("null values not allowed in pair");
+			first = f;
+			second = s;
+		}
+
+		public boolean equals(X cf, Y cs)
+		{
+			//neither first nor second is allowed to be null
+			if(first.equals(cf) && second.equals(cs))
+				return true;
+			return false;
+		}
+	}
+
+	class TxInt
+	{
+		List<BufferStack> bufferedupdates;
+		Set<Long> streamset;
+		Set<Pair<Long, Long>> readset;
+		TxInt()
+		{
+			bufferedupdates = new LinkedList<BufferStack>();
+			readset = new HashSet<Pair<Long, Long>>();
+		}
+		void buffer_update(BufferStack bs, long stream)
+		{
+			bufferedupdates.add(bs);
+			streamset.add(stream);
+		}
+		void mark_read(long object, long version)
+		{
+			readset.add(new Pair(object, version));
+		}
+	}
 
 	/**
 	 * Registers an object with the runtime
@@ -85,24 +172,90 @@ public class CorfuDBRuntime
 	{
 		objectmap = new HashMap<Long, CorfuDBObject>();
 		curbundle = sb;
+
+		queuelock = new ReentrantLock();
+		curqueue = new LinkedList<Object>();
+
+		//start the sync thread
+		new Thread(new Runnable()
+		{
+			public void run()
+			{
+				while(true)
+				{
+					sync();
+				}
+			}
+		}).start();
 	}
+
+
+
+	void BeginTX()
+	{
+		if(curtx.get()!=null) //there's already an executing tx
+			throw new RuntimeException("tx already executing!"); //should we do something different to support nested txes?
+		curtx.set(new TxInt());
+	}
+
+	boolean EndTX()
+	{
+		return false;
+	}
+
 
 	void queryhelper(long sid)
 	{
-		//later, asynchronously invoke the sync() thread
-		//so that there's only one outstanding sync at a time
-		sync();
+		if(curtx.get()==null) //not in a transactional context, sync immediately
+		{
+			Object syncobj = new Object();
+			synchronized (syncobj)
+			{
+				queuelock.lock();
+				curqueue.add(syncobj);
+				queuelock.unlock();
+				try
+				{
+					syncobj.wait();
+				}
+				catch (InterruptedException ie)
+				{
+					throw new RuntimeException(ie);
+				}
+			}
+		}
+		else //transactional context, update read set of tx intention
+		{
+			curtx.get().mark_read(sid, -1); //todo: what should the version number be?
+		}
 	}
 
 	void updatehelper(BufferStack update, long sid)
 	{
-		List<Long> streams = new LinkedList<Long>();
-		streams.add(sid);
-		curbundle.append(update, streams);
+		if(curtx.get()==null) //not in a transactional context, append immediately to the streambundle
+		{
+			List<Long> streams = new LinkedList<Long>();
+			streams.add(sid);
+			curbundle.append(update, streams);
+		}
+		else //in a transactional context, buffer for now
+			curtx.get().buffer_update(update, sid);
 	}
 
+	//runs in a single thread
+	//todo: currently sync keeps running even if there are no queries; we need to run it on demand
 	void sync()
 	{
+		queuelock.lock();
+		//to ensure linearizability, any pending queries have to wait for the conclusion
+		//a checkTail that started *after* they were issued. accordingly, when sync starts up,
+		//it rotates out the current queue of pending requests to stop new requests from entering it
+		List<Object> procqueue = curqueue;
+		curqueue = new LinkedList<Object>();
+		queuelock.unlock();
+
+
+		//check the current tail of the bundle, and then read the bundle until that position
 		long curtail = curbundle.checkTail();
 		LogEntry update = curbundle.readNext(curtail);
 		while(update!=null)
@@ -117,6 +270,18 @@ public class CorfuDBRuntime
 					throw new RuntimeException("entry for stream with no registered object");
 			}
 			update = curbundle.readNext(curtail);
+		}
+
+		//wake up all waiting query threads; they will now see a state that incorporates all updates
+		//that finished before they started
+		Iterator it = procqueue.iterator();
+		while(it.hasNext())
+		{
+			Object syncobj = it.next();
+			synchronized(syncobj)
+			{
+				syncobj.notifyAll();
+			}
 		}
 	}
 }
@@ -202,6 +367,64 @@ class CorfuDBTester implements Runnable
 		}
 	}
 
+}
+
+class ClientLibTester implements Runnable
+{
+	ClientLib cl;
+	public ClientLibTester(String master) throws Exception
+	{
+		cl = new ClientLib(master);
+	}
+	public void run()
+	{
+		try
+		{
+			System.out.println("starting cb tester thread");
+			while (true)
+			{
+				int op = 0;
+				if (op == 0)
+				{
+					byte x[] = new byte[cl.grainsize()];
+					List<Long> T = new LinkedList<Long>();
+					T.add(new Long(5));
+					cl.appendExtnt(x,x.length);
+				}
+				else
+					continue;
+			}
+		}
+		catch(CorfuException ce)
+		{
+			throw new RuntimeException(ce);
+		}
+	}
+}
+
+class StreamBundleTester implements Runnable
+{
+	StreamBundle sb;
+	public StreamBundleTester(StreamBundle tsb)
+	{
+		sb = tsb;
+	}
+	public void run()
+	{
+		System.out.println("starting sb tester thread");
+		while(true)
+		{
+			int op = 0;
+			if(op==0)
+			{
+				byte x[] = new byte[5];
+				List<Long> T = new LinkedList<Long>();
+				T.add(new Long(5));
+				sb.append(new BufferStack(x), T);
+			}
+			else continue;
+		}
+	}
 }
 
 
@@ -501,7 +724,9 @@ class StreamBundleImpl implements StreamBundle
 		mystreams = streamids;
 	}
 
-	public long append(BufferStack bs, List<Long> streamids)
+	//todo we are currently synchronizing on 'this' because ClientLib crashes on concurrent access;
+	//once ClientLib is fixed, we need to clean up StreamBundle's locking
+	public synchronized long append(BufferStack bs, List<Long> streamids)
 	{
 		long ret = ss.get_slot(streamids);
 		las.write(ret, bs);
@@ -511,9 +736,9 @@ class StreamBundleImpl implements StreamBundle
 
 	public synchronized long checkTail() //for now, using 'this' to synchronize curtail
 	{
-		System.out.println("Checking tail...");
+//		System.out.println("Checking tail...");
 		curtail = ss.check_tail();
-		System.out.println("tail is " + curtail);
+//		System.out.println("tail is " + curtail);
 		return curtail;
 
 	}
