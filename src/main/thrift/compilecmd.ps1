c26c8c30b0fede7c09b121ca8040094c30830275
Michael Wei <mwei@cs.ucsd.edu>
2015-01-24 16:44:27 -0800

    removing uneeded thrift stuff.

--- src/main/thrift/compilecmd.ps1
+++
@@ -1,6 +0,0 @@
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition | split-path -parent
-$thrift='c:\Program Files\thrift-0.9.1.exe'
-
-[scriptblock]$sb = { cd $scriptPath\thrift ; ls *.thrift | %{ write-host $_; & $thrift --gen java -out $scriptPath\java $_ } }
-& $sb
-
