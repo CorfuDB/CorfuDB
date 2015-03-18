5d35c592114bf7b75fc61e31373540a89c857bd5
Michael Wei <mwei@cs.ucsd.edu>
2015-03-18 15:54:41 -0700

    configuration master documentation

--- src/main/java/org/corfudb/client/configmasters/IConfigMaster.java
+++ src/main/java/org/corfudb/client/configmasters/IConfigMaster.java
@@ -31,12 +31,62 @@ import java.util.UUID;
 
 public interface IConfigMaster extends IServerProtocol {
 
+    /**
+     * Adds a new stream to the system.
+     *
+     * @param logID         The ID of the log the stream starts on.
+     * @param streamID      The streamID of the stream.
+     * @param startPos      The start position of the stream.
+     *
+     * @return              True if the stream was successfully added to the system, false otherwise.
+     */
     boolean addStream(UUID logID, UUID streamID, long startPos);
+
+    /**
+     * Gets information about a stream in the system.
+     *
+     * @param streamID      The ID of the stream to retrieve.
+     *
+     * @return              A StreamData object containing information about the stream, or null if the
+     *                      stream could not be found.
+     */
     StreamData getStream(UUID streamID);
+
+    /**
+     * Adds a log to the system.
+     *
+     * @param logID         The ID of the log to add.
+     * @param path          True, if the log was added to the system, or false otherwise.
+     */
     boolean addLog(UUID logID, String path);
+
+    /**
+     * Gets information about all logs known to the system.
+     *
+     * @return              A map containing all logs known to the system.
+     */
     Map<UUID, String> getAllLogs();
+
+    /**
+     * Gets the configuration string for a log, given its id.
+     *
+     * @param logID         The ID of the log to retrieve.
+     *
+     * @return              The configuration string used to access that log.
+     */
     String getLog(UUID logID);
+
+    /**
+     * Sends gossip to this configuration master. Unreliable.
+     *
+     * @param gossip        The gossip object to send to the remote configuration master.
+     */
     void sendGossip(IGossip gossip);
+
+    /**
+     * Resets the entire log, and increments the epoch. Use only during testing to restore the system to a
+     * known state.
+     */
     void resetAll();
 }
 
