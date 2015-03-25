186e6484de69b53609f818adae62a232e7bbbd88
Chris Rossbach <crossbach@vmware.com>
2015-03-24 22:55:13 -0700

    stream impl flag in script

--- bnc/btreetest.sh
+++ bnc/btreetest.sh
@@ -97,26 +97,27 @@ crashRecoverTest() {
   xdebug=${6}
   stats=${7}
   threads=${8}
+  streamimpl=${9}
 
   ./bin/corfuDBsingle.sh start > /dev/null 2>&1
-  ./bin/corfuDBTestRuntime.sh CorfuDBTester -m http://localhost:8002/corfu -C $class -A crash -z $crashop -t $threads -n 20 -k 20 -r 0.25 $verbose $xdebug -p 9091 -i bnc/initfs_$size.ser -w bnc/wkldfs_$size.ser $stats > bnc/data/crash-$size-$class-t$threads.txt
-  ./bin/corfuDBTestRuntime.sh CorfuDBTester -m http://localhost:8002/corfu -C $class -A recover -z $recoverop -t $threads -n 20 -k 20 -r 0.25 $verbose $xdebug -p 9091 -i bnc/initfs_$size.ser -w bnc/wkldfs_$size.ser $stats -L bnc/data/crash-$size-$class-t$threads.txt > bnc/data/recover-$size-$class-t$threads.txt
+  ./bin/corfuDBTestRuntime.sh CorfuDBTester -m http://localhost:8002/corfu -s $streamimpl -C $class -A crash -z $crashop -t $threads -n 20 -k 20 -r 0.25 $verbose $xdebug -p 9091 -i bnc/initfs_$size.ser -w bnc/wkldfs_$size.ser $stats > bnc/data/crash-$size-$class-t$threads-s$streamimpl.txt
+  ./bin/corfuDBTestRuntime.sh CorfuDBTester -m http://localhost:8002/corfu -s $streamimpl -C $class -A recover -z $recoverop -t $threads -n 20 -k 20 -r 0.25 $verbose $xdebug -p 9091 -i bnc/initfs_$size.ser -w bnc/wkldfs_$size.ser $stats -L bnc/data/crash-$size-$class-t$threads-s$streamimpl.txt > bnc/data/recover-$size-$class-t$threads-s$streamimpl.txt
   ./bin/corfuDBsingle.sh stop > /dev/null 2>&1
 
-  cat bnc/data/crash-$size-$class-t$threads.txt > tmp.txt
-  cat bnc/data/recover-$size-$class-t$threads.txt >> tmp.txt
-  echo $size-$class-t$threads >> bnc/data/tput-all.csv
+  cat bnc/data/crash-$size-$class-t$threads-s$streamimpl.txt > tmp.txt
+  cat bnc/data/recover-$size-$class-t$threads-s$streamimpl.txt >> tmp.txt
+  echo $size-$class-t$threads-s$streamimpl >> bnc/data/tput-all.csv
   grep TPUT tmp.txt >> bnc/data/tput-all.csv
-  grep TPUT tmp.txt > bnc/data/btree-tput-$size-$class-t$threads.csv
-  grep FSREQLAT tmp.txt > bnc/data/btree-reqlat-$size-$class-t$threads.csv
-  grep FSCMDLAT tmp.txt >> bnc/data/btree-reqlat-$size-$class-t$threads.csv
+  grep TPUT tmp.txt > bnc/data/btree-tput-$size-$class-t$threads-s$streamimpl.csv
+  grep FSREQLAT tmp.txt > bnc/data/btree-reqlat-$size-$class-t$threads-s$streamimpl.csv
+  grep FSCMDLAT tmp.txt >> bnc/data/btree-reqlat-$size-$class-t$threads-s$streamimpl.csv
 }
 
 ./bin/corfuDBsingle.sh stop > /dev/null 2>&1
 if [ "$record" = "TRUE" ]; then
   echo "recording workloads..."
-  #recordWorkload CDBLogicalBTree 30 miniscule 4 -v -x
-  #recordWorkload CDBLogicalBTree 50 tiny 5 -v -x
+  recordWorkload CDBLogicalBTree 30 miniscule 4 -v -x
+  recordWorkload CDBLogicalBTree 50 tiny 5 -v -x
   recordWorkload CDBLogicalBTree 100 small 7 -v -x
   #recordWorkload CDBLogicalBTree 1000 medium 10 -v -x
   #recordWorkload CDBLogicalBTree 10000 large 16 -v -x
@@ -125,14 +126,17 @@ fi
 
 #for size in miniscule tiny small medium large; do
 #for class in CDBLogicalBTree CDBPhysicalBTree; do
+#for streamimpl in 0 1; do
 
 echo "" > bnc/data/tput-all.csv
 for threads in 1; do
+for streamimpl in 0 1; do
 for size in miniscule tiny small; do
 for class in CDBLogicalBTree CDBPhysicalBTree; do
 
-crashRecoverTest $class $size 20 21 -v -x -S $threads
+crashRecoverTest $class $size 20 21 -v -x -S $threads $streamimpl
 
+done
 done
 done
 done
