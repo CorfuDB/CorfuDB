1dd1c8195d11a142711fa2fd47145529f987a56e
Michael Wei <mwei@cs.ucsd.edu>
2015-04-30 15:10:28 -0700

    memory sequencer protocol

---
+++ src/main/java/org/corfudb/runtime/protocols/sequencers/MemorySequencerProtocol.java
@@ -0,0 +1,142 @@
+package org.corfudb.runtime.protocols.sequencers;
+
+import org.corfudb.runtime.NetworkException;
+import org.corfudb.runtime.protocols.IServerProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by mwei on 4/30/15.
+ */
+public class MemorySequencerProtocol implements IStreamSequencer, ISimpleSequencer, IServerProtocol {
+
+    private Logger log = LoggerFactory.getLogger(MemorySequencerProtocol.class);
+    private Map<String,String> options;
+    private String host;
+    private Integer port;
+    private AtomicLong sequenceNumber;
+    private Long epoch;
+    public MemorySequencerProtocol() {
+        this("localhost", 0, new HashMap<String,String>(), 0L);
+    }
+
+    public static IServerProtocol protocolFactory(String host, Integer port, Map<String,String> options, Long epoch)
+    {
+        return new MemorySequencerProtocol(host, port, options, epoch);
+    }
+
+    public MemorySequencerProtocol(String host, Integer port, Map<String,String> options, Long epoch)
+    {
+        this.host = host;
+        this.port = port;
+        this.options = options;
+        this.epoch = epoch;
+        sequenceNumber = new AtomicLong();
+    }
+
+    /**
+     * Returns the full server string.
+     *
+     * @return A server string.
+     */
+    @Override
+    public String getFullString() {
+        return "ms";
+    }
+
+    /**
+     * Returns the host
+     *
+     * @return The hostname for the server.
+     */
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    /**
+     * Returns the port
+     *
+     * @return The port number of the server.
+     */
+    @Override
+    public Integer getPort() {
+        return port;
+    }
+
+    /**
+     * Returns the option map
+     *
+     * @return The option map for the server.
+     */
+    @Override
+    public Map<String, String> getOptions() {
+        return options;
+    }
+
+    /**
+     * Returns a boolean indicating whether or not the server was reachable.
+     *
+     * @return True if the server was reachable, false otherwise.
+     */
+    @Override
+    public boolean ping() {
+        return true;
+    }
+
+    /**
+     * Sets the epoch of the server. Used by the configuration master to switch epochs.
+     *
+     * @param epoch
+     */
+    @Override
+    public void setEpoch(long epoch) {
+        this.epoch =epoch;
+    }
+
+    /**
+     * Resets the server. Used by the configuration master to reset the state of the server.
+     * Should eliminate ALL hard state!
+     *
+     * @param epoch
+     */
+    @Override
+    public void reset(long epoch) throws NetworkException {
+        sequenceNumber.set(epoch);
+    }
+
+    @Override
+    public long sequenceGetNext() throws NetworkException {
+        return sequenceNumber.getAndIncrement();
+    }
+
+    @Override
+    public long sequenceGetNext(int numTokens) throws NetworkException {
+        return sequenceNumber.getAndAccumulate(numTokens, (p,n) -> n+p);
+    }
+
+    @Override
+    public long sequenceGetCurrent() throws NetworkException {
+        return sequenceNumber.get();
+    }
+
+    @Override
+    public long sequenceGetNext(UUID stream, int count) throws NetworkException {
+        return sequenceNumber.getAndAccumulate(count, (p, n) -> n + p);
+    }
+
+    @Override
+    public long sequenceGetCurrent(UUID stream) throws NetworkException {
+        return sequenceNumber.get();
+    }
+
+    @Override
+    public void setAllocationSize(UUID stream, int count) throws NetworkException {
+
+    }
+}
---
+++ src/test/java/org/corfudb/runtime/protocols/sequencers/MemorySequencerProtocolTest.java
@@ -0,0 +1,59 @@
+package org.corfudb.runtime.protocols.sequencers;
+
+import org.corfudb.runtime.protocols.logunits.MemoryLogUnitProtocol;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by mwei on 4/30/15.
+ */
+public class MemorySequencerProtocolTest {
+
+    @Test
+    public void checkIfSequencerIsAlwaysPingable() throws Exception
+    {
+        MemorySequencerProtocol msp = new MemorySequencerProtocol();
+        assertTrue(msp.ping());
+    }
+
+    @Test
+    public void tokensAlwaysIncrement() throws Exception {
+        MemorySequencerProtocol msp = new MemorySequencerProtocol();
+        assertEquals(msp.sequenceGetNext(), 0);
+        assertEquals(msp.sequenceGetNext(), 1);
+        for (int i = 1; i < 100; i++)
+        {
+            assertEquals(msp.sequenceGetNext(), 1 + i);
+        }
+    }
+
+    @Test
+    public void tokenIncrementsByStride() throws Exception {
+        MemorySequencerProtocol msp = new MemorySequencerProtocol();
+        assertEquals(msp.sequenceGetNext(1), 0);
+        assertEquals(msp.sequenceGetNext(2), 1);
+        long last = 3;
+        for (int i = 1; i < 100; i++) {
+            long current = msp.sequenceGetNext(i);
+            assertEquals(last+(i-1), current);
+            last = current;
+        }
+    }
+
+    @Test
+    public void tokenReturnCurrent() throws Exception {
+        MemorySequencerProtocol msp = new MemorySequencerProtocol();
+        assertEquals(msp.sequenceGetNext(), 0);
+        assertEquals(msp.sequenceGetCurrent(), 1);
+        assertEquals(msp.sequenceGetNext(), 1);
+        assertEquals(msp.sequenceGetCurrent(), 2);
+        for (int i = 1; i < 100; i++)
+        {
+            assertEquals(msp.sequenceGetNext(), 1 + i);
+            assertEquals(msp.sequenceGetCurrent(), 2 + i);
+        }
+    }
+
+}
