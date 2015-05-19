0f3025594a01e092bd45c9f087bc97bc79947f31
Michael Wei <mwei@cs.ucsd.edu>
2015-05-18 17:30:32 -0700

    reset integration tests

--- src/test/java/org/corfudb/runtime/CorfuDBRuntimeIT.java
+++ src/test/java/org/corfudb/runtime/CorfuDBRuntimeIT.java
@@ -1,8 +1,6 @@
 package org.corfudb.runtime;
 
-import org.corfudb.runtime.view.IWriteOnceAddressSpace;
-import org.corfudb.runtime.view.Sequencer;
-import org.corfudb.runtime.view.WriteOnceAddressSpace;
+import org.corfudb.runtime.view.*;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,5 +36,34 @@ public class CorfuDBRuntimeIT {
         woas.write(addr, "hello world".getBytes());
         assertThat(woas.read(addr))
                 .isEqualTo("hello world".getBytes());
+    }
+
+    @Test
+    public void isCorfuResettable() throws Exception
+    {
+        CorfuDBRuntime cdr = new CorfuDBRuntime("http://localhost:12700/corfu");
+        cdr.waitForViewReady();
+        IConfigurationMaster cm = new ConfigurationMaster(cdr);
+        cm.resetAll();
+
+        Sequencer s = new Sequencer(cdr);
+        assertThat(s.getCurrent())
+                .isEqualTo(0);
+        s.getNext();
+        cm.resetAll();
+        assertThat(s.getCurrent())
+                .isEqualTo(0);
+
+        IWriteOnceAddressSpace woas = new WriteOnceAddressSpace(cdr);
+        long addr = s.getNext();
+        woas.write(addr, "hello world".getBytes());
+        assertThat(woas.read(addr))
+                .isEqualTo("hello world".getBytes());
+
+        cm.resetAll();
+        addr = s.getNext();
+        woas.write(addr, "hello world 2".getBytes());
+        assertThat(woas.read(addr))
+                .isEqualTo("hello world 2".getBytes());
     }
 }
