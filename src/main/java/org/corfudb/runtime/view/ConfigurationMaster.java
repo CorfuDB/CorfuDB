bd80fd5a5f6b794958fb560b52df000cbc1f81e2
rossbach <chris@rossbach.to>
2015-05-26 12:06:06 -0700

    more visibility into failures at startup time...

--- src/main/java/org/corfudb/runtime/view/ConfigurationMaster.java
+++ src/main/java/org/corfudb/runtime/view/ConfigurationMaster.java
@@ -2,12 +2,19 @@ package org.corfudb.runtime.view;
 
 import org.corfudb.runtime.CorfuDBRuntime;
 import org.corfudb.runtime.NetworkException;
+import org.corfudb.runtime.protocols.IServerProtocol;
 import org.corfudb.runtime.protocols.configmasters.IConfigMaster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * Created by mwei on 5/1/15.
  */
 public class ConfigurationMaster implements IConfigurationMaster {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfigurationMaster.class);
 
     CorfuDBRuntime cdr;
 
@@ -19,10 +26,16 @@ public class ConfigurationMaster implements IConfigurationMaster {
     public void resetAll() {
         while (true) {
             try {
-                ((IConfigMaster) cdr.getView().getConfigMasters().get(0)).resetAll();
+                //((IConfigMaster) cdr.getView().getConfigMasters().get(0)).resetAll();
+                CorfuDBView view = cdr.getView();
+                List<IServerProtocol> masters = view.getConfigMasters();
+                IServerProtocol firstEntry = masters.get(0);
+                IConfigMaster master = (IConfigMaster) firstEntry;
+                master.resetAll();
+                log.info("successful resetAll()");
                 return;
             } catch (Exception e) {
-
+                log.debug("Exception in resetAll: " + e.getMessage() + ", retrying...");
             }
         }
     }
