3e59fc308e0bbe84893417181e6dcc232c1244b1
Michael Wei <mwei@cs.ucsd.edu>
2015-07-15 13:39:55 -0700

    build thrift script

---
+++ .travis_scripts/build_thrift.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+if [ ! -d "$HOME/protobuf/lib" ]; then
+    wget http://apache.mirrors.tds.net/thrift/0.9.2/thrift-0.9.2.tar.gz -O thrift.tar.gz;
+    tar -xzf thrift.tar.gz;
+    cd thrift-0.9.2 && ./configure --prefix=$HOME/thrift --without-ruby --without-python --without-c --without-erlang --without-go --without-nodejs && make && make install && cd  ..;
+else
+    echo "Using cached thrift";
+fi
