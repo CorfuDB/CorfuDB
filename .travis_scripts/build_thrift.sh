d16a1d772fa1a2261369b564180ba63f94852562
Michael Wei <mwei@cs.ucsd.edu>
2015-07-15 14:59:36 -0700

    fix thrift build script

--- .travis_scripts/build_thrift.sh
+++ .travis_scripts/build_thrift.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-if [ ! -d "$HOME/protobuf/lib" ]; then
+if [ ! -d "$HOME/thrift" ]; then
     wget http://apache.mirrors.tds.net/thrift/0.9.2/thrift-0.9.2.tar.gz -O thrift.tar.gz;
     tar -xzf thrift.tar.gz;
     cd thrift-0.9.2 && ./configure JAVA_PREFIX=$HOME/thrift --prefix=$HOME/thrift --without-ruby --without-python --without-c --without-erlang --without-go --without-nodejs && make && make install && cd  ..;
