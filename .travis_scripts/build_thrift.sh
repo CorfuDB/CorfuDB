bedd113006ae9aeea375d989910e5169d9498327
Michael Wei <mwei@cs.ucsd.edu>
2015-07-15 13:48:44 -0700

    set java prefix

--- .travis_scripts/build_thrift.sh
+++ .travis_scripts/build_thrift.sh
@@ -3,7 +3,7 @@ set -e
 if [ ! -d "$HOME/protobuf/lib" ]; then
     wget http://apache.mirrors.tds.net/thrift/0.9.2/thrift-0.9.2.tar.gz -O thrift.tar.gz;
     tar -xzf thrift.tar.gz;
-    cd thrift-0.9.2 && ./configure --prefix=$HOME/thrift --without-ruby --without-python --without-c --without-erlang --without-go --without-nodejs && make && make install && cd  ..;
+    cd thrift-0.9.2 && ./configure JAVA_PREFIX=$HOME/thrift --prefix=$HOME/thrift --without-ruby --without-python --without-c --without-erlang --without-go --without-nodejs && make && make install && cd  ..;
 else
     echo "Using cached thrift";
 fi
