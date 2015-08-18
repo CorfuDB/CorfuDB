ae0234874ed01a535d31bd35c26fd842dd48e12a
Michael Wei <mwei@cs.ucsd.edu>
2015-08-18 18:45:23 -0500

    maven repo from travis

---
+++ .travis_scripts/mvnrepo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]; then
+    echo -e "Publishing maven repository..."
+
+    mvn deploy
+    cp -R target/mvn-repo $HOME/mvn-repo
+    cd $HOME
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git clone --quiet --branch=mvn-repo https://${GH_TOKEN}@github.com/CorfuDB/CorfuDB mvn-repo > /dev/null
+
+    cd mvn-repo
+    cp -Rf $HOME/mvn-repo .
+    git add -f .
+    git commit -m "Updated maven repository from travis build $TRAVIS_BUILD_NUMBER"
+    git push -fq origin mvn-repo > /dev/null
+
+    echo -e "Maven artifacts built and pushed to github."
+fi
