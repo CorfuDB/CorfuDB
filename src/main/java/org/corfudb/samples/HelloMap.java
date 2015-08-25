f824b812f74cf74a6e29485ad5107f3216fde572
Dahlia Malkhi <dahliamalkhi@gmail.com>
2015-08-24 17:13:15 -0700

    built a simple map multi-threaded test

--- src/main/java/org/corfudb/samples/HelloMap.java
+++ src/main/java/org/corfudb/samples/HelloMap.java
@@ -5,16 +5,20 @@ import org.corfudb.runtime.collections.CDBSimpleMap;
 import org.corfudb.runtime.stream.IStream;
 import org.corfudb.runtime.view.ICorfuDBInstance;
 import org.corfudb.util.CorfuDBFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 
 /**
  * Created by dalia on 8/20/15.
  */
 public class HelloMap implements Runnable {
-    private static final String usage = "HelloMap <master>\n\n";
+    private Logger log = LoggerFactory.getLogger(HelloMap.class);
+    private static final String usage = "HelloMap <master> [nthreads]\n\n";
 
     public static void main(String[] args) throws Exception {
 
@@ -22,32 +26,41 @@ public class HelloMap implements Runnable {
         Thread r = new Thread(new HelloMap(args));
         r.start();
         synchronized (r) {
-            r.wait();
+            r.join();
         }
     }
 
     private String masterString = null;
+    private int nthreads = 2;
     CorfuDBRuntime cdr;
     ICorfuDBInstance instance;
 
     HelloMap(String[] args) {
         if (args.length < 1) {
-            System.out.println("Usage: " + usage);
+            log.info("Usage: {}", usage);
             System.exit(1);
         }
         masterString = args[0];
         Map<String, Object> opts = new HashMap<String, Object>() {{
             put("--master", masterString);
         }};
+        if (args.length > 1) {
+            log.info("using {} threads",  args[1]);
+            nthreads = Integer.parseInt(args[1]);
+        }
 
         CorfuDBFactory cdbFactory = new CorfuDBFactory(opts);
-        System.out.println("Creating CorfuDBRuntime...");
+        log.info("Creating CorfuDBRuntime...");
         cdr = cdbFactory.getRuntime();
         instance = cdr.getLocalInstance();
 
     }
 
+    CDBSimpleMap<Integer, Integer> testMap = null;
+
     public void run() {
+
+        Thread[] t = new Thread[nthreads];
 
        /*
          * The configuration master provides a resetAll command which resets the state
@@ -58,11 +71,36 @@ public class HelloMap implements Runnable {
         instance.getConfigurationMaster().resetAll();
 
         UUID streamID = UUID.randomUUID();
-        IStream s = instance.openStream(streamID);
-        CDBSimpleMap<Integer, Integer> testMap = instance.openObject(streamID, CDBSimpleMap.class);
+        testMap = instance.openObject(streamID, CDBSimpleMap.class);
+        int worksize = 8000 / nthreads;
 
-        testMap.put(0, 0);
-        System.out.println("get(0): " + testMap.get(0) );
-        System.out.println("done");
+        for (int j = 0; j < nthreads; j++) {
+            log.info("starting thread {}", j);
+            t[j] = new Thread(() -> {
+                int r = new Random().nextInt();
+                log.info("Thread starting puts with random seed r={}..", r);
+                for (int i = 0; i < worksize; i++)
+                    testMap.put(i, r+i);
+                log.info("Thread starting gets..");
+                for (int i = 0; i < worksize; i++) {
+                    if (testMap.get(i) != r+i)
+                        log.info("get({}) returns {}; another thread overwrote {}+{}={} (that's good)",
+                                i, testMap.get(i), r, i, r+i
+                        );
+
+                }
+                log.info("Thread done.");
+            }); t[j].start();
+        }
+
+        log.info("waiting for all threads to finish ..");
+        for (Thread tt : t) {
+            try {
+                tt.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        log.info("!!!!!!!!!!!!!!!All work completed!!!!!!!!!!!!!!!");
     }
 }
