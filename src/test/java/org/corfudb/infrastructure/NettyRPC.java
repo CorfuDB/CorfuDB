490421fa36e0f6bea9a57d98706881842668dc0f
Dahlia Malkhi <dahliamalkhi@gmail.com>
2015-10-01 16:52:10 -0700

    low level NettyRPC tester

--- src/test/java/org/corfudb/infrastructure/NettyRPC.java
+++ src/test/java/org/corfudb/infrastructure/NettyRPC.java
@@ -1,6 +1,7 @@
 package org.corfudb.infrastructure;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -8,6 +9,8 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.corfudb.infrastructure.wireprotocol.NettyCorfuMsg;
 import org.corfudb.runtime.protocols.NettyRPCChannelInboundHandlerAdapter;
 import org.corfudb.util.SizeBufferPool;
@@ -15,6 +18,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,9 +27,31 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class NettyRPC {
 
+    final int nmessages = 3;
+
+    @AllArgsConstructor
+    @Getter
+    class NettyTestMsg extends NettyCorfuMsg {
+        int mid;
+
+        @Override
+        public void serialize(ByteBuf buffer) {
+            super.serialize(buffer);
+            buffer.writeInt(mid);
+        }
+
+        @Override
+        public void fromBuffer(ByteBuf buffer) {
+            super.fromBuffer(buffer);
+            mid = buffer.readInt();
+        }
+    }
+
     // my Netty test server
     //
     class NettyTestServer extends AbstractNettyServer {
+
+        AtomicInteger nreceived = new AtomicInteger(0);
 
         @Override
         void parseConfiguration(Map<String, Object> configuration) {
@@ -34,7 +60,11 @@ public class NettyRPC {
 
         @Override
         void processMessage(NettyCorfuMsg msg, ChannelHandlerContext ctx) {
-            System.out.println("got mesg");
+            int r = nreceived.getAndIncrement();
+            if (nmessages < 5 || r % (nmessages/5) == 0)
+                System.out.println("got mesg :" + msg); // r + ">, requestID = " + msg.getRequestID() + ", epoch=" + msg.getEpoch()) ;
+            if (r == nmessages-1)
+                System.out.println("!!!!!!!!!!finished all " + r + " messages!!!!!!");
         }
 
         @Override
@@ -58,12 +88,12 @@ public class NettyRPC {
 
             @Override
             public void handleMessage(NettyCorfuMsg message) {
-
+                System.out.println("client receive response");
             }
         }
 
         public NettyTestClient() {
-            NioEventLoopGroup workerGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2);
+            NioEventLoopGroup workerGroup = new NioEventLoopGroup(/*Runtime.getRuntime().availableProcessors() * 2 */);
 
             handler = new NettyTestClientHandler();
 
@@ -75,22 +105,26 @@ public class NettyRPC {
             b.handler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 public void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+                    // ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
                     ch.pipeline().addLast(handler);
                 }
             });
 
-            for (int i = 0; i < 2; i++) {
-                if (!b.connect("localhost", 9999).awaitUninterruptibly(5000)) {
-                    throw new RuntimeException("Couldn't connect to endpoint ");
-                }
+            if (!b.connect("localhost", 9999).awaitUninterruptibly(5000)) {
+                throw new RuntimeException("Couldn't connect to endpoint ");
             }
         }
 
        public void run() {
             // send some requests ...
             System.out.println("send something");
-            handler.sendMessage(new SizeBufferPool(10), 0, new NettyCorfuMsg(NettyCorfuMsg.NettyCorfuMsgType.ERROR_OK));
+           UUID myid = new UUID(0,0);
+           for (int i = 0; i < nmessages; i++) {
+               NettyCorfuMsg msg = new NettyCorfuMsg(myid, (long)i, (long) 100, NettyCorfuMsg.NettyCorfuMsgType.ERROR_OK);
+               if (nmessages <  5 || i % (nmessages/5) == 0)
+                   System.out.println("send msg: " + msg);
+               handler.sendMessage(100, msg);
+           }
         }
 
     }
@@ -115,5 +149,11 @@ public class NettyRPC {
             NettyTestClient nc = new NettyTestClient();
             nc.run();
  //       }).start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
