6423c714676ec43e960fba4695f47bf9d5815736
Michael Wei <mwei@cs.ucsd.edu>
2015-12-11 00:51:30 -0800

    replication views

---
+++ src/main/java/org/corfudb/cmdlets/corfu_as.java
@@ -0,0 +1,73 @@
+package org.corfudb.cmdlets;
+
+import com.google.common.io.ByteStreams;
+import lombok.extern.slf4j.Slf4j;
+import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.protocols.LogUnitClient;
+import org.corfudb.runtime.protocols.NettyClientRouter;
+import org.corfudb.util.GitRepositoryState;
+import org.docopt.Docopt;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by mwei on 12/11/15.
+ */
+@Slf4j
+public class corfu_as implements ICmdlet {
+    private static final String USAGE =
+            "corfu_as, interact with the address space view.\n"
+                    + "\n"
+                    + "Usage:\n"
+                    + "\tcorfu_as write -c <config> -a <log-address> [-s <stream-ids>] [-d <level>]\n"
+                    + "\n"
+                    + "Options:\n"
+                    + " -c <config>, --config=<config>                 The config string to pass to the runtime. \n"
+                    + "                                                Usually a comma-delimited list of layout servers.\n"
+                    + " -a <log-address>, --log-address=<log-address>  The log address to use. \n"
+                    + " -i <interval>, --interval=<interval>           Interval to set garbage collection to. \n"
+                    + " -s <stream-ids>, --stream-ids=<stream-ids>     The stream ids to use, comma separated. \n"
+                    + " -t <stream-id>, --stream-id=<stream-id>        A stream id to use, comma separated. \n"
+                    + " -r <rank>, --rank=<rank>                       The rank to use [default: 0]. \n"
+                    + " -d <level>, --log-level=<level>                Set the logging level, valid levels are: \n"
+                    + "                                                ERROR,WARN,INFO,DEBUG,TRACE [default: INFO].\n"
+                    + " -h, --help  Show this screen\n"
+                    + " --version  Show version\n";
+
+    @Override
+    public void main(String[] args) {
+        // Parse the options given, using docopt.
+        Map<String, Object> opts =
+                new Docopt(USAGE).withVersion(GitRepositoryState.getRepositoryState().describe).parse(args);
+
+        // Configure base options
+        configureBase(opts);
+
+        // Get a runtime instance from the options.
+        CorfuRuntime rt = configureRuntime(opts);
+
+        try {
+            if ((Boolean)opts.get("write"))
+            {
+                write(rt, opts);
+            }
+        } catch (ExecutionException ex)
+        {
+            log.error("Exception", ex.getCause());
+            throw new RuntimeException(ex.getCause());
+        }
+        catch (Exception e)
+        {
+            log.error("Exception", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    void write(CorfuRuntime runtime, Map<String,Object> opts)
+            throws Exception
+    {
+        runtime.getAddressSpaceView().write(Long.parseLong((String) opts.get("--log-address")),
+                streamsFromString((String) opts.get("--stream-ids")), ByteStreams.toByteArray(System.in));
+    }
+}
--- src/main/java/org/corfudb/runtime/view/AddressSpaceView.java
+++ src/main/java/org/corfudb/runtime/view/AddressSpaceView.java
@@ -1,6 +1,10 @@
 package org.corfudb.runtime.view;
 
+import com.sun.net.httpserver.Filter;
 import org.corfudb.runtime.CorfuRuntime;
+
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Created by mwei on 12/10/15.
@@ -9,5 +13,13 @@ public class AddressSpaceView extends AbstractView {
     public AddressSpaceView(CorfuRuntime runtime)
     {
         super(runtime);
+    }
+
+    public void write(long address, Set<UUID> stream, Object data) {
+        layoutHelper(l -> {
+           AbstractReplicationView.getReplicationView(l, l.getReplicationMode(address))
+                   .write(address, stream, data);
+           return null;
+        });
     }
 }
