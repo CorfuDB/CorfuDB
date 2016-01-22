8c5ae582ce3f4127ee78d7f1a17ddbd00d81489c
Michael Wei <mwei@cs.ucsd.edu>
2016-01-22 00:19:35 -0800

    object tester

--- .travis_scripts/push_deb.sh
+++ .travis_scripts/push_deb.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]; then
+        echo -e "Shipping deb package..."
 
+#this is fragile and needs to account for changes in the filename
+        cp -R target/corfu_0.1~SNAPSHOT_all.deb  $HOME/$DEBNAME
+        cd $HOME
+        git config --global user.email "travis@travis-ci.org"
+        git config --global user.name "travis-ci"
+        git clone --quiet --branch=debian https://${GH_TOKEN}@github.com/CorfuDB/CorfuDB debian > /dev/null
+
+        cd debian
+        cp -Rf $HOME/mvn-repo-current/* .
+        git add -f .
+        git commit -m "Updated maven repository from travis build $TRAVIS_BUILD_NUMBER"
+        git push -fq origin mvn-repo > /dev/null
+    fi
+fi
--- src/main/java/org/corfudb/runtime/collections/ISMRMap.java
+++ src/main/java/org/corfudb/runtime/collections/ISMRMap.java
@@ -16,14 +16,22 @@ public interface ISMRMap<K,V> extends ISMRInterface, Map<K,V> {
             .add(new SMRMethod("size", new Class[0]))
             .add(new SMRMethod("isEmpty", new Class[0]))
             .add(new SMRMethod("containsKey", new Class[]{Object.class}))
+            .add(new SMRMethod("containsValue", new Class[]{Object.class}))
+            .add(new SMRMethod("get", new Class[]{Object.class}))
+            .add(new SMRMethod("keySet", new Class[0]))
+            .add(new SMRMethod("values", new Class[0]))
+            .add(new SMRMethod("entrySet", new Class[0]))
+            .add(new SMRMethod("remove", new Class[]{Object.class}))
             .add()
             .build();
 
     Set<SMRMethod> SMRMutators = ImmutableSet.<SMRMethod>builder()
             .add(new SMRMethod("clear", new Class[0], "clear"))
+            .add(new SMRMethod("putAll", new Class[]{Map.class}))
             .build();
 
     Set<SMRMethod> SMRMutatorAccessors = ImmutableSet.<SMRMethod>builder()
+            .add(new SMRMethod("put", new Class[]{Object.class, Object.class}))
             .build();
 
     @Override
