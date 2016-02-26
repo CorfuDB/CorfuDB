3f34dfdad6e278d79d29d8eecbbf2dfec225efbd
Michael Wei <mwei@cs.ucsd.edu>
2016-02-27 03:54:06 +0700

    add unit tests to satisfy coverage requirement

--- src/test/java/org/corfudb/runtime/view/ObjectsViewTest.java
+++ src/test/java/org/corfudb/runtime/view/ObjectsViewTest.java
@@ -10,10 +10,12 @@ import org.corfudb.runtime.object.Accessor;
 import org.corfudb.runtime.object.Mutator;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Created by mwei on 2/18/16.
@@ -49,4 +51,35 @@ public class ObjectsViewTest extends AbstractViewTest  {
                 .doesNotContainEntry("b", "b");
     }
 
+    @Test
+    @SuppressWarnings("unchecked")
+    public void cannotCopyNonCorfuObject()
+            throws Exception {
+        // default layout is chain replication.
+        addServerForTest(getDefaultEndpoint(), new LayoutServer(defaultOptionsMap()));
+        addServerForTest(getDefaultEndpoint(), new LogUnitServer(defaultOptionsMap()));
+        addServerForTest(getDefaultEndpoint(), new SequencerServer(defaultOptionsMap()));
+        wireRouters();
+
+        //begin tests
+        CorfuRuntime r = getRuntime().connect();
+        assertThatThrownBy(() -> {
+            r.getObjectsView().copy(new HashMap<String,String>(), CorfuRuntime.getStreamID("test"));
+        }).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void canAbortNoTransaction()
+            throws Exception {
+        // default layout is chain replication.
+        addServerForTest(getDefaultEndpoint(), new LayoutServer(defaultOptionsMap()));
+        addServerForTest(getDefaultEndpoint(), new LogUnitServer(defaultOptionsMap()));
+        addServerForTest(getDefaultEndpoint(), new SequencerServer(defaultOptionsMap()));
+        wireRouters();
+
+        //begin tests
+        CorfuRuntime r = getRuntime().connect();
+        r.getObjectsView().TXAbort();
+    }
 }
