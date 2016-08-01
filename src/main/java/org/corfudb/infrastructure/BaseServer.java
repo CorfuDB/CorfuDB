0fe7d33c0fbfa6c8272bd65ec6562c582b7e91a6
Michael Wei <mwei@cs.ucsd.edu>
2016-08-01 16:38:04 -0700

    show basic stats like uptime

--- src/main/java/org/corfudb/infrastructure/BaseServer.java
+++ src/main/java/org/corfudb/infrastructure/BaseServer.java
@@ -2,6 +2,7 @@ package org.corfudb.infrastructure;
 
 import io.netty.channel.ChannelHandlerContext;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.protocols.wireprotocol.CorfuMsg;
 import org.corfudb.protocols.wireprotocol.CorfuSetEpochMsg;
@@ -9,18 +10,26 @@ import org.corfudb.protocols.wireprotocol.JSONPayloadMsg;
 import org.corfudb.protocols.wireprotocol.VersionInfo;
 import org.corfudb.util.CorfuMsgHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mwei on 12/8/15.
  */
 @Slf4j
 public class BaseServer extends AbstractServer {
 
+    /** Options map, if available */
+    @Getter
+    @Setter
+    public Map<String, Object> optionsMap = new HashMap<>();
+
     /** Handler for the base server */
     @Getter
     private CorfuMsgHandler handler = new CorfuMsgHandler()
             .addHandler(CorfuMsg.CorfuMsgType.PING, BaseServer::ping)
             .addHandler(CorfuMsg.CorfuMsgType.SET_EPOCH, BaseServer::setEpoch)
-            .addHandler(CorfuMsg.CorfuMsgType.VERSION_REQUEST, BaseServer::getVersion);
+            .addHandler(CorfuMsg.CorfuMsgType.VERSION_REQUEST, this::getVersion);
 
     /** Respond to a ping message.
      *
@@ -57,11 +66,10 @@ public class BaseServer extends AbstractServer {
      * @param ctx   The channel context
      * @param r     The server router.
      */
-    private static void getVersion(CorfuMsg msg, ChannelHandlerContext ctx, IServerRouter r) {
-        VersionInfo vi = new VersionInfo();
+    private void getVersion(CorfuMsg msg, ChannelHandlerContext ctx, IServerRouter r) {
+        VersionInfo vi = new VersionInfo(optionsMap);
         r.sendResponse(ctx, msg, new JSONPayloadMsg<>(vi, CorfuMsg.CorfuMsgType.VERSION_RESPONSE));
     }
-
 
     @Override
     public void reset() {
--- src/main/java/org/corfudb/runtime/clients/BaseClient.java
+++ src/main/java/org/corfudb/runtime/clients/BaseClient.java
@@ -27,26 +27,53 @@ import java.util.concurrent.CompletableFuture;
 public class BaseClient implements IClient {
 
     /**
-     * The messages this client should handle.
-     */
-    @Getter
-    public final Set<CorfuMsg.CorfuMsgType> HandledTypes =
-            new ImmutableSet.Builder<CorfuMsg.CorfuMsgType>()
-                    .add(CorfuMsg.CorfuMsgType.PING)
-                    .add(CorfuMsg.CorfuMsgType.PONG)
-                    .add(CorfuMsg.CorfuMsgType.ACK)
-                    .add(CorfuMsg.CorfuMsgType.NACK)
-                    .add(CorfuMsg.CorfuMsgType.SET_EPOCH)
-                    .add(CorfuMsg.CorfuMsgType.WRONG_EPOCH)
-                    .add(CorfuMsg.CorfuMsgType.VERSION_RESPONSE)
-                    .build();
-    /**
      * The router to use for the client.
      */
     @Getter
     @Setter
     public IClientRouter router;
 
+    /** Public functions which are exposed to clients. */
+
+    /**
+     * Ping the endpoint, synchronously.
+     *
+     * @return True, if the endpoint was reachable, false otherwise.
+     */
+    public boolean pingSync() {
+        try {
+            return ping().get();
+        } catch (Exception e) {
+            log.trace("Ping failed due to exception", e);
+            return false;
+        }
+    }
+
+    public CompletableFuture<Boolean> setRemoteEpoch(long newEpoch) {
+        // Set our own epoch to this epoch.
+        router.setEpoch(newEpoch);
+        return router.sendMessageAndGetCompletable(
+                new CorfuSetEpochMsg(CorfuMsg.CorfuMsgType.SET_EPOCH, newEpoch));
+    }
+
+    public CompletableFuture<VersionInfo> getVersionInfo() {
+        return router.sendMessageAndGetCompletable(
+                new CorfuMsg(CorfuMsg.CorfuMsgType.VERSION_REQUEST));
+    }
+
+
+    /**
+     * Ping the endpoint, asynchronously.
+     *
+     * @return A completable future which will be completed with True if
+     * the endpoint is reachable, otherwise False or exceptional completion.
+     */
+    public CompletableFuture<Boolean> ping() {
+        return router.sendMessageAndGetCompletable(
+                new CorfuMsg(CorfuMsg.CorfuMsgType.PING));
+    }
+
+    /** The handler and handlers which implement this client. */
     @Getter
     public ClientMsgHandler msgHandler = new ClientMsgHandler(this)
             .addHandler(CorfuMsg.CorfuMsgType.PING, BaseClient::handlePing)
@@ -124,41 +151,4 @@ public class BaseClient implements IClient {
         return msg.getPayload();
     }
 
-    /**
-     * Ping the endpoint, synchronously.
-     *
-     * @return True, if the endpoint was reachable, false otherwise.
-     */
-    public boolean pingSync() {
-        try {
-            return ping().get();
-        } catch (Exception e) {
-            log.trace("Ping failed due to exception", e);
-            return false;
-        }
-    }
-
-    public CompletableFuture<Boolean> setRemoteEpoch(long newEpoch) {
-        // Set our own epoch to this epoch.
-        router.setEpoch(newEpoch);
-        return router.sendMessageAndGetCompletable(
-                new CorfuSetEpochMsg(CorfuMsg.CorfuMsgType.SET_EPOCH, newEpoch));
-    }
-
-    public CompletableFuture<VersionInfo> getVersionInfo() {
-        return router.sendMessageAndGetCompletable(
-                new CorfuMsg(CorfuMsg.CorfuMsgType.VERSION_REQUEST));
-    }
-
-
-    /**
-     * Ping the endpoint, asynchronously.
-     *
-     * @return A completable future which will be completed with True if
-     * the endpoint is reachable, otherwise False or exceptional completion.
-     */
-    public CompletableFuture<Boolean> ping() {
-        return router.sendMessageAndGetCompletable(
-                new CorfuMsg(CorfuMsg.CorfuMsgType.PING));
-    }
 }
