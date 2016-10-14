539a3cc25c06f7cba35f9b9d8d62ea6be09e3371
Zeeshan Lokhandwala <zalokhan@ncsu.edu>
2016-10-14 11:46:50 -0700

    Created LayoutWorkflowManager to modify the layout

---
+++ src/main/java/org/corfudb/runtime/exceptions/LayoutModificationException.java
@@ -0,0 +1,11 @@
+package org.corfudb.runtime.exceptions;
+
+/**
+ * Created by zlokhandwala on 10/12/16.
+ */
+public class LayoutModificationException extends Exception {
+
+    public LayoutModificationException(String message) {
+        super(message);
+    }
+}
