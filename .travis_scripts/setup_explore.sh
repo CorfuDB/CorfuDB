a61ece873440e7de63aac9d754e52ca8b34d063a
Scott Lystig Fritchie <slfritchie@snookles.com>
2016-10-28 21:33:59 -0400

    17.5 is in the installation list but can't be installed, interesting

--- .travis_scripts/setup_explore.sh
+++ .travis_scripts/setup_explore.sh
@@ -10,6 +10,11 @@ kerl list releases
 kerl list builds
 kerl list installations
 
+which erl
+ls -l ~/.kerl/builds/17.5/
+ls -l ~/.kerl/builds/
+
 kerl install 17.5 ~/e
 . ~/e/activate
+which erl
 erl -s erlang halt
