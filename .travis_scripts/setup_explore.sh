42c160e0a934aefc79ebaadf7b66fe5d12c1abf1
Scott Lystig Fritchie <slfritchie@snookles.com>
2016-10-28 21:22:48 -0400

    How to use TravisCI's kerl?
    https://docs.travis-ci.com/user/languages/erlang/ doesn't give
    enough info, so I need another round of push & TravisCI run, sorry.

--- .travis_scripts/setup_explore.sh
+++ .travis_scripts/setup_explore.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -x
+
 whoami
 ls -al ~
 cat ~/.kerlrc
 which kerl
 kerl list releases
+kerl list builds
 kerl list installations
+
+kerl install 17.5 ~/e
+. ~/e/activate
+erl -s erlang halt
