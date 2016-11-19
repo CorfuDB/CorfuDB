07eadc9dd426c05ba85a505127258e1871f1b651
Dahlia Malkhi <dahliamalkhi@gmail.com>
2016-11-18 16:35:04 -0800

    done?

--- tutorials/src/main/java/org/corfudb/tutorials/FirstCustomCorfuObject.java
+++ tutorials/src/main/java/org/corfudb/tutorials/FirstCustomCorfuObject.java
@@ -2,9 +2,12 @@ package org.corfudb.tutorials;
 
 import org.corfudb.annotations.Accessor;
 import org.corfudb.annotations.CorfuObject;
+import org.corfudb.annotations.Mutator;
 import org.corfudb.annotations.MutatorAccessor;
 import org.corfudb.runtime.object.*;
 import org.corfudb.runtime.CorfuRuntime;
+
+import java.util.Random;
 
 /**
  * Created by dalia on 11/17/16.
@@ -12,19 +15,20 @@ import org.corfudb.runtime.CorfuRuntime;
  * Corfu allows application developers to create custom-made objects from any Java class.
  * A Corfu object has its state backed by the Corfu log. All object methods are transparently wrapped by Corfu proxies.
  *
- * There are three types of methods:
+ * There are several types of methods:
  *
- * Transactional: A method that both accesses state and modifies it. Transactional methods execute atomically with serializable isolation guarantees.
- * Accessor:
- * Mutator:
+ * @Accessor: Indicates that this method only looks at the object state and does not modify it
+ * @Mutator: Indicates that this method only sets (overwrites) the object state, and does not need to look at it first
+ * @MutatorAccessor: Indicates that this method needs to both look at the object state and modify it; execution should be atomic
+ * @Transactional: Indicates that this is a transactions method that may access and/or modify multiple objects. In a sense, this is simply a "compound" method.
  *
  * This tutorial illustrates a very simple Corfu object, a shared value.
- */
+ * */
 
 /**
- * Implement ICorfuSMRObject turns this into a Corfu object
+ * The annotation @CorfuObject turns this into a Corfu object
  */
-//@CorfuObject
+@CorfuObject
 public class FirstCustomCorfuObject {
     Integer value = 0;
 
@@ -32,24 +36,35 @@ public class FirstCustomCorfuObject {
      * Increment() method is annotated as a TransactionalMethod.
      * This guarantee that concurrent threads and processes invoking Increment on an object behave as if occurring sequentially, one after another.
      *
-     * @return the old value
+     * @return the old value of the counter, before incrementing
      */
-/*
     @MutatorAccessor(name="Increment")
     public int Increment() {
         int tmp = value;
         value++;
         return tmp;
     }
-*/
 
-//    @Accessor
+    /**
+     * Get() method is annotates as an Accessor method.
+     * This guarantees that the runtime synchronizes with the latest state of the object, and then looks at it.
+     * @return the latest value of the counter
+     */
+    @Accessor
     public int Get() { return value; }
 
     static CorfuRuntime getRuntimeAndConnect(String configurationString) {
         CorfuRuntime corfuRuntime = new CorfuRuntime(configurationString).connect();
         return corfuRuntime;
     }
+
+    /**
+     * Set() method is annotates as a Mutator method.
+     * This guarantees that the runtime will capture the update and append it to the Corfu log, and only then return.
+     */
+    @Mutator
+    public void Set(int newvalue) { value = newvalue; }
+
 
     private CorfuRuntime runtime;
 
@@ -63,8 +78,9 @@ public class FirstCustomCorfuObject {
                 .open();
 
         System.out.println("Counter value before increment: " + cntr.Get());
-        //System.out.println("Counter value before increment: " + cntr.Increment());
-        //System.out.println("Counter value before increment: " + cntr.Increment());
+        System.out.println("Counter value before increment: " + cntr.Increment());
+        cntr.Set(new Random(System.currentTimeMillis()).nextInt() % 1000);
+        System.out.println("Counter value before increment: " + cntr.Increment());
     }
 
     public static void main(String[] args) {
