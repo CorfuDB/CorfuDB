ae0bda8538c0ae8a6bbf59b6fcfa6f6cca07970b
Michael Wei <mwei@cs.ucsd.edu>
2016-12-09 15:12:04 -0800

    misunderstanding of iterations for this test

--- test/src/test/java/org/corfudb/infrastructure/PeriodicPollPolicyTest.java
+++ test/src/test/java/org/corfudb/infrastructure/PeriodicPollPolicyTest.java
@@ -112,7 +112,7 @@ public class PeriodicPollPolicyTest extends AbstractViewTest {
         expectedResult.put(getEndpoint(SERVERS.PORT_2), false);
 
         Map<String, Boolean> actualResult = new HashMap<>();
-        for (int i = 0; i < PARAMETERS.NUM_ITERATIONS_VERY_LOW; i++) {
+        for (int i = 0; i < PARAMETERS.NUM_ITERATIONS_LOW; i++) {
             failureDetectorPolicy.getServerStatus().forEach(actualResult::putIfAbsent);
             Thread.sleep(PARAMETERS.TIMEOUT_SHORT.toMillis());
             if (actualResult.equals(expectedResult)) break;
@@ -136,7 +136,7 @@ public class PeriodicPollPolicyTest extends AbstractViewTest {
         expectedResult.remove(getEndpoint(SERVERS.PORT_0));
 
         actualResult = new HashMap<>();
-        for (int i = 0; i < PARAMETERS.NUM_ITERATIONS_VERY_LOW; i++) {
+        for (int i = 0; i < PARAMETERS.NUM_ITERATIONS_LOW; i++) {
             failureDetectorPolicy.getServerStatus().forEach(actualResult::putIfAbsent);
             Thread.sleep(PARAMETERS.TIMEOUT_SHORT.toMillis());
             if (actualResult.equals(expectedResult)) break;
