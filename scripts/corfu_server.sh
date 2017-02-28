0f030c05910215ec7e799511ad4c05f2087fb28a
Ming Wen <wenm@vmware.com>
2017-02-28 14:43:26 -0800

    Fix corfu server startup script
    Change the name of jar file that JVM is looking for.
    
    Issue: #
    Change-Id: I047cd487d730e94d9bd2fda27b8696f1aff650c3

--- scripts/corfu_server.sh
+++ scripts/corfu_server.sh
@@ -19,7 +19,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 if ls "${DIR}"/../target/*.jar > /dev/null 2>&1; then
   # echo "Running from development source"
-  CLASSPATH=("${DIR}"/../infrastructure/target/infrastructure-*-shaded.jar)
+  CLASSPATH=("${DIR}"/../infrastructure/target/nsx-corfu-*-shaded.jar)
 else
   CLASSPATH=("${CORFUDB_PREFIX}"/share/corfu/lib/*.jar)
 fi
