f49ea030c1da078f453c995e238e3c206ea3f10e
Michael Wei <mwei@cs.ucsd.edu>
2017-03-20 23:14:13 +0000

    clean up map interface, add undo function for putAll

--- runtime/src/main/java/org/corfudb/runtime/collections/ISMRMap.java
+++ runtime/src/main/java/org/corfudb/runtime/collections/ISMRMap.java
@@ -1,14 +1,8 @@
 package org.corfudb.runtime.collections;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableMap;
 import org.corfudb.annotations.*;
-import org.corfudb.runtime.object.ISMRInterface;
-
 import java.util.*;
-import java.util.concurrent.ConcurrentMap;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 /**
  * Created by mwei on 1/9/16.
@@ -16,120 +10,60 @@ import java.util.function.Function;
 public interface ISMRMap<K, V> extends Map<K, V>, ISMRObject {
 
     /**
-     * Returns the number of key-value mappings in this map.  If the
-     * map contains more than <tt>Integer.MAX_VALUE</tt> elements, returns
-     * <tt>Integer.MAX_VALUE</tt>.
+     * {@inheritDoc}
      *
-     * @return the number of key-value mappings in this map
+     * Conflicts: this operation conflicts with any modification to
+     * the map, since the size of the map could be potentially changed.
      */
     @Accessor
     @Override
     int size();
 
     /**
-     * Returns <tt>true</tt> if this map contains no key-value mappings.
+     * {@inheritDoc}
      *
-     * @return <tt>true</tt> if this map contains no key-value mappings
+     * Conflicts: this operation conflicts with any modification to
+     * the map, since the size of the map could be potentially changed.
      */
     @Accessor
     @Override
     boolean isEmpty();
 
     /**
-     * Returns <tt>true</tt> if this map contains a mapping for the specified
-     * key.  More formally, returns <tt>true</tt> if and only if
-     * this map contains a mapping for a key <tt>k</tt> such that
-     * <tt>(key==null ? k==null : key.equals(k))</tt>.  (There can be
-     * at most one such mapping.)
+     * {@inheritDoc}
      *
-     * @param key key whose presence in this map is to be tested
-     * @return <tt>true</tt> if this map contains a mapping for the specified
-     * key
-     * @throws ClassCastException   if the key is of an inappropriate type for
-     *                              this map
-     *                              (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException if the specified key is null and this map
-     *                              does not permit null keys
-     *                              (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
+     * Conflicts: this operation conflicts with any operation on the
+     * given key.
      */
     @Accessor
     @Override
     boolean containsKey(@ConflictParameter Object key);
 
     /**
-     * Returns <tt>true</tt> if this map maps one or more keys to the
-     * specified value.  More formally, returns <tt>true</tt> if and only if
-     * this map contains at least one mapping to a value <tt>v</tt> such that
-     * <tt>(value==null ? v==null : value.equals(v))</tt>.  This operation
-     * will probably require time linear in the map size for most
-     * implementations of the <tt>Map</tt> interface.
+     * {@inheritDoc}
      *
-     * @param value value whose presence in this map is to be tested
-     * @return <tt>true</tt> if this map maps one or more keys to the
-     * specified value
-     * @throws ClassCastException   if the value is of an inappropriate type for
-     *                              this map
-     *                              (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException if the specified value is null and this
-     *                              map does not permit null values
-     *                              (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
+     * Conflicts: this operation conflicts with any modification to
+     * the map, since the presence of values could be potentially changed.
      */
     @Accessor
     @Override
     boolean containsValue(Object value);
 
     /**
-     * Returns the value to which the specified key is mapped,
-     * or {@code null} if this map contains no mapping for the key.
-     * <p>
-     * <p>More formally, if this map contains a mapping from a key
-     * {@code k} to a value {@code v} such that {@code (key==null ? k==null :
-     * key.equals(k))}, then this method returns {@code v}; otherwise
-     * it returns {@code null}.  (There can be at most one such mapping.)
-     * <p>
-     * <p>If this map permits null values, then a return value of
-     * {@code null} does not <i>necessarily</i> indicate that the map
-     * contains no mapping for the key; it's also possible that the map
-     * explicitly maps the key to {@code null}.  The {@link #containsKey
-     * containsKey} operation may be used to distinguish these two cases.
+     * {@inheritDoc}
      *
-     * @param key the key whose associated value is to be returned
-     * @return the value to which the specified key is mapped, or
-     * {@code null} if this map contains no mapping for the key
-     * @throws ClassCastException   if the key is of an inappropriate type for
-     *                              this map
-     *                              (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException if the specified key is null and this map
-     *                              does not permit null keys
-     *                              (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
+     * Conflicts: this operation conflicts with any operation on the
+     * given key.
      */
     @Accessor
     @Override
     V get(@ConflictParameter Object key);
 
     /**
-     * Associates the specified value with the specified key in this map
-     * (optional operation).  If the map previously contained a mapping for
-     * the key, the old value is replaced by the specified value.  (A map
-     * <tt>m</tt> is said to contain a mapping for a key <tt>k</tt> if and only
-     * if {@link #containsKey(Object) m.containsKey(k)} would return
-     * <tt>true</tt>.)
+     * {@inheritDoc}
      *
-     * @param key   key with which the specified value is to be associated
-     * @param value value to be associated with the specified key
-     * @return the previous value associated with <tt>key</tt>, or
-     * <tt>null</tt> if there was no mapping for <tt>key</tt>.
-     * (A <tt>null</tt> return can also indicate that the map
-     * previously associated <tt>null</tt> with <tt>key</tt>,
-     * if the implementation supports <tt>null</tt> values.)
-     * @throws UnsupportedOperationException if the <tt>put</tt> operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the class of the specified key or value
-     *                                       prevents it from being stored in this map
-     * @throws NullPointerException          if the specified key or value is null
-     *                                       and this map does not permit null keys or values
-     * @throws IllegalArgumentException      if some property of the specified key
-     *                                       or value prevents it from being stored in this map
+     * Conflicts: this operation produces a conflict with any other
+     * operation on the given key.
      */
     @MutatorAccessor(name = "put", undoFunction = "undoPut", undoRecordFunction = "undoPutRecord")
     @Override
@@ -137,23 +71,16 @@ public interface ISMRMap<K, V> extends Map<K, V>, ISMRObject {
 
 
     /**
-     * Associates the specified value with the specified key in this map
-     * without return the previous value.  If the map previously contained a
-     * mapping for the key, the old value is replaced by the specified value.
-     * (A map <tt>m</tt> is said to contain a mapping for a key <tt>k</tt> if
-     * and only if {@link #containsKey(Object) m.containsKey(k)} would return
-     * <tt>true</tt>.)
+     * This operation behaves like a put operation, but does not
+     * return the previous value, and does not result in a read
+     * of the map.
      *
-     * @param key   key with which the specified value is to be associated
-     * @param value value to be associated with the specified key
-     * @throws UnsupportedOperationException if the <tt>put</tt> operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the class of the specified key or value
-     *                                       prevents it from being stored in this map
-     * @throws NullPointerException          if the specified key or value is null
-     *                                       and this map does not permit null keys or values
-     * @throws IllegalArgumentException      if some property of the specified key
-     *                                       or value prevents it from being stored in this map
+     * Calling this operation produces the same put record as calling
+     * "put" directly. However, the runtime will not try to sync
+     * the object to obtain an upcall.
+     *
+     * Conflicts: this operation produces a conflict with any other
+     * operation on the given key.
      */
     @Mutator(name = "put", noUpcall = true)
     default void blindPut(@ConflictParameter K key, V value) {
@@ -193,34 +120,10 @@ public interface ISMRMap<K, V> extends Map<K, V>, ISMRObject {
     }
 
     /**
-     * Removes the mapping for a key from this map if it is present
-     * (optional operation).   More formally, if this map contains a mapping
-     * from key <tt>k</tt> to value <tt>v</tt> such that
-     * <code>(key==null ?  k==null : key.equals(k))</code>, that mapping
-     * is removed.  (The map can contain at most one such mapping.)
-     * <p>
-     * <p>Returns the value to which this map previously associated the key,
-     * or <tt>null</tt> if the map contained no mapping for the key.
-     * <p>
-     * <p>If this map permits null values, then a return value of
-     * <tt>null</tt> does not <i>necessarily</i> indicate that the map
-     * contained no mapping for the key; it's also possible that the map
-     * explicitly mapped the key to <tt>null</tt>.
-     * <p>
-     * <p>The map will not contain a mapping for the specified key once the
-     * call returns.
+     * {@jnheritDoc}
      *
-     * @param key key whose mapping is to be removed from the map
-     * @return the previous value associated with <tt>key</tt>, or
-     * <tt>null</tt> if there was no mapping for <tt>key</tt>.
-     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the key is of an inappropriate type for
-     *                                       this map
-     *                                       (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException          if the specified key is null and this
-     *                                       map does not permit null keys
-     *                                       (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
+     * Conflicts: this operation produces a conflict with any other
+     * operation on the given key.
      */
     @MutatorAccessor(name="remove", undoFunction = "undoRemove", undoRecordFunction = "undoRemoveRecord")
     @Override
@@ -254,92 +157,88 @@ public interface ISMRMap<K, V> extends Map<K, V>, ISMRObject {
     }
 
     /**
-     * Copies all of the mappings from the specified map to this map
-     * (optional operation).  The effect of this call is equivalent to that
-     * of calling {@link #put(Object, Object) put(k, v)} on this map once
-     * for each mapping from key <tt>k</tt> to value <tt>v</tt> in the
-     * specified map.  The behavior of this operation is undefined if the
-     * specified map is modified while the operation is in progress.
+     * {@inheritDoc}
      *
-     * @param m mappings to be stored in this map
-     * @throws UnsupportedOperationException if the <tt>putAll</tt> operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the class of a key or value in the
-     *                                       specified map prevents it from being stored in this map
-     * @throws NullPointerException          if the specified map is null, or if
-     *                                       this map does not permit null keys or values, and the
-     *                                       specified map contains null keys or values
-     * @throws IllegalArgumentException      if some property of a key or value in
-     *                                       the specified map prevents it from being stored in this map
+     * Conflicts: this operation currently conflicts with the entire map, until
+     * custom conflict parameter generation is introduced.
      */
-    @Mutator(name="putAll")
+    @Mutator(name="putAll", undoFunction="undoPutAll", undoRecordFunction="undoPutAllRecord")
     @Override
     void putAll(Map<? extends K, ? extends V> m);
 
-    /**
-     * Removes all of the mappings from this map (optional operation).
-     * The map will be empty after this call returns.
+
+    /** Generate an undo record for putAll, given the previous state of the map
+     * and the parameters to the putAll call.
      *
-     * @throws UnsupportedOperationException if the <tt>clear</tt> operation
-     *                                       is not supported by this map
+     * @param previousState     The previous state of the map
+     * @param m                 The map from the putAll call
+     * @return                  An undo record, which for a putAll is all the
+     *                          previous entries in the map.
+     */
+    default Map<K,V> undoPutAllRecord(ISMRMap<K,V> previousState, Map<? extends K, ? extends V> m) {
+        ImmutableMap.Builder<K,V> builder = ImmutableMap.builder();
+        m.keySet().forEach(k -> builder.put(k, previousState.get(k)));
+        return builder.build();
+    }
+
+    /** Undo a remove, given the current state of the map, an undo record
+     * and the arguments to the remove command to undo.
+     *
+     * @param map           The state of the map after the put to undo
+     * @param undoRecord    The undo record generated by undoRemoveRecord
+     */
+    default void undoPutAll(ISMRMap<K,V> map, Map<K,V> undoRecord, Map<? extends K, ? extends V> m) {
+        undoRecord.entrySet().forEach(e -> {
+                    if (e.getValue() == null) { map.remove(e.getKey()); }
+                    else { map.put(e.getKey(), e.getValue()); }
+                });
+    }
+
+
+    /**
+     * {@inheritDoc}
+     *
+     * Conflicts: this operation conflicts with the entire map, since it drops
+     * all mappings which are present.
      */
     @Mutator(name="clear", reset=true)
     @Override
     void clear();
 
     /**
-     * Returns a {@link Set} view of the keys contained in this map.
-     * The set is backed by the map, so changes to the map are
-     * reflected in the set, and vice-versa.  If the map is modified
-     * while an iteration over the set is in progress (except through
-     * the iterator's own <tt>remove</tt> operation), the results of
-     * the iteration are undefined.  The set supports element removal,
-     * which removes the corresponding mapping from the map, via the
-     * <tt>Iterator.remove</tt>, <tt>Set.remove</tt>,
-     * <tt>removeAll</tt>, <tt>retainAll</tt>, and <tt>clear</tt>
-     * operations.  It does not support the <tt>add</tt> or <tt>addAll</tt>
-     * operations.
+     * {@inheritDoc}
      *
-     * @return a set view of the keys contained in this map
+     * This function currently does not return a view like the java.util implementation,
+     * and changes to the keySet will *not* be reflected in the map.
+     *
+     * Conflicts: This operation currently conflicts with any modification
+     * to the map.
      */
     @Accessor
     @Override
     Set<K> keySet();
 
     /**
-     * Returns a {@link Collection} view of the values contained in this map.
-     * The collection is backed by the map, so changes to the map are
-     * reflected in the collection, and vice-versa.  If the map is
-     * modified while an iteration over the collection is in progress
-     * (except through the iterator's own <tt>remove</tt> operation),
-     * the results of the iteration are undefined.  The collection
-     * supports element removal, which removes the corresponding
-     * mapping from the map, via the <tt>Iterator.remove</tt>,
-     * <tt>Collection.remove</tt>, <tt>removeAll</tt>,
-     * <tt>retainAll</tt> and <tt>clear</tt> operations.  It does not
-     * support the <tt>add</tt> or <tt>addAll</tt> operations.
+     * {@inheritDoc}
      *
-     * @return a collection view of the values contained in this map
+     * This function currently does not return a view like the java.util implementation,
+     * and changes to the values will *not* be reflected in the map.
+     *
+     * Conflicts: This operation currently conflicts with any modification
+     * to the map.
      */
     @Accessor
     @Override
     Collection<V> values();
 
     /**
-     * Returns a {@link Set} view of the mappings contained in this map.
-     * The set is backed by the map, so changes to the map are
-     * reflected in the set, and vice-versa.  If the map is modified
-     * while an iteration over the set is in progress (except through
-     * the iterator's own <tt>remove</tt> operation, or through the
-     * <tt>setValue</tt> operation on a map entry returned by the
-     * iterator) the results of the iteration are undefined.  The set
-     * supports element removal, which removes the corresponding
-     * mapping from the map, via the <tt>Iterator.remove</tt>,
-     * <tt>Set.remove</tt>, <tt>removeAll</tt>, <tt>retainAll</tt> and
-     * <tt>clear</tt> operations.  It does not support the
-     * <tt>add</tt> or <tt>addAll</tt> operations.
+     * {@inheritDoc}
      *
-     * @return a set view of the mappings contained in this map
+     * This function currently does not return a view like the java.util implementation,
+     * and changes to the entrySet will *not* be reflected in the map.
+     *
+     * Conflicts: This operation currently conflicts with any modification
+     * to the map.
      */
     @Accessor
     @Override
