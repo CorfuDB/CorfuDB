3d6985bf1f1a85856ad58a7254955d826d6d64c2
Konstantin Spirov <kspirov@gmail.com>
2017-04-11 20:51:43 -0700

    Bringing back to life the IRetry, turning the implementation upside (#528)
    down
    
    Preserved the approach with the generics used to transfer exceptions to the run method -
    it is really cool (hack). A the exception management and auto decision whether we should
    retry or not is cut with the big scissors:
    
    1) The runnable now *explicitly* responsible to ask for retry by throwing RetryNeededException
    2) All other exceptions are left to the user to handle them in the
    standard, java way. The user is able to decide whether they need to
    be handled from the lambda or outside, when run() throws them.
    3) We don't swallow the InterruptedException any longer neither other exception
    4) I added new features to ExponentialBackoffRetry, the base can be configured, and some portion of the
    wait can be randomized, additionally a fixed wait can be appended to the timeout.
    5) Added some unit tests

---
+++ runtime/src/main/java/org/corfudb/util/retry/AbstractRetry.java
@@ -0,0 +1,23 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.corfudb.util.retry;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+/**
+ * Parent class for all internal IRetry implementations
+ *
+ * Created by Konstantin Spirov on 4/6/2017.
+ */
+@AllArgsConstructor
+abstract class AbstractRetry <E extends Exception, F extends Exception, G extends Exception, H extends Exception, O, A extends IRetry>
+        implements IRetry<E, F, G, H, O, org.corfudb.util.retry.ExponentialBackoffRetry> {
+
+    @Getter
+    final IRetryable<E, F, G, H, O> runFunction;
+
+}
