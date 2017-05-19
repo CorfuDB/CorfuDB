39b192abcd442e9a505a79ef1ce2c1441e765ad1
Michael Wei <mwei@cs.ucsd.edu>
2017-05-19 09:06:49 -0700

    accessWrap implementation

--- annotations/src/main/java/org/corfudb/annotations/Accessor.java
+++ annotations/src/main/java/org/corfudb/annotations/Accessor.java
@@ -21,4 +21,19 @@ public @interface Accessor {
      */
     String conflictParameterFunction() default "";
 
+    /** Whether to wrap the return in a wrapper which ensures
+     * that accesses occur inside a lock.
+     * @return  True, if the return value should be wrapped,
+     *          False otherwise.
+     */
+    boolean accessWrapReturn() default false;
+
+    /** An array of function names which deep access wrapping should be
+     * enabled on. Deep wrapped functions are only used if accessWrapReturn
+     * is set to true.
+     *
+     * @return  A list of functions in which deep access wrapping will be
+     *          run against.
+     */
+    String[] deepAccessWrap() default {};
 }
