32f443a5498b3dafafbec014d58732fedd0a8dac
Vjeko Brajkovic <vjeko@brajkovic.org>
2017-05-26 12:36:15 -0700

    Add a test for the stream API
    Test to see if the stream API works in case of a failover.

--- test/src/test/java/org/corfudb/integration/StreamIT.java
+++ test/src/test/java/org/corfudb/integration/StreamIT.java
@@ -1,19 +1,22 @@
 package org.corfudb.integration;
 
+import com.google.common.io.Files;
 import org.corfudb.protocols.wireprotocol.ILogData;
 import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.runtime.view.stream.IStreamView;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.io.File;
 import java.util.Random;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * A set integration tests that exercise the stream API.
  */
-
 public class StreamIT extends AbstractIT {
     static String corfuSingleNodeHost;
     static int corfuSingleNodePort;
@@ -24,14 +27,43 @@ public class StreamIT extends AbstractIT {
         corfuSingleNodePort = Integer.parseInt((String) PROPERTIES.get("corfuSingleNodePort"));
     }
 
-//    @Test
-    public void simpleStreamTest() throws Exception {
+    @Test
+    public void simpleStreamFailOverTest() throws Exception {
+
+        File corfuLog = Files.createTempDir();
+        corfuLog.deleteOnExit();
+
+        Process corfuServerProcess = new CorfuServerRunner()
+                .setHost(corfuSingleNodeHost)
+                .setPort(corfuSingleNodePort)
+                .setLogPath(corfuLog.getAbsolutePath())
+                .runServer();
+
+        assertThat(shutdownCorfuServer(corfuServerProcess)).isTrue();
+
+        corfuServerProcess = new CorfuServerRunner()
+                .setHost(corfuSingleNodeHost)
+                .setPort(corfuSingleNodePort)
+                .setLogPath(corfuLog.getAbsolutePath())
+                .runServer();
+
+        simpleStreamRun();
+        assertThat(shutdownCorfuServer(corfuServerProcess)).isTrue();
+    }
+
+    @Test
+    public void simpleStreamNoFailOverTest() throws Exception {
 
         Process corfuServerProcess = new CorfuServerRunner()
                 .setHost(corfuSingleNodeHost)
                 .setPort(corfuSingleNodePort)
                 .runServer();
 
+        simpleStreamRun();
+        assertThat(shutdownCorfuServer(corfuServerProcess)).isTrue();
+    }
+
+    private void simpleStreamRun() {
         CorfuRuntime rt = createDefaultRuntime();
         rt.setCacheDisabled(true);
 
@@ -42,8 +74,8 @@ public class StreamIT extends AbstractIT {
         IStreamView s1 = rt.getStreamsView().get(streamId);
 
         // Verify that the stream is empty
-        assertThat(s1.hasNext())
-                .isFalse();
+        final int N_HASNEXT_TESTS = 20;
+        IntStream.range(0, N_HASNEXT_TESTS).forEach(n -> assertThat(s1.hasNext()).isFalse());
 
         // Generate and append random data
         int entrySize = Integer.valueOf(PROPERTIES.getProperty("largeEntrySize"));
@@ -62,7 +94,5 @@ public class StreamIT extends AbstractIT {
 
             assertThat(tmp).isEqualTo(data[x]);
         }
-
-        assertThat(shutdownCorfuServer(corfuServerProcess)).isTrue();
     }
 }
