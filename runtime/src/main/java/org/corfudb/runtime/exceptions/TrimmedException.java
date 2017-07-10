f5a0b346b16a033c77c024bfa9df82e7a6ce6841
Maithem <maithem.mun@gmail.com>
2017-07-10 00:26:07 -0700

    Fix Version Object Trim Race
    This patch fixes a race when a transaction starts with a time
    stamp that is greater than the checkpoint snapshot time, but
    then fails with a TrimmedException.

--- runtime/src/main/java/org/corfudb/runtime/exceptions/TrimmedException.java
+++ runtime/src/main/java/org/corfudb/runtime/exceptions/TrimmedException.java
@@ -1,15 +1,27 @@
 package org.corfudb.runtime.exceptions;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * This exception is thrown when a client tries to read an address
  * that has been trimmed.
  */
 public class TrimmedException extends LogUnitException {
-    public TrimmedException() {
 
+    /*
+     * This flag determins whether the operation that caused this exception
+     * can retry or not.
+     */
+    @Getter
+    @Setter
+    private boolean retriable = true;
+
+    public TrimmedException() {
     }
 
     public TrimmedException(String message) {
         super(message);
+        retriable = true;
     }
 }
