55c35ea1c37e75de3186d576afd59597934ece78
Roger Michoud <rmichoud@vmware.com>
2017-07-22 00:23:23 -0700

    Do not reconstruct tail of empty streams
    When we do a checkpoint of an empty stream, the start
    address of the stream will be -1 (don't exist). When
    we reconstruct the tails, if we encounter a checkpoint
    of an empty stream, we should not reconstruct its tail.

--- test/src/test/java/org/corfudb/recovery/FastSmrMapsLoaderTest.java
+++ test/src/test/java/org/corfudb/recovery/FastSmrMapsLoaderTest.java
@@ -1125,5 +1125,56 @@ public class FastSmrMapsLoaderTest extends AbstractViewTest {
 
     }
 
+    /**
+     * Ensure that an empty stream (stream that was opened but never had any updates)
+     * will not have its tail reconstructed. Tail of such an empty stream will be -1.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void doNotReconstructEmptyCheckpoints() throws Exception {
+        CorfuRuntime rt1 = getDefaultRuntime();
+        int mapCount = 0;
+        Map<String, String> map1 = createMap("Map1", rt1);
+        mapCount++;
+        Map<String, String> map2 = createMap("Map2", rt1);
+        mapCount++;
+        Map<String, String> emptyMap = createMap("EmptyMap", rt1);
+        mapCount++;
+
+        map1.put("k1", "v1");
+        map1.put("k2", "v2");
+        map2.put("k3", "v3");
+        map2.put("k4", "v4");
+
+
+        UUID stream1 = CorfuRuntime.getStreamID("Map1");
+        UUID stream2 = CorfuRuntime.getStreamID("Map2");
+        UUID emptyStream = CorfuRuntime.getStreamID("EmptyMap");
+
+        MultiCheckpointWriter mcw = new MultiCheckpointWriter();
+        mcw.addMap((SMRMap) map1);
+        mcw.addMap((SMRMap) map2);
+        mcw.addMap((SMRMap) emptyMap);
+        mcw.appendCheckpoints(getRuntime(), "author");
+
+        long tail1 = rt1.getSequencerView().nextToken(Collections.singleton(stream1), 0).getToken().getTokenValue();
+        long tail2 = rt1.getSequencerView().nextToken(Collections.singleton(stream2), 0).getToken().getTokenValue();
+
+        CorfuRuntime rt2 = new CorfuRuntime(getDefaultConfigurationString())
+                .setLoadSmrMapsAtConnect(true)
+                .connect();
+
+        FastSmrMapsLoader fsm = new FastSmrMapsLoader(rt2);
+        fsm.setRecoverSequencerMode(true);
+        fsm.loadMaps();
+
+        Map<UUID, Long> streamTails = fsm.getStreamTails();
+        assertThat(streamTails.get(stream1)).isEqualTo(tail1);
+        assertThat(streamTails.get(stream2)).isEqualTo(tail2);
+        assertThat(streamTails.get(emptyStream)).isNull();
+
+    }
+
 
 }
