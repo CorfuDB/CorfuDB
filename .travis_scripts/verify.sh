3fd8374425f4134515c5c7830ce50f78f102bf87
Michael Wei <mwei@cs.ucsd.edu>
2017-07-26 18:01:05 -0700

    don't submit to sonar on external (#882)

--- .travis_scripts/verify.sh
+++ .travis_scripts/verify.sh
@@ -10,9 +10,13 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true
         -Dsonar.organization=corfudb \
         -Dsonar.login=$SONAR_TOKEN
 else
-     if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-     echo "Not submitting sonar status to github because this is an external PR"
-     fi
-    ./mvnw verify -Dmaven.javadoc.skip=true -Dsonar.host.url=https://sonarqube.com \
+    if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ] ; then
+        echo "Not submitting sonar status to github because this is an external build"
+        ./mvnw verify -Dmaven.javadoc.skip=true -Dsonar.host.url=https://sonarqube.com \
+        -Dsonar.organization=corfudb -Dsonar.analysis.mode=preview \
+        -Dtest.travisBuild=true sonar:sonar
+    else
+        ./mvnw verify -Dmaven.javadoc.skip=true -Dsonar.host.url=https://sonarqube.com \
         -Dsonar.organization=corfudb -Dsonar.login=$SONAR_TOKEN -Dtest.travisBuild=true sonar:sonar
+    fi
 fi
