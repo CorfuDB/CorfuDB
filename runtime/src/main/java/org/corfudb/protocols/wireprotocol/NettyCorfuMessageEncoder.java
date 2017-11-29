caba2416c4cb7418e4afd93bd92063bdd10477a5
Michael Wei <mwei@cs.ucsd.edu>
2017-11-29 13:26:48 -0800

    Convert to MessageToMessageEncoder/Decoder

--- runtime/src/main/java/org/corfudb/protocols/wireprotocol/NettyCorfuMessageEncoder.java
+++ runtime/src/main/java/org/corfudb/protocols/wireprotocol/NettyCorfuMessageEncoder.java
@@ -1,9 +1,13 @@
 package org.corfudb.protocols.wireprotocol;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
+import io.netty.handler.codec.MessageToMessageEncoder;
+import java.util.List;
+import javax.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.LongAccumulator;
@@ -13,27 +17,31 @@ import java.util.function.LongBinaryOperator;
  * Created by mwei on 10/1/15.
  */
 @Slf4j
-public class NettyCorfuMessageEncoder extends MessageToByteEncoder<CorfuMsg> {
+@Sharable
+public class NettyCorfuMessageEncoder extends MessageToMessageEncoder<CorfuMsg> {
 
 
-    final LongAccumulator maxValue = new LongAccumulator(Math::max, Long.MIN_VALUE);
+    private final LongAccumulator maxValue = new LongAccumulator(Math::max, Long.MIN_VALUE);
 
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext,
-                          CorfuMsg corfuMsg,
-                          ByteBuf byteBuf) throws Exception {
+    protected void encode(
+        @Nonnull ChannelHandlerContext ctx,
+        @Nonnull CorfuMsg corfuMsg,
+        @Nonnull List<Object> list) throws Exception {
+        // Nice if the size could be known
+        final ByteBuf buffer = ctx.alloc().directBuffer();
         try {
-            corfuMsg.serialize(byteBuf);
-            if(log.isDebugEnabled()) {
+            corfuMsg.serialize(buffer);
+            if (log.isDebugEnabled()) {
                 long prev = maxValue.get();
-                maxValue.accumulate(byteBuf.readableBytes());
+                maxValue.accumulate(buffer.readableBytes());
                 long curr = maxValue.get();
                 // The max value has been updated.
                 if (prev < curr) {
                     log.debug("encode: New max write buffer found {}", curr);
                 }
             }
-
+            list.add(buffer);
         } catch (Exception e) {
             log.error("encode: Error during serialization!", e);
         }
