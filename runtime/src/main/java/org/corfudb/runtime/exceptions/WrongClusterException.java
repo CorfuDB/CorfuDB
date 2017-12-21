df0fe6041905bb8b22011dd269dc188e54c3f780
Michael Wei <mwei@cs.ucsd.edu>
2017-12-20 19:35:39 -0800

    Missing file

---
+++ runtime/src/main/java/org/corfudb/runtime/exceptions/WrongClusterException.java
@@ -0,0 +1,30 @@
+package org.corfudb.runtime.exceptions;
+
+import java.util.UUID;
+import javax.annotation.Nullable;
+import lombok.Getter;
+import org.corfudb.util.UuidUtils;
+
+public class WrongClusterException extends RuntimeException {
+
+    /** The cluster we expected to connect to. */
+    @Getter
+    UUID expectedCluster;
+
+    /** The cluster we actually ended up connecting to. */
+    @Getter
+    UUID actualCluster;
+
+    /** Create a new {@link org.corfudb.runtime.exceptions.WrongClusterException}.
+     *
+     * @param expectedCluster   The cluster we expected to connect to.
+     * @param actualCluster     The cluster we actually ended up connecting to.
+     */
+    public WrongClusterException(@Nullable UUID expectedCluster,
+                                 @Nullable UUID actualCluster) {
+        super("Expected: "
+            + ((expectedCluster == null) ? "null" : UuidUtils.asBase64(expectedCluster))
+            + " Actual: "
+            + ((actualCluster == null) ? "null" : UuidUtils.asBase64(actualCluster)));
+    }
+}
