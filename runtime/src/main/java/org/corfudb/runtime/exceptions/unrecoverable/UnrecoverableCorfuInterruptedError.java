7c028946705ba0aae8f1f42d392826f1b0760356
Michael Wei <mwei@cs.ucsd.edu>
2017-12-27 12:51:07 -0800

    Updated comments

--- runtime/src/main/java/org/corfudb/runtime/exceptions/unrecoverable/UnrecoverableCorfuInterruptedError.java
+++ runtime/src/main/java/org/corfudb/runtime/exceptions/unrecoverable/UnrecoverableCorfuInterruptedError.java
@@ -4,13 +4,12 @@ import javax.annotation.Nonnull;
 
 /** Thrown when a Corfu Client/Server is interrupted in an unrecoverable manner.
  *  Once interrupted in this manner, Corfu can only guarantee safety if the application
- *  exits, as Corfu safe is no longer recoverable.
+ *  exits, as Corfu safety is no longer recoverable.
  */
 public class UnrecoverableCorfuInterruptedError extends UnrecoverableCorfuError {
 
     /** Construct a new {@link UnrecoverableCorfuInterruptedError} with a root cause.
      *
-     * <p> Also resets the interrupt pending flag.
      * @param cause     The root cause of the exception.
      */
     public UnrecoverableCorfuInterruptedError(@Nonnull InterruptedException cause) {
@@ -20,7 +19,6 @@ public class UnrecoverableCorfuInterruptedError extends UnrecoverableCorfuError 
     /** Construct a new {@link UnrecoverableCorfuInterruptedError} with a given message
      * and root cause.
      *
-     * <p> Also resets the interrupt pending flag.
      * @param message   A message describing the exception.
      * @param cause     The root cause of the exception.
      */
