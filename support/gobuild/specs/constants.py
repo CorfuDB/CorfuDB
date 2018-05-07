7ee77a3d541e2f9a1d348e745f2b0b6176327fae
Edwin Chiu <echiu@vmware.com>
2018-05-07 11:11:11 -0700

    Update RELEASENAME to eclipse
    Change-Id: I2e5556b7ad278f9b98739c56fe14359608120d47
    Release: eclipse
    Issue: #
    Jira: #NP-913

--- support/gobuild/specs/constants.py
+++ support/gobuild/specs/constants.py
@@ -1,5 +1,5 @@
-RELEASENAME = 'equinox'
-BRANCH = 'nsx-dropkick'
+RELEASENAME = 'eclipse'
+BRANCH = 'nsx-corfu'
 
 UBUNTU_MIRROR = "http://ubtmirror.nsbu.eng.vmware.com/ubuntu"
 APT_MIRROR = "http://apt-cache.nsbu.eng.vmware.com/apt/%s" % RELEASENAME
