55d970ac8ad90e33fada33411ddb9c11269a716c
Sam Behnam <seresearch@gmail.com>
2018-07-12 14:00:28 -0700

    Null Check Specific Exception Tight Visibility

--- runtime/src/main/java/org/corfudb/runtime/view/workflows/AddNode.java
+++ runtime/src/main/java/org/corfudb/runtime/view/workflows/AddNode.java
@@ -1,12 +1,12 @@
 package org.corfudb.runtime.view.workflows;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.protocols.wireprotocol.orchestrator.CreateWorkflowResponse;
 import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.runtime.clients.ManagementClient;
 import org.corfudb.runtime.view.Layout;
 
-import javax.annotation.Nonnull;
 import java.time.Duration;
 import java.util.UUID;
 import java.util.concurrent.TimeoutException;
@@ -21,9 +21,9 @@ import java.util.concurrent.TimeoutException;
 @Slf4j
 public class AddNode extends WorkflowRequest {
 
-    public AddNode(@Nonnull String endpointToAdd, @Nonnull CorfuRuntime runtime,
-                   @Nonnull int retry, @Nonnull Duration timeout,
-                   @Nonnull Duration pollPeriod) {
+    public AddNode(@NonNull String endpointToAdd, @NonNull CorfuRuntime runtime,
+                   int retry, @NonNull Duration timeout,
+                   @NonNull Duration pollPeriod) {
         this.nodeForWorkflow = endpointToAdd;
         this.runtime = runtime;
         this.retry = retry;
@@ -32,7 +32,7 @@ public class AddNode extends WorkflowRequest {
     }
 
     @Override
-    protected UUID sendRequest(@Nonnull ManagementClient managementClient) throws TimeoutException {
+    protected UUID sendRequest(@NonNull ManagementClient managementClient) throws TimeoutException {
         // Select the current tail node and send an add node request to the orchestrator
         CreateWorkflowResponse resp = managementClient.addNodeRequest(nodeForWorkflow);
         log.info("sendRequest: requested to add {} on orchestrator {}:{}",
@@ -42,7 +42,7 @@ public class AddNode extends WorkflowRequest {
     }
 
     @Override
-    protected boolean verifyRequest(@Nonnull Layout layout) {
+    protected boolean verifyRequest(@NonNull Layout layout) {
         // Verify that the node has been added and that the address space isn't
         // segmented
         log.info("verifyRequest: {} to {}", this, layout);
