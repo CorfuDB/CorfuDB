a4743fe3d8137443bf722324924ad89041ab4366
Nate West <westn@vmware.com>
2018-07-17 15:54:33 -0700

    Revert "Merge tag 'eclipse-rtqa-16'" Jira: #NP-1005 This reverts commit 6e0ba749007dfbabcbb1f2ab4d5d2bb191d19f31.
    Change-Id: If186dc68418f83177202b42992d419acc317b88d

--- support/gobuild/README.txt
+++
@@ -1,2 +0,0 @@
-This directory contains files necessary for GoBuild support. See
-https://wiki.eng.vmware.com/GoBuild for details.
--- support/gobuild/specs/constants.py
+++
@@ -1,6 +0,0 @@
-RELEASENAME = 'eclipse'
-BRANCH = 'nsx-eclipse'
-
-UBUNTU_MIRROR = "http://ubtmirror.nsbu.eng.vmware.com/ubuntu"
-APT_MIRROR = "http://apt-cache.nsbu.eng.vmware.com/apt/%s" % RELEASENAME
-ALLOW_OFFICIAL_KEY = 'allow.elfofficialkey'
