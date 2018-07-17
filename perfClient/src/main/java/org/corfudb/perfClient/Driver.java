17fe1d94840996f8a3b62cb020d2f8d02cd5d58c
Shriya Vohra <shriya.vohra@gmail.com>
2018-07-17 13:42:24 -0700

    Add package in perfClient and make runnable from command line

--- perfClient/src/main/java/org/corfudb/perfClient/Driver.java
+++ perfClient/src/main/java/org/corfudb/perfClient/Driver.java
@@ -7,8 +7,6 @@ import org.corfudb.runtime.CorfuRuntime;
 public class Driver {
 
     public static void main(String[] args) throws Exception {
-        // use cons prod APIs now instead of all this other stuff
-
         String connString = args[0];
         final int numRt = Integer.valueOf(args[1]);
         final int numProd = Integer.valueOf(args[2]);
@@ -29,15 +27,11 @@ public class Driver {
 
         // Producer
         System.out.println("Producer");
-        int[] numWrites = new int[numProd];
-        long[] lastAddress = new long[numProd];
-        boolean[] completed = {false};
+        int[] numWrites = {0};
+        long[] lastAddress = {0};
         for (int x = 0; x < numProd; x++) {
             final CorfuRuntime rt = rts[x%numRt];
-            final int ind = x;
             Runnable r = () -> {
-                System.out.println("[p] x: " + (ind)); // should be diff for all threads
-
                 long time = 0;
 
                 for (int i = 0; i < numReq; i++) {
@@ -46,20 +40,15 @@ public class Driver {
                     TokenResponse tr = rt.getSequencerView().next();
                     Token t = tr.getToken();
                     rt.getAddressSpaceView().write(t, payload);
-
-                    lastAddress[ind] = t.getTokenValue(); // this is the new last address
-                    System.out.println("Written at: " + (t.getTokenValue()));
-                    numWrites[ind] += 1;
+                    numWrites[0] += 1;
 
                     long ts2 = System.currentTimeMillis();
                     time += (ts2 - ts1);
                 }
+                lastAddress[0] += rt.getSequencerView().next().getTokenValue();
 
-                System.out.println("(Producer) Latency [ms/op]: " + ((time*1.0)/numReq)
-                        + " " + ((time*1.0)/numWrites[ind]));
-                System.out.println("(Producer) Throughput [ops/ms]: " + (numReq/(time*1.0))
-                        + " " + (numWrites[ind])/(time*1.0));
-                System.out.println("Num Writes: " + numWrites[ind]);
+                System.out.println("(Producer) Latency [ms/op]: " + ((time*1.0)/numReq));
+                System.out.println("(Producer) Throughput [ops/ms]: " + (numReq/(time*1.0)));
             };
 
             prodThreads[x] = new Thread(r); // performs lambda fn from above
@@ -67,61 +56,32 @@ public class Driver {
 
         // Consumer
         System.out.println("Consumer");
-        int[] numReads = new int[numCons];
+        int[] numReads = {0};
         for (int x = 0; x < numCons; x++) {
             final CorfuRuntime rt = rts[x%numRt];
-            final int ind = x;
             Runnable r = () -> {
-                System.out.println("[c] x: " + (ind)); // should be diff for all threads
-
-                long time = 0;
                 long localOffset = -1;
 
-                boolean lastTime = false;
+                while (localOffset < lastAddress[0]) {
+                    TokenResponse tr = rt.getSequencerView().query();
+                    long globalOffset = tr.getTokenValue();
 
-                while (!completed[0] || lastTime) {
-                    long ts1 = System.currentTimeMillis();
-
-                    long maxAddress = 0;
-                    for (int i = 0; i < numProd; i++) {
-                        if (lastAddress[i] > maxAddress) {
-                            maxAddress = lastAddress[i];
-                        }
-                    }
-
-                    long globalOffset = maxAddress;
                     long delta = globalOffset - localOffset;
 
-                    if (maxAddress != 0) {
-                        if (delta == 0) {
-                            try {
-                                Thread.sleep(1000);
-                            } catch (Exception e) {
-                                System.out.println("Error: could not sleep!");
-                            }
-                        } else {
-                            for (long i = localOffset + 1; i <= globalOffset; i++) {
-                                rt.getAddressSpaceView().read(i);
-                                System.out.println("Read from: " + (i));
-                                numReads[ind] += 1;
-                            }
-                            localOffset = globalOffset;
+                    if (delta == 0) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            System.out.println("Error: could not sleep!");
                         }
-                    }
-
-                    long ts2 = System.currentTimeMillis();
-                    time += (ts2 - ts1);
-
-                    if (lastTime) {
-                        lastTime = false;
-                    } else if (completed[0]) {
-                        lastTime = true;
+                    } else {
+                        for (long i = localOffset + 1; i <= globalOffset; i++) {
+                            rt.getAddressSpaceView().read(i);
+                            numReads[0] += 1;
+                        }
+                        localOffset = globalOffset;
                     }
                 }
-
-                System.out.println("(Consumer) Latency [ms/op]: " + ((time*1.0)/numReads[ind]));
-                System.out.println("(Consumer) Throughput [ops/ms]: " + (numReads[ind]/(time*1.0)));
-                System.out.println("Num Reads: " + numReads[ind]);
             };
 
             consThreads[x] = new Thread(r); // performs lambda fn from above
@@ -138,31 +98,18 @@ public class Driver {
         for (int x = 0; x < numProd; x++) {
             prodThreads[x].join();
         }
-
-        // set last address here (do the .query)
-        completed[0] = true;
-
         for (int x = 0; x < numCons; x++) {
             consThreads[x].join();
         }
 
         long ts2 = System.currentTimeMillis();
 
-        int totalWrites = 0;
-        for (int i = 0; i < numProd; i++) {
-            totalWrites += numWrites[i];
-        }
-        int totalReads = 0;
-        for (int i = 0; i < numCons; i++) {
-            totalReads += numReads[i];
-        }
-
         System.out.println("\nOverall");
-        System.out.println(totalWrites);
-        System.out.println(totalReads);
+        System.out.println(numWrites[0]);
+        System.out.println(numReads[0]);
 
         System.out.println("Total Time [ms]: " + (ts2 - ts1));
-        System.out.println("Num Ops: " + (totalWrites + totalReads));
-        System.out.println("Throughput: " + (((totalWrites + totalReads)*1.0) / (ts2 - ts1)));
+        System.out.println("Num Ops: " + (numWrites[0] + numReads[0]));
+        System.out.println("Throughput: " + (((numWrites[0] + numReads[0]) * 1.0) / (ts2 - ts1)));
     }
 }
