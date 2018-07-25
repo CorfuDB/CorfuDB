5050a2e67d7cc0b1160a0e1c0ed66dc28af89679
Shriya Vohra <shriya.vohra@gmail.com>
2018-07-25 15:58:39 -0700

    remove debug statements

--- perfClient/src/main/java/org/corfudb/perfClient/Driver.java
+++ perfClient/src/main/java/org/corfudb/perfClient/Driver.java
@@ -144,8 +144,86 @@ public class Driver {
         System.out.println("Throughput: " + (((totalWrites + totalReads)*1.0) / (time)));
     }
 
+    public static void runCluster(String[] args, String prodClass) throws Exception {
+        String connString = args[0];
+        List<String> hosts = new ArrayList<>();
+        hosts.add(connString + ":9000");
+        hosts.add(connString + ":9001");
+        hosts.add(connString + ":9002");
+        hosts.add(connString + ":9003");
+
+        final int numProd = Integer.valueOf(args[1]);
+        final int numReq = Integer.valueOf(args[2]);
+        final int payloadSize = Integer.valueOf(args[3]);
+        final UUID streamID = UUID.randomUUID();
+
+        Thread[] prodThreads = new Thread[numProd];
+
+        byte[] payload = new byte[payloadSize];
+
+        // Producer
+        int[] numWrites = new int[numProd];
+        for (int x = 0; x < numProd; x++) {
+            final CorfuRuntime rt = new CorfuRuntime(hosts.get(x % hosts.size())).connect();
+            //System.out.println("Connected! " + hosts.get(x % hosts.size()));
+            final int ind = x;
+            Runnable r = () -> {
+                long startTime = System.currentTimeMillis();
+                //System.out.println("Inside producer " + (ind));
+
+                Producer producer = null;
+                if (prodClass.equals("AddressSpaceProducer")) {
+                    producer = new AddressSpaceProducer(rt);
+                } else if (prodClass.equals("StreamsProducer")) {
+                    producer = new StreamsProducer(rt, streamID);
+                }
+
+                for (int i = 0; i < numReq; i++) {
+                    producer.send(payload);
+                    numWrites[ind] += 1;
+                    //System.out.println("Incremented num writes to " + (numWrites[ind]));
+                }
+
+                long endTime = System.currentTimeMillis();
+                long time = endTime - startTime;
+
+//                System.out.println("(Producer) Latency [ms/op]: " + ((time*1.0)/numReq)
+//                        + " " + ((time*1.0)/numWrites[ind]));
+//                System.out.println("(Producer) Throughput [ops/ms]: " + (numReq/(time*1.0))
+//                        + " " + (numWrites[ind])/(time*1.0));
+//                System.out.println("Num Writes: " + numWrites[ind]);
+            };
+
+            prodThreads[x] = new Thread(r); // performs lambda fn from above
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        for (int x = 0; x < numProd; x++) {
+            prodThreads[x].start();
+        }
+
+        for (int x = 0; x < numProd; x++) {
+            prodThreads[x].join();
+        }
+
+        long endTime = System.currentTimeMillis();
+        long time = endTime - startTime;
+
+        int totalWrites = 0;
+        for (int i = 0; i < numProd; i++) {
+            totalWrites += numWrites[i];
+        }
+
+        System.out.println("\nOverall");
+        System.out.println("Total Writes: " + (totalWrites));
+        System.out.println("Total Time [ms]: " + (time));
+        System.out.println("Throughput: " + ((totalWrites*1.0) / (time)));
+    }
+
     public static void main(String[] args) throws Exception {
-        runProducerConsumer(args, "AddressSpaceProducer", "AddressSpaceConsumer");
+        //runProducerConsumer(args, "AddressSpaceProducer", "AddressSpaceConsumer");
         //runProducerConsumer(args, "StreamsProducer", "StreamConsumer");
+        runCluster(args, "AddressSpaceProducer");
     }
 }
