bea2a4c577a61d9297394e44b8018901fbf4a87d
Sam Behnam <seresearch@gmail.com>
2018-10-23 12:44:19 -0700

    Interface for Failure Decision Engine

---
+++ infrastructure/src/main/java/org/corfudb/infrastructure/management/ClusterRecommendationEngineFactory.java
@@ -0,0 +1,37 @@
+package org.corfudb.infrastructure.management;
+
+
+/**
+ * This is a factory for creating concrete {@link ClusterRecommendationEngine}s based on
+ * the requested {@link ClusterRecommendationStrategy}s.
+ *
+ * Created by Sam Behnam on 10/19/18.
+ */
+public class ClusterRecommendationEngineFactory {
+
+    private ClusterRecommendationEngineFactory() {
+        // To prevent instantiation of the factory class.
+    }
+
+    /**
+     * Create an instance of {@link ClusterRecommendationEngine} based on the provided
+     * {@link ClusterRecommendationStrategy}.
+     *
+     * @param strategy a {@link ClusterRecommendationStrategy} representing desired
+     *               algorithm to be used for determining failure and healing status of Corfu
+     *               servers.
+     * @return a concrete instance of {@link ClusterRecommendationEngine} specific to the
+     * provided strategy.
+     */
+    public ClusterRecommendationEngine createForStrategy(ClusterRecommendationStrategy strategy) {
+        switch (strategy) {
+            case FULLY_CONNECTED_CLUSTER:
+                throw new UnsupportedOperationException(strategy.name());
+            case CENTRALIZED_CLUSTER:
+                throw new UnsupportedOperationException(strategy.name());
+            default:
+                throw new UnsupportedOperationException("Unknown ClusterRecommendationStrategy");
+
+        }
+    }
+}
