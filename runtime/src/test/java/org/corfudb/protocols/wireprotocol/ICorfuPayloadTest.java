51f928a1dcc59a265c52fecc7a644d7a5b607516
Viacheslav <xrw.null@gmail.com>
2018-12-06 14:07:03 -0800

    ICorfuPayload unit testing (#1588)

---
+++ runtime/src/test/java/org/corfudb/protocols/wireprotocol/ICorfuPayloadTest.java
@@ -0,0 +1,53 @@
+package org.corfudb.protocols.wireprotocol;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.corfudb.protocols.logprotocol.CheckpointEntry.CheckpointEntryType;
+import org.corfudb.protocols.wireprotocol.IMetadata.DataRank;
+import org.corfudb.runtime.view.Layout;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+
+public class ICorfuPayloadTest {
+
+    @Test
+    public void checkConstructorMap() {
+        List<Class<?>> types = Arrays.asList(
+                Byte.class, Integer.class, Long.class, Boolean.class, Double.class, Float.class, String.class,
+                Layout.class, DataRank.class, CheckpointEntryType.class, UUID.class, byte[].class, ByteBuf.class
+        );
+
+        assertThat(ICorfuPayload.constructorMap.keySet()).containsAll(types);
+    }
+
+    @Test
+    public void testBuildPayloadFromBuffer(){
+        final int value = 12345;
+        ByteBuf payload = Unpooled.buffer().writeInt(value);
+        Integer result = ICorfuPayload.fromBuffer(payload, Integer.class);
+
+        assertThat(result).isEqualTo(value);
+    }
+
+    @Test
+    public void testSerialize(){
+        ByteBuf buf = Unpooled.buffer();
+
+        Set<String> payload = new HashSet<>();
+        payload.add("value1");
+        payload.add("value2");
+
+        ICorfuPayload.serialize(buf, payload);
+
+        assertThat(ICorfuPayload.setFromBuffer(buf, String.class)).isEqualTo(payload);
+    }
+
+}
