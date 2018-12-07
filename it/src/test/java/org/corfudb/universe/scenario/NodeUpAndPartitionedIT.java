b3e8c36411208a73734debdd0d819e06ae4da434
Sam Behnam <seresearch@gmail.com>
2018-12-06 23:16:16 -0800

    Remove data path asserts

--- it/src/test/java/org/corfudb/universe/scenario/NodeUpAndPartitionedIT.java
+++ it/src/test/java/org/corfudb/universe/scenario/NodeUpAndPartitionedIT.java
@@ -4,15 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.corfudb.universe.scenario.ScenarioUtils.waitForLayoutChange;
 import static org.corfudb.universe.scenario.ScenarioUtils.waitForUnresponsiveServersChange;
 import static org.corfudb.universe.scenario.fixture.Fixtures.TestFixtureConst.DEFAULT_STREAM_NAME;
-import static org.corfudb.universe.scenario.fixture.Fixtures.TestFixtureConst.DEFAULT_TABLE_ITER;
 
 import org.corfudb.runtime.collections.CorfuTable;
-import org.corfudb.runtime.view.ClusterStatusReport;
 import org.corfudb.universe.GenericIntegrationTest;
 import org.corfudb.universe.group.cluster.CorfuCluster;
 import org.corfudb.universe.node.client.CorfuClient;
 import org.corfudb.universe.node.server.CorfuServer;
-import org.corfudb.util.Sleep;
 import org.junit.Test;
 
 import java.time.Duration;
@@ -48,7 +45,7 @@ public class NodeUpAndPartitionedIT extends GenericIntegrationTest {
             CorfuClient corfuClient = corfuCluster.getLocalCorfuClient();
 
             CorfuTable<String, String> table = corfuClient.createDefaultCorfuTable(DEFAULT_STREAM_NAME);
-            for (int i = 0; i < DEFAULT_TABLE_ITER; i++) {
+            for (int i = 0; i < 5; i++) {
                 table.put(String.valueOf(i), String.valueOf(i));
             }
 
@@ -86,31 +83,6 @@ public class NodeUpAndPartitionedIT extends GenericIntegrationTest {
                         .containsExactly(server0.getEndpoint());
                 assertThat(corfuClient.getLayout().getAllActiveServers())
                         .contains(server1.getEndpoint());
-
-                // Verify cluster status. Cluster status should be DEGRADED after one node is
-                // marked unresponsive
-                ClusterStatusReport clusterStatusReport = corfuClient.getManagementView().getClusterStatus();
-                // TODO: uncomment the following line after ClusterStatus API is fixed for partial partition
-                // assertThat(clusterStatusReport.getClusterStatus()).isEqualTo(ClusterStatus.DEGRADED);
-                // TODO: add node status check after we redefine NodeStatus semantics
-
-                // Heal all the link failures
-                server0.reconnect(Arrays.asList(server1, server2));
-                waitForUnresponsiveServersChange(size -> size == 0, corfuClient);
-
-                final Duration sleepDuration = Duration.ofSeconds(1);
-                // Verify cluster status is STABLE
-                clusterStatusReport = corfuClient.getManagementView().getClusterStatus();
-                while (!clusterStatusReport.getClusterStatus().equals(ClusterStatusReport.ClusterStatus.STABLE)) {
-                    clusterStatusReport = corfuClient.getManagementView().getClusterStatus();
-                    Sleep.sleepUninterruptibly(sleepDuration);
-                }
-                assertThat(clusterStatusReport.getClusterStatus()).isEqualTo(ClusterStatusReport.ClusterStatus.STABLE);
-
-                // Verify data path is working fine
-                for (int i = 0; i < DEFAULT_TABLE_ITER; i++) {
-                    assertThat(table.get(String.valueOf(i))).isEqualTo(String.valueOf(i));
-                }
             });
         });
     }
