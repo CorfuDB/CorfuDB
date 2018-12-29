af5e03eadbd154645a6baa310aa3e12b1419f514
Anny Martinez <amartinezman@vmware.com>
2018-12-28 18:41:14 -0800

    Add fixed reconnect backoff on channelCloseFuture

--- runtime/src/main/java/org/corfudb/runtime/clients/NettyClientRouter.java
+++ runtime/src/main/java/org/corfudb/runtime/clients/NettyClientRouter.java
@@ -3,6 +3,7 @@ package org.corfudb.runtime.clients;
 import com.codahale.metrics.Timer;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
@@ -20,10 +21,27 @@ import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.annotation.Nonnull;
+import javax.net.ssl.SSLException;
+
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+
 import org.corfudb.protocols.wireprotocol.ClientHandshakeHandler;
 import org.corfudb.protocols.wireprotocol.ClientHandshakeHandler.ClientHandshakeEvent;
 import org.corfudb.protocols.wireprotocol.CorfuMsg;
@@ -45,20 +63,6 @@ import org.corfudb.util.CorfuComponent;
 import org.corfudb.util.MetricsUtils;
 import org.corfudb.util.NodeLocator;
 import org.corfudb.util.Sleep;
-
-import javax.annotation.Nonnull;
-import javax.net.ssl.SSLException;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicLong;
 
 
 /**
@@ -323,7 +327,8 @@ public class NettyClientRouter extends SimpleChannelInboundHandler<CorfuMsg>
             });
             // If we aren't shutdown, reconnect.
             if (!shutdown) {
-                log.info("addReconnectionOnCloseFuture[{}]: reconnecting", node);
+                Sleep.sleepUninterruptibly(parameters.getConnectionRetryRate());
+                log.info("addReconnectionOnCloseFuture[{}]: reconnecting...", node);
                 // Asynchronously connect again.
                 connectAsync(bootstrap);
             }
@@ -362,8 +367,8 @@ public class NettyClientRouter extends SimpleChannelInboundHandler<CorfuMsg>
             // Otherwise, the connection failed. If we're not shutdown, try reconnecting after
             // a sleep period.
             if (!shutdown) {
-                log.info("connectAsync[{}]: Channel connection failed, reconnecting...", node);
                 Sleep.sleepUninterruptibly(parameters.getConnectionRetryRate());
+                log.info("connectAsync[{}]: Channel connection failed, reconnecting...", node);
                 // Call connect, which will retry the call again.
                 // Note that this is not recursive, because it is called in the
                 // context of the handler future.
