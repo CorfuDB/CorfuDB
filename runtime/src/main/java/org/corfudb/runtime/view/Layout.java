acb0742889fea795f19eec2e292acf03be3e3221
Zeeshan Lokhandwala <zalokhan@ncsu.edu>
2019-02-21 22:01:55 -0800

    Avoid wait after holeFill in range read.

--- runtime/src/main/java/org/corfudb/runtime/view/Layout.java
+++ runtime/src/main/java/org/corfudb/runtime/view/Layout.java
@@ -394,7 +394,7 @@ public class Layout {
                     return new ChainReplicationProtocol(new NeverHoleFillPolicy(100));
                 } else {
                     return new ChainReplicationProtocol(
-                            new ReadWaitHoleFillPolicy(r.getParameters().getRequestTimeout(),
+                            new ReadWaitHoleFillPolicy(r.getParameters().getHoleFillTimeout(),
                                     r.getParameters().getHoleFillRetryThreshold()));
                 }
             }
@@ -437,7 +437,7 @@ public class Layout {
                     return new QuorumReplicationProtocol(new NeverHoleFillPolicy(100));
                 } else {
                     return new QuorumReplicationProtocol(
-                            new ReadWaitHoleFillPolicy(r.getParameters().getRequestTimeout(),
+                            new ReadWaitHoleFillPolicy(r.getParameters().getHoleFillTimeout(),
                                     r.getParameters().getHoleFillRetryThreshold()));
                 }
             }
