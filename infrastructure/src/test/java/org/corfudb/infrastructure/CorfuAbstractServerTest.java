c89d1911f6770a0b945679f82c49a69d0dd94640
Viacheslav <xrw.null@gmail.com>
2019-03-05 18:31:30 -0800

    Simplify AbstarctServer

--- infrastructure/src/test/java/org/corfudb/infrastructure/CorfuAbstractServerTest.java
+++ infrastructure/src/test/java/org/corfudb/infrastructure/CorfuAbstractServerTest.java
@@ -9,16 +9,27 @@ import org.corfudb.protocols.wireprotocol.CorfuMsg;
 import org.corfudb.protocols.wireprotocol.CorfuMsgType;
 import org.junit.Test;
 
-public class AbstractServerTest {
+import java.util.concurrent.ExecutorService;
 
+public class CorfuAbstractServerTest {
+
+    /**
+     * Check that handler is not executed if the server  is in shutdown state.
+     */
     @Test
     public void testShutdown() {
         final CorfuMsgHandler handler = mock(CorfuMsgHandler.class);
+        final ExecutorService executor = mock(ExecutorService.class);
 
         AbstractServer server = new AbstractServer() {
             @Override
             public CorfuMsgHandler getHandler() {
                 return handler;
+            }
+
+            @Override
+            public ExecutorService getExecutor() {
+                return executor;
             }
         };
 
