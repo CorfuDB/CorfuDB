65fe5446a1d03cbc0cdbf3bd5be5da923a7eb601
Maithem <Maithem@users.noreply.github.com>
2019-04-08 12:23:29 -0700

    Cache-After-Write Race + Data Loss Bug (#1805) (#1819)
    While a thread writes (i.e. replicate), other threads within the
    same client can read the write from the server before it gets
    cached. This results in losing undo records and thus causes
    objects to experience NoRollBackException.
    
    Porting #1805 from master to corfu-0.2.2
    
    Furthermore, this patch also fixes a correctness bug introduced
    in 2ab39da845ba3e40a66e31ad550d54d59e3db103 where empty log
    slots are ignored and not hole filled.

--- infrastructure/src/main/java/org/corfudb/infrastructure/orchestrator/actions/StateTransfer.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/orchestrator/actions/StateTransfer.java
@@ -81,7 +81,7 @@ public class StateTransfer {
             long ts1 = System.currentTimeMillis();
 
             Map<Long, ILogData> dataMap = runtime.getAddressSpaceView()
-                    .cacheFetch(ContiguousSet.create(
+                    .fetchRange(ContiguousSet.create(
                             Range.closed(chunkStart, chunkEnd),
                             DiscreteDomain.longs()));
 
