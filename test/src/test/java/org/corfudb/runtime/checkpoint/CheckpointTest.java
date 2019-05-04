24796a18fceb20ca628a33ca1fe8268c214cc3eb
Anny Martinez <annymartinez.m@gmail.com>
2019-05-03 18:54:02 -0700

    Fix async prefix trim issue

--- test/src/test/java/org/corfudb/runtime/checkpoint/CheckpointTest.java
+++ test/src/test/java/org/corfudb/runtime/checkpoint/CheckpointTest.java
@@ -10,9 +10,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.corfudb.protocols.wireprotocol.CorfuMsgType;
 import org.corfudb.protocols.wireprotocol.Token;
 import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.runtime.MultiCheckpointWriter;
+import org.corfudb.runtime.clients.TestRule;
 import org.corfudb.runtime.collections.SMRMap;
 import org.corfudb.runtime.exceptions.AbortCause;
 import org.corfudb.runtime.exceptions.TransactionAbortedException;
@@ -72,6 +74,8 @@ public class CheckpointTest extends AbstractObjectTest {
         }
     }
 
+    volatile Token lastValidCheckpoint = Token.UNINITIALIZED;
+
     /**
      * checkpoint the maps, and then trim the log
      */
@@ -99,6 +103,7 @@ public class CheckpointTest extends AbstractObjectTest {
                 rt.getAddressSpaceView().invalidateServerCaches();
                 rt.getAddressSpaceView().invalidateClientCache();
                 lastCheckpointAddress = checkpointAddress;
+                lastValidCheckpoint = checkpointAddress;
             } catch (TrimmedException te) {
                 // shouldn't happen
                 te.printStackTrace();
@@ -121,7 +126,7 @@ public class CheckpointTest extends AbstractObjectTest {
      * @param mapSize
      * @param expectedFullsize
      */
-    void validateMapRebuild(int mapSize, boolean expectedFullsize) {
+    void validateMapRebuild(int mapSize, boolean expectedFullsize, boolean trimCanHappen) {
         CorfuRuntime rt = getARuntime();
         try {
             Map<String, Long> localm2A = openMap(rt, streamNameA);
@@ -137,9 +142,11 @@ public class CheckpointTest extends AbstractObjectTest {
                 assertThat(localm2B.size()).isEqualTo(mapSize);
             }
         } catch (TrimmedException te) {
-            // shouldn't happen
-            te.printStackTrace();
-            throw te;
+            if (!trimCanHappen) {
+                // shouldn't happen
+                te.printStackTrace();
+                throw te;
+            }
         } finally {
             rt.shutdown();
         }
@@ -201,14 +208,14 @@ public class CheckpointTest extends AbstractObjectTest {
         // they should rebuild from the latest checkpoint (if available).
         // performs some sanity checks on the map state
         scheduleConcurrently(PARAMETERS.NUM_ITERATIONS_LOW, ignored_task_num -> {
-            validateMapRebuild(mapSize, false);
+            validateMapRebuild(mapSize, false, false);
         });
 
         executeScheduled(PARAMETERS.CONCURRENCY_SOME, PARAMETERS.TIMEOUT_LONG);
 
         // finally, after all three threads finish, again we start a fresh runtime and instantiate the maps.
         // This time the we check that the new map instances contains all values
-        validateMapRebuild(mapSize, true);
+        validateMapRebuild(mapSize, true, false);
 
         rt.shutdown();
     }
@@ -240,14 +247,14 @@ public class CheckpointTest extends AbstractObjectTest {
         // thread 2: repeat ITERATIONS_LOW times starting a fresh runtime, and instantiating the maps.
         // they should be empty.
         scheduleConcurrently(PARAMETERS.NUM_ITERATIONS_LOW, ignored_task_num -> {
-            validateMapRebuild(mapSize, true);
+            validateMapRebuild(mapSize, true, false);
         });
 
         executeScheduled(PARAMETERS.CONCURRENCY_SOME, PARAMETERS.TIMEOUT_LONG);
 
         // Finally, after the two threads finish, again we start a fresh runtime and instantiate the maps.
         // Then verify they are empty.
-        validateMapRebuild(mapSize, true);
+        validateMapRebuild(mapSize, true, false);
 
         rt.shutdown();
     }
@@ -298,14 +305,14 @@ public class CheckpointTest extends AbstractObjectTest {
         // they should rebuild from the latest checkpoint (if available).
         // this thread checks that all values are present in the maps
         scheduleConcurrently(PARAMETERS.NUM_ITERATIONS_LOW, ignored_task_num -> {
-            validateMapRebuild(mapSize, true);
+            validateMapRebuild(mapSize, true, false);
         });
 
         executeScheduled(PARAMETERS.CONCURRENCY_SOME, PARAMETERS.TIMEOUT_LONG);
 
         // Finally, after all three threads finish, again we start a fresh runtime and instantiate the maps.
         // This time the we check that the new map instances contains all values
-        validateMapRebuild(mapSize, true);
+        validateMapRebuild(mapSize, true, false);
 
         rt.shutdown();
     }
@@ -353,14 +360,81 @@ public class CheckpointTest extends AbstractObjectTest {
             // they should rebuild from the latest checkpoint (if available).
             // performs some sanity checks on the map state
             scheduleConcurrently(PARAMETERS.NUM_ITERATIONS_LOW, ignored_task_num -> {
-                validateMapRebuild(mapSize, false);
+                validateMapRebuild(mapSize, false, true);
             });
 
             executeScheduled(PARAMETERS.CONCURRENCY_SOME, PARAMETERS.TIMEOUT_LONG);
 
+            // Verify that last trim cycle completed (async task) before validating map rebuild
+            Token currentTrimMark =  Token.UNINITIALIZED;
+            while(currentTrimMark.getSequence() != lastValidCheckpoint.getSequence() + 1L) {
+                currentTrimMark = getARuntime().getAddressSpaceView().getTrimMark();
+            }
+
             // finally, after all three threads finish, again we start a fresh runtime and instantiate the maps.
             // This time we check that the new map instances contain all values
-            validateMapRebuild(mapSize, true);
+            validateMapRebuild(mapSize, true, false);
+        } finally {
+            rt.shutdown();
+        }
+    }
+
+    /**
+     * This test verifies that a stream can be rebuilt from a checkpoint even
+     * when the prefix trim message has not reached the sequencer but yet addresses
+     * have been actually trimmed from the log.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void prefixTrimNotReachingSequencer() throws Exception {
+        final int mapSize = PARAMETERS.NUM_ITERATIONS_LOW;
+        CorfuRuntime rt = getARuntime();
+
+        TestRule dropSequencerTrim = new TestRule()
+                .matches(corfuMsg -> corfuMsg.getMsgType().equals(CorfuMsgType.SEQUENCER_TRIM_REQ))
+                .drop();
+        addClientRule(rt, dropSequencerTrim);
+
+        try {
+            Map<String, Long> mapA = openMap(rt, streamNameA);
+            Map<String, Long> mapB = openMap(rt, streamNameB);
+
+            // thread 1: populates the maps with mapSize items
+            scheduleConcurrently(1, ignored_task_num -> {
+                populateMaps(mapSize, mapA, mapB);
+            });
+
+            // thread 2: periodic checkpoint of the maps, repeating ITERATIONS_VERY_LOW times,
+            // and immediate prefix-trim of the log up to the checkpoint position
+            scheduleConcurrently(1, ignored_task_num -> {
+                mapCkpointAndTrim(rt, (SMRMap) mapA, (SMRMap) mapB);
+            });
+
+            // thread 3: repeated ITERATION_LOW times starting a fresh runtime, and instantiating the maps.
+            // they should rebuild from the latest checkpoint (if available).
+            // performs some sanity checks on the map state
+
+            // In this test checkpoints and trims are happening in a loop for several iterations,
+            // so a trim exception can occur if after loading from the latest checkpoint,
+            // updates to the stream have been already trimmed (1st trimmedException), the stream
+            // is reset, and on the second iteration the same scenario can happen (2nd trimmedException)
+            // which is the total number of retries.
+            scheduleConcurrently(PARAMETERS.NUM_ITERATIONS_LOW, ignored_task_num -> {
+                validateMapRebuild(mapSize, false, true);
+            });
+
+            executeScheduled(PARAMETERS.CONCURRENCY_SOME, PARAMETERS.TIMEOUT_LONG);
+
+            // Verify that last trim cycle completed (async task) before validating map rebuild
+            Token currentTrimMark =  Token.UNINITIALIZED;
+            while(currentTrimMark.getSequence() != lastValidCheckpoint.getSequence() + 1L) {
+                currentTrimMark = getARuntime().getAddressSpaceView().getTrimMark();
+            }
+
+            // finally, after all three threads finish, again we start a fresh runtime and instantiate the maps.
+            // This time we check that the new map instances contain all values
+            validateMapRebuild(mapSize, true, false);
         } finally {
             rt.shutdown();
         }
