8ab20d5c727b9a08dcc7b01585888f0e1f705758
Wenbin Zhu <zwbhsefz@gmail.com>
2019-06-03 16:03:36 -0700

    LogUnit server GC phase 1: refactoring, params and add skeleton codes.
    Refactor StreamLogFiles, extract and add more stream log parameters for
    compaction, add skeleton codes for compaction including compaction
    policy, segment manager, garbage log segment etc.

--- infrastructure/src/main/java/org/corfudb/infrastructure/LogUnitServer.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/LogUnitServer.java
@@ -9,6 +9,7 @@ import org.corfudb.infrastructure.log.InMemoryStreamLog;
 import org.corfudb.infrastructure.log.StreamLog;
 import org.corfudb.infrastructure.log.StreamLogCompaction;
 import org.corfudb.infrastructure.log.StreamLogFiles;
+import org.corfudb.infrastructure.log.StreamLogParams;
 import org.corfudb.protocols.wireprotocol.CorfuMsg;
 import org.corfudb.protocols.wireprotocol.CorfuMsgType;
 import org.corfudb.protocols.wireprotocol.CorfuPayloadMsg;
@@ -132,7 +133,7 @@ public class LogUnitServer extends AbstractServer {
                     .convertToByteStringRepresentation(config.getMaxCacheSize()));
             streamLog = new InMemoryStreamLog();
         } else {
-            streamLog = new StreamLogFiles(serverContext, config.isNoVerify());
+            streamLog = new StreamLogFiles(serverContext.getStreamLogParams(), serverContext.getStreamLogDataStore());
         }
 
         dataCache = new LogUnitServerCache(config, streamLog);
@@ -468,7 +469,6 @@ public class LogUnitServer extends AbstractServer {
         private final double cacheSizeHeapRatio;
         private final long maxCacheSize;
         private final boolean memoryMode;
-        private final boolean noVerify;
         private final boolean noSync;
 
         /**
@@ -484,7 +484,6 @@ public class LogUnitServer extends AbstractServer {
                     .cacheSizeHeapRatio(cacheSizeHeapRatio)
                     .maxCacheSize((long) (Runtime.getRuntime().maxMemory() * cacheSizeHeapRatio))
                     .memoryMode(Boolean.valueOf(opts.get("--memory").toString()))
-                    .noVerify((Boolean) opts.get("--no-verify"))
                     .noSync((Boolean) opts.get("--no-sync"))
                     .build();
         }
