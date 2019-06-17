9d94c50697c11b2bb4ccd0061de14629b12d77bc
Wenbin Zhu <zwbhsefz@gmail.com>
2019-06-17 15:14:25 -0700

    LogUnit server GC phase 2: SegmentManager, StreamLogSegment, etc
    Replenish SegmentManager, StreamLogSegment, move some functionalities
    like read/write from global StreamLogFiles to StreamLogSegment, plus
    other changes made ready for sparse trim.

--- infrastructure/src/main/java/org/corfudb/infrastructure/log/AbstractLogSegment.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/log/AbstractLogSegment.java
@@ -2,44 +2,122 @@ package org.corfudb.infrastructure.log;
 
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.corfudb.protocols.wireprotocol.LogData;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.corfudb.infrastructure.ResourceQuota;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.channels.FileChannel;
-import java.util.Iterator;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystems;
+import java.nio.file.StandardOpenOption;
+import java.util.EnumSet;
+
+import static org.corfudb.infrastructure.utils.Persistence.syncDirectory;
 
 /**
  * TODO: add javadoc.
- *
+ * <p>
  * Created by WenbinZhu on 5/28/19.
  */
+@Slf4j
 @Getter
-@RequiredArgsConstructor
 public abstract class AbstractLogSegment implements AutoCloseable {
 
-    final long startAddress;
+    final long ordinal;
 
     @NonNull
-    final String fileName;
+    final String filePath;
 
     @NonNull
-    final FileChannel writeChannel;
+    final StreamLogParams logParams;
 
     @NonNull
-    final FileChannel readChannel;
+    final ResourceQuota logSizeQuota;
+
+    @NonNull
+    final SegmentMetaData segmentMetaData;
+
+    @NonNull
+    FileChannel writeChannel;
+
+    @NonNull
+    FileChannel readChannel;
+
+    @NonNull
+    FileChannel scanChannel;
 
     // TODO: persist revision in header.
-    // Used for checking equality between stream log and garbage log segment in recovery.
+    // Used for checking equality between stream log segment and garbage log segment in recovery.
     long compactRevision;
 
-    // public abstract LogData read(long address);
+    AbstractLogSegment(long ordinal, String fileName,
+                       StreamLogParams logParams,
+                       ResourceQuota logSizeQuota,
+                       SegmentMetaData segmentMetaData) {
+        this.ordinal = ordinal;
+        this.filePath = fileName;
+        this.logParams = logParams;
+        this.logSizeQuota = logSizeQuota;
+        this.segmentMetaData = segmentMetaData;
 
-    public abstract void append(long address, LogData entry);
+        try {
+            this.writeChannel = getChannel(fileName, false);
+            this.readChannel = getChannel(fileName, true);
+            this.scanChannel = getChannel(fileName, true);
+        } catch (IOException ioe) {
+            log.error("Error opening file {}", fileName, ioe);
+            IOUtils.closeQuietly(writeChannel);
+            IOUtils.closeQuietly(readChannel);
+            IOUtils.closeQuietly(scanChannel);
+            throw new IllegalStateException(ioe);
+        }
+    }
 
-    // public Iterator<LogData> iterator() {
-    //     throw new NotImplementedException();
-    // }
-
+    /**
+     * Close the segment, releasing the resources if any.
+     */
     public abstract void close();
+
+    /**
+     * Opens the file channel to manipulate the file, creating a new file if
+     * it does not exist. If the readOnly parameter is set, the channel will
+     * be opened in read-only mode.
+     *
+     * @param filePath the path of the file to open
+     * @param readOnly if the file should be opened only for read
+     * @return the file channel to manipulate the specified file
+     */
+    private FileChannel getChannel(String filePath, boolean readOnly) throws IOException {
+        if (readOnly) {
+            if (!new File(filePath).exists()) {
+                throw new FileNotFoundException(filePath);
+            }
+            return FileChannel.open(
+                    FileSystems.getDefault().getPath(filePath),
+                    EnumSet.of(StandardOpenOption.READ)
+            );
+        }
+
+        try {
+            EnumSet<StandardOpenOption> options = EnumSet.of(
+                    StandardOpenOption.READ,
+                    StandardOpenOption.WRITE,
+                    StandardOpenOption.CREATE_NEW);
+            FileChannel channel = FileChannel.open(FileSystems.getDefault().getPath(filePath), options);
+
+            // First time creating this segment file, need to sync the parent directory
+            File segFile = new File(filePath);
+            syncDirectory(segFile.getParent());
+            return channel;
+
+        } catch (FileAlreadyExistsException ex) {
+            return FileChannel.open(
+                    FileSystems.getDefault().getPath(filePath),
+                    EnumSet.of(StandardOpenOption.READ, StandardOpenOption.WRITE)
+            );
+        }
+    }
 }
