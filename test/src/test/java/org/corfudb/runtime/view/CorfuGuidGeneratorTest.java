40d2c37526d565578de9cb563b579a1b59e0c3c5
Sundar Sridharan <sundararamas@vmware.com>
2019-06-17 17:00:42 -0700

    CorfuGuidGenerator: Globally unique ID generator
    On the first request to fetch GUID, transactionally
    increment a global distributed counter.
    Then on, use this distributed counter + timestamp to generate
    incremental values.
    Keep the distributed sync up to a minimal footprint.

--- test/src/test/java/org/corfudb/runtime/view/CorfuGuidGeneratorTest.java
+++ test/src/test/java/org/corfudb/runtime/view/CorfuGuidGeneratorTest.java
@@ -8,37 +8,41 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
+ * Validate that Corfu token based Guid Generator satisfies the following two criteria:
+ * 1. The guids are unique.
+ * 2. The guids are monotonically increasing.
  * Created by Sundar Sridharan on May 23, 2019.
  */
-public class SnowflakeGuidGeneratorTest {
+public class CorfuGuidGeneratorTest extends AbstractViewTest {
 
     @Test
     public void areUniqueAndOrderedLong() {
-        final int iterations = 1000;
-        OrderedGuidGenerator guidGenerator = new SnowflakeGuidGenerator(System.identityHashCode(this));
+        final int iterations = PARAMETERS.NUM_ITERATIONS_MODERATE;
+        OrderedGuidGenerator guidGenerator = new CorfuGuidGenerator(getDefaultRuntime().connect());
         long lastValue = 0;
         HashSet<Long> uniq = new HashSet<>(iterations);
         for (int i = 0; i < iterations; i++) {
             long current = guidGenerator.nextLong();
+            assertThat(uniq).doesNotContain(current);
             assertThat(current).isGreaterThan(lastValue);
             lastValue = current;
-            assertThat(uniq).doesNotContain(current);
             uniq.add(current);
         }
     }
 
     @Test
     public void areUniqueAndOrderedUUID() {
-        final int iterations = 1000;
-        OrderedGuidGenerator guidGenerator = new SnowflakeGuidGenerator(System.identityHashCode(this));
+        final int iterations = PARAMETERS.NUM_ITERATIONS_MODERATE;
+        OrderedGuidGenerator guidGenerator = new CorfuGuidGenerator(getDefaultRuntime().connect());
         UUID lastValue = new UUID(0,0);
         HashSet<UUID> uniq = new HashSet<>(iterations);
         for (int i = 0; i < iterations; i++) {
             UUID current = guidGenerator.nextUUID();
+            assertThat(uniq).doesNotContain(current);
             assertThat(current).isGreaterThan(lastValue);
             lastValue = current;
-            assertThat(uniq).doesNotContain(current);
             uniq.add(current);
         }
     }
 }
+
