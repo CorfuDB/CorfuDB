82ca85e599507975557fbd4e30e5e47c6686d5cd
Anny Martinez <annymartinez.m@gmail.com>
2019-06-20 22:59:38 -0700

    Bug Fix Stream Layer Ignore Trimmed Exceptions (#1946)

---
+++ test/src/test/java/org/corfudb/runtime/view/stream/AddressMapStreamViewTest.java
@@ -0,0 +1,8 @@
+package org.corfudb.runtime.view.stream;
+
+public class AddressMapStreamViewTest extends AbstractStreamViewTest {
+
+    public AddressMapStreamViewTest() {
+        super(false);
+    }
+}
