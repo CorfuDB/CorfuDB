ade96d4c5477a8ebb3a8d948ba0316c5c474b49f
Viacheslav <xrw.null@gmail.com>
2019-07-02 20:49:32 -0700

    org.corfudb.util.Sleep refactoring/ (#1982)

--- infrastructure/src/main/java/org/corfudb/infrastructure/ManagementAgent.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/ManagementAgent.java
@@ -13,6 +13,7 @@ import org.corfudb.util.Sleep;
 import org.corfudb.util.concurrent.SingletonResource;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static org.corfudb.infrastructure.RecoveryHandler.runRecoveryReconfiguration;
 
@@ -148,7 +149,7 @@ public class ManagementAgent {
         try {
             while (!shutdown && serverContext.getManagementLayout() == null) {
                 log.warn("initializationTask: Management Server waiting to be bootstrapped");
-                Sleep.MILLISECONDS.sleepRecoverably(CHECK_BOOTSTRAP_INTERVAL.toMillis());
+                TimeUnit.MILLISECONDS.sleep(CHECK_BOOTSTRAP_INTERVAL.toMillis());
             }
         } catch (InterruptedException e) {
             // Due to shutdown, which interrupts this thread
@@ -173,7 +174,7 @@ public class ManagementAgent {
 
                 log.error("initializationTask: Recovery failed {} times. Retrying in {}s.",
                         ++recoveryAttempts, RECOVERY_RETRY_INTERVAL);
-                Sleep.MILLISECONDS.sleepRecoverably(RECOVERY_RETRY_INTERVAL.toMillis());
+                TimeUnit.MILLISECONDS.sleep(RECOVERY_RETRY_INTERVAL.toMillis());
             } catch (InterruptedException e) {
                 // Due to shutdown, which interrupts this thread
                 log.debug("initializationTask: Initialization task interrupted without being recovered");
--- runtime/src/main/java/org/corfudb/util/retry/IntervalRetry.java
+++ runtime/src/main/java/org/corfudb/util/retry/IntervalRetry.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.util.Sleep;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * This class implements a basic interval-based retry.
  *
@@ -33,7 +35,7 @@ public class IntervalRetry<E extends Exception, F extends Exception,
      */
     @Override
     public void nextWait() throws InterruptedException {
-        Sleep.MILLISECONDS.sleepUninterruptibly(retryInterval);
+        TimeUnit.MILLISECONDS.sleep(retryInterval);
     }
 
 }
