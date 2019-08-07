b749c355f68fa98ba00a62229b220598c46b8e4d
Wenbin Zhu <zwbhsefz@gmail.com>
2019-08-06 17:47:15 -0700

    GC: Unify LogEntry types: SMREntry, MultiSMREntry, MultiObjectSMREntry.
    We have three SMR LogEntry types, namely SMREntry for a non-transactional
    update, MultiObjectSMREntry for transactional updates and MultiSMREntry
    for checkpoint entry. This creates a lot of confusion, boilerplates and
    difficulties when handling log updates. With this patch, all transactional,
    non-transactional updates and checkpoint entries are composed of a single
    log entry type: SMRLogEntry, which is similar to MultiObjectSMREntry. And
    each SMR update is turned into SMRRecord, which is similar to SMREntry,
    but is not inherited from LogEntry any more, because now the only LogEntry
    type persisted on log unit server would be SMRLogEntry.

--- runtime/src/test/java/org/corfudb/protocols/wireprotocol/LogDataTest.java
+++ runtime/src/test/java/org/corfudb/protocols/wireprotocol/LogDataTest.java
@@ -2,12 +2,16 @@ package org.corfudb.protocols.wireprotocol;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import org.corfudb.protocols.logprotocol.SMREntry;
 import org.corfudb.protocols.logprotocol.SMREntryGarbageInfo;
+import org.corfudb.protocols.logprotocol.SMRLogEntry;
+import org.corfudb.protocols.logprotocol.SMRRecord;
+import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.util.serializer.ISerializer;
 import org.corfudb.util.serializer.Serializers;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.UUID;
 
 public class LogDataTest {
     @Test
@@ -16,18 +20,22 @@ public class LogDataTest {
         Object[] smrArguments = new Object[] {"a"};
         ISerializer iSerializer = Serializers.JSON;
 
-        SMREntry smrEntry = new SMREntry(smrMethod, smrArguments, iSerializer);
-        LogData logData = new LogData(DataType.DATA, smrEntry);
+        SMRLogEntry smrLogEntry = new SMRLogEntry();
+        SMRRecord smrRecord = new SMRRecord(smrMethod, smrArguments, iSerializer);
+        UUID streamId = CorfuRuntime.getStreamID("stream1");
+        smrLogEntry.addTo(streamId, smrRecord);
+        LogData logData = new LogData(DataType.DATA, smrLogEntry);
         ByteBuf buf = Unpooled.buffer();
         logData.doSerialize(buf);
         LogData deserializedLogData = new LogData(buf);
 
         // TODO(Xin): runtime is not used in the test. Future patch will eliminate the dependency on runtime.
-        SMREntry deserializedSMREntry = (SMREntry) deserializedLogData.getPayload(null);
+        SMRLogEntry deserializedSMREntry = (SMRLogEntry) deserializedLogData.getPayload(null);
+        SMRRecord deserializedSMRRecord = deserializedSMREntry.getSMRUpdates(streamId).get(0);
 
-        Assert.assertEquals(smrEntry.getSMRMethod(), deserializedSMREntry.getSMRMethod());
-        Assert.assertEquals(smrEntry.getSMRArguments(), deserializedSMREntry.getSMRArguments());
-        Assert.assertEquals(smrEntry.getSerializerType(), deserializedSMREntry.getSerializerType());
+        Assert.assertEquals(smrRecord.getSMRMethod(), deserializedSMRRecord.getSMRMethod());
+        Assert.assertEquals(smrRecord.getSMRArguments(), deserializedSMRRecord.getSMRArguments());
+        Assert.assertEquals(smrRecord.getSerializerType(), deserializedSMRRecord.getSerializerType());
     }
 
     @Test
