c207ef7042d82ad78ca0f78169df63f54a5acf8d
lixinchengdu <lixinchengdu@gmail.com>
2019-08-08 13:51:01 -0700

    change ISMRGarbageInfo to adopt new form of SMREntry (#2106)

---
+++ runtime/src/main/java/org/corfudb/protocols/logprotocol/SMRGarbageRecord.java
@@ -0,0 +1,78 @@
+package org.corfudb.protocols.logprotocol;
+
+
+import io.netty.buffer.ByteBuf;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.corfudb.runtime.CorfuRuntime;
+
+@ToString(callSuper = true)
+@NoArgsConstructor
+public class SMRGarbageRecord {
+
+    /**
+     * Global address that marked the associated SMREntry as garbage.
+     */
+    @Getter
+    private long markerAddress;
+
+    /**
+     * The size of the associated SMREntry in Byte.
+     */
+    @Getter
+    private int smrEntrySize;
+
+    /**
+     * Constructor for SMREntryGarbageInfo
+     * @param markerAddress The Global address of the SRMRecord that supersedes this SMRRecord.
+     * @param smrEntrySize  Serialized size of this SMRRecord in byte.
+     */
+    public SMRGarbageRecord(long markerAddress, int smrEntrySize) {
+        this.markerAddress = markerAddress;
+        this.smrEntrySize = smrEntrySize;
+    }
+
+    /**
+     * Get the serialized size of garbage-identified SMREntries that has marker address up to addressUpTo.
+     * @param  addressUpTo Upper-bound marker address.
+     * @return size in Byte.
+     */
+    public int getGarbageSizeUpTo(long addressUpTo) {
+        return markerAddress <= addressUpTo ? smrEntrySize : 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof SMRGarbageRecord) {
+            SMRGarbageRecord otherGarbageInfo = (SMRGarbageRecord) other;
+            return otherGarbageInfo.markerAddress == markerAddress
+                    && otherGarbageInfo.smrEntrySize == smrEntrySize;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Serialize the given SRMRecordGarbageInfo into a given byte buffer.
+     *
+     * @param b The buffer to serialize into.
+     */
+    public void serialize(ByteBuf b) {
+        b.writeLong(markerAddress);
+        b.writeInt(smrEntrySize);
+    }
+
+    /**
+     * Deserialize SMRGarbageRecord from a given byte buffer.
+     * @param b  The buffer to deserialize.
+     * @param rt corfu runtime.
+     */
+    void deserializeBuffer(ByteBuf b, CorfuRuntime rt) {
+        markerAddress = b.readLong();
+        smrEntrySize = b.readInt();
+    }
+}
