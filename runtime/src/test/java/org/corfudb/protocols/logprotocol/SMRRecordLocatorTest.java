fbc029278789dc24897aef0ec76a676347d182a6
lixinchengdu <lixinchengdu@gmail.com>
2019-08-14 10:47:58 -0700

    Add SMRRecordLocator (#1911)

---
+++ runtime/src/test/java/org/corfudb/protocols/logprotocol/SMRRecordLocatorTest.java
@@ -0,0 +1,30 @@
+package org.corfudb.protocols.logprotocol;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.corfudb.runtime.view.Address;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Xin Li on 05/23/19.
+ */
+public class SMRRecordLocatorTest {
+    @Test
+    public void testSMRRecordLocatorSerializeDeserialize() {
+
+        long globalAddress = Address.getMinAddress();
+        UUID steramId = UUID.randomUUID();
+        int index = 0;
+        SMRRecordLocator locator = new SMRRecordLocator(globalAddress, steramId, index);
+
+        ByteBuf buf = Unpooled.buffer();
+        locator.serialize(buf);
+        SMRRecordLocator deserializedLocator = SMRRecordLocator.deserialize(buf);
+
+        assertEquals(locator, deserializedLocator);
+    }
+}
