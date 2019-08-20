eded092760d98d816bc7cdb14c71cc184f9e0ae4
Xiaoqin Ma <maxi@maxi-a01.vmware.com>
2019-08-20 16:33:55 -0700

    Add a simpletrace class and measure object size.

--- benchmarks/src/main/java/org/corfudb/benchmarks/SequencerFlood.java
+++ benchmarks/src/main/java/org/corfudb/benchmarks/SequencerFlood.java
@@ -1,4 +1,4 @@
-package org.corfudb.testclient;
+package org.corfudb.benchmarks;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
@@ -7,13 +7,12 @@ import org.HdrHistogram.Histogram;
 import org.HdrHistogram.Recorder;
 import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.util.Sleep;
-
+import org.corfudb.benchmarks.ObjectFetcher;
 import java.time.Duration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
-
 
 /**
  * Created by Maithem on 8/1/19.
@@ -58,6 +57,7 @@ public class SequencerFlood {
         for (int x = 0; x < rts.length; x++) {
             rts[x] = new CorfuRuntime(cmdArgs.endpoint).connect();
         }
+
         log.info("Connected {} runtimes...", numRuntimes);
 
         ExecutorService service = Executors.newFixedThreadPool(numThreads);
