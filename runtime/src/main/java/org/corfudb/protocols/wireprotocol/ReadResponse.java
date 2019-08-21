a59b231787d306614cc81b4855d9bd96531e1ac6
lixinchengdu <lixinchengdu@gmail.com>
2019-08-20 17:03:15 -0700

    Add compaction mark in ReadResponse (#1967)

--- runtime/src/main/java/org/corfudb/protocols/wireprotocol/ReadResponse.java
+++ runtime/src/main/java/org/corfudb/protocols/wireprotocol/ReadResponse.java
@@ -4,10 +4,12 @@ import io.netty.buffer.ByteBuf;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Created by mwei on 8/15/16.
@@ -19,12 +21,18 @@ public class ReadResponse implements ICorfuPayload<ReadResponse> {
     @Getter
     Map<Long, LogData> addresses;
 
+    @Setter
+    @Getter
+    Map<UUID, Long> compactionMarks;
+
     public ReadResponse(ByteBuf buf) {
         addresses = ICorfuPayload.mapFromBuffer(buf, Long.class, LogData.class);
+        compactionMarks = ICorfuPayload.mapFromBuffer(buf, UUID.class, Long.class);
     }
 
     public ReadResponse() {
-        addresses = new HashMap<Long, LogData>();
+        addresses = new HashMap<>();
+        compactionMarks = new HashMap<>();
     }
 
     public void put(Long address, LogData data) {
@@ -34,5 +42,6 @@ public class ReadResponse implements ICorfuPayload<ReadResponse> {
     @Override
     public void doSerialize(ByteBuf buf) {
         ICorfuPayload.serialize(buf, addresses);
+        ICorfuPayload.serialize(buf, compactionMarks);
     }
 }
