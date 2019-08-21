ed4b8baba65ce5f762cdbda1845943d84a841f9b
Xiaoqin Ma <maxi@maxi-a01.vmware.com>
2019-08-21 14:30:03 -0700

    Fix some comments.

--- benchmarks/src/main/java/org/corfudb/benchmarks/SimpleTrace.java
+++ benchmarks/src/main/java/org/corfudb/benchmarks/SimpleTrace.java
@@ -5,13 +5,13 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class SimpleTrace {
-    String name;
-    long total; //in microseconds
-    long cnt;
-    long start; //in nanoseconds
+    String name; //name of the tracing object
+    long total; //sum of latencise for all operations in microseconds
+    long cnt;   //total number of the operations.
+    long start; //the current operation start time in nanoseconds
 
-    SimpleTrace(String s) {
-        name = s;
+    SimpleTrace(String name) {
+        name = name;
         total = 0;
         cnt = 0;
         start = System.nanoTime();
@@ -23,7 +23,7 @@ public class SimpleTrace {
 
     void end() {
         long interval = TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - start);
-        //avoid to overflow the value, do a reset.
+        //avoid to overflow the value, do a reset. Keep half of the history.
         if (Long.MAX_VALUE - total < interval) {
             this.log(false);
             cnt = cnt/2;
@@ -34,21 +34,21 @@ public class SimpleTrace {
         cnt++;
     }
 
-    void log(boolean info) {
-        if (info == true)
+    void log(boolean infoMode) {
+        if (infoMode == true)
             log.info("{} cnt {} total {} ms  average {} micros", name, cnt, total/1000, total/(1.0*cnt));
         else
             log.warn("{} reset cnt {} total {} ms  average {} micros", name, cnt, total/1000, total/(1.0*cnt));
     }
 
     static void log(SimpleTrace[] traces, String s) {
-        long total_elapse = 0;
-        long total_cnt = 0;
+        long totalElapse = 0;
+        long totalCnt = 0;
         for (int i = 0; i < traces.length; i++) {
-            total_elapse += traces[i].total;
-            total_cnt += traces[i].cnt;
+            totalElapse += traces[i].total;
+            totalCnt += traces[i].cnt;
         }
         log.info ("{} aggregate cnt {} total {} ms  average {} micros",
-                s, total_cnt, total_elapse/1000, total_elapse/(1.0*total_cnt));
+                s, totalCnt, totalElapse/1000, totalElapse/(1.0*totalCnt));
     }
 }
