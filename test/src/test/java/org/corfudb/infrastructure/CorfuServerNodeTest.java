6c2a54946cb141b84df76204203b212efe1664ae
Pankti Majmudar <pmajmudar@vmware.com>
2019-09-11 15:25:24 -0700

    Optimize creating CorfuRuntime params, shutdown server and runtime, and add comments.

--- test/src/test/java/org/corfudb/infrastructure/CorfuServerNodeTest.java
+++ test/src/test/java/org/corfudb/infrastructure/CorfuServerNodeTest.java
@@ -5,6 +5,7 @@ import com.codahale.metrics.MetricRegistry;
 import org.corfudb.AbstractCorfuTest;
 import org.corfudb.protocols.wireprotocol.PriorityLevel;
 import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.util.NodeLocator;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +14,19 @@ import static org.junit.Assert.assertEquals;
 
 public class CorfuServerNodeTest extends AbstractCorfuTest {
 
+    private final String connectionString = "localhost:9000";
+    private final String hostname = "localhost";
+
+    /**
+     * Verifies that the Corfu runtime can successfully connect to a test Corfu
+     * Server and has layout servers populated correctly.
+     */
     @Test
     public void testCorfuServerNodeStart() {
 
         // Start the Corfu Server
         ServerContext context = new ServerContextBuilder()
-            .setAddress("localhost").setImplementation("auto").build();
+            .setAddress(hostname).setImplementation("auto").build();
         MetricRegistry metricRegistry = CorfuRuntime.getDefaultMetrics();
         CorfuServerNode serverNode = new CorfuServerNode(context, metricRegistry);
         serverNode.start();
@@ -26,17 +34,21 @@ public class CorfuServerNodeTest extends AbstractCorfuTest {
         // Create CorfuRuntime and connect to the server
         CorfuRuntime runtime = CorfuRuntime.fromParameters(
             buildCorfuRuntimeParameters());
-        String connectionString = "localhost:9000";
-        runtime.parseConfigurationString(connectionString);
         runtime.connect();
 
-        // print the layout servers
+        // Verify that the localhost has been added as a layout server
         assertEquals(1, runtime.getLayoutServers().size());
         assertEquals(connectionString, runtime.getLayoutServers().get(0));
+
+        // Shutdown
+        runtime.shutdown();
+        serverNode.close();
     }
 
     private CorfuRuntime.CorfuRuntimeParameters buildCorfuRuntimeParameters() {
+        NodeLocator locator = NodeLocator.parseString(connectionString);
         return CorfuRuntime.CorfuRuntimeParameters.builder()
+            .layoutServer(locator)
             .useFastLoader(false)
             .cacheDisabled(true)
             .priorityLevel(PriorityLevel.HIGH)
