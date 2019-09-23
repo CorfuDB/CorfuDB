2ddd3a5ae8c39023c37faabe25aebd4bad16e7f4
Lin Dong <lindongchn@gmail.com>
2019-09-23 14:02:15 -0700

    add SeqeuncerBenchmarkTest

--- benchmark/src/main/java/org/corfudb/benchmark/SequencerBenchmarkTest.java
+++ benchmark/src/main/java/org/corfudb/benchmark/SequencerBenchmarkTest.java
@@ -1,4 +1,36 @@
 package org.corfudb.benchmark;
 
-public class SequencerBenchmarkTest {
+import org.corfudb.runtime.CorfuRuntime;
+
+/**
+ * This class is for Sequencer's benchmark tests.
+ * operationName can be: query, raw, multistream, tx, getstreamaddr,
+ * each name maps to a kind of token request and API in Sequencer Server.
+ */
+public class SequencerBenchmarkTest extends BenchmarkTest {
+    String operationName = null;
+    SequencerBenchmarkTest(ParseArgs parseArgs) {
+        super(parseArgs);
+        operationName = parseArgs.getOp();
+    }
+
+    private void runProducer() {
+        for (int i = 0; i < numThreads; i++) {
+            CorfuRuntime rt = runtimes.getRuntime(i);
+            SequencerOperations sequencerOperations = new SequencerOperations(operationName, rt, numRequests);
+            runProducer(sequencerOperations);
+        }
+    }
+
+    private void runTest() {
+        runProducer();
+        runConsumers();
+        waitForAppToFinish();
+    }
+
+    public static void main(String[] args) {
+        ParseArgs parseArgs = new ParseArgs(args);
+        SequencerBenchmarkTest sequencerBenchmarkTest = new SequencerBenchmarkTest(parseArgs);
+        sequencerBenchmarkTest.runTest();
+    }
 }
--- benchmark/src/main/java/org/corfudb/benchmark/SequencerOperations.java
+++ benchmark/src/main/java/org/corfudb/benchmark/SequencerOperations.java
@@ -1,4 +1,127 @@
 package org.corfudb.benchmark;
 
-public class SequencerOperations {
+import com.codahale.metrics.Timer;
+import lombok.extern.slf4j.Slf4j;
+import org.corfudb.protocols.wireprotocol.StreamAddressRange;
+import org.corfudb.protocols.wireprotocol.Token;
+import org.corfudb.protocols.wireprotocol.TokenResponse;
+import org.corfudb.protocols.wireprotocol.TxResolutionInfo;
+import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.view.Address;
+import com.codahale.metrics.MetricRegistry;
+import org.corfudb.util.CorfuComponent;
+import org.corfudb.util.MetricsUtils;
+
+import java.util.*;
+
+/**
+ * Operation of Sequencer, contains query, raw token request, multi stream token request,
+ * transaction token request, get stream address space operations.
+ */
+@Slf4j
+public class SequencerOperations extends Operation {
+    private Timer queryTimer;
+    private Timer rawTimer;
+    private Timer multiStreamTimer;
+    private Timer txTimer;
+    private Timer getStreamSpaceTimer;
+    SequencerOperations(String name, CorfuRuntime rt, int numRequest) {
+        super(rt);
+        shortName = name;
+        this.numRequest = numRequest;
+        setTimer();
+    }
+
+    private void setTimer() {
+        MetricRegistry metricRegistry = CorfuRuntime.getDefaultMetrics();
+        queryTimer = metricRegistry.timer(CorfuComponent.CLIENT_SEQUENCER +
+                "benchmark-query-token");
+        rawTimer = metricRegistry.timer(CorfuComponent.CLIENT_SEQUENCER +
+                "benchmark-raw-token");
+        multiStreamTimer = metricRegistry.timer(CorfuComponent.CLIENT_SEQUENCER +
+                "benchmark-multistream-token");
+        txTimer = metricRegistry.timer(CorfuComponent.CLIENT_SEQUENCER +
+                "benchmark-tx-token");
+        getStreamSpaceTimer = metricRegistry.timer(CorfuComponent.CLIENT_SEQUENCER +
+                "benchmark-getstreamspace-token");
+
+    }
+
+    private void tokenQuery() {
+        for (int i = 0; i < numRequest; i++) {
+            try(Timer.Context context = MetricsUtils.getConditionalContext(queryTimer)) {
+                TokenResponse tokenResponse = rt.getSequencerView().query();
+            }
+        }
+    }
+
+    private void tokenRaw() {
+        for (int i = 0; i < numRequest; i++) {
+            try(Timer.Context context = MetricsUtils.getConditionalContext(rawTimer)) {
+                rt.getSequencerView().next();
+            }
+        }
+    }
+
+    private void tokenMultiStream() {
+        for (int i = 0; i < numRequest; i++) {
+            UUID stream = UUID.nameUUIDFromBytes("Stream".getBytes());
+            try(Timer.Context context = MetricsUtils.getConditionalContext(multiStreamTimer)) {
+                rt.getSequencerView().next(stream);
+            }
+        }
+    }
+
+    private void tokenTx() {
+        for (int i = 0; i < numRequest; i++) {
+            UUID transactionID = UUID.nameUUIDFromBytes("transaction".getBytes());
+            UUID stream = UUID.randomUUID();
+            Map<UUID, Set<byte[]>> conflictMap = new HashMap<>();
+            Set<byte[]> conflictSet = new HashSet<>();
+            byte[] value = new byte[]{0, 0, 0, 1};
+            conflictSet.add(value);
+            conflictMap.put(stream, conflictSet);
+
+            Map<UUID, Set<byte[]>> writeConflictParams = new HashMap<>();
+            Set<byte[]> writeConflictSet = new HashSet<>();
+            byte[] value1 = new byte[]{0, 0, 0, 1};
+            writeConflictSet.add(value1);
+            writeConflictParams.put(stream, writeConflictSet);
+
+            TxResolutionInfo conflictInfo = new TxResolutionInfo(transactionID,
+                    new Token(0, -1),
+                    conflictMap,
+                    writeConflictParams);
+            try(Timer.Context context = MetricsUtils.getConditionalContext(txTimer)) {
+                rt.getSequencerView().next(conflictInfo, stream);
+            }
+        }
+    }
+
+    private void getStreamAddress() {
+        for (int i = 0; i < numRequest; i++) {
+            UUID stream = UUID.nameUUIDFromBytes("Stream".getBytes());
+            final int tokenCount = 3;
+            try(Timer.Context context = MetricsUtils.getConditionalContext(getStreamSpaceTimer)) {
+                rt.getSequencerView().getStreamAddressSpace(new StreamAddressRange(stream, tokenCount, Address.NON_ADDRESS));
+            }
+        }
+    }
+
+    @Override
+    public void execute() {
+        if (shortName.equals("query")) {
+            tokenQuery();
+        } else if (shortName.equals("raw")) {
+            tokenRaw();
+        } else if (shortName.equals("multistream")) {
+            tokenMultiStream();
+        } else if (shortName.equals("tx")) {
+            tokenTx();
+        } else if (shortName.equals("getstreamaddr")) {
+            getStreamAddress();
+        } else {
+            log.error("no such operation for sequencer.");
+        }
+    }
 }
