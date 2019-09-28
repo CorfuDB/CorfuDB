5d97b8611c98c49f6da32d93cce8498846df6d55
Wenbin Zhu <zwbhsefz@gmail.com>
2019-09-27 17:44:04 -0700

    Enhance compactor logging, refactor, fix compaction policy and other bugs.

--- infrastructure/src/main/java/org/corfudb/infrastructure/log/SnapshotLengthFirstPolicy.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/log/SnapshotLengthFirstPolicy.java
@@ -5,6 +5,7 @@ import org.corfudb.infrastructure.ResourceQuota;
 import org.corfudb.runtime.view.Address;
 
 import java.nio.file.FileStore;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -39,19 +40,28 @@ public class SnapshotLengthFirstPolicy extends AbstractCompactionPolicy {
      */
     @Override
     public List<Long> getSegmentsToCompact(List<CompactionMetadata> compactibleSegments) {
-        // Force compaction to override the policy if out of disk quota.
+        // Force compaction to override the garbage ratio check if out of disk quota.
         if (requireForceCompaction(params, fileStore, logSizeQuota, compactibleSegments)) {
-            log.info("Force compaction needed, ignoring compaction policy.");
-            return getSegmentsToForceCompact(compactibleSegments);
+            long nextCompactionUpperBound = newCompactionUpperBound();
+            // Check if log unit is initialized and has data.
+            if (Address.isAddress(nextCompactionUpperBound)) {
+                // Set currCompactionUpperBound to current global tail to increase the
+                // number of entries that could be compacted in this cycle.
+                CompactionMetadata.setCurrCompactionUpperBound(nextCompactionUpperBound);
+                CompactionMetadata.setNextCompactionUpperBound(nextCompactionUpperBound);
+                log.info("Force compaction needed, ignoring garbage threshold check, " +
+                                "set currCompactionUpperBound={}, nextCompactionUpperBound={}",
+                        CompactionMetadata.getCurrCompactionUpperBound(),
+                        CompactionMetadata.getNextCompactionUpperBound());
+                return getSegmentsToForceCompact(compactibleSegments);
+            }
+            return Collections.emptyList();
         }
 
         // Skip compaction for the first cycle after startup.
         if (CompactionMetadata.getNextCompactionUpperBound() == Address.MAX) {
-            // Minus 1 to avoid handling the case where log tail not moving when
-            // being compacted in the next cycle and the log tail happens to be a
-            // hole. In that case, if the hole is compacted and server restarted,
-            // the rebuilt global log tail could regress.
-            long nextCompactionUpperBound = logMetadata.getGlobalTail() - 1;
+            long nextCompactionUpperBound = newCompactionUpperBound();
+            // Check if log unit is initialized and has data.
             if (Address.isAddress(nextCompactionUpperBound)) {
                 CompactionMetadata.setNextCompactionUpperBound(nextCompactionUpperBound);
             }
@@ -72,8 +82,23 @@ public class SnapshotLengthFirstPolicy extends AbstractCompactionPolicy {
         // compaction marks after currCompactionUpperBound will not be trimmed, guaranteeing compaction
         // marks not moving for X amount of time, where X is the compaction cycle interval.
         CompactionMetadata.setCurrCompactionUpperBound(CompactionMetadata.getNextCompactionUpperBound());
-        CompactionMetadata.setNextCompactionUpperBound(logMetadata.getGlobalTail());
+        CompactionMetadata.setNextCompactionUpperBound(newCompactionUpperBound());
+        log.info("getSegmentsToCompact: set currCompactionUpperBound={}, nextCompactionUpperBound={}",
+                CompactionMetadata.getCurrCompactionUpperBound(), CompactionMetadata.getNextCompactionUpperBound());
 
         return segmentsToCompact;
+    }
+
+    /**
+     * Return the next compaction upper bound which would be used in the
+     * next compaction cycle to filter out the entries which would move
+     * the compaction mark after this bound.
+     */
+    private long newCompactionUpperBound() {
+        // Minus 1 to avoid handling the case where log tail not moving when
+        // being compacted in the next cycle and the log tail happens to be a
+        // hole. In that case, if the hole is compacted and server restarted,
+        // the rebuilt global log tail could regress.
+        return logMetadata.getGlobalTail() - 1;
     }
 }
