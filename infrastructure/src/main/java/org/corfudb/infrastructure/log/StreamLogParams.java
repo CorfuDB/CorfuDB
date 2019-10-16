b474e3e32411b47d15f8f582a5bbf6fbfe4b5621
Wenbin Zhu <zwbhsefz@gmail.com>
2019-10-16 01:58:36 -0700

    Introduce segment manager cache.
    This patch introduces segment manager cache, which uses Caffeine cache
    to manage the open segment mappings with a limit of the number of open
    segments. In this way, opened segments can be closed and evicted from
    cache automatically, preventing memory from growing unbounded.

--- infrastructure/src/main/java/org/corfudb/infrastructure/log/StreamLogParams.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/log/StreamLogParams.java
@@ -1,7 +1,6 @@
 package org.corfudb.infrastructure.log;
 
 import lombok.Builder;
-import lombok.Builder.Default;
 import lombok.ToString;
 import org.corfudb.format.Types;
 import org.corfudb.infrastructure.log.CompactionPolicy.CompactionPolicyType;
@@ -33,39 +32,32 @@ public class StreamLogParams {
     // Region: stream log parameters
     public String logPath;
 
-    @Default
     public boolean verifyChecksum;
 
-    @Default
     public double logSizeQuotaPercentage;
+
+    public int maxOpenStreamSegments;
+
+    public int maxOpenGarbageSegments;
     // End region
 
     // Region: compactor parameters
-    @Default
     public CompactionPolicyType compactionPolicyType;
 
-    @Default
     public int compactionInitialDelayMin;
 
-    @Default
     public int compactionPeriodMin;
 
-    @Default
     public int compactionWorkers;
 
-    @Default
     public int maxSegmentsForCompaction;
 
-    @Default
     public int protectedSegments;
 
-    @Default
     public double segmentGarbageRatioThreshold;
 
-    @Default
     public double segmentGarbageSizeThresholdMB;
 
-    @Default
     public double totalGarbageSizeThresholdMB;
     // End region
 }
