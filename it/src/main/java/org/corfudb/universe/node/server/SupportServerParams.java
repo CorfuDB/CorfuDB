9e553cf78bc8baca49444f9db25b5ae0355473b6
Viacheslav <xrw.null@gmail.com>
2019-10-21 23:37:33 -0700

    Bump lombok version to 1.18.10

--- it/src/main/java/org/corfudb/universe/node/server/SupportServerParams.java
+++ it/src/main/java/org/corfudb/universe/node/server/SupportServerParams.java
@@ -11,8 +11,6 @@ import lombok.NonNull;
 import lombok.ToString;
 import org.corfudb.universe.node.Node.NodeParams;
 import org.corfudb.universe.node.Node.NodeType;
-import org.corfudb.universe.node.server.CorfuServer.Mode;
-import org.corfudb.universe.node.server.CorfuServer.Persistence;
 import org.slf4j.event.Level;
 
 import java.time.Duration;
@@ -22,7 +20,7 @@ import java.util.Set;
 
 @Builder
 @AllArgsConstructor
-@EqualsAndHashCode(exclude = {"logLevel", "stopTimeout"})
+@EqualsAndHashCode
 @ToString
 public class SupportServerParams implements NodeParams {
     private static final Map<NodeType, Integer> PORTS = ImmutableMap.<NodeType, Integer>builder()
@@ -37,6 +35,7 @@ public class SupportServerParams implements NodeParams {
     @Default
     @NonNull
     @Getter
+    @EqualsAndHashCode.Exclude
     private final Level logLevel = Level.DEBUG;
 
     @NonNull
@@ -50,6 +49,7 @@ public class SupportServerParams implements NodeParams {
     @Getter
     @Default
     @NonNull
+    @EqualsAndHashCode.Exclude
     private final Duration stopTimeout = Duration.ofSeconds(1);
 
     @Override
