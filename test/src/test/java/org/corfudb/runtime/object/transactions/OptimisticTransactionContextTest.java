f7df9d95bf2918bf9ea918584523d94626353127
lixinchengdu <lixinchengdu@gmail.com>
2019-11-07 13:30:55 -0800

    fix not thread-safe usage of gcUnsafe (#2223)

--- test/src/test/java/org/corfudb/runtime/object/transactions/OptimisticTransactionContextTest.java
+++ test/src/test/java/org/corfudb/runtime/object/transactions/OptimisticTransactionContextTest.java
@@ -918,11 +918,7 @@ public class OptimisticTransactionContextTest extends AbstractTransactionContext
         }
 
         // run compaction
-        t(1, () -> getRuntime().getGarbageInformer().gcUnsafe());
-        t(1, () -> getLogUnit(SERVERS.PORT_0).runCompaction()); // compactionMark = RECORD_PER_SEGMENT
-        getRuntime().getAddressSpaceView().resetCaches();
-        getRuntime().getAddressSpaceView().invalidateServerCaches();
-
+        t(1, () -> startCompaction(getRuntime(), getLogUnit(SERVERS.PORT_0)));
         t(2, () -> get("k"))
                 .assertThrows().hasCauseInstanceOf(TrimmedException.class);
         t(2, this::TXEnd);
@@ -939,13 +935,7 @@ public class OptimisticTransactionContextTest extends AbstractTransactionContext
         t(1, this::SnapshotTXBegin);
 
         // run compaction
-
-        t(1, () -> getRuntime().getGarbageInformer().gcUnsafe());
-        t(1, () -> getLogUnit(SERVERS.PORT_0).runCompaction());
-        getRuntime().getAddressSpaceView().resetCaches();
-        getRuntime().getAddressSpaceView().invalidateServerCaches();
-
-
+        t(1, () -> startCompaction(getRuntime(), getLogUnit(SERVERS.PORT_0)));
         t(1, () -> get("k"))
                 .assertResult().isEqualTo("v" + entryNum); // SnapshotTimeStamp = RECORDS_PER_SEGMENT
         t(1, this::TXEnd);
