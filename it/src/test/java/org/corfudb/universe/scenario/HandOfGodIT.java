d8343d3038438490e90395d3e06fd3468bed6473
pzaytsev <zaytsev.pavel.a@gmail.com>
2019-11-08 09:53:12 -0800

    Increase timeouts on a test

--- it/src/test/java/org/corfudb/universe/scenario/HandOfGodIT.java
+++ it/src/test/java/org/corfudb/universe/scenario/HandOfGodIT.java
@@ -76,7 +76,7 @@ public class HandOfGodIT extends GenericIntegrationTest {
             ClusterStatusReport clusterStatusReport = corfuClient.getManagementView().getClusterStatus();
             assertThat(clusterStatusReport.getClusterStatus()).isEqualTo(ClusterStatus.STABLE);
 
-            ScenarioUtils.waitUninterruptibly(Duration.ofSeconds(20));
+            ScenarioUtils.waitUninterruptibly(Duration.ofSeconds(30));
 
             // Verify data path working
             for (int i = 0; i < DEFAULT_TABLE_ITER; i++) {
