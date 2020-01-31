cf83e57b33b8fccb54e5588949bc65564fd97258
dinkar <dinkarr@vmware.com>
2020-01-30 16:55:25 -0800

    Add comments
    Add comments for message metadata

--- log-replication/src/main/java/org/corfudb/MessageMetadata.java
+++ log-replication/src/main/java/org/corfudb/MessageMetadata.java
@@ -2,11 +2,28 @@ package org.corfudb;
 
 public class MessageMetadata {
 
+    /*
+     * Used to determine the type of the metadata:
+     * - snapshot type for reading/writing the snapshot timestamp,
+     * - log entry type for reading/writing the entry and previous entry timestamps,
+     */
+    private MessageType messageMetadataType;
+
+    /*
+     * From Tx -> Rx: timestamp of the entry enqueued for shipping
+     * From Rx -> Tx: timestamp of the entry applied on the receiving side
+     */
     private long entryTimeStamp;
 
+    /*
+     * Used to chain sparse entries for ordering
+     */
     private long previousEntryTimestamp;
 
-    // Used for snapshot Sync
+    /*
+     * Used to keep track of the time used for snapshots.
+     * Read by the log entry shipper to determine which point to read the log from.
+     */
     private long snapshotTimestamp;
 
 }
