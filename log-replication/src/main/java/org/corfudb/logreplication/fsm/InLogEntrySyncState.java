91d8f05c1b1be561084a38977d320ecd82a2dcd0
Anny Martinez <annymartinez.m@gmail.com>
2020-01-31 20:41:17 -0800

    Fix package name on SnapshotReader (#2345)
    * Fix package name on SnapshotReader
    * State and Events redefined
    * Add Log Replication Event Support
    * Add Logger + Missing Definitions

--- log-replication/src/main/java/org/corfudb/logreplication/fsm/InLogEntrySyncState.java
+++ log-replication/src/main/java/org/corfudb/logreplication/fsm/InLogEntrySyncState.java
@@ -1,16 +1,23 @@
 package org.corfudb.logreplication.fsm;
 
+import lombok.extern.slf4j.Slf4j;
+import org.corfudb.logreplication.transmitter.LogEntryReader;
+
 /**
  * A class that represents the 'In Log Entry Sync' state of the Log Replication FSM.
  *
  * In this state incremental (delta) updates are being synced to the remote site.
  */
+@Slf4j
 public class InLogEntrySyncState implements LogReplicationState {
 
     LogReplicationContext context;
 
+    LogEntryReader logEntryReader;
+
     public InLogEntrySyncState(LogReplicationContext context) {
         this.context = context;
+        logEntryReader = new LogEntryReader(context);
     }
 
     @Override
@@ -19,11 +26,11 @@ public class InLogEntrySyncState implements LogReplicationState {
             case SNAPSHOT_SYNC_REQUEST:
                 return new InSnapshotSyncState(context);
             case TRIMMED_EXCEPTION:
-                return new InRequireSnaphotSyncState(context);
-            case LOG_REPLICATION_STOP:
-                return new StoppedState(context);
+                return new InRequireSnapshotSyncState(context);
+            case REPLICATION_STOP:
+                return new InitializedState(context);
             default: {
-                // Log unexpected LogReplicationEvent when in initialized state
+                log.warn("Unexpected log replication event {} when in log entry sync state.", event.getType());
             }
         }
         return this;
@@ -31,7 +38,12 @@ public class InLogEntrySyncState implements LogReplicationState {
 
     @Override
     public void onEntry(LogReplicationState from) {
-
+        // Execute snapshot transaction for every table to be replicated
+        try {
+            context.getBlockingOpsScheduler().submit(logEntryReader::sync);
+        } catch (Throwable t) {
+            // Log Error
+        }
     }
 
     @Override
--- log-replication/src/main/java/org/corfudb/logreplication/transmitter/ReplicationManager.java
+++ log-replication/src/main/java/org/corfudb/logreplication/transmitter/ReplicationManager.java
@@ -1,13 +1,21 @@
 package org.corfudb.logreplication.transmitter;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.corfudb.logreplication.fsm.LogReplicationConfig;
 import org.corfudb.logreplication.fsm.LogReplicationContext;
+import org.corfudb.logreplication.fsm.LogReplicationEvent;
 import org.corfudb.logreplication.fsm.LogReplicationFSM;
 import org.corfudb.runtime.CorfuRuntime;
 
+import java.util.concurrent.Executors;
+
 @Data
+@Slf4j
 public class ReplicationManager {
+
+    // Thread priority for operation scheduler.
+    private static final int SCHEDULER_THREAD_PRIORITY = 7;
 
     private LogReplicationContext context;
 
@@ -22,13 +30,34 @@ public class ReplicationManager {
                 .snapshotListener(snapshotListener)
                 .corfuRuntime(runtime)
                 .replicationManager(this)
+                .blockingOpsScheduler(Executors.newScheduledThreadPool(6, (r) -> {
+                    Thread t = Executors.defaultThreadFactory().newThread(r);
+                    t.setDaemon(true);
+                    t.setPriority(SCHEDULER_THREAD_PRIORITY);
+                    return t;
+                }))
                 .config(config)
                 .build();
         this.logReplicationFSM = new LogReplicationFSM(context);
     }
 
-    public void startSnapshotSync() {
-        // Add SNAPSHOT_SYNC_REQUEST event to logReplicationFSM queue
-        // logReplicationFSM.input(LogReplicationEvent.LogReplicationEventType.SNAPHOT_SYNC_REQUEST);
+    public void startSnapshotSync(SnapshotSyncContext context) {
+        // Enqueue event into Log Replication FSM
+        logReplicationFSM.input(new LogReplicationEvent(LogReplicationEvent.LogReplicationEventType.SNAPSHOT_SYNC_REQUEST));
+    }
+
+    public void startReplication() {
+        // Enqueue event into Log Replication FSM
+        logReplicationFSM.input(new LogReplicationEvent(LogReplicationEvent.LogReplicationEventType.REPLICATION_START));
+    }
+
+    public void stopReplication() {
+        // Enqueue event into Log Replication FSM
+        logReplicationFSM.input(new LogReplicationEvent(LogReplicationEvent.LogReplicationEventType.REPLICATION_STOP));
+    }
+
+    public void cancelSnapshotSync(SnapshotSyncContext context) {
+        // Enqueue event into Log Replication FSM
+        logReplicationFSM.input(new LogReplicationEvent(LogReplicationEvent.LogReplicationEventType.SNAPSHOT_SYNC_CANCEL));
     }
 }
