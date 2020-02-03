ad029de7afe5c11d317a1ca60dd6fbc7b6ff8d34
Anny Martinez <annymartinez.m@gmail.com>
2020-02-03 15:42:49 -0800

    More changes + Initial Test (not working yet) (#2358)

--- log-replication/src/main/java/org/corfudb/logreplication/fsm/InLogEntrySyncState.java
+++ log-replication/src/main/java/org/corfudb/logreplication/fsm/InLogEntrySyncState.java
@@ -13,11 +13,11 @@ import java.util.concurrent.Future;
 @Slf4j
 public class InLogEntrySyncState implements LogReplicationState {
 
-    LogReplicationContext context;
+    private LogReplicationContext context;
 
-    LogEntryReader logEntryReader;
+    private LogEntryReader logEntryReader;
 
-    Future<?> logEntrySyncFuture;
+    private Future<?> logEntrySyncFuture;
 
     public InLogEntrySyncState(LogReplicationContext context) {
         this.context = context;
@@ -28,20 +28,41 @@ public class InLogEntrySyncState implements LogReplicationState {
     public LogReplicationState processEvent(LogReplicationEvent event) {
         switch (event.getType()) {
             case SNAPSHOT_SYNC_REQUEST:
-                return new InSnapshotSyncState(context);
+                /*
+                  Cancel log entry sync if still in progress, if sync cannot be canceled
+                  we cannot transition to the new state, as there is no guarantee that upper layers
+                  can distinguish between a log entry sync and a snapshot sync.
+                 */
+                return cancelLogEntrySync("snapshot sync request.") ?
+                        new InSnapshotSyncState(context) : this;
             case TRIMMED_EXCEPTION:
                 return new InRequireSnapshotSyncState(context);
             case REPLICATION_STOP:
-                if (logEntrySyncFuture != null & !logEntrySyncFuture.isDone()) {
-                    logEntrySyncFuture.cancel(true);
-                    log.info("Log Entry sync has been canceled.");
-                }
-                return new InitializedState(context);
+                /*
+                  Cancel log entry sync if still in progress, if sync cannot be canceled
+                  we cannot transition to the new state.
+                 */
+                return cancelLogEntrySync("replication being stopped.") ?
+                        new InitializedState(context) : this;
             default: {
                 log.warn("Unexpected log replication event {} when in log entry sync state.", event.getType());
             }
         }
         return this;
+    }
+
+    private boolean cancelLogEntrySync(String cancelCause) {
+        // Cancel log entry sync if still in progress
+        if (logEntrySyncFuture != null && !logEntrySyncFuture.isDone()) {
+            boolean cancel = logEntrySyncFuture.cancel(true);
+            // Verify if task could not be canceled due to normal completion.
+            if (!cancel && !logEntrySyncFuture.isDone()) {
+                log.error("Snapshot sync in progress could not be canceled.");
+                return false;
+            }
+        }
+        log.info("Log Entry sync has been canceled due to {}", cancelCause);
+        return true;
     }
 
     @Override
