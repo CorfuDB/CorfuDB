2592b4e89b15d3f251aa7df0bf3501775844dc42
Anny Martinez <annymartinez.m@gmail.com>
2020-02-03 15:42:49 -0800

    More changes + Initial Test (not working yet) (#2358)

--- log-replication/src/main/java/org/corfudb/logreplication/receiver/RxMessage.java
+++ log-replication/src/main/java/org/corfudb/logreplication/receiver/RxMessage.java
@@ -1,7 +1,11 @@
 package org.corfudb.logreplication.receiver;
 
+import lombok.Builder;
+import lombok.Data;
 import org.corfudb.logreplication.MessageMetadata;
 
+@Builder
+@Data
 public class RxMessage {
 
     private MessageMetadata metadata;
