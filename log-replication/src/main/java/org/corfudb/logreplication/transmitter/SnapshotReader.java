9af748903f27fb520a354b2a6899b79c6eaeffd6
maxi <maxi@vmware.com>
2020-02-03 15:02:13 -0800

    Implementation of SnapshotWriter.
    - Draft for delta sync.

--- log-replication/src/main/java/org/corfudb/logreplication/transmitter/SnapshotReader.java
+++ log-replication/src/main/java/org/corfudb/logreplication/transmitter/SnapshotReader.java
@@ -1,50 +1,71 @@
 package org.corfudb.logreplication.transmitter;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.SerializationUtils;
 import org.corfudb.logreplication.MessageType;
 import org.corfudb.logreplication.fsm.LogReplicationContext;
 import org.corfudb.protocols.logprotocol.SMREntry;
 import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.exceptions.TrimmedException;
 import org.corfudb.runtime.object.StreamViewSMRAdapter;
+import org.corfudb.runtime.view.Address;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-
+@Slf4j
 /**
+ * The AR will call sync API and pass in the api for fullSyncDone(), handlerMsg()
  * Read streams one by one, get all the entries.
  */
 public class SnapshotReader {
-    private final int MAX_BATCH_SIZE = 1000;
+    //Todo: will change the max_batch_size while Maithem finish the new API
+    private final int MAX_BATCH_SIZE = 1;
     private final MessageType MSG_TYPE = MessageType.SNAPSHOT_MESSAGE;
     private long globalSnapshot;
-    private Set<UUID> streams;
-    private LinkedList<UUID> streamsToSend;
+    private List<String> streams;
     private CorfuRuntime rt;
     private LogReplicationContext replicationContext;
+    private long preMsgTs;
+    private long currentMsgTs;
 
     /**
      * Set runtime and callback function to pass message to network
-     * @param runtime
+     * @param context
      */
-    public SnapshotReader(CorfuRuntime runtime) {
-        rt = runtime;
-    }
-
     public SnapshotReader(LogReplicationContext context) {
         replicationContext = context;
         rt = replicationContext.getCorfuRuntime();
-    }
-
-    void init() {
-        globalSnapshot = rt.getAddressSpaceView().getLogTail();
-        streamsToSend = new LinkedList<>(streams);
+        streams = replicationContext.getConfig().getStreamsToReplicate();
     }
 
     /**
-     * get all entries upto globalSnapshot for an given stream
+     * For test case to use
+     * @param rt
+     * @param streams
+     */
+    public SnapshotReader(CorfuRuntime rt, List<String> streams) {
+        this.rt = rt;
+        this.streams = streams;
+    }
+
+    /**
+     * setup globalSnapshot
+     */
+    void setup() {
+        preMsgTs = Address.NON_ADDRESS;
+        currentMsgTs = Address.NON_ADDRESS;
+        globalSnapshot = rt.getAddressSpaceView().getLogTail();
+    }
+
+    /**
+     * get all entries for a stream up to the globalSnapshot
      * @param streamID
      * @return
      */
@@ -54,37 +75,61 @@ public class SnapshotReader {
     }
 
     TxMessage generateMessage(List<SMREntry> entries) {
-        TxMessage txMessage = new TxMessage();
-        //set metadata
-        //set data
-        return  txMessage;
+        currentMsgTs = entries.get(entries.size() - 1).getGlobalAddress();
+        TxMessage txMsg = new TxMessage(MSG_TYPE, currentMsgTs, preMsgTs, globalSnapshot);
+
+        // todo: using Maithem API to generate msg data with entries.
+        ByteBuf buf = Unpooled.buffer();
+        entries.get(0).serialize(buf);
+        txMsg.setData(buf.array());
+
+        log.debug("Generate TxMsg {}", txMsg.getMetadata());
+        //set data, will plug in meithem's new api
+        return  txMsg;
     }
 
-    List<TxMessage> nextMsgs() {
-        List<TxMessage> msgList = new ArrayList<>();
-        //get the firstStream to process
-        ArrayList<SMREntry> entries = new ArrayList<>(readStream(streamsToSend.poll()));
+    /**
+     * Given a stream name, get all entries for this stream,
+     * put entries in a message and call the callback handler
+     * to pass the message to the other site.
+     * @param streamName
+     */
+    void next(String streamName) {
+        UUID streamID = CorfuRuntime.getStreamID(streamName);
+        ArrayList<SMREntry> entries = new ArrayList<>(readStream(streamID));
+        preMsgTs = Address.NON_ADDRESS;
+
         for (int i = 0; i < entries.size(); i += MAX_BATCH_SIZE) {
             List<SMREntry> msg_entries = entries.subList(i, i + MAX_BATCH_SIZE);
             TxMessage txMsg = generateMessage(msg_entries);
-            msgList.add(txMsg);
+
+            //todo: callback pass msg to AR
+
+            //update preMsgTs only after process a msg successfully
+            preMsgTs = currentMsgTs;
+            log.debug("Succesfully pass a TxMsg {}", txMsg.getMetadata());
         }
-        return msgList;
+
+        log.info("Succesfully pass a stream {} for globalSnapshot {}", streamName, globalSnapshot);
+        return;
     }
 
     /**
      * while sync finish put an event to the queue
      */
     public void sync() {
-        init();
+        setup();
         try {
-            while (!streamsToSend.isEmpty()) {
-                List<TxMessage> msgs = nextMsgs();
-                //call back handler
-                //handle_msg(msgs);
+            for (String streamName : streams) {
+                next(streamName);
             }
         } catch (Exception e) {
             //handle exception
+            log.warn("Sync call get an exception ", e);
+            throw e;
         }
+
+        //todo: update metadata to record a Snapshot Reader done
+        log.info("Succesfully do a sync read for globalSnapshot {}", globalSnapshot);
     }
 }
--- log-replication/src/main/java/org/corfudb/logreplication/transmitter/TxMessage.java
+++ log-replication/src/main/java/org/corfudb/logreplication/transmitter/TxMessage.java
@@ -1,14 +1,21 @@
 package org.corfudb.logreplication.transmitter;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.corfudb.logreplication.MessageMetadata;
+import org.corfudb.logreplication.MessageType;
 
 @Data
 public class TxMessage {
 
-    private MessageMetadata metadata;
+    @Getter
+    public MessageMetadata metadata;
 
+    @Setter
     private byte[] data;
 
-
+    TxMessage(MessageType type, long entryTS, long preTS, long snapshot) {
+        metadata = new MessageMetadata(type, entryTS, preTS, snapshot);
+    }
 }
