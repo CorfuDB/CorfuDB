1be3b4af3e942b03e2ed6561f514ee2d3704a1f3
Anny Martinez <annymartinez.m@gmail.com>
2020-02-03 11:13:35 -0800

    Add onExit logic for SnapshotSync and LogEntrySync (#2352)
    * Rename entry classes and change cancelation entry point (#2354)
    * Add accessors to TxMessage (#2357)

--- log-replication/src/main/java/org/corfudb/logreplication/transmitter/TxMessage.java
+++ log-replication/src/main/java/org/corfudb/logreplication/transmitter/TxMessage.java
@@ -1,10 +1,14 @@
 package org.corfudb.logreplication.transmitter;
 
+import lombok.Data;
 import org.corfudb.logreplication.MessageMetadata;
 
+@Data
 public class TxMessage {
 
     private MessageMetadata metadata;
 
     private byte[] data;
+
+
 }
