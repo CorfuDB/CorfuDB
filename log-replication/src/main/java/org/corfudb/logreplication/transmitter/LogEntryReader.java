54bc1f7e7369dac8e80fc31e41201d8923ac89d9
Xiaoqin Ma <maxi@vmware.com>
2020-02-04 09:43:15 -0800

    Add comments to reader/writer. (#2360)

--- log-replication/src/main/java/org/corfudb/logreplication/transmitter/LogEntryReader.java
+++ log-replication/src/main/java/org/corfudb/logreplication/transmitter/LogEntryReader.java
@@ -9,21 +9,17 @@ import org.corfudb.runtime.view.ObjectsView;
 import org.corfudb.runtime.view.StreamOptions;
 import org.corfudb.runtime.view.stream.IStreamView;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.corfudb.runtime.view.Address.MAX;
 @Slf4j
 public class LogEntryReader {
     private IStreamView stream;
     private long globalBaseSnapshot;
     private CorfuRuntime rt;
-    private final int MAX_BATCH_SIZE = 1000;
     private final MessageType MSG_TYPE = MessageType.LOG_ENTRY_MESSAGE;
     private LogReplicationContext context;
     private long preMsgTs;
     private long currentMsgTs;
-    private long lastReadAddress;
 
     void initStream() {
         StreamOptions options = StreamOptions.builder()
@@ -44,48 +40,29 @@ public class LogEntryReader {
         return stream.remaining();
     }
 
-    TxMessage generateMessage(List<ILogData> entries) {
-        currentMsgTs = entries.get(entries.size() - 1).getGlobalAddress();
+    TxMessage generateMessage(ILogData entry) {
+        currentMsgTs = entry.getGlobalAddress();
         TxMessage txMessage = new TxMessage(MSG_TYPE, currentMsgTs, preMsgTs, globalBaseSnapshot);
         //set data with Maithem's new api
         preMsgTs = currentMsgTs;
         return  txMessage;
     }
 
-    List<TxMessage> nextMsgs(List inputs) {
-        ArrayList entries = new ArrayList(inputs);
-        List<TxMessage> msgList = new ArrayList<>();
-
-        if (entries.isEmpty()) {
-            return msgList;
+    void nextMsgs(List<ILogData> inputs) {
+        for (ILogData entry : inputs) {
+            context.getSnapshotListener().onNext(generateMessage(entry));
         }
-
-        for (int i = 0; i < entries.size(); i += MAX_BATCH_SIZE) {
-            List<ILogData> msg_entries = entries.subList(i, i + MAX_BATCH_SIZE);
-            TxMessage txMsg = generateMessage(entries);
-            msgList.add(txMsg);
-        }
-
-        return msgList;
     }
 
     public void resetGlobalBaseSnapshot(long snapshot) {
         globalBaseSnapshot = snapshot;
-        lastReadAddress = snapshot;
         preMsgTs = snapshot;
     }
 
     public void sync() {
         try {
-            List entries = poll(lastReadAddress + 1);
-            List<TxMessage> msgs = nextMsgs(entries);
-
-            // call callback to process message
-
-            // only update lastReadAddress after successful process message
-            lastReadAddress = preMsgTs;
+            nextMsgs(poll(preMsgTs + 1));
         } catch (Exception e) {
-
             log.warn("Sync caught an exception ", e);
             throw(e);
         }
