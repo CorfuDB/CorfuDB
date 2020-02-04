5f5a08d0f752b54f45ba129ca946956f5f50e627
Xiaoqin Ma <maxi@vmware.com>
2020-02-04 09:43:15 -0800

    Add comments to reader/writer. (#2360)

--- log-replication/src/main/java/org/corfudb/logreplication/receiver/DataReceiver.java
+++ log-replication/src/main/java/org/corfudb/logreplication/receiver/DataReceiver.java
@@ -1,9 +1,10 @@
 package org.corfudb.logreplication.receiver;
 
+import org.corfudb.logreplication.fsm.LogReplicationContext;
 import org.corfudb.runtime.CorfuRuntime;
 
 public class DataReceiver {
-
+    private LogReplicationContext context;
     private CorfuRuntime runtime;
 
     public DataReceiver(CorfuRuntime rt) {
@@ -13,5 +14,6 @@ public class DataReceiver {
     public void apply(RxMessage message) {
         // Buffer data (out of order) and apply
     }
+
 
 }
