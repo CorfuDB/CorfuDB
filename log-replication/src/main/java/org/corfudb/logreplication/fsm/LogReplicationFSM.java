176d7d29adc9ad5001f7033169b8f2b89630e5b3
Anny Martinez <annymartinez.m@gmail.com>
2020-02-04 12:08:10 -0800

    Snapshot Reader Interface and Implementation (#2361)

--- log-replication/src/main/java/org/corfudb/logreplication/fsm/LogReplicationFSM.java
+++ log-replication/src/main/java/org/corfudb/logreplication/fsm/LogReplicationFSM.java
@@ -3,11 +3,22 @@ package org.corfudb.logreplication.fsm;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.corfudb.logreplication.transmitter.LogEntryListener;
+import org.corfudb.logreplication.transmitter.LogEntryReader;
+import org.corfudb.logreplication.transmitter.LogEntryTransmitter;
+import org.corfudb.logreplication.transmitter.SnapshotListener;
+import org.corfudb.logreplication.transmitter.SnapshotReader;
+import org.corfudb.logreplication.transmitter.SnapshotTransmitter;
+import org.corfudb.logreplication.transmitter.StreamsLogEntryReader;
+import org.corfudb.logreplication.transmitter.StreamsSnapshotReader;
+import org.corfudb.runtime.CorfuRuntime;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
-
 
 /**
  * This class represents the Log Replication Finite State Machine.
@@ -22,6 +33,18 @@ public class LogReplicationFSM {
     private LogReplicationState state;
 
     /**
+     * Map of all Log Replication FSM States
+     */
+    @Getter
+    private Map<LogReplicationStateType, LogReplicationState> states = new HashMap<>();
+
+    /**
+     * Executor service for FSM state tasks
+     */
+    @Getter
+    private ExecutorService stateMachineWorker;
+
+    /**
      * A queue of events.
      */
     private final LinkedBlockingQueue<LogReplicationEvent> eventQueue = new LinkedBlockingQueue<>();
@@ -29,17 +52,58 @@ public class LogReplicationFSM {
     @Getter
     private ObservableValue numTransitions = new ObservableValue(0);
 
-    /**
-     * Constructor.
-     *
-     * @param context LogReplicationContext.
-     */
-    public LogReplicationFSM(LogReplicationContext context) {
-        this.state = new InitializedState(context);
+
+
+
+    public LogReplicationFSM(CorfuRuntime runtime, LogReplicationConfig config, SnapshotListener snapshotListener,
+                             LogEntryListener logEntryListener, ExecutorService workers) {
+
+        // Create transmitters to be used by the the sync states (Snapshot and LogEntry) to read and transmit data
+        // through the callbacks provided by the application
+        SnapshotReader defaultSnapshotReader = new StreamsSnapshotReader(runtime, config);
+        LogEntryReader defaultLogEntryReader = new StreamsLogEntryReader(runtime, config);
+
+        SnapshotTransmitter snapshotTransmitter = new SnapshotTransmitter(runtime, defaultSnapshotReader, snapshotListener, this);
+        LogEntryTransmitter logEntryTransmitter = new LogEntryTransmitter(runtime, defaultLogEntryReader, logEntryListener);
+
+        initializeStates(snapshotTransmitter, logEntryTransmitter);
+        this.state = states.get(LogReplicationStateType.INITIALIZED);
+        this.stateMachineWorker = workers;
+
         // Consumer thread will run on a dedicated single thread (poll queue for incoming events)
         ThreadFactory consumerThreadFactory =
-                new ThreadFactoryBuilder().setNameFormat("log-replication-consumer-%d").build();
+                new ThreadFactoryBuilder().setNameFormat("replication-fsm-consumer-%d").build();
         Executors.newSingleThreadExecutor(consumerThreadFactory).submit(this::consume);
+    }
+
+    public LogReplicationFSM(CorfuRuntime runtime, LogReplicationConfig config,
+                             SnapshotReader snapshotReader, SnapshotListener snapshotListener,
+                             LogEntryReader logEntryReader, LogEntryListener logEntryListener, ExecutorService workers) {
+
+        // Create transmitters to be used by the the sync states (Snapshot and LogEntry) to read and transmit data
+        // through the callbacks provided by the application
+        SnapshotTransmitter snapshotTransmitter = new SnapshotTransmitter(runtime, snapshotReader, snapshotListener, this);
+        LogEntryTransmitter logEntryTransmitter = new LogEntryTransmitter(runtime, logEntryReader, logEntryListener);
+
+        // Initialize Log Replication 5 FSM states - single instance per state
+        initializeStates(snapshotTransmitter, logEntryTransmitter);
+
+        // Set INITIALIZED as the initial state
+        this.state = states.get(LogReplicationStateType.INITIALIZED);
+        this.stateMachineWorker = workers;
+
+        // Consumer thread will run on a dedicated single thread (poll queue for incoming events)
+        ThreadFactory consumerThreadFactory =
+                new ThreadFactoryBuilder().setNameFormat("replication-fsm-consumer-%d").build();
+        Executors.newSingleThreadExecutor(consumerThreadFactory).submit(this::consume);
+    }
+
+    private void initializeStates(SnapshotTransmitter snapshotTransmitter, LogEntryTransmitter logEntryTransmitter) {
+        states.put(LogReplicationStateType.INITIALIZED, new InitializedState(this));
+        states.put(LogReplicationStateType.IN_SNAPSHOT_SYNC, new InSnapshotSyncState(this, snapshotTransmitter));
+        states.put(LogReplicationStateType.IN_LOG_ENTRY_SYNC, new InLogEntrySyncState(this, logEntryTransmitter));
+        states.put(LogReplicationStateType.IN_REQUIRE_SNAPSHOT_SYNC, new InRequireSnapshotSyncState(this));
+        states.put(LogReplicationStateType.STOPPED, new StoppedState());
     }
 
     /**
@@ -77,8 +141,6 @@ public class LogReplicationFSM {
                 // Block until an event shows up in the queue.
                 LogReplicationEvent event = eventQueue.take();
 
-                System.out.println("Processing event: " + event.getType());
-
                 // Process the event
                 LogReplicationState newState = state.processEvent(event);
 
@@ -94,10 +156,8 @@ public class LogReplicationFSM {
     }
 
     void transition(LogReplicationState from, LogReplicationState to) {
-        if (from != to) {
-            from.onExit(to);
-
-            to.onEntry(from);
-        }
+        from.onExit(to);
+        to.clear();
+        to.onEntry(from);
     }
 }
