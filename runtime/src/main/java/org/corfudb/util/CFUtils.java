d86321716334211804bfc242a10847dfeb15d283
Anmol Ahuja <aanmol@vmware.com>
2020-02-04 23:04:56 -0800

    Address comments, add unit test

--- runtime/src/main/java/org/corfudb/util/CFUtils.java
+++ runtime/src/main/java/org/corfudb/util/CFUtils.java
@@ -1,7 +1,6 @@
 package org.corfudb.util;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
 import org.corfudb.runtime.exceptions.unrecoverable.UnrecoverableCorfuInterruptedError;
 
 import java.time.Duration;
