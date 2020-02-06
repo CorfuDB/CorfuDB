9f38a280a4e583da56d18e20aab2269ddb86c684
Xiaoqin Ma <maxi@vmware.com>
2020-02-06 09:58:12 -0800

    Implementation of StreamSnapshotReader with Opaquestream. (#2371)

--- log-replication/src/main/java/org/corfudb/logreplication/MessageMetadata.java
+++ log-replication/src/main/java/org/corfudb/logreplication/MessageMetadata.java
@@ -1,41 +1,42 @@
 package org.corfudb.logreplication;
 
+import lombok.Data;
 import lombok.Getter;
-
+@Data
 public class MessageMetadata {
     /*
      * Used to determine the type of the metadata:
      * - snapshot type for reading/writing the snapshot timestamp,
      * - log entry type for reading/writing the entry and previous entry timestamps,
      */
-    @Getter
     private MessageType messageMetadataType;
 
     /*
      * From Tx -> Rx: timestamp of the entry enqueued for shipping
      * From Rx -> Tx: timestamp of the entry applied on the receiving side
      */
-    @Getter
     public long entryTimeStamp;
+    //for full sync when the entryTimeStamp == snapshotTimestamp, it means the end of the stream.
 
     /*
      * Used to chain sparse entries for ordering
      */
-    @Getter
     private long previousEntryTimestamp;
 
     /*
      * Used to keep track of the time used for snapshots.
      * Read by the log entry shipper to determine which point to read the log from.
      */
-    @Getter
     private long snapshotTimestamp;
 
-    public MessageMetadata(MessageType type, long entryTimeStamp, long previousEntryTimestamp, long snapshotTimestamp) {
+    private long fullSyncSeqNum; //used by fullsync only, zero means the start of the stream.
+
+    public MessageMetadata(MessageType type, long entryTimeStamp, long previousEntryTimestamp, long snapshotTimestamp, long sequence) {
         this.messageMetadataType = type;
         this.entryTimeStamp = entryTimeStamp;
         this.previousEntryTimestamp = previousEntryTimestamp;
         this.snapshotTimestamp = snapshotTimestamp;
+        this.fullSyncSeqNum = sequence;
     }
 }
 
