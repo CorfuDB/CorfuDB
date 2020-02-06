51e186b0fc76365312c74b11bf336512703d36b9
Anny Martinez <annymartinez.m@gmail.com>
2020-02-05 17:08:43 -0800

    Add LogReplicationError + Transition Event Id for Error Handling (#2367)
    - Expose StartSnapshot call on ReplicationRxManager (#2368)

---
+++ log-replication/src/main/java/org/corfudb/logreplication/LogReplicationError.java
@@ -0,0 +1,36 @@
+package org.corfudb.logreplication;
+
+/**
+ * Log Replication Error
+ */
+public enum LogReplicationError {
+    TRIM_SNAPSHOT_SYNC(0, "A trim exception has occurred during snapshot sync."),
+    TRIM_LOG_ENTRY_SYNC(1, "A trim exception has occurred during log entry sync.");
+
+    private final int code;
+    private final String description;
+
+    /**
+     * Constructor
+     *
+     * @param code error code
+     * @param description error description
+     */
+    private LogReplicationError(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return code + ": " + description;
+    }
+}
