dd469066373d12a2ef720c24431344ccf9964764
Anny Martinez <annymartinez.m@gmail.com>
2020-02-07 23:28:25 -0800

    Add Comments (#2384)

--- log-replication/src/main/java/org/corfudb/logreplication/receiver/ReplicationRxManager.java
+++ log-replication/src/main/java/org/corfudb/logreplication/receiver/ReplicationRxManager.java
@@ -1,5 +1,7 @@
 package org.corfudb.logreplication.receiver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.corfudb.logreplication.fsm.LogReplicationConfig;
 import org.corfudb.logreplication.transmitter.DataMessage;
 import org.corfudb.runtime.CorfuRuntime;
 
@@ -9,16 +11,22 @@ import java.util.List;
  * A class that represents the Replication Receiver Manager.
  * This is the entry point for destination site.
  */
+@Slf4j
 public class ReplicationRxManager {
+
+    // TODO: THIS CLASS NEEDS TO BE WORKED
+
     private CorfuRuntime runtime;
+    private StreamsSnapshotWriter snapshotWriter;
 
     /**
      * Constructor
      *
      * @param rt Corfu Runtime
      */
-    public ReplicationRxManager(CorfuRuntime rt) {
+    public ReplicationRxManager(CorfuRuntime rt, LogReplicationConfig config) {
         this.runtime = rt;
+        this.snapshotWriter = new StreamsSnapshotWriter(rt, config);
     }
 
     /**
@@ -27,7 +35,14 @@ public class ReplicationRxManager {
      * @param message
      */
     public void apply(DataMessage message) {
+        // @maxi, how do we distinguish log entry apply from snapshot apply?
         // Buffer data (out of order) and apply
+        try {
+            this.snapshotWriter.apply(message);
+        } catch (Exception e) {
+
+        }
+
     }
 
     /**
@@ -44,5 +59,6 @@ public class ReplicationRxManager {
      */
     public void startSnapshot() {
         // Signal start of snapshot sync to the receiver, so data can be cleared.
+        // Metadata to clear?
     }
 }
