69472edf2383dd92749e7fce75db09f771d617ff
maxi <maxi@vmware.com>
2020-02-10 14:38:40 -0800

    Put persistent metadata in manager.

--- log-replication/src/main/java/org/corfudb/logreplication/transmitter/PersistedReaderMetadata.java
+++ log-replication/src/main/java/org/corfudb/logreplication/transmitter/PersistedReaderMetadata.java
@@ -8,34 +8,57 @@ import org.corfudb.util.serializer.Serializers;
 
 import java.util.UUID;
 
+/**
+ * The persistent table at the corfu reader cluster.
+ * The table is used to record the snapshot timestamp and last ack
+ * has received from the destination corfu writer cluster.
+ * As one corfu reader cluster can have multiple destination corfu writer cluster,
+ * Using the writer corfu cluster id to uniquely identify the table.
+ *
+ * Todo:
+ * If this table is not registered at checkpoint compactor, this information can be trimmed.
+ */
 @Data
 public class PersistedReaderMetadata {
-    private final String TABLE_PREFIX_NAME = "READER-";
+    private final String TABLE_PREFIX_NAME = "READER-FOR-";
     private final String LAST_SENT_SNAP_TS = "lastSentBaseSnapshotTimeStamp";
     private final String LAST_ACK_SNAP_TS = "lastAckedTimeStamp";
 
     private long lastSentBaseSnapshotTimestamp; //used by fullsync transmitter
     private long lastAckedTimestamp; //used by fullsync transmitter
 
-    //private long lastProcessedSrcTimestamp; // Receiver will update this value
-
     private CorfuTable<String, Long> readerMetaDataTable;
 
-    public PersistedReaderMetadata(CorfuRuntime rt, UUID src, UUID dst) {
+    /**
+     * Open an corfu table to record the lastSentBaseSnapshotTs and
+     * lastAckedTs.
+     * @param rt
+     * @param dst the corfu writer cluster id.
+     */
+    public PersistedReaderMetadata(CorfuRuntime rt, UUID dst) {
         readerMetaDataTable = rt.getObjectsView()
                 .build()
-                .setStreamName(TABLE_PREFIX_NAME + src.toString() + "-" + dst.toString())
+                .setStreamName(TABLE_PREFIX_NAME + dst.toString())
                 .setTypeToken(new TypeToken<CorfuTable<String, Long>>() {
                 })
                 .setSerializer(Serializers.JSON)
                 .open();
     }
 
+    /**
+     * Set the snapshot fullsync timestamp when a snapshot read is done.
+     * @param ts
+     */
     public void setLastSentBaseSnapshotTimestamp(long ts) {
         readerMetaDataTable.put(LAST_SENT_SNAP_TS,ts);
         lastSentBaseSnapshotTimestamp = ts;
     }
 
+    /**
+     * Record the ack from the writer, this information record the timestamp that
+     * the writer has synced up to.
+     * @param ts
+     */
     public void setLastAckedTimestamp(long ts) {
         readerMetaDataTable.put(LAST_ACK_SNAP_TS,ts);
         lastAckedTimestamp = ts;
