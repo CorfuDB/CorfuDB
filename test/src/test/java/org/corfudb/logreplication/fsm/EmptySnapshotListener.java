ecafac7daa3a0b31efe89485ef1cc62a7de5052f
Anny Martinez <annymartinez.m@gmail.com>
2020-02-11 16:53:35 -0800

    Move classes into Packages (#2393)

--- test/src/test/java/org/corfudb/logreplication/fsm/EmptySnapshotListener.java
+++ test/src/test/java/org/corfudb/logreplication/fsm/EmptySnapshotListener.java
@@ -1,6 +1,6 @@
 package org.corfudb.logreplication.fsm;
 
-import org.corfudb.logreplication.LogReplicationError;
+import org.corfudb.logreplication.transmit.LogReplicationError;
 import org.corfudb.logreplication.transmit.SnapshotListener;
 import org.corfudb.logreplication.message.DataMessage;
 
