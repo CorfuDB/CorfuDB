0ef1da1f56ce6861a7ec7dd0885fd200b408d7dd
Xiaoqin Ma <maxi@vmware.com>
2020-02-18 18:29:30 -0800

    Fixes (#2419)
    * Implement a sliding window with arraylist for log entry reader.
    For each data message, we records the time the message is sent and number of
    retries for resending. While it reaches the max retries, it will generate a
    replication exception.
    * Add serilization and deserilization for message data.
    * Fix bug in DataMessage setData.
    * Remove setData in writer.

--- log-replication/src/main/java/org/corfudb/logreplication/receive/StreamsSnapshotWriter.java
+++ log-replication/src/main/java/org/corfudb/logreplication/receive/StreamsSnapshotWriter.java
@@ -108,7 +108,9 @@ public class StreamsSnapshotWriter implements SnapshotWriter {
             throw new ReplicationWriterException("Message is out of order");
         }
 
+        //message.setData(message.getData());
         OpaqueEntry opaqueEntry = OpaqueEntry.deserialize(Unpooled.wrappedBuffer(message.getData()));
+
         if (opaqueEntry.getEntries().keySet().size() != 1) {
             log.error("The opaqueEntry has more than one entry {}", opaqueEntry);
             return;
