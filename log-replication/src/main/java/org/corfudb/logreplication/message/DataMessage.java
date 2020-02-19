9ce24ca74d639da4e69fa0abeb8c0db172ffad0b
Xiaoqin Ma <maxi@vmware.com>
2020-02-18 18:29:30 -0800

    Fixes (#2419)
    * Implement a sliding window with arraylist for log entry reader.
    For each data message, we records the time the message is sent and number of
    retries for resending. While it reaches the max retries, it will generate a
    replication exception.
    * Add serilization and deserilization for message data.
    * Fix bug in DataMessage setData.
    * Remove setData in writer.

--- log-replication/src/main/java/org/corfudb/logreplication/message/DataMessage.java
+++ log-replication/src/main/java/org/corfudb/logreplication/message/DataMessage.java
@@ -1,9 +1,15 @@
 package org.corfudb.logreplication.message;
 
 import com.google.common.annotations.VisibleForTesting;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.corfudb.protocols.logprotocol.OpaqueEntry;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
 
 /**
  * This class represents the data message, it contains the data to be replicated and metadata required for sequencing.
@@ -36,5 +42,48 @@ public class DataMessage {
     public DataMessage(MessageType type, long entryTS, long preTS, long snapshot, long sequence, byte[] data) {
         metadata = new MessageMetadata(type, entryTS, preTS, snapshot, sequence);
         this.data = data;
+    }
+
+    public DataMessage(MessageType type, long entryTS, long preTS, long snapshot, long sequence,
+                       OpaqueEntry opaqueEntry) {
+        metadata = new MessageMetadata(type, entryTS, preTS, snapshot, sequence);
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeInt(metadata.getMessageMetadataType().getVal());
+        buf.writeLong(metadata.getPreviousTimestamp());
+        //buf.writeLong(metadata.getSnapshotRequestId().getMostSignificantBits());
+        //buf.writeLong(metadata.getSnapshotRequestId().getLeastSignificantBits());
+        buf.writeLong(metadata.getSnapshotSyncSeqNum());
+        buf.writeLong(metadata.getSnapshotTimestamp());
+        buf.writeLong(metadata.getTimestamp());
+
+        ByteBuf opaqueBuf = Unpooled.buffer();
+        OpaqueEntry.serialize(opaqueBuf, opaqueEntry);
+        int size = opaqueBuf.readableBytes();
+
+        buf.writeInt(size);
+        buf.writeBytes(opaqueBuf.array());
+        this.data = buf.array();
+    }
+
+    public void setData(byte[] data) {
+
+        MessageMetadata metadata = new MessageMetadata();
+        ByteBuf byteBuf = Unpooled.wrappedBuffer(data);
+
+        metadata.setMessageMetadataType(MessageType.fromValue(byteBuf.readInt()));
+        metadata.setPreviousTimestamp(byteBuf.readLong());
+
+        //UUID uuid = new UUID(byteBuf.readLong(), byteBuf.readLong());
+        //metadata.setSnapshotRequestId(uuid);
+
+        metadata.setSnapshotSyncSeqNum(byteBuf.readLong());
+        metadata.setSnapshotTimestamp(byteBuf.readLong());
+        metadata.setTimestamp(byteBuf.readLong());
+        this.metadata = metadata;
+
+        int size = byteBuf.readInt();
+        ByteBuf dataBuf = Unpooled.buffer();
+        byteBuf.readBytes(dataBuf, size);
+        this.data = dataBuf.array();
     }
 }
--- log-replication/src/main/java/org/corfudb/logreplication/message/MessageType.java
+++ log-replication/src/main/java/org/corfudb/logreplication/message/MessageType.java
@@ -1,8 +1,26 @@
 package org.corfudb.logreplication.message;
 
+import lombok.Getter;
+
 public enum MessageType {
-    LOG_ENTRY_MESSAGE,
-    SNAPSHOT_MESSAGE,
-    LOG_ENTRY_REPLICATED,
-    SNAPSHOT_REPLICATED
+    LOG_ENTRY_MESSAGE(1),
+    SNAPSHOT_MESSAGE(2),
+    LOG_ENTRY_REPLICATED(3),
+    SNAPSHOT_REPLICATED(4);
+
+    @Getter
+    int val;
+    MessageType(int newVal) {
+        val  = newVal;
+    }
+
+    public static MessageType fromValue(int newVal) {
+        for (MessageType messageType: values()) {
+            if (messageType.getVal() == newVal) {
+                return messageType;
+            }
+        }
+        throw new IllegalArgumentException("wrong value " + newVal);
+    }
+
 }
