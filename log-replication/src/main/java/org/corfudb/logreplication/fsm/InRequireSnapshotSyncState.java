f6eee52cb17c009025ab6a84364e69af6eca468c
Anny Martinez <annymartinez.m@gmail.com>
2020-02-20 16:48:06 -0800

    Data Control + Specific Exception Handling (#2431)
    - Introduce new exceptions for Log Entry Sync
    - Add Data Control support
    - Add test snapshot sync no replicated data present

--- log-replication/src/main/java/org/corfudb/logreplication/fsm/InRequireSnapshotSyncState.java
+++ log-replication/src/main/java/org/corfudb/logreplication/fsm/InRequireSnapshotSyncState.java
@@ -1,6 +1,8 @@
 package org.corfudb.logreplication.fsm;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.corfudb.logreplication.DataControl;
 
 /**
  * This class represents the InRequireSnapshotSync state of the Log Replication State Machine.
@@ -15,8 +17,14 @@ public class InRequireSnapshotSyncState implements LogReplicationState {
      */
     private final LogReplicationFSM fsm;
 
-    public InRequireSnapshotSyncState(LogReplicationFSM logReplicationFSM) {
+    /*
+     * Data Control Implementation
+     */
+    private final DataControl dataControl;
+
+    public InRequireSnapshotSyncState(@NonNull LogReplicationFSM logReplicationFSM, @NonNull DataControl dataControl) {
         this.fsm = logReplicationFSM;
+        this.dataControl = dataControl;
     }
 
     @Override
@@ -41,6 +49,7 @@ public class InRequireSnapshotSyncState implements LogReplicationState {
     public void onEntry(LogReplicationState from) {
         // TODO: since a SNAPSHOT_SYNC_REQUEST is the only event that can take us out of this state,
         //  we need a scheduler to re-notify the remote site of the error, in case the request was lost.
+        dataControl.requestSnapshotSync();
     }
 
     @Override
