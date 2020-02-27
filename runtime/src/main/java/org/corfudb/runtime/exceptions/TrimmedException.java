e4ad6833a5ec5ef9e1234d92c5877dcc3ccb02cc
Xiaoqin Ma <maxi@vmware.com>
2020-02-26 22:26:29 -0800

    Test snapshot reader and log entry reader can get trimmed exception. (#2447)

--- runtime/src/main/java/org/corfudb/runtime/exceptions/TrimmedException.java
+++ runtime/src/main/java/org/corfudb/runtime/exceptions/TrimmedException.java
@@ -2,9 +2,11 @@ package org.corfudb.runtime.exceptions;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 /**
  * This exception is thrown when a client tries to read an address
  * that has been trimmed.
@@ -31,6 +33,7 @@ public class TrimmedException extends LogUnitException {
 
     public TrimmedException(List<Long> trimmed) {
         super(String.format("Trimmed addresses " + trimmed));
+        log.error("Trimmed adddresses {}", trimmed);
         trimmedAddresses = trimmed;
     }
 
