0d5cc6777d0fcc736fddd1da038fc596ef0f7abc
Anny Martinez <annymartinez.m@gmail.com>
2020-03-09 17:10:49 -0700

    Move Log Replication triggers to CorfuReplicationManager (#2468)

---
+++ log-replication/src/main/java/org/corfudb/logreplication/infrastructure/CorfuReplicationManager.java
@@ -0,0 +1,18 @@
+package org.corfudb.logreplication.infrastructure;
+
+public class CorfuReplicationManager {
+
+    /**
+     * Once determined this is a Lead Sender (on primary site), start log replication.
+     */
+    public static void startLogReplication() {
+
+    }
+
+    /**
+     * Once determined this is a Lead Receiver (on standby site), start log receivers.
+     */
+    public static void startLogReceive() {
+
+    }
+}
