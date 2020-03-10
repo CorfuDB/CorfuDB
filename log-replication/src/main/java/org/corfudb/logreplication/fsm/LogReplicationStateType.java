ccb836245c7c7b351e7e08bfdec2410b4fbe8f54
Anny Martinez <annymartinez.m@gmail.com>
2020-03-09 23:29:49 -0700

    Remove DataControl
    - Remove DataControl no longer needed, remove IN_REQUIRE_SNAPSHOT sync
    from LogReplication FSM. Fix tests to new FSM.

--- log-replication/src/main/java/org/corfudb/logreplication/fsm/LogReplicationStateType.java
+++ log-replication/src/main/java/org/corfudb/logreplication/fsm/LogReplicationStateType.java
@@ -9,7 +9,5 @@ public enum LogReplicationStateType {
     INITIALIZED,                    // Represents the init state of log replication FSM
     IN_SNAPSHOT_SYNC,               // Represents the state in which snapshot sync (full-sync) is being performed
     IN_LOG_ENTRY_SYNC,              // Represents the state in which log entry sync (delta-sync) is being performed
-    IN_REQUIRE_SNAPSHOT_SYNC,       // Represents the state in which a snapshot sync is required due to a cancel or
-                                    // error (e.g., Trim Exception)
     STOPPED                         // Represents the state where the FSM is stopped.
 }
