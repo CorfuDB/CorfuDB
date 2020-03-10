8133a40c548ef71f3d88fdf9a8822415ae4fba70
Anny Martinez <annymartinez.m@gmail.com>
2020-03-09 23:32:45 -0700

    Remove DataControl (#2469)
    - Remove DataControl no longer needed, remove IN_REQUIRE_SNAPSHOT sync
    from LogReplication FSM. Fix tests to new FSM.

--- log-replication/src/main/java/org/corfudb/logreplication/infrastructure/CorfuReplicationManager.java
+++ log-replication/src/main/java/org/corfudb/logreplication/infrastructure/CorfuReplicationManager.java
@@ -1,18 +1,68 @@
 package org.corfudb.logreplication.infrastructure;
 
+import org.corfudb.logreplication.DataControl;
+import org.corfudb.logreplication.DataSender;
+import org.corfudb.logreplication.SourceManager;
+import org.corfudb.logreplication.fsm.LogReplicationConfig;
+
 public class CorfuReplicationManager {
+
+    enum LogReplicationNegotiationResult {
+        SNAPSHOT_SYNC,
+        LOG_ENTRY_SYNC,
+        LEADERSHIP_LOST,
+        CONNECTION_LOST,
+        UNKNOWN
+    }
 
     /**
      * Once determined this is a Lead Sender (on primary site), start log replication.
      */
-    public static void startLogReplication() {
+    public void startLogReplication(String remoteEndpoint) {
+        // Initiate Log Replication Negotiation Protocol 
+        LogReplicationNegotiationResult negotiationResult = startNegotiation(remoteEndpoint);
 
+        switch (negotiationResult) {
+            case SNAPSHOT_SYNC:
+                startSnapshotSync();
+                break;
+            case LOG_ENTRY_SYNC:
+                startLogEntrySync();
+                break;
+            case LEADERSHIP_LOST:
+                break;
+            case CONNECTION_LOST:
+                break;
+            case UNKNOWN:
+                break;
+        }
     }
+
+
+    private void startSnapshotSync() {
+        // Start Log Replication
+        //Runtime
+        //      DataSender
+        //DataControl
+        //LogReplicationConfig
+        //SourceManager sourceManager = new SourceManager();
+        //sourceManager.
+    }
+
+    private void startLogEntrySync() {
+    }
+
 
     /**
      * Once determined this is a Lead Receiver (on standby site), start log receivers.
      */
-    public static void startLogReceive() {
+    public void startLogReceive() {
 
     }
+
+    private LogReplicationNegotiationResult startNegotiation(String remoteEndpoint) {
+        return LogReplicationNegotiationResult.UNKNOWN;
+    }
+
+
 }
