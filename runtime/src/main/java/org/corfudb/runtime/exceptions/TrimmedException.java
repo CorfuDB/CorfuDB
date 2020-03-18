07ae967782f659fad00ae171880c5dd7e0fd59ff
Anny Martinez <annymartinez.m@gmail.com>
2020-03-18 16:24:17 -0700

    Fix Trimmed Addresses Message

--- runtime/src/main/java/org/corfudb/runtime/exceptions/TrimmedException.java
+++ runtime/src/main/java/org/corfudb/runtime/exceptions/TrimmedException.java
@@ -30,7 +30,7 @@ public class TrimmedException extends LogUnitException {
     }
 
     public TrimmedException(List<Long> trimmed) {
-        super(String.format("Trimmed addresses {}", trimmed));
+        super(String.format("Trimmed addresses %s", trimmed));
         trimmedAddresses = trimmed;
     }
 
