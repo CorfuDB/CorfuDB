4f54ff8852d3d78e1b9de0824691008ef3fff733
maxi <maxi@vmware.com>
2020-05-05 14:17:29 -0700

    Using notficyAll interface intead of semphore.

--- log-replication/src/main/java/org/corfudb/logreplication/infrastructure/DefaultSiteManager.java
+++ log-replication/src/main/java/org/corfudb/logreplication/infrastructure/DefaultSiteManager.java
@@ -38,7 +38,7 @@ public class DefaultSiteManager extends CorfuReplicationSiteManagerAdapter {
     Thread thread = new Thread(siteManagerCallback);
 
     DefaultSiteManager() {
-        notification = new Semaphore(0);
+        //notification = new Semaphore(0);
     }
 
     public void start() {
@@ -104,7 +104,8 @@ public class DefaultSiteManager extends CorfuReplicationSiteManagerAdapter {
         }
     }
 
-    public CrossSiteConfiguration query() throws IOException {
+    @Override
+    public synchronized CrossSiteConfiguration query() throws IOException {
         return readConfig();
     }
 
@@ -147,12 +148,12 @@ public class DefaultSiteManager extends CorfuReplicationSiteManagerAdapter {
         @Override
         public void run() {
             boolean shouldChange = true;
-            while (true) {
+            while (shouldChange) {
                 try {
                     //System.out.print("\nwill sleep then change the site role");
                     sleep(changeInveral);
                     if (shouldChange) {
-                        siteManager.update(changePrimary(readConfig()));
+                        siteManager.update(changePrimary(siteManager.getCrossSiteConfiguration()));
                         shouldChange = false;
                     }
                 } catch (Exception e) {
