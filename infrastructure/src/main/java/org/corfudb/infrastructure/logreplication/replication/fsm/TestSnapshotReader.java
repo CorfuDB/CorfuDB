4ed004838dd976089819b0ab98e82c59f05d7b6b
Nan Zhang <znan@vmware.com>
2020-07-01 18:36:10 -0700

    Fix topology config change logic (#2591)
    * Fix site change

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/fsm/TestSnapshotReader.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/fsm/TestSnapshotReader.java
@@ -19,7 +19,7 @@ import java.util.UUID;
  */
 public class TestSnapshotReader implements SnapshotReader {
 
-    private long siteEpoch = 0;
+    private long topologyConfigId = 0;
 
     private final int FIRST_ADDRESS = 0;
 
@@ -45,7 +45,7 @@ public class TestSnapshotReader implements SnapshotReader {
             Object data = runtime.getAddressSpaceView().read((long)i).getPayload(runtime);
             // For testing we don't have access to the snapshotSyncId so we fill in with a random UUID
             // and overwrite it in the TestDataSender with the correct one, before sending the message out
-            LogReplicationEntryMetadata metadata = new LogReplicationEntryMetadata(MessageType.SNAPSHOT_MESSAGE, siteEpoch,
+            LogReplicationEntryMetadata metadata = new LogReplicationEntryMetadata(MessageType.SNAPSHOT_MESSAGE, topologyConfigId,
                     snapshotRequestId, i, config.getNumEntries(), UUID.randomUUID());
             messages.add(new LogReplicationEntry(metadata, (byte[])data));
             globalIndex++;
@@ -60,8 +60,8 @@ public class TestSnapshotReader implements SnapshotReader {
     }
 
     @Override
-    public void setSiteEpoch(long siteEpoch) {
-        this.siteEpoch = siteEpoch;
+    public void setTopologyConfigId(long topologyConfigId) {
+        this.topologyConfigId = topologyConfigId;
     }
 
     public void setBatchSize(int batchSize) {
