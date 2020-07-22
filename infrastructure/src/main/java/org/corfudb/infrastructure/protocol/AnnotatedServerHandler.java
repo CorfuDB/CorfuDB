b10a76076d3da0d432e90d2a0bb5e5e78135bd9c
zfrenette <66146392+zfrenette@users.noreply.github.com>
2020-07-21 20:39:21 -0400

    Initial Commit -- Primarily includes some server-side infrastructure for new Protobuf RPC message types.

---
+++ infrastructure/src/main/java/org/corfudb/infrastructure/protocol/AnnotatedServerHandler.java
@@ -0,0 +1,20 @@
+package org.corfudb.infrastructure.protocol;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.corfudb.common.protocol.proto.CorfuProtocol.MessageType;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface AnnotatedServerHandler {
+    /**
+     * Returns the message type
+     * @return the type of Corfu message request
+     */
+    MessageType type();
+}
