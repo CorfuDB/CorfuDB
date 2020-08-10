550933841d0dc0c5a6048746ac2f09045a9469c0
Anny Martinez <annymartinez.m@gmail.com>
2020-08-10 12:32:56 -0700

    Fix testSingleLockClient intermittent failure

--- test/src/test/java/org/corfudb/utils/TestLockListener.java
+++ test/src/test/java/org/corfudb/utils/TestLockListener.java
@@ -6,6 +6,10 @@ import org.corfudb.common.util.ObservableValue;
 import org.corfudb.utils.lock.LockDataTypes;
 import org.corfudb.utils.lock.LockListener;
 
+/**
+ * This lock listener implementation is for testing purposes and updates
+ * an observable for each acquired and revoked event.
+ */
 @Slf4j
 public class TestLockListener implements LockListener {
 
@@ -21,16 +25,16 @@ public class TestLockListener implements LockListener {
     @Override
     public void lockAcquired(LockDataTypes.LockId lockId) {
         lockAcquiredCount++;
-        log.debug("***** Lock has been acquired : " + lockAcquiredCount + " for: " + lockId.getLockName());
+        log.debug("Lock has been acquired for {}, count={}", lockId.getLockName(), lockAcquiredCount);
         // Update observable value, indicating lock has been acquired
         lockAcquired.setValue(lockAcquiredCount);
     }
 
     @Override
     public void lockRevoked(LockDataTypes.LockId lockId) {
-        log.debug("***** Lock has been revoked for: " + lockId.getLockName());
-        // Update observable value, indicating lock has been revoked
         lockRevokedCount++;
-        lockAcquired.setValue(lockRevokedCount);
+        log.debug("Lock has been revoked for {}, count={}", lockId.getLockName(), lockRevokedCount);
+        // Update observable value, indicating lock has been revoked
+        lockRevoked.setValue(lockRevokedCount);
     }
 }
