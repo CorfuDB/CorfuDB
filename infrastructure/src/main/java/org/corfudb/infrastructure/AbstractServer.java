7e04b42b79e53d20ce335fc4c7917c28b4f6094a
zfrenette <66146392+zfrenette@users.noreply.github.com>
2020-08-10 22:18:44 -0400

    Initial commit

--- infrastructure/src/main/java/org/corfudb/infrastructure/AbstractServer.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/AbstractServer.java
@@ -2,6 +2,11 @@ package org.corfudb.infrastructure;
 
 import io.netty.channel.ChannelHandlerContext;
 import lombok.extern.slf4j.Slf4j;
+import org.corfudb.common.protocol.API;
+import org.corfudb.common.protocol.proto.CorfuProtocol.Header;
+import org.corfudb.common.protocol.proto.CorfuProtocol.Request;
+import org.corfudb.common.protocol.proto.CorfuProtocol.Response;
+import org.corfudb.infrastructure.RequestHandlerMethods;
 import org.corfudb.protocols.wireprotocol.CorfuMsg;
 import org.corfudb.protocols.wireprotocol.CorfuMsgType;
 
@@ -26,6 +31,16 @@ public abstract class AbstractServer {
     public abstract HandlerMethods getHandler();
 
     /**
+     * Get the request handlers for this instance.
+     *
+     * @return The request handler methods.
+     */
+    public RequestHandlerMethods getHandlerMethods() {
+        //TODO: Make abstract once other servers are implemented.
+        return null;
+    }
+
+    /**
      * Seal the server with the epoch.
      *
      * @param epoch Epoch to seal with
@@ -37,6 +52,16 @@ public abstract class AbstractServer {
     public abstract boolean isServerReadyToHandleMsg(CorfuMsg msg);
 
     /**
+     * Determine if the server is ready to handle a request.
+     * @param requestHeader The incoming request message header.
+     * @return True if the server is ready to handle this request, and false otherwise.
+     */
+    public boolean isServerReadyToHandleReq(Header requestHeader) {
+        //TODO: Make abstract once other servers are implemented
+        return false;
+    }
+
+    /**
      * A stub that handlers can override to manage their threading, otherwise
      * the requests will be executed on the IO threads
      * @param msg
@@ -46,6 +71,18 @@ public abstract class AbstractServer {
     protected void processRequest(CorfuMsg msg, ChannelHandlerContext ctx, IServerRouter r) {
         getHandler().handle(msg, ctx, r);
     }
+
+    /**
+     * A stub that handlers can override to manage their threading, otherwise
+     * the requests will be executed on the IO threads
+     * @param req An incoming request message.
+     * @param ctx The channel handler context.
+     * @param r The router that took in the request.
+     */
+    protected void processRequest(Request req, ChannelHandlerContext ctx, IRequestRouter r) {
+        getHandlerMethods().handle(req, ctx, r);
+    }
+
 
     /**
      * Handle a incoming Netty message.
@@ -66,6 +103,32 @@ public abstract class AbstractServer {
         }
 
         processRequest(msg, ctx, r);
+    }
+
+    /**
+     * Handle a incoming request message.
+     *
+     * @param req An incoming request message.
+     * @param ctx The channel handler context.
+     * @param r   The router that took in the request message.
+     */
+    public final void handleRequest(Request req, ChannelHandlerContext ctx, IRequestRouter r) {
+        if (getState() == ServerState.SHUTDOWN) {
+            log.warn("handleRequest: Server received {} but is already shutdown.", req.getHeader().getType().toString());
+            return;
+        }
+
+        if(!isServerReadyToHandleReq(req.getHeader())) {
+            r.sendResponse(getNotReadyError(req.getHeader()), ctx);
+            return;
+        }
+
+        processRequest(req, ctx, r);
+    }
+
+    private Response getNotReadyError(Header requestHeader) {
+        return API.getErrorResponseNoPayload(API.generateResponseHeader(requestHeader, false, true),
+                API.getNotReadyServerError("Server is not ready to handle request messages."));
     }
 
     /**
--- infrastructure/src/main/java/org/corfudb/infrastructure/BaseServer.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/BaseServer.java
@@ -4,6 +4,13 @@ import io.netty.channel.ChannelHandlerContext;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.corfudb.common.protocol.API;
+import org.corfudb.common.protocol.proto.CorfuProtocol;
+import org.corfudb.common.protocol.proto.CorfuProtocol.Header;
+import org.corfudb.common.protocol.proto.CorfuProtocol.Request;
+import org.corfudb.common.protocol.proto.CorfuProtocol.Response;
+import org.corfudb.infrastructure.RequestHandler;
+import org.corfudb.infrastructure.RequestHandlerMethods;
 import org.corfudb.protocols.wireprotocol.CorfuMsg;
 import org.corfudb.protocols.wireprotocol.CorfuMsgType;
 import org.corfudb.protocols.wireprotocol.CorfuPayloadMsg;
@@ -31,8 +38,19 @@ public class BaseServer extends AbstractServer {
     @Getter
     private final HandlerMethods handler = HandlerMethods.generateHandler(MethodHandles.lookup(), this);
 
+    /** RequestHandlerMethods for the base server. */
+    private final RequestHandlerMethods handlerMethods = RequestHandlerMethods.generateHandler(MethodHandles.lookup(), this);
+
+    @Override
+    public RequestHandlerMethods getHandlerMethods() { return handlerMethods; }
+
     @Override
     public boolean isServerReadyToHandleMsg(CorfuMsg msg) {
+        return getState() == ServerState.READY;
+    }
+
+    @Override
+    public boolean isServerReadyToHandleReq(Header requestHeader) {
         return getState() == ServerState.READY;
     }
 
@@ -45,6 +63,11 @@ public class BaseServer extends AbstractServer {
     @Override
     protected void processRequest(CorfuMsg msg, ChannelHandlerContext ctx, IServerRouter r) {
         executor.submit(() -> getHandler().handle(msg, ctx, r));
+    }
+
+    @Override
+    protected void processRequest(Request req, ChannelHandlerContext ctx, IRequestRouter r) {
+        executor.submit(() -> getHandlerMethods().handle(req, ctx, r));
     }
 
     @Override
@@ -63,6 +86,23 @@ public class BaseServer extends AbstractServer {
     @ServerHandler(type = CorfuMsgType.PING)
     private void ping(CorfuMsg msg, ChannelHandlerContext ctx, IServerRouter r) {
         r.sendResponse(ctx, msg, CorfuMsgType.PONG.msg());
+    }
+
+    /**
+     * Respond to a ping request.
+     *
+     * @param req   The incoming request message.
+     * @param ctx   The channel context.
+     * @param r     The server router.
+     */
+    @RequestHandler(type = CorfuProtocol.MessageType.PING)
+    private void handlePing(Request req, ChannelHandlerContext ctx, IRequestRouter r) {
+        log.info("handlePing[{}]: Ping message received from {} {}", req.getHeader().getRequestId(),
+                req.getHeader().getClientId().getMsb(), req.getHeader().getClientId().getLsb());
+
+        Header responseHeader = API.generateResponseHeader(req.getHeader(), false, true);
+        Response response = API.getPingResponse(responseHeader);
+        r.sendResponse(response, ctx);
     }
 
     /**
@@ -125,6 +165,40 @@ public class BaseServer extends AbstractServer {
     }
 
     /**
+     * Respond to a epoch change request.
+     * This method also executes sealing logic on each individual server type.
+     * @param req The incoming request message.
+     * @param ctx The channel context.
+     * @param r The server router.
+     */
+    @RequestHandler(type = CorfuProtocol.MessageType.SEAL)
+    private synchronized void handleSeal(Request req, ChannelHandlerContext ctx, IRequestRouter r) {
+        try {
+            long epoch = req.getSealRequest().getEpoch();
+            String remoteHostAddress;
+            try {
+                remoteHostAddress = ((InetSocketAddress)ctx.channel().remoteAddress()).getAddress().getHostAddress();
+            } catch(NullPointerException ex) {
+                remoteHostAddress = "unavailable";
+            }
+
+            log.info("handleSeal[{}]: Received SEAL from (clientId={}:{}), moving to new epoch {},",
+                    req.getHeader().getRequestId(), req.getHeader().getClientId(), remoteHostAddress, epoch);
+
+            // TODO(Zach):
+            // serverContext.setServerEpoch(epoch, r);
+            Header responseHeader = API.generateResponseHeader(req.getHeader(), false, true);
+            Response response = API.getSealResponse(responseHeader);
+            r.sendResponse(response, ctx);
+        } catch (WrongEpochException e) {
+            log.debug("handleSeal[{}]: Rejected SEAL current={}, requested={}",
+                    req.getHeader().getRequestId(), e.getCorrectEpoch(), req.getSealRequest().getEpoch());
+
+            r.sendWrongEpochError(req.getHeader(), ctx);
+        }
+    }
+
+    /**
      * Reset the JVM. This mechanism leverages that corfu_server runs in a bash script
      * which monitors the exit code of Corfu. If the exit code is 100, then it resets
      * the server and DELETES ALL EXISTING DATA.
@@ -141,6 +215,25 @@ public class BaseServer extends AbstractServer {
     }
 
     /**
+     * Reset the JVM. This mechanism leverages that corfu_server runs in a bash script
+     * which monitors the exit code of Corfu. If the exit code is 100, then it resets
+     * the server and DELETES ALL EXISTING DATA.
+     * @param req The incoming request message.
+     * @param ctx The channel context.
+     * @param r The server router.
+     */
+    @RequestHandler(type = CorfuProtocol.MessageType.RESET)
+    private void handleReset(Request req, ChannelHandlerContext ctx, IRequestRouter r) {
+        log.warn("handleReset[{}]: Remote reset requested from client {}",
+                req.getHeader().getRequestId(), req.getHeader().getClientId());
+
+        Header responseHeader = API.generateResponseHeader(req.getHeader(), false, true);
+        Response response = API.getResetResponse(responseHeader);
+        r.sendResponse(response, ctx);
+        CorfuServer.restartServer(true);
+    }
+
+    /**
      * Restart the JVM. This mechanism leverages that corfu_server runs in a bash script
      * which monitors the exit code of Corfu. If the exit code is 200, then it restarts
      * the server.
@@ -153,6 +246,26 @@ public class BaseServer extends AbstractServer {
     private void doRestart(CorfuMsg msg, ChannelHandlerContext ctx, IServerRouter r) {
         log.warn("Remote restart requested from client {}", msg.getClientID());
         r.sendResponse(ctx, msg, CorfuMsgType.ACK.msg());
+        CorfuServer.restartServer(false);
+    }
+
+    /**
+     * Restart the JVM. This mechanism leverages that corfu_server runs in a bash script
+     * which monitors the exit code of Corfu. If the exit code is 200, then it restarts
+     * the server.
+     *
+     * @param req   The incoming request message.
+     * @param ctx   The channel context.
+     * @param r     The server router.
+     */
+    @RequestHandler(type = CorfuProtocol.MessageType.RESTART)
+    private void handleRestart(Request req, ChannelHandlerContext ctx, IRequestRouter r) {
+        log.warn("handleRestart[{}]: Remote restart requested from client {}",
+                req.getHeader().getRequestId(), req.getHeader().getClientId());
+
+        Header responseHeader = API.generateResponseHeader(req.getHeader(), false, true);
+        Response response = API.getRestartResponse(responseHeader);
+        r.sendResponse(response, ctx);
         CorfuServer.restartServer(false);
     }
 }
