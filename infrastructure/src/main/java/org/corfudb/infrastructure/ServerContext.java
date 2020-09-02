45a4b7eef815d28daecaee2e8e61ee41414a3a5b
Maithem <Maithem@users.noreply.github.com>
2020-09-02 02:17:27 -0700

    Remove The Boss EventLoopGroup
    This patch makes both boss and worker threads use the same
    EventLoopGroup. Since CorfuDB clients tend to have long lived
    connections and the rate at which new connections are made is
    relatively low reusing the same thread pool for both tasks
    shouldn't impact performance.

--- infrastructure/src/main/java/org/corfudb/infrastructure/ServerContext.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/ServerContext.java
@@ -1,9 +1,30 @@
 package org.corfudb.infrastructure;
 
+import static org.corfudb.infrastructure.logreplication.LogReplicationConfig.DEFAULT_MAX_NUM_MSG_PER_BATCH;
+import static org.corfudb.infrastructure.logreplication.LogReplicationConfig.MAX_DATA_MSG_SIZE_SUPPORTED;
+import static org.corfudb.util.MetricsUtils.isMetricsReportingSetUp;
+
+
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.channel.EventLoopGroup;
+import java.io.File;
+import java.nio.file.Files;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.annotation.Nonnull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,28 +47,9 @@ import org.corfudb.util.NodeLocator;
 import org.corfudb.util.UuidUtils;
 import org.corfudb.utils.lock.Lock;
 
-import javax.annotation.Nonnull;
-import java.io.File;
-import java.nio.file.Files;
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static org.corfudb.infrastructure.logreplication.LogReplicationConfig.MAX_DATA_MSG_SIZE_SUPPORTED;
-import static org.corfudb.infrastructure.logreplication.LogReplicationConfig.DEFAULT_MAX_NUM_MSG_PER_BATCH;
-import static org.corfudb.util.MetricsUtils.isMetricsReportingSetUp;
 
 /**
  * Server Context:
@@ -142,9 +144,6 @@ public class ServerContext implements AutoCloseable {
     private final EventLoopGroup clientGroup;
 
     @Getter
-    private final EventLoopGroup bossGroup;
-
-    @Getter
     private final EventLoopGroup workerGroup;
 
     @Getter (AccessLevel.PACKAGE)
@@ -179,11 +178,9 @@ public class ServerContext implements AutoCloseable {
         if (providedEventLoops) {
             clientGroup = getServerConfig(EventLoopGroup.class, "client");
             workerGroup = getServerConfig(EventLoopGroup.class, "worker");
-            bossGroup = getServerConfig(EventLoopGroup.class, "boss");
         } else {
             clientGroup = getNewClientGroup();
             workerGroup = getNewWorkerGroup();
-            bossGroup = getNewBossGroup();
         }
 
         nodeLocator = NodeLocator
@@ -668,21 +665,6 @@ public class ServerContext implements AutoCloseable {
     }
 
     /**
-     * Get a new "boss" group, which services (accepts) incoming connections.
-     *
-     * @return A boss group.
-     */
-    private EventLoopGroup getNewBossGroup() {
-        final ThreadFactory threadFactory = new ThreadFactoryBuilder()
-                .setNameFormat(getThreadPrefix() + "accept-%d")
-                .build();
-        EventLoopGroup group = getChannelImplementation().getGenerator()
-                .generate(1, threadFactory);
-        log.info("getBossGroup: Type {}", group.getClass().getSimpleName());
-        return group;
-    }
-
-    /**
      * Get a new "worker" group, which services incoming requests.
      *
      * @return A worker group.
@@ -754,11 +736,6 @@ public class ServerContext implements AutoCloseable {
         // Shutdown the active event loops unless they were provided to us
         if (!getChannelImplementation().equals(ChannelImplementation.LOCAL)) {
             clientGroup.shutdownGracefully(
-                    params.getNettyShutdownQuitePeriod(),
-                    params.getNettyShutdownTimeout(),
-                    TimeUnit.MILLISECONDS
-            );
-            bossGroup.shutdownGracefully(
                     params.getNettyShutdownQuitePeriod(),
                     params.getNettyShutdownTimeout(),
                     TimeUnit.MILLISECONDS
