15b2570b324f9477ecef01e3addf92b2d7af44d4
zfrenette <66146392+zfrenette@users.noreply.github.com>
2020-10-13 16:32:17 -0400

    Implementation of LogUnit service API methods, except for Read, Write and RangeWrite.

--- runtime/src/main/java/org/corfudb/protocols/CorfuProtocolCommon.java
+++ runtime/src/main/java/org/corfudb/protocols/CorfuProtocolCommon.java
@@ -1,12 +1,18 @@
 package org.corfudb.protocols;
 
+import com.google.protobuf.ByteString;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.protocols.wireprotocol.Token;
 import org.corfudb.runtime.proto.Common.LayoutMsg;
+import org.corfudb.runtime.proto.Common.StreamAddressSpaceMsg;
 import org.corfudb.runtime.proto.Common.TokenMsg;
 import org.corfudb.runtime.proto.Common.UuidMsg;
 import org.corfudb.runtime.view.Layout;
+import org.corfudb.runtime.view.stream.StreamAddressSpace;
+import org.roaringbitmap.longlong.Roaring64NavigableMap;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.util.UUID;
 
 @Slf4j
@@ -37,5 +43,35 @@ public class CorfuProtocolCommon {
                 .setEpoch(token.getEpoch())
                 .setSequence(token.getSequence())
                 .build();
+    }
+
+    public static StreamAddressSpaceMsg getStreamAddressSpaceMsg(StreamAddressSpace addressSpace) {
+        StreamAddressSpaceMsg.Builder addressSpaceMsgBuilder = StreamAddressSpaceMsg.newBuilder();
+        addressSpaceMsgBuilder.setTrimMark(addressSpace.getTrimMark());
+
+        try(ByteString.Output bso = ByteString.newOutput()) {
+            try(DataOutputStream dos = new DataOutputStream(bso)) {
+                addressSpace.getAddressMap().serialize(dos);
+                addressSpaceMsgBuilder.setAddressMap(bso.toByteString());
+            }
+        } catch (Exception ex) {
+            log.error("getStreamAddressSpaceMsg: error=[{}, {}] " +
+                    "while serializing roaring64NavigableMap", ex, ex.getCause());
+        }
+
+        return addressSpaceMsgBuilder.build();
+    }
+
+    public static StreamAddressSpace getStreamAddressSpace(StreamAddressSpaceMsg msg) {
+        Roaring64NavigableMap roaring64NavigableMap = new Roaring64NavigableMap();
+
+        try(DataInputStream dis = new DataInputStream(msg.getAddressMap().newInput())) {
+            roaring64NavigableMap.deserialize(dis);
+        } catch (Exception ex) {
+            log.error("getStreamAddressSpace: error=[{}, {}] " +
+                    "while deserializing roaring64NavigableMap", ex, ex.getCause());
+        }
+
+        return new StreamAddressSpace(msg.getTrimMark(), roaring64NavigableMap);
     }
 }
