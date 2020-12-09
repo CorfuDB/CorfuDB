d30836f8d4a28e98d7b4ca3957206497aaf918f9
Maithem <Maithem@users.noreply.github.com>
2020-12-09 14:54:04 -0800

    Collect Metrics for JVM Safe Point Stats (#2839)
    Excessive JVM pauses can adversely affect the fault detection system
    rendering the cluster instable because of unnecessary reconfigurations.
    Safepoint metrics are crucial for debugging such problems, so this patch
    adds safe point stats to the standard collection process.

---
+++ common/src/main/java/org/corfudb/common/metrics/micrometer/JVMMetrics.java
@@ -0,0 +1,44 @@
+package org.corfudb.common.metrics.micrometer;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.Optional;
+import lombok.Data;
+import lombok.Getter;
+import sun.management.HotspotRuntimeMBean;
+import sun.management.ManagementFactoryHelper;
+
+/** Provides JVM level metrics. */
+
+public final class JVMMetrics {
+
+    @Getter(lazy=true)
+    private final static HotspotRuntimeMBean runtimeMBean = ManagementFactoryHelper.getHotspotRuntimeMBean();
+
+    @Data
+    private static class SafePointStats {
+        long safepointTime;
+        long safepointCount;
+    }
+
+    public static void register(Optional<MeterRegistry> metricsRegistry) {
+
+        if (metricsRegistry.isPresent()) {
+            SafePointStats safePointStats = new SafePointStats();
+
+            Gauge.builder("jvm.safe_point_time", safePointStats, data -> {
+                long current = getRuntimeMBean().getTotalSafepointTime();
+                long delta = current - data.safepointTime;
+                data.safepointTime = current;
+                return delta;
+            }).strongReference(true).register(metricsRegistry.get());
+
+            Gauge.builder("jvm.safe_point_count", safePointStats, data -> {
+                long current = getRuntimeMBean().getSafepointCount();
+                long delta = current - data.safepointCount;
+                data.safepointCount = current;
+                return delta;
+            }).strongReference(true).register(metricsRegistry.get());
+        }
+    }
+}
