CorfuDB/CorfuDB

commit d90dc8d55d1d4aecf8dfdd75cad6d7640b031671

Server Parameter Parsing Fix (#2880)
This patch fixes a couple of bugs related to argument parsing
for server options. Since Docopt interprets Docopt params as
strings, an extra conversion is needed on the string representation
of the integer param.

infrastructure/src/main/java/org/corfudb/infrastructure/ServerContext.java
@@ -182,23 +182,18 @@ public class ServerContext implements AutoCloseable {
     }
 
     int getBaseServerThreadCount() {
-        Integer threadCount = getServerConfig(Integer.class, "--base-server-threads");
-        return threadCount == null ? 1 : threadCount;
-    }
-
-    int getLayoutServerThreadCount() {
-        Integer threadCount = getServerConfig(Integer.class, "--layout-server-threads");
-        return threadCount == null ? 1 : threadCount;
+        int threadCount = Integer.parseInt(getServerConfig(String.class, "--base-server-threads"));
+        return threadCount == 0 ? 1 : threadCount;
     }
 
     public int getLogunitThreadCount() {
-        Integer threadCount = getServerConfig(Integer.class, "--logunit-threads");
-        return threadCount == null ? Runtime.getRuntime().availableProcessors() * 2 : threadCount;
+        int threadCount = Integer.parseInt(getServerConfig(String.class, "--logunit-threads"));
+        return threadCount == 0 ? Runtime.getRuntime().availableProcessors() * 2 : threadCount;
     }
 
     public int getManagementServerThreadCount() {
-        Integer threadCount = getServerConfig(Integer.class, "--management-server-threads");
-        return threadCount == null ? 4 : threadCount;
+        int threadCount = Integer.parseInt(getServerConfig(String.class, "--management-server-threads"));
+        return threadCount == 0 ? 4 : threadCount;
     }
 
     public String getPluginConfigFilePath() {

test/src/test/java/org/corfudb/infrastructure/ServerContextBuilder.java
@@ -58,6 +58,9 @@ public class ServerContextBuilder {
                 .put("--single", single)
                 .put("--memory", memory)
                 .put("--Threads", numThreads)
+                .put("--base-server-threads", numThreads)
+                .put("--management-server-threads", numThreads)
+                .put("--logunit-threads", numThreads)
                 .put("--HandshakeTimeout", handshakeTimeout)
                 .put("--sequencer-cache-size", seqCache)
                 .put("--log-size-quota-percentage", logSizeLimitPercentage)

common/pom.xml
@@ -32,16 +32,6 @@
             <version>1.5.5</version>
         </dependency>
         <dependency>
-            <groupId>io.prometheus</groupId>
-            <artifactId>simpleclient_servlet</artifactId>
-            <version>0.5.0</version>
-        </dependency>
-        <dependency>
-            <groupId>org.eclipse.jetty</groupId>
-            <artifactId>jetty-webapp</artifactId>
-            <version>9.4.34.v20201102</version>
-        </dependency>
-        <dependency>
             <groupId>net.jpountz.lz4</groupId>
             <artifactId>lz4</artifactId>
             <version>1.3.0</version>

infrastructure/src/main/java/org/corfudb/infrastructure/CorfuServer.java
@@ -51,7 +51,7 @@ public class CorfuServer {
                     + "[--max-replication-data-message-size=<msg-size>] "
                     + "[-c <ratio>] [-d <level>] [-p <seconds>] "
                     + "[--plugin=<plugin-config-file-path>]"
-                    + "[--layout-server-threads=<layout_server_threads>] [--base-server-threads=<base_server_threads>] "
+                    + "[--base-server-threads=<base_server_threads>] "
                     + "[--log-size-quota-percentage=<max_log_size_percentage>]"
                     + "[--logunit-threads=<logunit_threads>] [--management-server-threads=<management_server_threads>]"
                     + "[-e [-u <keystore> -f <keystore_password_file>] [-r <truststore> -w <truststore_password_file>] "
@@ -156,20 +156,17 @@ public class CorfuServer {
                     + "                                                                          "
                     + "              [default: TLSv1.1,TLSv1.2].\n"
                     + " --base-server-threads=<base_server_threads>                              "
-                    + "              Number of threads dedicated for the base server.\n          "
+                    + "              Number of threads dedicated for the base server [default: 0].\n"
                     + " --log-size-quota-percentage=<max_log_size_percentage>                    "
                     + "              The max size as percentage of underlying file-store size.\n "
                     + "              If this limit is exceeded "
                     + "              write requests will be rejected [default: 100.0].\n         "
                     + "                                                                          "
-                    + " --layout-server-threads=<layout_server_threads>                          "
-                    + "              Number of threads dedicated for the layout server.\n        "
-                    + "                                                                          "
                     + " --management-server-threads=<management_server_threads>                  "
-                    + "              Number of threads dedicated for the management server.\n"
+                    + "              Number of threads dedicated for the management server [default: 0].\n"
                     + "                                                                          "
                     + " --logunit-threads=<logunit_threads>                  "
-                    + "              Number of threads dedicated for the logunit server.\n"
+                    + "              Number of threads dedicated for the logunit server [default: 0].\n"
                     + " --metrics                                                                "
                     + "              Enable metrics provider.\n                                  "
                     + " --snapshot-batch=<batch-size>                                            "

infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/CorfuInterClusterReplicationServer.java
@@ -52,7 +52,7 @@ public class CorfuInterClusterReplicationServer implements Runnable {
                     + "[--lock-lease=<lease-duration>]"
                     + "[-c <ratio>] [-d <level>] [-p <seconds>] "
                     + "[--plugin=<plugin-config-file-path>]"
-                    + "[--layout-server-threads=<layout_server_threads>] [--base-server-threads=<base_server_threads>] "
+                    + "[--base-server-threads=<base_server_threads>] "
                     + "[--log-size-quota-percentage=<max_log_size_percentage>]"
                     + "[--logunit-threads=<logunit_threads>] [--management-server-threads=<management_server_threads>]"
                     + "[-e [-u <keystore> -f <keystore_password_file>] [-r <truststore> -w <truststore_password_file>] "
@@ -154,20 +154,17 @@ public class CorfuInterClusterReplicationServer implements Runnable {
                     + "                                                                          "
                     + "              [default: TLSv1.1,TLSv1.2].\n"
                     + " --base-server-threads=<base_server_threads>                              "
-                    + "              Number of threads dedicated for the base server.\n          "
+                    + "              Number of threads dedicated for the base server [default: 0].\n"
                     + " --log-size-quota-percentage=<max_log_size_percentage>                    "
                     + "              The max size as percentage of underlying file-store size.\n "
                     + "              If this limit is exceeded "
                     + "              write requests will be rejected [default: 100.0].\n         "
                     + "                                                                          "
-                    + " --layout-server-threads=<layout_server_threads>                          "
-                    + "              Number of threads dedicated for the layout server.\n        "
-                    + "                                                                          "
                     + " --management-server-threads=<management_server_threads>                  "
-                    + "              Number of threads dedicated for the management server.\n"
+                    + "              Number of threads dedicated for the management server [default: 0].\n"
                     + "                                                                          "
                     + " --logunit-threads=<logunit_threads>                  "
-                    + "              Number of threads dedicated for the logunit server.\n"
+                    + "              Number of threads dedicated for the logunit server [default: 0].\n"
                     + " --metrics                                                                "
                     + "              Enable metrics provider.\n                                  "
                     + " --snapshot-batch=<batch-size>                                            "

commit 83fcba02af00ebfdd8eb8bedc3c81ccb3f221f7b

Fix server thread agurment parsing
Argument parsing should always operate under the assumption that an
arugment might be invalid or missing. More specifically, this patch
resolves a regression that was introduced by PR #2880.

infrastructure/src/main/java/org/corfudb/infrastructure/LogUnitServer.java
@@ -135,7 +135,7 @@ public class LogUnitServer extends AbstractServer {
     public LogUnitServer(ServerContext serverContext, LogUnitServerInitializer serverInitializer) {
         this.serverContext = serverContext;
         config = LogUnitServerConfig.parse(serverContext.getServerConfig());
-        executor = serverContext.getExecutorService(serverContext.getLogunitThreadCount(), "LogUnit-");
+        executor = serverContext.getExecutorService(serverContext.getLogUnitThreadCount(), "LogUnit-");
 
         if (config.isMemoryMode()) {
             log.warn("Log unit opened in-memory mode (Maximum size={}). "

infrastructure/src/main/java/org/corfudb/infrastructure/ServerContext.java
@@ -182,18 +182,18 @@ public class ServerContext implements AutoCloseable {
     }
 
     int getBaseServerThreadCount() {
-        int threadCount = Integer.parseInt(getServerConfig(String.class, "--base-server-threads"));
-        return threadCount == 0 ? 1 : threadCount;
+        Optional<String> threadCount = getServerConfig("--base-server-threads");
+        return threadCount.map(Integer::parseInt).orElse(1);
     }
 
-    public int getLogunitThreadCount() {
-        int threadCount = Integer.parseInt(getServerConfig(String.class, "--logunit-threads"));
-        return threadCount == 0 ? Runtime.getRuntime().availableProcessors() * 2 : threadCount;
+    public int getLogUnitThreadCount() {
+        Optional<String> threadCount = getServerConfig("--logunit-threads");
+        return threadCount.map(Integer::parseInt).orElse(Runtime.getRuntime().availableProcessors() * 2);
     }
 
     public int getManagementServerThreadCount() {
-        int threadCount = Integer.parseInt(getServerConfig(String.class, "--management-server-threads"));
-        return threadCount == 0 ? 4 : threadCount;
+        Optional<String> threadCount = getServerConfig("--management-server-threads");
+        return threadCount.map(Integer::parseInt).orElse(4);
     }
 
     public String getPluginConfigFilePath() {
@@ -375,6 +375,17 @@ public class ServerContext implements AutoCloseable {
         return (T) getServerConfig().get(optionName);
     }
 
+    /**
+     * Get a field from the server configuration map.
+     *
+     * @param optionName    The name of the option to retrieve.
+     * @param <T>           The type of the field to return.
+     * @return              The field with the give option name.
+     */
+    @SuppressWarnings("unchecked")
+    public <T> Optional<T> getServerConfig(String optionName) {
+        return Optional.ofNullable((T) getServerConfig().get(optionName));
+    }
 
     /**
      * Install a single node layout if and only if no layout is currently installed.

test/src/test/java/org/corfudb/infrastructure/ServerContextBuilder.java
@@ -58,9 +58,6 @@ public class ServerContextBuilder {
                 .put("--single", single)
                 .put("--memory", memory)
                 .put("--Threads", numThreads)
-                .put("--base-server-threads", numThreads)
-                .put("--management-server-threads", numThreads)
-                .put("--logunit-threads", numThreads)
                 .put("--HandshakeTimeout", handshakeTimeout)
                 .put("--sequencer-cache-size", seqCache)
                 .put("--log-size-quota-percentage", logSizeLimitPercentage)