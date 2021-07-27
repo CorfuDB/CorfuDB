7835f4d500340616ea6a6da412756ec34809d3d0
nvaishampayan517 <neiluv@icloud.com>
2021-07-27 12:48:53 -0700

    refactored impl to remove ByteBuffer -fixed possible bug with ByteBuffer.array() -used System.arrayCopy instead for fast performance

--- infrastructure/src/main/java/org/corfudb/infrastructure/remotecorfutable/utils/KeyEncodingUtil.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/remotecorfutable/utils/KeyEncodingUtil.java
@@ -1,11 +1,10 @@
 package org.corfudb.infrastructure.remotecorfutable.utils;
 
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Longs;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import javax.xml.crypto.dsig.keyinfo.KeyValue;
-import java.nio.ByteBuffer;
 
 /**
  * This utility class aids with constructing a RocksDB store key from a RemoteCorfuTable key and a timestamp, and
@@ -15,23 +14,17 @@ import java.nio.ByteBuffer;
  * <p>Created by nvaishampayan517 on 7/26/21.
  */
 public final class KeyEncodingUtil {
-    //TODO: explore reusing byte buffer -> maybe have pool of ByteBuffers?
-    //TODO: possibly remove byte buffers entirely -> work only with loops and byte[]
     //prevent instantiation
     private KeyEncodingUtil() {}
 
     /**
      * Creates RocksDB key from RemoteCorfuTable key and timestamp.
      * @param encodedRemoteCorfuTableKey Encoded key from client-side table.
-     * @param encodedKeySize Size of encoded key.
      * @param timestamp Versioning timestamp of request.
      * @return Key for storage in RocksDB.
      */
-    public static byte[] constructDatabaseKey(byte[] encodedRemoteCorfuTableKey, int encodedKeySize, long timestamp) {
-        int bufferSize = Integer.BYTES + encodedKeySize + Long.BYTES;
-        ByteBuffer keyBuffer = ByteBuffer.allocate(bufferSize);
-        keyBuffer.putInt(encodedKeySize).put(encodedRemoteCorfuTableKey, 0, encodedKeySize).putLong(timestamp);
-        return keyBuffer.array();
+    public static byte[] constructDatabaseKey(byte[] encodedRemoteCorfuTableKey, long timestamp) {
+        return Bytes.concat(encodedRemoteCorfuTableKey, Longs.toByteArray(timestamp));
     }
 
     /**
@@ -39,13 +32,48 @@ public final class KeyEncodingUtil {
      * @param rocksDBKey Encoded database key.
      * @return VersionedKey instance containing original encoded key, key size, and timestamp.
      */
-    public static VersionedKey extractEncodedKey(byte[] rocksDBKey) {
-        ByteBuffer keyBuffer = ByteBuffer.wrap(rocksDBKey);
-        int keySize = keyBuffer.getInt(0);
+    public static VersionedKey extractVersionedKey(byte[] rocksDBKey) {
+        int keySize = rocksDBKey.length - 8;
         byte[] key = new byte[keySize];
-        keyBuffer.get(key, 4, keySize);
-        long timestamp = keyBuffer.getLong(4 + keySize);
-        return new VersionedKey(key, keySize, timestamp);
+        byte[] version = new byte[8];
+        System.arraycopy(rocksDBKey, 0, key, 0, keySize);
+        System.arraycopy(rocksDBKey, keySize, version, 0, 8);
+        return new VersionedKey(key, Longs.fromByteArray(version));
+    }
+
+    /**
+     * Extracts timestamp as byte array from database key - use to avoid converting the byte array
+     * timestamp stored in the key to a long and converting back to a byte array.
+     * @param rocksDBKey Encoded database key.
+     * @return Byte array representation of timestamp associated with key.
+     */
+    public static byte[] extractTimestampAsByteArray(byte[] rocksDBKey) {
+        byte[] timestampBytes = new byte[8];
+        System.arraycopy(rocksDBKey, rocksDBKey.length-8, timestampBytes, 0, 8);
+        return timestampBytes;
+    }
+
+    /**
+     * Extracts the timestamp from a database key.
+     * @param rocksDBKey Encoded database key.
+     * @return Long representation of timestamp associated with key.
+     */
+    public static long extractTimestamp(byte[] rocksDBKey) {
+        byte[] timestampBytes = new byte[8];
+        System.arraycopy(rocksDBKey, rocksDBKey.length-8, timestampBytes, 0, 8);
+        return Longs.fromByteArray(timestampBytes);
+    }
+
+    /**
+     * Extracts the encoded table key from a database key.
+     * @param rocksDBKey Encoded database key.
+     * @return Byte array representation of table key.
+     */
+    public static byte[] extractEncodedKey(byte[] rocksDBKey) {
+        int keysize = rocksDBKey.length - 8;
+        byte[] key = new byte[keysize];
+        System.arraycopy(rocksDBKey, 0, key, 0, keysize);
+        return key;
     }
 
     /**
@@ -56,7 +84,6 @@ public final class KeyEncodingUtil {
     @Getter
     public static class VersionedKey {
         private byte[] encodedRemoteCorfuTableKey;
-        private int encodedKeySize;
         private long timestamp;
     }
 }
