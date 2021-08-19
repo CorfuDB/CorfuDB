ca2451137f997b3ae2b1457ef430c14233e807b3
nvaishampayan517 <neiluv@icloud.com>
2021-08-19 16:50:53 -0700

    created operation interface for logListener use

---
+++ infrastructure/src/main/java/org/corfudb/infrastructure/remotecorfutable/loglistener/smr/ClearOperation.java
@@ -0,0 +1,41 @@
+package org.corfudb.infrastructure.remotecorfutable.loglistener.smr;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.corfudb.infrastructure.remotecorfutable.DatabaseHandler;
+import org.corfudb.runtime.collections.remotecorfutable.RemoteCorfuTableSMRMethods;
+import org.rocksdb.RocksDBException;
+
+import java.util.UUID;
+
+/**
+ * This class contains the logic for the clear operation from stream listener to database.
+ *
+ * Created by nvaishampayan517 on 08/19/21
+ */
+@AllArgsConstructor
+public class ClearOperation implements SMROperation {
+
+    private final long timestamp;
+    @NonNull
+    private final UUID streamId;
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     *     Performs clear database operation with given keys.
+     * </p>
+     */
+    @Override
+    public void applySMRMethod(@NonNull DatabaseHandler dbHandler) throws RocksDBException {
+        dbHandler.clear(streamId, timestamp);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public RemoteCorfuTableSMRMethods getType() {
+        return RemoteCorfuTableSMRMethods.CLEAR;
+    }
+}
