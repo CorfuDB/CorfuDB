c57ece6a8b03aa0b43eaea4015bb26c3eec22f6a
George Lu <jielu@vmware.com>
2021-09-12 19:33:31 -0700

    Introduce KeyDynamicProtobufSerializer and OpaqueCorfuDynamicRecord
    Deserializing CorfuDynamicRecord can be expensive. Protobuf DynamicMessage
    comes with a high memory overhead. Also there are some functionalities that
    doesn't require deserializing CorfuDynamicRecord, such as Compactor. The
    new serializer and opaque record will allow to keep the payload and metadata
    in ByteString format and only deserialize the Key message.

---
+++ runtime/src/main/java/org/corfudb/util/serializer/KeyDynamicProtobufSerializer.java
@@ -0,0 +1,135 @@
+package org.corfudb.util.serializer;
+
+import com.google.protobuf.Any;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
+import lombok.extern.slf4j.Slf4j;
+import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.CorfuStoreMetadata;
+import org.corfudb.runtime.collections.CorfuDynamicKey;
+import org.corfudb.runtime.collections.OpaqueCorfuDynamicRecord;
+import org.corfudb.runtime.exceptions.SerializerException;
+
+import java.io.IOException;
+
+@Slf4j
+public class KeyDynamicProtobufSerializer extends DynamicProtobufSerializer {
+
+    public KeyDynamicProtobufSerializer(CorfuRuntime corfuRuntime) {
+        super(corfuRuntime);
+    }
+
+    /**
+     * Deserialize an object from a given byte buffer.
+     *
+     * @param b The bytebuf to deserialize.
+     * @return The deserialized object.
+     */
+    @Override
+    public Object deserialize(ByteBuf b, CorfuRuntime rt) {
+
+        try (ByteBufInputStream bbis = new ByteBufInputStream(b)) {
+            ProtobufSerializer.MessageType type = ProtobufSerializer.MessageType.valueOf(bbis.readInt());
+            int size = bbis.readInt();
+            byte[] data = new byte[size];
+            bbis.readFully(data);
+            CorfuStoreMetadata.Record corfuRecord = CorfuStoreMetadata.Record.parseFrom(data);
+            Any payload = corfuRecord.getPayload();
+
+            String fullMessageName = getFullMessageName(payload);
+            if (!messagesFdProtoNameMap.containsKey(fullMessageName)) {
+                log.error("messagesFdProtoNameMap doesn't contain the message type {} of payload {}." +
+                                "Please check if the related table is properly opened with correct schema.",
+                        fullMessageName, payload);
+                log.error("messagesFdProtoNameMap keySet is {}", messagesFdProtoNameMap.keySet());
+            }
+
+            if (type.equals(ProtobufSerializer.MessageType.KEY)) {
+                Descriptors.FileDescriptor valueFileDescriptor
+                        = getDescriptor(messagesFdProtoNameMap.get(fullMessageName));
+                Descriptors.Descriptor valueDescriptor = valueFileDescriptor.findMessageTypeByName(getMessageName(payload));
+                DynamicMessage value = DynamicMessage.parseFrom(valueDescriptor, payload.getValue());
+                return new CorfuDynamicKey(payload.getTypeUrl(), value);
+            }
+
+            String metadataTypeUrl = null;
+            ByteString metaDataByteString = null;
+            if (corfuRecord.hasMetadata()) {
+                Any anyMetadata = corfuRecord.getMetadata();
+                metadataTypeUrl = anyMetadata.getTypeUrl();
+                metaDataByteString = anyMetadata.getValue();
+
+                String fullMetadataMessageName = getFullMessageName(anyMetadata);
+                if (!messagesFdProtoNameMap.containsKey(fullMetadataMessageName)) {
+                    log.error("messagesFdProtoNameMap doesn't contain the message type {} of metadata {}." +
+                                    "Please check if the related table is properly opened with correct schema.",
+                            fullMetadataMessageName, anyMetadata);
+                    log.error("messagesFdProtoNameMap keySet is {}", messagesFdProtoNameMap.keySet());
+                }
+            }
+            return new OpaqueCorfuDynamicRecord(payload.getTypeUrl(), payload.getValue(), metadataTypeUrl, metaDataByteString);
+
+        } catch (IOException | Descriptors.DescriptorValidationException ie) {
+            log.error("Exception during deserialization!", ie);
+            throw new SerializerException(ie);
+        }
+    }
+
+    /**
+     * Serialize an object into a given byte buffer.
+     *
+     * @param o The object to serialize.
+     * @param b The bytebuf to serialize it into.
+     */
+    @Override
+    public void serialize(Object o, ByteBuf b) {
+
+        CorfuStoreMetadata.Record corfuRecord;
+        ProtobufSerializer.MessageType messageType;
+
+        if (o instanceof OpaqueCorfuDynamicRecord) {
+            OpaqueCorfuDynamicRecord opaqueRecord = (OpaqueCorfuDynamicRecord) o;
+
+            Any message = Any.newBuilder()
+                    .setTypeUrl(opaqueRecord.getPayloadTypeUrl())
+                    .setValue(opaqueRecord.getPayload())
+                    .build();
+            CorfuStoreMetadata.Record.Builder recordBuilder = CorfuStoreMetadata.Record.newBuilder()
+                    .setPayload(message);
+            if (opaqueRecord.getMetadata() != null) {
+                Any metadata = Any.newBuilder()
+                        .setTypeUrl(opaqueRecord.getMetadataTypeUrl())
+                        .setValue(opaqueRecord.getMetadata())
+                        .build();
+                recordBuilder.setMetadata(metadata);
+            }
+            corfuRecord = recordBuilder.build();
+            messageType = ProtobufSerializer.MessageType.VALUE;
+        } else {
+            CorfuDynamicKey corfuKey = (CorfuDynamicKey) o;
+            Any message = Any.newBuilder()
+                    .setTypeUrl(corfuKey.getKeyTypeUrl())
+                    .setValue(corfuKey.getKey().toByteString())
+                    .build();
+            corfuRecord = CorfuStoreMetadata.Record.newBuilder()
+                    .setPayload(message)
+                    .build();
+            messageType = ProtobufSerializer.MessageType.KEY;
+        }
+        byte[] data = corfuRecord.toByteArray();
+
+        try (ByteBufOutputStream bbos = new ByteBufOutputStream(b)) {
+            bbos.writeInt(messageType.val);
+            bbos.writeInt(data.length);
+            bbos.write(data);
+        } catch (IOException ie) {
+            log.error("Exception during serialization!", ie);
+            throw new SerializerException(ie);
+        }
+    }
+
+}
