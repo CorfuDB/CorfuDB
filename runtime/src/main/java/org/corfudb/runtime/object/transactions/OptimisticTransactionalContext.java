aa9116257c6b51b48c45f5694504b9f74d70acf5
Maithem <Maithem@users.noreply.github.com>
2021-10-15 00:18:11 -0700

    Incorrectly Computed Commit Address and Streaming Data Loss
    When a read only transaction commits it should return the max
    stream tail observed among all streams accessed, because it
    guarantees that, time cannot regress beyond that point. This
    patch fixes a concurrency bug where the stream tails are accessed
    at a different version than the commit, thus create gaps in
    the snapshot. An incorrectly returned snapshot can cause data
    loss when clients try to use the Txn.commit snapshot to listen
    to new updates.

--- runtime/src/main/java/org/corfudb/runtime/object/transactions/OptimisticTransactionalContext.java
+++ runtime/src/main/java/org/corfudb/runtime/object/transactions/OptimisticTransactionalContext.java
@@ -86,8 +86,7 @@ public class OptimisticTransactionalContext extends AbstractTransactionalContext
 
                             // Obtain the stream position as when transaction context last
                             // remembered it.
-                            long streamReadPosition = getKnownStreamPosition()
-                                    .getOrDefault(proxy.getStreamID(), ts);
+                            long streamReadPosition = knownStreamsPosition.getOrDefault(proxy.getStreamID(), ts);
 
                             return (
                                     (stream == null || stream.isStreamCurrentContextThreadCurrentContext())
@@ -103,12 +102,9 @@ public class OptimisticTransactionalContext extends AbstractTransactionalContext
                             // committed changes, or apply forward committed changes.
                             syncWithRetryUnsafe(o, getSnapshotTimestamp(), proxy,
                                     o::setUncommittedChanges);
-
-                            // Update the global positions map. The value obtained from underlying
-                            // object must be under object's write-lock.
-                            getKnownStreamPosition().put(proxy.getStreamID(), o.getVersionUnsafe());
                         },
-                        accessFunction::access
+                        accessFunction::access,
+                        version -> updateKnownStreamPosition(proxy.getStreamID(), version)
         );
     }
 
@@ -249,7 +245,7 @@ public class OptimisticTransactionalContext extends AbstractTransactionalContext
         // subscription, the later could lead to data loss scenarios.
         if (getWriteSetInfo().getWriteSet().getEntryMap().isEmpty()) {
             log.trace("Commit[{}] Read-only commit (no write)", this);
-            return getMaxAddress(getReadSetInfo());
+            return getMaxAddressRead();
         }
 
         Set<UUID> affectedStreamsIds = new HashSet<>(getWriteSetInfo()
@@ -297,18 +293,9 @@ public class OptimisticTransactionalContext extends AbstractTransactionalContext
 
         log.trace("Commit[{}] Acquire address {}", this, address);
 
-        super.commitTransaction();
         commitAddress = address;
         log.trace("Commit[{}] Written to {}", this, address);
         return address;
-    }
-
-    private long getMaxAddress(ConflictSetInfo readSetInfo) {
-        long maxAddress = AbstractTransactionalContext.NOWRITE_ADDRESS;
-        for (ICorfuSMRProxyInternal proxy : readSetInfo.getConflicts().keySet()) {
-            maxAddress = Long.max(maxAddress, proxy.getUnderlyingObject().getVersionUnsafe());
-        }
-        return maxAddress;
     }
 
     @Override
