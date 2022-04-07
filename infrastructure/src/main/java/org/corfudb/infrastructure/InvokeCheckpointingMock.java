c04ce373bce9a34e5c9b2c913a3e3d094cafffac
Sravanthi Ashok Kumar <ashokkumars@vmware.com>
2022-04-07 14:22:03 -0700

    Moving the distributor compactor leader into CompactorService - lets client side compaction possible Added new tests for the new design Merging with ClientSide compaction code

---
+++ infrastructure/src/main/java/org/corfudb/infrastructure/InvokeCheckpointingMock.java
@@ -0,0 +1,23 @@
+package org.corfudb.infrastructure;
+
+import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.DistributedCompactor;
+
+public class InvokeCheckpointingMock implements IInvokeCheckpointing {
+
+    private final DistributedCompactor distributedCompactor;
+
+    public InvokeCheckpointingMock(CorfuRuntime runtime, CorfuRuntime cpRuntime) {
+        this.distributedCompactor =
+                new DistributedCompactor(runtime, cpRuntime, null, true);
+    }
+
+    @Override
+    public void invokeCheckpointing() {
+        distributedCompactor.startCheckpointing();
+    }
+
+    @Override
+    public void shutdown() {
+    }
+}
