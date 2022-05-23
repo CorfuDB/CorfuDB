287966ad2a47d05689e893dcdc663143c0b35c71
Sravanthi Ashok Kumar <ashokkumars@vmware.com>
2022-05-23 08:26:14 -0700

    Resolving codacy and formatting changes

--- infrastructure/src/main/java/org/corfudb/infrastructure/IInvokeCheckpointing.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/IInvokeCheckpointing.java
@@ -2,8 +2,12 @@ package org.corfudb.infrastructure;
 
 public interface IInvokeCheckpointing {
     void invokeCheckpointing();
+
     boolean isRunning();
+
     boolean isInvoked();
+
     void setIsInvoked(boolean isInvoked);
+
     void shutdown();
 }
