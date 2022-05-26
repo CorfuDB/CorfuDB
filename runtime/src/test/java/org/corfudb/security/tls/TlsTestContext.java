eddead965bd2949f5c5a1781869bf9f87d99eddd
Viacheslav <xrw.null@gmail.com>
2022-05-26 12:31:41 -0700

    Certificate expiry time check: fix warnings

--- runtime/src/test/java/org/corfudb/security/tls/TlsTestContext.java
+++ runtime/src/test/java/org/corfudb/security/tls/TlsTestContext.java
@@ -5,14 +5,20 @@ import org.corfudb.security.tls.TlsUtils.CertStoreConfig.TrustStoreConfig;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class TlsTestContext {
+public final class TlsTestContext {
+
+    private TlsTestContext() {
+        //prevent creating new instances
+    }
 
     public static final Path CERT_DIR = Paths.get("src/test/resources/security/reload");
     public static final Path PASSWORD_FILE = CERT_DIR.resolve("password");
 
+    public static final String CLIENT_TRUST_WITH_SERVER_FILE_NAME = "client_trust_with_server.jks";
+
     public static final TrustStoreConfig SERVER_TRUST_WITH_CLIENT = buildTrustStore("server_trust_with_client.jks");
     public static final TrustStoreConfig SERVER_TRUST_NO_CLIENT = buildTrustStore("server_trust_no_client.jks");
-    public static final TrustStoreConfig CLIENT_TRUST_WITH_SERVER = buildTrustStore("client_trust_with_server.jks");
+    public static final TrustStoreConfig CLIENT_TRUST_WITH_SERVER = buildTrustStore(CLIENT_TRUST_WITH_SERVER_FILE_NAME);
     public static final TrustStoreConfig CLIENT_TRUST_NO_SERVER = buildTrustStore("client_trust_no_server.jks");
 
     public static final TrustStoreConfig FAKE_LOCATION_AND_PASS = TrustStoreConfig.from(
@@ -20,7 +26,7 @@ public class TlsTestContext {
             "fake password"
     );
     public static final TrustStoreConfig FAKE_PASS = new TrustStoreConfig(
-            CERT_DIR.resolve("client_trust_with_server.jks"),
+            CERT_DIR.resolve(CLIENT_TRUST_WITH_SERVER_FILE_NAME),
             CERT_DIR.resolve("fake-password")
     );
 
@@ -30,8 +36,4 @@ public class TlsTestContext {
 
     public static final Path CLIENT_CERT = CERT_DIR.resolve("client.cert");
     public static final Path SERVER_CERT = CERT_DIR.resolve("server.cert");
-
-    private TlsTestContext() {
-        //prevent creating new instances
-    }
 }
