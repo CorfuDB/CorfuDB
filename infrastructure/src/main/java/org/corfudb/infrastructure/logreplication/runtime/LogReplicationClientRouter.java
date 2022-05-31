5c319a4c782bf17426eedafa297ccad68d14a5b6
Pankti Majmudar <pmajmudar@vmware.com>
2022-05-31 15:55:01 -0700

    Extend LR to Work with multiple Source Clusters and Sink Clusters.
    fixes for multi site standby to rebase on master
    Test Cleanup to make them flexible and reusable. (#3317)
    +Merge conflict resolution Oct 26
    +Merge conflict resolution Feb 13
    +Merge conflict resolution May 9
    +Merge conflict resolution Jul 31

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/runtime/LogReplicationClientRouter.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/runtime/LogReplicationClientRouter.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
@@ -190,6 +191,8 @@ public class LogReplicationClientRouter implements IClientRouter {
             try {
                 header.setClientId(getUuidMsg(parameters.getClientId()));
                 header.setRequestId(requestId);
+                header.setClusterId(getUuidMsg(
+                    UUID.fromString(parameters.getLocalClusterId())));
 
                 // If no endpoint is specified, the message is to be sent to the remote leader node.
                 // We should block until a connection to the leader is established.
--- infrastructure/src/test/java/org/corfudb/infrastructure/LogReplicationServerTest.java
+++ infrastructure/src/test/java/org/corfudb/infrastructure/LogReplicationServerTest.java
@@ -3,11 +3,13 @@ package org.corfudb.infrastructure;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.corfudb.infrastructure.logreplication.infrastructure.LogReplicationServer;
 import org.corfudb.infrastructure.logreplication.replication.receive.LogReplicationMetadataManager;
 import org.corfudb.infrastructure.logreplication.replication.receive.LogReplicationSinkManager;
 import org.corfudb.runtime.LogReplication.LogReplicationEntryMsg;
 import org.corfudb.runtime.LogReplication.LogReplicationLeadershipRequestMsg;
 import org.corfudb.runtime.LogReplication.LogReplicationMetadataRequestMsg;
+import org.corfudb.runtime.proto.RpcCommon.UuidMsg;
 import org.corfudb.runtime.proto.service.CorfuMessage;
 import org.corfudb.runtime.proto.service.CorfuMessage.HeaderMsg;
 import org.corfudb.runtime.proto.service.CorfuMessage.RequestMsg;
@@ -16,16 +18,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
+import static org.corfudb.protocols.CorfuProtocolCommon.getUUID;
 import static org.corfudb.protocols.service.CorfuProtocolMessage.getRequestMsg;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.same;
-import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.atMost;
 
 /**
  * Tests {@link LogReplicationServer} message handing.
@@ -42,19 +42,19 @@ public class LogReplicationServerTest {
     ChannelHandlerContext mockHandlerContext;
     IServerRouter mockServerRouter;
 
+    UuidMsg clusterId = UuidMsg.newBuilder().setLsb(5).setMsb(5).build();
+    String sourceClusterId = getUUID(clusterId).toString();
+
     /**
-     * Stub most of the {@link LogReplicationServer} functionality,
-     * but spy on the actual instance.
+     * Stub most of the {@link LogReplicationServer} functionality, but spy on the actual instance.
      */
     @Before
     public void setup() {
         context = mock(ServerContext.class);
         metadataManager = mock(LogReplicationMetadataManager.class);
         sinkManager = mock(LogReplicationSinkManager.class);
-        lrServer = spy(new LogReplicationServer(
-                context,
-                metadataManager,
-                sinkManager, "nodeId"));
+        doReturn(sourceClusterId).when(sinkManager).getSourceClusterId();
+        lrServer = spy(new LogReplicationServer(context, sinkManager,"nodeId"));
         mockHandlerContext = mock(ChannelHandlerContext.class);
         mockServerRouter = mock(IServerRouter.class);
     }
@@ -67,18 +67,19 @@ public class LogReplicationServerTest {
     public void testHandleMetadataRequest() {
         final LogReplicationMetadataRequestMsg metadataRequest = LogReplicationMetadataRequestMsg
                 .newBuilder().build();
-        final RequestMsg request = getRequestMsg(HeaderMsg.newBuilder().build(),
+        final RequestMsg request =
+            getRequestMsg(HeaderMsg.newBuilder().setClusterId(clusterId).build(),
                 CorfuMessage.RequestPayloadMsg.newBuilder()
                 .setLrMetadataRequest(metadataRequest).build());
         final ResponseMsg response = ResponseMsg.newBuilder().build();
 
-        doReturn(true).when(lrServer).isLeader(same(request), any(), any(), anyBoolean());
+        lrServer.setLeadership(true);
         doReturn(metadataManager).when(sinkManager).getLogReplicationMetadataManager();
         doReturn(response).when(metadataManager).getMetadataResponse(any());
 
-        lrServer.createHandlerMethods().handle(request, mockHandlerContext, mockServerRouter);
-        
-        verify(lrServer).isLeader(same(request), any(), any(), anyBoolean());
+        lrServer.getHandlerMethods().handle(request, mockHandlerContext,
+            mockServerRouter);
+
         verify(sinkManager).getLogReplicationMetadataManager();
         verify(metadataManager).getMetadataResponse(any());
     }
@@ -89,30 +90,34 @@ public class LogReplicationServerTest {
      */
     @Test
     public void testHandleLeadershipQuery() {
-        final LogReplicationLeadershipRequestMsg leadershipQuery = LogReplicationLeadershipRequestMsg
-                .newBuilder().build();
-        final RequestMsg request = getRequestMsg(HeaderMsg.newBuilder().build(),
+        final LogReplicationLeadershipRequestMsg leadershipQuery =
+            LogReplicationLeadershipRequestMsg.newBuilder().build();
+        final RequestMsg request =
+            getRequestMsg(HeaderMsg.newBuilder().setClusterId(clusterId).build(),
                 CorfuMessage.RequestPayloadMsg.newBuilder()
                 .setLrLeadershipQuery(leadershipQuery).build());
 
         doReturn(SAMPLE_HOSTNAME).when(context).getLocalEndpoint();
 
-        //set leadership to true
-        lrServer.setLeadership(true);
-
-        //leadership response is false when cluster role not STANDBY
-        lrServer.createHandlerMethods().handle(request, mockHandlerContext, mockServerRouter);
+        // verify the response when not the leader
+        lrServer.getHandlerMethods().handle(request, mockHandlerContext,
+            mockServerRouter);
         ArgumentCaptor<ResponseMsg> argument = ArgumentCaptor.forClass(ResponseMsg.class);
         verify(mockServerRouter).sendResponse(argument.capture(), any());
-        Assertions.assertThat(argument.getValue().getPayload().getLrLeadershipResponse().getIsLeader()).isFalse();
+        Assertions.assertThat(argument.getValue().getPayload()
+            .getLrLeadershipResponse().getIsLeader()).isFalse();
 
-        lrServer.setStandby(true);
 
-        // leadership response true, when cluster role STANDBY
-        lrServer.createHandlerMethods().handle(request, mockHandlerContext, mockServerRouter);
+        //set leadership to true and verify the response
+        lrServer.setLeadership(true);
+
+        lrServer.getHandlerMethods().handle(request, mockHandlerContext,
+            mockServerRouter);
         argument = ArgumentCaptor.forClass(ResponseMsg.class);
-        verify(mockServerRouter, atMost(2)).sendResponse(argument.capture(), any());
-        Assertions.assertThat(argument.getValue().getPayload().getLrLeadershipResponse().getIsLeader()).isTrue();
+        verify(mockServerRouter, atMost(2))
+            .sendResponse(argument.capture(), any());
+        Assertions.assertThat(argument.getValue().getPayload()
+            .getLrLeadershipResponse().getIsLeader()).isTrue();
     }
 
     /**
@@ -123,24 +128,30 @@ public class LogReplicationServerTest {
     public void testHandleEntry() {
         final LogReplicationEntryMsg logEntry = LogReplicationEntryMsg
                 .newBuilder().build();
-        final RequestMsg request = getRequestMsg(HeaderMsg.newBuilder().build(),
+        final RequestMsg request =
+            getRequestMsg(HeaderMsg.newBuilder().setClusterId(clusterId).build(),
                 CorfuMessage.RequestPayloadMsg.newBuilder()
                         .setLrEntry(logEntry).build());
         final LogReplicationEntryMsg ack = LogReplicationEntryMsg.newBuilder().build();
 
-        doReturn(true).when(lrServer).isLeader(same(request), any(), any(), anyBoolean());
+        // Make this server the leader and verify the response
+        lrServer.setLeadership(true);
         doReturn(ack).when(sinkManager).receive(same(request.getPayload().getLrEntry()));
 
-        // When cluster role not STANDBY, drop the request.
-        lrServer.createHandlerMethods().handle(request, mockHandlerContext, mockServerRouter);
-        verifyNoInteractions(mockServerRouter);
-
-        // Set the cluster role to STANDBY, verify that the request is handled appropriately
-        lrServer.setStandby(true);
-
-        lrServer.createHandlerMethods().handle(request, mockHandlerContext, mockServerRouter);
+        lrServer.getHandlerMethods().handle(request, mockHandlerContext,
+            mockServerRouter);
         ArgumentCaptor<ResponseMsg> argument = ArgumentCaptor.forClass(ResponseMsg.class);
         verify(mockServerRouter).sendResponse(argument.capture(), any());
         Assertions.assertThat(argument.getValue().getPayload().getLrEntryAck()).isNotNull();
+
+        // Remove leadership from this server and verify the response
+        lrServer.setLeadership(false);
+        lrServer.getHandlerMethods().handle(request, mockHandlerContext,
+            mockServerRouter);
+        argument = ArgumentCaptor.forClass(ResponseMsg.class);
+        verify(mockServerRouter, atMost(2))
+            .sendResponse(argument.capture(), any());
+        Assertions.assertThat(argument.getValue().getPayload()
+            .getLrLeadershipLoss()).isNotNull();
     }
 }
