ebc31b01d9d878cf6777ef32c7df64660cc5ea9b
Pankti Majmudar <pmajmudar@vmware.com>
2022-05-31 15:55:01 -0700

    Extend LR to Work with multiple Source Clusters and Sink Clusters.
    fixes for multi site standby to rebase on master
    Test Cleanup to make them flexible and reusable. (#3317)
    +Merge conflict resolution Oct 26

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/LogReplicationEventListener.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/LogReplicationEventListener.java
@@ -2,26 +2,44 @@ package org.corfudb.infrastructure.logreplication.infrastructure;
 
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.infrastructure.logreplication.proto.LogReplicationMetadata.ReplicationEvent;
+import org.corfudb.infrastructure.logreplication.replication.receive.LogReplicationMetadataManager;
+import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.collections.CorfuStore;
 import org.corfudb.runtime.collections.CorfuStreamEntries;
 import org.corfudb.runtime.collections.CorfuStreamEntry;
 import org.corfudb.runtime.collections.StreamListener;
 
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 public final class LogReplicationEventListener implements StreamListener {
     private final CorfuReplicationDiscoveryService discoveryService;
+    private final CorfuStore corfuStore;
 
-    public  LogReplicationEventListener(CorfuReplicationDiscoveryService discoveryService) {
+    public LogReplicationEventListener(CorfuReplicationDiscoveryService discoveryService, CorfuRuntime runtime) {
         this.discoveryService = discoveryService;
+        this.corfuStore = new CorfuStore(runtime);
     }
 
     public void start() {
-        discoveryService.getLogReplicationMetadataManager().subscribeReplicationEventTable(this);
+        log.info("LogReplication start listener for table {}", LogReplicationMetadataManager.REPLICATION_EVENT_TABLE_NAME);
+        try {
+            // Subscription can fail if the table was not opened, opened with an incorrect tag or the address at
+            // which subscription is attempted has been trimmed.  None of these are likely in this case as this is an
+            // internal table opened on MetadataManager init(completed before) and subscription is done at the log tail.
+            // However, if there is a failure, simply log it and continue such that normal replication flow is not
+            // interrupted.
+            corfuStore.subscribeListener(this, LogReplicationMetadataManager.NAMESPACE,
+                LogReplicationMetadataManager.LR_STREAM_TAG, Collections.singletonList(
+                    LogReplicationMetadataManager.REPLICATION_EVENT_TABLE_NAME));
+        } catch (Exception e) {
+            log.error("Failed to subscribe to the ReplicationEvent Table", e);
+        }
     }
 
     public void stop() {
-        discoveryService.getLogReplicationMetadataManager().unsubscribeReplicationEventTable(this);
+        corfuStore.unsubscribeListener(this);
     }
 
     @Override
@@ -42,12 +60,11 @@ public final class LogReplicationEventListener implements StreamListener {
                 for (CorfuStreamEntry entry : entryList) {
                     ReplicationEvent event = (ReplicationEvent) entry.getPayload();
                     log.info("ReplicationEventListener received an event with id {}, type {}, cluster id {}",
-                            event.getEventId(), event.getType(), event.getClusterId());
+                        event.getEventId(), event.getType(), event.getClusterId());
                     if (event.getType().equals(ReplicationEvent.ReplicationEventType.FORCE_SNAPSHOT_SYNC)) {
                         discoveryService.input(new DiscoveryServiceEvent(
-                                DiscoveryServiceEvent.DiscoveryServiceEventType.ENFORCE_SNAPSHOT_SYNC,
-                                event.getClusterId(),
-                                event.getEventId()));
+                            DiscoveryServiceEvent.DiscoveryServiceEventType.ENFORCE_SNAPSHOT_SYNC, event.getClusterId(),
+                            event.getEventId()));
                     }
                 }
             }
