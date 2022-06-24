79926815c2f13e56fc5492bb3752b38373d29e02
Viacheslav <xrw.null@gmail.com>
2022-06-24 11:57:29 -0700

    Disable Cert expiry check

--- runtime/src/test/java/org/corfudb/security/tls/ReloadableTrustManagerTest.java
+++ runtime/src/test/java/org/corfudb/security/tls/ReloadableTrustManagerTest.java
@@ -10,6 +10,7 @@ import java.io.ByteArrayInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.time.LocalDateTime;
@@ -20,6 +21,7 @@ import static org.corfudb.security.tls.TlsTestContext.CLIENT_TRUST_WITH_SERVER;
 import static org.corfudb.security.tls.TlsTestContext.SERVER_CERT;
 import static org.corfudb.security.tls.TlsTestContext.SERVER_TRUST_NO_CLIENT;
 import static org.corfudb.security.tls.TlsTestContext.SERVER_TRUST_WITH_CLIENT;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
@@ -29,7 +31,9 @@ public class ReloadableTrustManagerTest {
     public void testServerCheckClient() throws Exception {
         ReloadableTrustManager manager = new ReloadableTrustManager(SERVER_TRUST_WITH_CLIENT);
         X509Certificate cert = getCertificate(CLIENT_CERT);
-        manager.checkClientTrusted(new X509Certificate[]{cert}, "RSA");
+        assertThrows(CertificateExpiredException.class, () -> {
+            manager.checkClientTrusted(new X509Certificate[]{cert}, "RSA");
+        });
     }
 
     @Test
@@ -50,7 +54,9 @@ public class ReloadableTrustManagerTest {
     public void testClientCheckServer() throws Exception {
         ReloadableTrustManager manager = new ReloadableTrustManager(CLIENT_TRUST_WITH_SERVER);
         X509Certificate cert = getCertificate(SERVER_CERT);
-        manager.checkServerTrusted(new X509Certificate[]{cert}, "RSA");
+        assertThrows(CertificateExpiredException.class, () -> {
+            manager.checkServerTrusted(new X509Certificate[]{cert}, "RSA");
+        });
     }
 
     @Test
--- runtime/src/test/java/org/corfudb/security/tls/TlsTestContext.java
+++ runtime/src/test/java/org/corfudb/security/tls/TlsTestContext.java
@@ -12,12 +12,13 @@ public final class TlsTestContext {
 
     public static final String CLIENT_TRUST_WITH_SERVER_FILE_NAME = "client_trust_with_server.jks";
 
+    public static final Path DISABLE_CERT_EXPIRY_CHECK_FILE_NAME = CERT_DIR.resolve("DISABLE_CERT_EXPIRY_CHECK");
+
     public static final TrustStoreConfig SERVER_TRUST_WITH_CLIENT = buildTrustStore("server_trust_with_client.jks");
     public static final TrustStoreConfig SERVER_TRUST_NO_CLIENT = buildTrustStore("server_trust_no_client.jks");
     public static final TrustStoreConfig CLIENT_TRUST_WITH_SERVER = buildTrustStore(CLIENT_TRUST_WITH_SERVER_FILE_NAME);
     public static final TrustStoreConfig CLIENT_TRUST_NO_SERVER = buildTrustStore("client_trust_no_server.jks");
 
-    public static final Path DISABLE_CERT_EXPIRY_CHECK_FILE_NAME = CERT_DIR.resolve("DISABLE_CERT_EXPIRY_CHECK");
     public static final TrustStoreConfig FAKE_LOCATION_AND_PASS = TrustStoreConfig.from(
             "definitely fake location",
             "fake password"
@@ -25,7 +26,7 @@ public final class TlsTestContext {
     public static final TrustStoreConfig FAKE_PASS = new TrustStoreConfig(
             CERT_DIR.resolve(CLIENT_TRUST_WITH_SERVER_FILE_NAME),
             CERT_DIR.resolve("fake-password"),
-            CERT_DIR.resolve(DISABLE_CERT_EXPIRY_CHECK_FILE_NAME)
+            DISABLE_CERT_EXPIRY_CHECK_FILE_NAME
     );
 
     public static final Path CLIENT_CERT = CERT_DIR.resolve("client.cert");
