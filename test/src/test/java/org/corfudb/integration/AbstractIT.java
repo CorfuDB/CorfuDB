d0bd17c9acff7ba90a8be907bed1909bca377f09
Hua Cao <stan.github@gmail.com>
2022-07-27 00:00:43 -0700

    separate report for integration test and unit test

--- test/src/test/java/org/corfudb/integration/AbstractIT.java
+++ test/src/test/java/org/corfudb/integration/AbstractIT.java
@@ -26,9 +26,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -58,7 +56,7 @@ public class AbstractIT extends AbstractCorfuTest {
     private static final String FORCE_KILL_ALL_CORFU_COMMAND = "jps | grep -e CorfuServer -e CorfuInterClusterReplicationServer|awk '{print $1}'| xargs kill -9";
     private static final String GRACEFULLY_KILL_ALL_CORFU_COMMAND = "jps | grep -e CorfuServer -e CorfuInterClusterReplicationServer|awk '{print $1}'| xargs kill -SIGINT";
     public boolean shouldForceKill = false;
-
+    private static final String SHOW_ALL_CORFU_COMMAND = "jps | grep -e CorfuInterClusterReplicationServer| awk '{print $1}'";
 
     private static final int SHUTDOWN_RETRIES = 10;
     private static final long SHUTDOWN_RETRY_WAIT = 500;
@@ -114,6 +112,34 @@ public class AbstractIT extends AbstractCorfuTest {
             gracefullyShutdownAllCorfuServers();
         }
         FileUtils.cleanDirectory(new File(CORFU_LOG_PATH));
+    }
+
+    public static void showAllCorfuCommand(){
+        List<String> paramsPs = Arrays.asList("/bin/sh", "-c", SHOW_ALL_CORFU_COMMAND);
+        String result = runCommandForOutput(paramsPs);
+
+        System.out.println("test integration test - show all corfu process");
+        System.out.println(result);
+    }
+
+    public static String runCommandForOutput(List<String> params) {
+        ProcessBuilder pb = new ProcessBuilder(params);
+        Process p;
+        String result = "";
+        try {
+            p = pb.start();
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            StringJoiner sj = new StringJoiner(System.getProperty("line.separator"));
+            reader.lines().iterator().forEachRemaining(sj::add);
+            result = sj.toString();
+
+            p.waitFor();
+            p.destroy();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     /**
