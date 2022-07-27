da87d1c0ad496b520ed44db56ca85e700c31a891
Pavel Zaytsev <zaytsev.pavel.a@gmail.com>
2022-07-27 16:00:24 -0700

    Add some jvm memory metrics

--- common/src/main/java/org/corfudb/common/metrics/micrometer/JVMMetrics.java
+++ common/src/main/java/org/corfudb/common/metrics/micrometer/JVMMetrics.java
@@ -2,14 +2,15 @@ package org.corfudb.common.metrics.micrometer;
 
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
-
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import lombok.Data;
 import lombok.Getter;
 import sun.management.HotspotRuntimeMBean;
 import sun.management.ManagementFactoryHelper;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Provides JVM level metrics.
@@ -26,28 +27,44 @@ public final class JVMMetrics {
         long safepointCount;
     }
 
+    private static void subscribeThreadMetrics(MeterRegistry meterRegistry) {
+        JvmThreadMetrics threadMetrics = new JvmThreadMetrics();
+        threadMetrics.bindTo(meterRegistry);
+    }
+
+    private static void subscribeMemoryMetrics(MeterRegistry meterRegistry) {
+        JvmMemoryMetrics memoryMetrics = new JvmMemoryMetrics();
+        memoryMetrics.bindTo(meterRegistry);
+    }
+
+    private static void subscribeSafePointMetrics(MeterRegistry meterRegistry) {
+        SafePointStats safePointStats = new SafePointStats();
+        Gauge.builder("jvm.safe_point_time", safePointStats, data -> {
+            long current = getRuntimeMBean().getTotalSafepointTime();
+            long delta = current - data.safepointTime;
+            data.safepointTime = current;
+            return delta;
+        }).baseUnit(TimeUnit.MILLISECONDS.toString())
+                .strongReference(true)
+                .register(meterRegistry);
+
+        Gauge.builder("jvm.safe_point_count", safePointStats, data -> {
+            long current = getRuntimeMBean().getSafepointCount();
+            long delta = current - data.safepointCount;
+            data.safepointCount = current;
+            return delta;
+        }).baseUnit(TimeUnit.MILLISECONDS.toString())
+                .strongReference(true)
+                .register(meterRegistry);
+    }
+
     public static void register(Optional<MeterRegistry> metricsRegistry) {
 
         if (metricsRegistry.isPresent()) {
-            SafePointStats safePointStats = new SafePointStats();
-
-            Gauge.builder("jvm.safe_point_time", safePointStats, data -> {
-                long current = getRuntimeMBean().getTotalSafepointTime();
-                long delta = current - data.safepointTime;
-                data.safepointTime = current;
-                return delta;
-            }).baseUnit(TimeUnit.MILLISECONDS.toString())
-                    .strongReference(true)
-                    .register(metricsRegistry.get());
-
-            Gauge.builder("jvm.safe_point_count", safePointStats, data -> {
-                long current = getRuntimeMBean().getSafepointCount();
-                long delta = current - data.safepointCount;
-                data.safepointCount = current;
-                return delta;
-            }).baseUnit(TimeUnit.MILLISECONDS.toString())
-                    .strongReference(true)
-                    .register(metricsRegistry.get());
+            final MeterRegistry meterRegistry = metricsRegistry.get();
+            subscribeMemoryMetrics(meterRegistry);
+            subscribeThreadMetrics(meterRegistry);
+            subscribeSafePointMetrics(meterRegistry);
         }
     }
 }
