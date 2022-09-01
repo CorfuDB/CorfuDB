e5ce60335b237b706fb7cce24e5b5dbe72fe1e1c
Zachary Frenette <66146392+zfrenette@users.noreply.github.com>
2022-08-31 20:02:01 -0400

    Fix sequencer cache eviction policy (#3010)
    The sequencer cache implements a sliding window to help with cache
    eviction, which incorrectly assumes that conflict keys are unique.
    This can lead to a memory leak when the same keys are written over
    and over again. This patch also fixes a bug where the cache evicts
    partial conflicts keys for a particular timestamp. This can lead to
    incorrect resolutions and can cause data loss.

--- infrastructure/src/main/java/org/corfudb/infrastructure/SequencerServerCache.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/SequencerServerCache.java
@@ -1,52 +1,61 @@
 package org.corfudb.infrastructure;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.common.metrics.micrometer.MicroMeterUtils;
-import org.corfudb.common.util.Memory;
 import org.corfudb.runtime.view.Address;
 
 import javax.annotation.concurrent.NotThreadSafe;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
-import java.util.function.Supplier;
 
 /**
  * Sequencer server cache.
  * Contains transaction conflict-resolution data structures.
  * <p>
- * The cache map maps conflict keys (stream id + key) to versions (long), illustrated below:
+ * The conflictKeyMap maps conflict keys (stream id + key) to versions (long), illustrated below:
  * Conflict Key | ck1 | ck2 | ck3 | ck4
  * Version | v1 | v1 | v2 | v3
- * Consider the case where we need to insert a new conflict key (ck), but the cache is full,
- * we need to evict the oldest conflict keys. In the example above, we can't just evict ck1,
- * we need to evict all keys that map to v1, so we need to evict ck1 and ck2,
- * this eviction policy is FIFO on the version number. The simple FIFO approach in Caffein etc doesn't work here,
- * as it may evict ck1, but not ck2. Notice that we also can't evict ck3 before the keys for v1,
- * that's because it will create holes in the resolution window and can lead to incorrect resolutions.
+ * Consider the case where we need to insert a new conflict key (ck), but the cache is full.
+ * We need to evict the oldest conflict keys. In the example above, we can't just evict ck1,
+ * we need to evict all keys that map to v1, so we need to evict ck1 and ck2. This eviction
+ * policy is FIFO on the version number. Notice that we also can't evict ck3 before the keys
+ * for v1, that's because it will create holes in the resolution window and can lead to
+ * incorrect resolutions.
  * <p>
- * We use priority queue as a sliding window on the versions, where a version can map to multiple keys,
- * so we also need to maintain the beginning of the window which is the maxConflictWildcard variable.
+ * We use LinkedHashMap as a sliding window on the versions, since versions are added in strictly increasing
+ * order. We also maintain the beginning of the window which is the maxConflictWildcard variable.
  * <p>
- * SequencerServerCache achieves consistency by using single threaded cache. It's done by following code:
- * `.executor(Runnable::run)`
+ * SequencerServerCache achieves consistency by using single threaded cache.
  */
 @NotThreadSafe
 @Slf4j
 public class SequencerServerCache implements AutoCloseable {
-    /**
-     * TX conflict-resolution information:
-     * a cache of recent conflict keys and their latest global-log position.
-     */
 
-    // As the sequencer cache is used by a single thread, it is safe to use hashmap.
-    private final HashMap<ConflictTxStream, Long> conflictKeys;
-    private final PriorityQueue<ConflictTxStream> cacheEntries; //sorted according to address
+    /**
+     * A mapping between conflict keys and their latest global-log position.
+     * This map is backed by a LinkedHashMap in order to maintain insertion order.
+     * Since the timestamps inserted into the cache are strictly increasing, the sliding
+     * window can be maintained within this same data structure.
+     */
+    @VisibleForTesting
+    @Getter(AccessLevel.PUBLIC)
+    private final Map<ConflictTxStream, Long> conflictKeyMap;
+
+    /**
+     * The max number of entries that the SequencerServerCache may contain.
+     */
     @Getter
-    private final int cacheSize; // the max number of entries in SequencerServerCache
+    private final int capacity;
 
     /**
      * A "wildcard" representing the maximal update timestamp of
@@ -56,165 +65,152 @@ public class SequencerServerCache implements AutoCloseable {
     private long maxConflictWildcard;
 
     /**
+     * Since the LinkedHashMap does not provide easy access to the largest
+     * timestamp, this is maintained explicitly for precondition validation.
+     */
+    @Getter
+    private long maxTimestampInserted = Address.NON_ADDRESS;
+
+    /**
      * maxConflictNewSequencer represents the max update timestamp of all the conflict keys
      * which were evicted from the cache by the time this server is elected
      * the primary sequencer. This means that any snapshot timestamp below this
      * actual threshold would abort due to NEW_SEQUENCER cause.
      */
     @Getter
-    private long maxConflictNewSequencer;
+    private final long maxConflictNewSequencer;
+
+    private static final String CONFLICT_KEYS_COUNTER_NAME = "sequencer.conflict-keys.size";
 
     /**
-     * It is used to calculate the size of ServerCache. Each entry relates two pointers
-     * used by HashMap, one pointer in PriorityQueue.
+     * The Sequencers conflict key cache, limited by size.
+     * @param capacity                The max capacity of the cache.
+     * @param maxConflictNewSequencer The new max update timestamp of all conflict keys evicted
+     *                                from the cache by the time this server is elected as
+     *                                primary sequencer.
      */
-    private static final int ENTRY_OVERHEAD = 24;
+    public SequencerServerCache(int capacity, long maxConflictNewSequencer) {
+        Preconditions.checkArgument(capacity > 0, "sequencer cache capacity must be positive.");
 
-    //As calculating object size is expensive, used the value calculated by deepSize
-    private final int CONFLICTTXSTREAM_OBJ_SIZE = 80; //by calculated by deepSize
-
-    @Getter
-    private final String conflictKeysCounterName = "sequencer.conflict-keys.size";
-    @Getter
-    private final String windowSizeName = "sequencer.cache.window";
-    /**
-     * The cache limited by size.
-     * For a synchronous cache we are using a same-thread executor (Runnable::run)
-     * https://github.com/ben-manes/caffeine/issues/90
-     *
-     * @param cacheSize cache size
-     */
-
-    public SequencerServerCache(int cacheSize, long maxConflictNewSequencer) {
-        this.cacheSize = cacheSize;
-        maxConflictWildcard = maxConflictNewSequencer;
+        this.capacity = capacity;
+        this.maxConflictWildcard = maxConflictNewSequencer;
         this.maxConflictNewSequencer = maxConflictNewSequencer;
-        Supplier<PriorityQueue<ConflictTxStream>> queueSupplier = () ->
-                new PriorityQueue<>(cacheSize, Comparator.comparingLong(conflict ->
-                        conflict.txVersion));
-        cacheEntries = MicroMeterUtils.gauge(windowSizeName, queueSupplier.get(), PriorityQueue::size)
-                .orElseGet(queueSupplier);
-        conflictKeys = MicroMeterUtils
-                .gauge(conflictKeysCounterName, new HashMap<ConflictTxStream, Long>(), HashMap::size)
-                .orElseGet(HashMap::new);
+
+        this.conflictKeyMap = MicroMeterUtils
+                .gauge(CONFLICT_KEYS_COUNTER_NAME, new LinkedHashMap<ConflictTxStream, Long>(), LinkedHashMap::size)
+                .orElseGet(LinkedHashMap::new);
     }
 
     /**
-     * Returns the value associated with the {@code key} in this cache,
-     * or {@code null} if there is no cached value for the {@code key}.
-     *
-     * @param conflictKey conflict stream
-     * @return global address
+     * Returns the global address associated with the conflict key in this cache,
+     * or {@code Address.NON_ADDRESS} if there is no such cached address.
+     * @param conflictKey The conflict key.
+     * @return The global address associated with this hashed conflict key, if present.
      */
-    public Long get(ConflictTxStream conflictKey) {
-        return conflictKeys.getOrDefault(conflictKey, Address.NON_ADDRESS);
+    public long get(@NonNull ConflictTxStream conflictKey) {
+        return conflictKeyMap.getOrDefault(conflictKey, Address.NON_ADDRESS);
     }
 
     /**
-     * The first address in the priority queue.
+     * Returns the first (smallest) address in the cache.
+     * @return The smallest address currently in the cache.
      */
+    @VisibleForTesting
     public long firstAddress() {
-        if (cacheEntries.isEmpty()) {
+        if (conflictKeyMap.isEmpty()) {
             return Address.NOT_FOUND;
         }
-        return cacheEntries.peek().txVersion;
+
+        return conflictKeyMap.entrySet().iterator().next().getValue();
     }
-
-  /**
-   * Invalidate the records with the minAddress. It could be one or multiple records
-   *
-   * @return the number of entries has been invalidated and removed from the cache.
-   */
-  private int invalidateSmallestTxVersion() {
-    ConflictTxStream firstEntry = cacheEntries.peek();
-    if (cacheEntries.size() == 0) {
-      return 0;
-    }
-
-    int numEntries = 0;
-
-    while (firstAddress() == firstEntry.txVersion) {
-      if (log.isTraceEnabled()) {
-        log.trace(
-            "invalidateSmallestTxVersion: items evicted {} min address {}",
-            numEntries,
-            firstAddress());
-      }
-      ConflictTxStream entry = cacheEntries.poll();
-      conflictKeys.remove(entry);
-      numEntries++;
-    }
-
-    maxConflictWildcard = Math.max(maxConflictWildcard, firstEntry.txVersion);
-    return numEntries;
-  }
 
     /**
-     * Invalidate all records up to a trim mark (not included).
+     * Evict the records with the smallest address in the cache.
+     * It could be one or multiple records.
      *
-     * @param trimMark trim mark
+     * @return The number of entries that have been evicted from the cache.
      */
-    public void invalidateUpTo(long trimMark) {
-        log.debug("Invalidate sequencer cache. Trim mark: {}", trimMark);
-        int entries = 0;
-        int pqEntries = 0;
-        while (Address.isAddress(firstAddress()) && firstAddress() < trimMark) {
-            pqEntries += invalidateSmallestTxVersion();
-            entries++;
-        }
-        final int numPqEntries = pqEntries;
-        MicroMeterUtils.measure(numPqEntries, "sequencer.cache.evictions");
-        log.info("Invalidated entries {} addresses {}", pqEntries, entries);
+    private void evictSmallestTxVersion() {
+        evictUpTo(firstAddress() + 1);
     }
 
     /**
-     * The cache size as the number of entries
-     *
-     * @return cache size
+     * Evict all records up to the given address (not included).
+     * @param address The given address.
+     */
+    public void evictUpTo(long address) {
+        long numDeletedEntries = 0;
+        long smallestAddress;
+
+        Iterator<Map.Entry<ConflictTxStream, Long>> it = conflictKeyMap.entrySet().iterator();
+
+        while (it.hasNext()) {
+            smallestAddress = it.next().getValue();
+            if (smallestAddress >= address) {
+                break;
+            }
+
+            it.remove();
+            numDeletedEntries++;
+            maxConflictWildcard = Math.max(maxConflictWildcard, smallestAddress);
+        }
+
+        log.trace("evictUpTo[{}]: entries={}", address, numDeletedEntries);
+        MicroMeterUtils.measure(numDeletedEntries, "sequencer.cache.evictions");
+    }
+
+    /**
+     * Evict records from the cache until the number of records is
+     * equal to or less than the cache capacity. Eviction is performed
+     * by repeatedly evicting all of the records with the smallest address.
+     */
+    private void evict() {
+        while (conflictKeyMap.size() > capacity) {
+            evictSmallestTxVersion();
+        }
+    }
+
+    /**
+     * The cache size as the number of entries.
+     * @return The current cache size.
      */
     public int size() {
-        return conflictKeys.size();
+        return conflictKeyMap.size();
     }
 
     /**
-     * The memory space used by the entries and also the space used by
-     * priority queue and hashmap to store the pointers
-     * @return the memory space used in bytes:
+     * Put the provided conflict keys into the cache, and evict older
+     * entries as necessary.
+     * @param conflictKeys The conflict keys to add into the cache.
+     * @param txVersion    The timestamp associated with these conflict keys.
      */
-    public long byteSize() {
-        log.debug("the cache has {} entries,  the object size used {}, calculated by beepSize {}",
-                size(), CONFLICTTXSTREAM_OBJ_SIZE,
-                cacheEntries.isEmpty() ? 0 : Memory.sizeOf.deepSizeOf(cacheEntries.peek()));
-        return size() * (ENTRY_OVERHEAD + CONFLICTTXSTREAM_OBJ_SIZE);
-    }
+    public void put(@NonNull List<ConflictTxStream> conflictKeys, long txVersion) {
+        // Validate that the conflictKeys can all fit into the cache.
+        Preconditions.checkState(conflictKeys.size() <= capacity,
+                "too many conflict keys for the capacity=%s of the cache", capacity);
 
-    /*
-     * Put a value in the cache
-     *
-     * @param conflictStream conflict stream
-     */
-    public boolean put(ConflictTxStream conflictStream) {
+        // Validate that the timestamps are being inserted in strictly increasing order.
+        Preconditions.checkState(txVersion > maxTimestampInserted,
+                "txVersion=%s is not larger than the previous timestamp=%s inserted",
+                txVersion, maxTimestampInserted);
 
-        Long val = conflictKeys.getOrDefault(conflictStream, Address.NON_ADDRESS);
-        if (val > conflictStream.txVersion) {
-            log.error("For key {} the new entry address {} is smaller than the entry " +
-                            "address {} in cache. There is a sequencer regression.",
-                    conflictStream, conflictStream.txVersion, conflictKeys.get(conflictStream));
-            return false;
-        }
+        conflictKeys.forEach(conflictKey -> {
+            // This remove is required since inserting a duplicate conflict key into
+            // the LinkedHashMap does not update the insertion order.
+            this.conflictKeyMap.remove(conflictKey);
+            this.conflictKeyMap.put(conflictKey, txVersion);
+        });
 
-        cacheEntries.add(conflictStream);
-        conflictKeys.put(conflictStream, conflictStream.txVersion);
+        maxTimestampInserted = txVersion;
 
-        while (conflictKeys.size() > cacheSize) {
-            invalidateSmallestTxVersion();
-        }
-        return true;
+        // If applicable, evict entries until we are below the max cache capacity.
+        // Note: this can trigger the eviction of multiple conflict keys from multiple timestamps.
+        evict();
     }
 
     @Override
     public void close() {
-        MicroMeterUtils.removeGaugesWithNoTags(windowSizeName, conflictKeysCounterName);
+        MicroMeterUtils.removeGaugesWithNoTags(CONFLICT_KEYS_COUNTER_NAME);
     }
 
     /**
@@ -225,21 +221,18 @@ public class SequencerServerCache implements AutoCloseable {
 
         @Getter
         private final UUID streamId;
+
         @Getter
         private final byte[] conflictParam;
 
-        @EqualsAndHashCode.Exclude
-        public final long txVersion;
-
-        public ConflictTxStream(UUID streamId, byte[] conflictParam, long address) {
+        public ConflictTxStream(UUID streamId, byte[] conflictParam) {
             this.streamId = streamId;
             this.conflictParam = conflictParam;
-            txVersion = address;
         }
 
         @Override
         public String toString() {
-            return streamId.toString() + conflictParam;
+            return streamId.toString() + Arrays.toString(conflictParam);
         }
     }
 }
