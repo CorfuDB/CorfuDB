65fbc825b1bbaa02ee131bac1b5c5db0d70cd1de
Shama Hegde <shama358@gmail.com>
2022-09-23 09:05:17 -0700

    replace "active/standby" by "source/sink" (#3348)

--- test/src/test/java/org/corfudb/integration/LogReplicationSinkWriterIT.java
+++ test/src/test/java/org/corfudb/integration/LogReplicationSinkWriterIT.java
@@ -52,25 +52,25 @@ public class LogReplicationSinkWriterIT extends LogReplicationAbstractIT {
     @Test
     @SuppressWarnings("checkstyle:magicnumber")
     public void testFilterAndApplyRegistryTableEntries() throws Exception {
-        setupActiveAndStandbyCorfu();
+        setupSourceAndSinkCorfu();
         openLogReplicationStatusTable();
 
         // Initial setup for streams to replicate for both source and sink side
-        mapNameToMapActive = new HashMap<>();
-        mapNameToMapStandby = new HashMap<>();
+        mapNameToMapSource = new HashMap<>();
+        mapNameToMapSink = new HashMap<>();
 
         /* The first 4 tables are for snapshot sync */
         prepareTablesToVerify(1, 2, 4);
 
         // Add Data for Snapshot Sync
-        writeToActive(0, NUM_WRITES);
+        writeToSource(0, NUM_WRITES);
         // Confirm data does exist on Source Cluster
-        for(Table<StringKey, IntValueTag, Metadata> map : mapNameToMapActive.values()) {
+        for(Table<StringKey, IntValueTag, Metadata> map : mapNameToMapSource.values()) {
             assertThat(map.count()).isEqualTo(NUM_WRITES);
         }
 
         // Confirm data does not exist on Sink Cluster
-        for(Table<StringKey, IntValueTag, Metadata> map : mapNameToMapStandby.values()) {
+        for(Table<StringKey, IntValueTag, Metadata> map : mapNameToMapSink.values()) {
             assertThat(map.count()).isEqualTo(0);
         }
 
@@ -91,22 +91,22 @@ public class LogReplicationSinkWriterIT extends LogReplicationAbstractIT {
         // It will be verified that records already exist in Sink side registry table won't be overwritten during
         // Log Entry sync. LR needs to be stopped to open some tables on both Source and Sink with different metadata,
         // otherwise it is possible that registry table entries from Source get replicated before Sink side opens them.
-        stopActiveLogReplicator();
+        stopSourceLogReplicator();
         // Clear these two map as different set of tables will be verified
-        mapNameToMapActive.clear();
-        mapNameToMapStandby.clear();
+        mapNameToMapSource.clear();
+        mapNameToMapSink.clear();
 
         prepareTablesToVerify(5, 6, 8);
 
         // Restart Source LR after opening the tables
-        startActiveLogReplicator();
+        startSourceLogReplicator();
 
-        // Write data to Source side, note that mapNameToMapActive now only have the last 4 tables
-        writeToActive(0, NUM_WRITES);
+        // Write data to Source side, note that mapNameToMapSource now only have the last 4 tables
+        writeToSource(0, NUM_WRITES);
 
-        // At this point mapNameToMapStandby only has 1 table. This step is also used as a barrier to indicate
+        // At this point mapNameToMapSink only has 1 table. This step is also used as a barrier to indicate
         // it's a good point to check records on Sink side
-        verifyDataOnStandby(NUM_WRITES);
+        verifyDataOnSink(NUM_WRITES);
         // Verify Sink side record for Table005 is retained on the Registry table, and records for Table007 and
         // Table008 are replicated.
         // Also verify all the table's contents for log entry sync
@@ -130,32 +130,32 @@ public class LogReplicationSinkWriterIT extends LogReplicationAbstractIT {
         // different metadata. Verify that Sink side's records are retained.
         for (int i = start; i < splitter; i++) {
             String mapName = TABLE_PREFIX + i;
-            Table<StringKey, IntValueTag, Metadata> tableActive =
-                    corfuStoreActive.openTable(NAMESPACE, mapName, StringKey.class,
+            Table<StringKey, IntValueTag, Metadata> tableSource =
+                    corfuStoreSource.openTable(NAMESPACE, mapName, StringKey.class,
                             IntValueTag.class, Metadata.class,
                             TableOptions.fromProtoSchema(IntValueTag.class));
-            mapNameToMapActive.put(mapName, tableActive);
+            mapNameToMapSource.put(mapName, tableSource);
 
             // Open tables on Sink side with different schema options
-            corfuStoreStandby.openTable(NAMESPACE, mapName, StringKey.class, IntValueTag.class, Metadata.class,
+            corfuStoreSink.openTable(NAMESPACE, mapName, StringKey.class, IntValueTag.class, Metadata.class,
                     TableOptions.fromProtoSchema(IntValue.class));
         }
 
         // From splitter to end - Open only in Source side. Verify that their records are replicated to Sink.
         for (int i = splitter; i <= end; i++) {
             String mapName = TABLE_PREFIX + i;
-            Table<StringKey, IntValueTag, Metadata> tableActive =
-                    corfuStoreActive.openTable(NAMESPACE, mapName, StringKey.class,
+            Table<StringKey, IntValueTag, Metadata> tableSource =
+                    corfuStoreSource.openTable(NAMESPACE, mapName, StringKey.class,
                             IntValueTag.class, Metadata.class,
                             TableOptions.fromProtoSchema(IntValueTag.class));
-            mapNameToMapActive.put(mapName, tableActive);
+            mapNameToMapSource.put(mapName, tableSource);
             if (i == splitter) {
                 // Open tables on Sink side with different schema options
-                Table<StringKey, IntValueTag, Metadata> tableStandby =
-                        corfuStoreStandby.openTable(NAMESPACE, mapName, StringKey.class,
+                Table<StringKey, IntValueTag, Metadata> tableSink =
+                        corfuStoreSink.openTable(NAMESPACE, mapName, StringKey.class,
                                 IntValueTag.class, Metadata.class,
                                 TableOptions.fromProtoSchema(IntValueTag.class));
-                mapNameToMapStandby.put(mapName, tableStandby);
+                mapNameToMapSink.put(mapName, tableSink);
             }
         }
     }
@@ -165,7 +165,7 @@ public class LogReplicationSinkWriterIT extends LogReplicationAbstractIT {
      */
     private void verifyTableContentsAndRecords(int start, int splitter, int end) throws Exception {
         // Verify Sink side records for tables before the splitter are overwritten, and records for the rest of
-        // the tables are replicated. Add the tables from splitter to end to mapNameToMapStandby to verify all the
+        // the tables are replicated. Add the tables from splitter to end to mapNameToMapSink to verify all the
         // tables' contents
         for (int i = start; i <= end; i++) {
             String tableName = TABLE_PREFIX + i;
@@ -174,39 +174,39 @@ public class LogReplicationSinkWriterIT extends LogReplicationAbstractIT {
                     .setNamespace(NAMESPACE)
                     .build();
             CorfuRecord<TableDescriptors, TableMetadata> corfuRecord =
-                    standbyRuntime.getTableRegistry().getRegistryTable().get(tableNameKey);
+                    sinkRuntime.getTableRegistry().getRegistryTable().get(tableNameKey);
             if (i < splitter) {
                 // Table with is_federated = false will be dropped
                 Assert.assertFalse(corfuRecord.getMetadata().getTableOptions().getIsFederated());
-                Table<StringKey, IntValueTag, Metadata> tableStandby =
-                        corfuStoreStandby.openTable(NAMESPACE, tableName, StringKey.class,
+                Table<StringKey, IntValueTag, Metadata> tableSink =
+                        corfuStoreSink.openTable(NAMESPACE, tableName, StringKey.class,
                                 IntValueTag.class, Metadata.class,
                                 TableOptions.fromProtoSchema(IntValue.class, TableOptions.builder().build()));
-                Assert.assertEquals(tableStandby.count(), 0);
+                Assert.assertEquals(tableSink.count(), 0);
             } else {
                 Assert.assertTrue(corfuRecord.getMetadata().getTableOptions().getIsFederated());
                 Assert.assertEquals(corfuRecord.getMetadata().getTableOptions().getStreamTagCount(), 1);
                 Assert.assertEquals(corfuRecord.getMetadata().getTableOptions().getStreamTag(0), "test");
-                Table<StringKey, IntValueTag, Metadata> tableStandby =
-                        corfuStoreStandby.openTable(NAMESPACE, tableName, StringKey.class,
-                                IntValueTag.class, Metadata.class,
-                                TableOptions.fromProtoSchema(IntValueTag.class, TableOptions.builder().build()));
-                mapNameToMapStandby.put(tableName, tableStandby);
+                Table<StringKey, IntValueTag, Metadata> tableSink =
+                    corfuStoreSink.openTable(NAMESPACE, tableName, StringKey.class,
+                        IntValueTag.class, Metadata.class,
+                        TableOptions.fromProtoSchema(IntValueTag.class, TableOptions.builder().build()));
+                mapNameToMapSink.put(tableName, tableSink);
             }
         }
         // Verify contents
-        verifyDataOnStandby(NUM_WRITES);
+        verifyDataOnSink(NUM_WRITES);
     }
 
     private void openLogReplicationStatusTable() throws Exception {
-        corfuStoreActive.openTable(LogReplicationMetadataManager.NAMESPACE,
+        corfuStoreSource.openTable(LogReplicationMetadataManager.NAMESPACE,
                 REPLICATION_STATUS_TABLE,
                 LogReplicationMetadata.ReplicationStatusKey.class,
                 ReplicationStatusVal.class,
                 null,
                 TableOptions.fromProtoSchema(ReplicationStatusVal.class));
 
-        corfuStoreStandby.openTable(LogReplicationMetadataManager.NAMESPACE,
+        corfuStoreSink.openTable(LogReplicationMetadataManager.NAMESPACE,
                 REPLICATION_STATUS_TABLE,
                 LogReplicationMetadata.ReplicationStatusKey.class,
                 ReplicationStatusVal.class,
