9b76b2a5a78b835c7777bc466fef67452ee429a9
Shama Hegde <shama358@gmail.com>
2022-11-14 00:40:19 -0800

    Revert "Avoid passing the LR Server's Channel Context to the Client Router. (#3298)"
    This reverts commit aac4e276e95099af98b0a692d39ec05482bc7669.

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/LogReplicationContext.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/LogReplicationContext.java
@@ -2,11 +2,13 @@ package org.corfudb.infrastructure.logreplication.infrastructure;
 
 import lombok.Getter;
 import org.corfudb.infrastructure.logreplication.LogReplicationConfig;
+import org.corfudb.infrastructure.logreplication.transport.IChannelContext;
 
 /**
  * This class represents the Log Replication Context.
  *
- * It contains all abstractions required to initiate log replication either as a source cluster or as sink cluster.
+ * It contains all abstractions required to initiate log replication either as
+ * an active cluster (source) or as standby cluster (standby).
  *
  * @author amartinezman
  */
@@ -21,12 +23,17 @@ public class LogReplicationContext {
     @Getter
     private String localCorfuEndpoint;
 
+    @Getter
+    private IChannelContext channelContext;
+
     /**
      * Constructor
      **/
-    public LogReplicationContext(LogReplicationConfig config, TopologyDescriptor topology, String localCorfuEndpoint) {
+    public LogReplicationContext(LogReplicationConfig config,  TopologyDescriptor topology, String localCorfuEndpoint,
+                                 IChannelContext channelContext) {
         this.config = config;
         this.topology = topology;
         this.localCorfuEndpoint = localCorfuEndpoint;
+        this.channelContext = channelContext;
     }
 }
