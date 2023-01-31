47f97f617b32ffe24acd4e9d91d6ecbbd4984fde
Xinyu (Chris) Chang <xchang@vmware.com>
2023-01-30 22:52:41 -0500

    Cleanup for LR config & config manager (#3496)

--- test/src/test/java/org/corfudb/integration/SourceForwardingDataSender.java
+++ test/src/test/java/org/corfudb/integration/SourceForwardingDataSender.java
@@ -6,7 +6,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.common.util.ObservableValue;
 import org.corfudb.infrastructure.logreplication.DataSender;
-import org.corfudb.infrastructure.logreplication.LogReplicationConfig;
+import org.corfudb.infrastructure.logreplication.infrastructure.LogReplicationContext;
 import org.corfudb.infrastructure.logreplication.infrastructure.ReplicationSession;
 import org.corfudb.infrastructure.logreplication.proto.LogReplicationMetadata;
 import org.corfudb.infrastructure.logreplication.replication.receive.LogReplicationMetadataManager;
@@ -14,7 +14,6 @@ import org.corfudb.infrastructure.logreplication.replication.receive.LogReplicat
 import org.corfudb.infrastructure.logreplication.replication.LogReplicationSourceManager;
 import org.corfudb.infrastructure.logreplication.replication.send.LogReplicationError;
 import org.corfudb.infrastructure.logreplication.replication.fsm.ObservableAckMsg;
-import org.corfudb.infrastructure.logreplication.utils.LogReplicationConfigManager;
 import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.integration.DefaultDataControl.DefaultDataControlConfig;
 import org.corfudb.runtime.LogReplication;
@@ -98,7 +97,7 @@ public class SourceForwardingDataSender extends AbstractIT implements DataSender
     private static final String REPLICATION_STATUS_TABLE = "LogReplicationStatus";
 
     @SneakyThrows
-    public SourceForwardingDataSender(String destinationEndpoint, LogReplicationConfigManager configManager,
+    public SourceForwardingDataSender(String destinationEndpoint, LogReplicationContext replicationContext,
                                       LogReplicationIT.TestConfig testConfig,
                                       LogReplicationMetadataManager metadataManager,
                                       String pluginConfigFilePath, LogReplicationIT.TransitionSource function) {
@@ -113,7 +112,7 @@ public class SourceForwardingDataSender extends AbstractIT implements DataSender
 
         // TODO pankti: This test-only constructor can be removed
         this.destinationLogReplicationManager = new LogReplicationSinkManager(runtime.getLayoutServers().get(0),
-            configManager, metadataManager, pluginConfigFilePath, replicationSession);
+            replicationContext, metadataManager, pluginConfigFilePath, replicationSession);
 
         this.ifDropMsg = testConfig.getDropMessageLevel();
         this.delayedApplyCycles = testConfig.getDelayedApplyCycles();
