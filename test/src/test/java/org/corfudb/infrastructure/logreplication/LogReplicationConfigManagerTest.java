c525f351a3c906bd12691251a9b1b4578b9747a7
Sourav Kumar <souravk1@vmware.com>
2023-02-02 16:00:28 -0800

    Extend LR config manager UTs

--- test/src/test/java/org/corfudb/infrastructure/logreplication/LogReplicationConfigManagerTest.java
+++ test/src/test/java/org/corfudb/infrastructure/logreplication/LogReplicationConfigManagerTest.java
@@ -2,6 +2,7 @@ package org.corfudb.infrastructure.logreplication;
 
 import com.google.protobuf.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.corfudb.infrastructure.logreplication.proto.Sample;
 import org.corfudb.infrastructure.logreplication.proto.Sample.IntValueTag;
 import org.corfudb.infrastructure.logreplication.proto.Sample.Metadata;
 import org.corfudb.infrastructure.logreplication.proto.Sample.StringKey;
@@ -11,28 +12,23 @@ import org.corfudb.runtime.collections.CorfuStore;
 import org.corfudb.runtime.collections.TableOptions;
 import org.corfudb.runtime.view.AbstractViewTest;
 import org.corfudb.runtime.view.TableRegistry;
+import org.corfudb.test.SampleSchema;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
-import static org.corfudb.runtime.view.TableRegistry.PROTOBUF_DESCRIPTOR_TABLE_NAME;
-import static org.corfudb.runtime.view.TableRegistry.REGISTRY_TABLE_NAME;
-import static org.corfudb.runtime.view.TableRegistry.CORFU_SYSTEM_NAMESPACE;
+import static org.corfudb.runtime.view.TableRegistry.*;
 
 @Slf4j
 public class LogReplicationConfigManagerTest extends AbstractViewTest {
     private CorfuRuntime runtime;
     private CorfuStore corfuStore;
-    
+
+    private Set<String> streamsToReplicate = new HashSet<>();
+    private Set<UUID> streamsToDrop = new HashSet<>();
     private Map<UUID, List<UUID>> streamToTagsMap = new HashMap<>();
 
     private static final String NAMESPACE = "LR-Test";
@@ -65,7 +61,7 @@ public class LogReplicationConfigManagerTest extends AbstractViewTest {
         Set<String> streamNamesToDrop = new HashSet<>();
         streamNamesToDrop.add(TABLE3);
         streamNamesToDrop.add(TABLE4);
-        //setupStreamsToDrop(streamNamesToDrop, ReplicationSubscriber.getDefaultReplicationSubscriber(), IntValue.class);
+        setupStreamsToDrop(streamNamesToDrop, Sample.IntValue.class);
     }
 
     private <K extends Message> void setupStreamsToReplicateAndTagsMap(Set<String> streamNamesToReplicate,
@@ -85,6 +81,7 @@ public class LogReplicationConfigManagerTest extends AbstractViewTest {
             } else {
                 fullyQualifiedTableName = TableRegistry.getFullyQualifiedTableName(NAMESPACE, stream);
             }
+            streamsToReplicate.add(fullyQualifiedTableName);
 
             if (!stream.equals(REGISTRY_TABLE_NAME) && !stream.equals(PROTOBUF_DESCRIPTOR_TABLE_NAME)) {
                 List<UUID> streamTags = tableOptions.getSchemaOptions().getStreamTagList().stream().map(streamTag ->
@@ -93,7 +90,18 @@ public class LogReplicationConfigManagerTest extends AbstractViewTest {
                 streamToTagsMap.put(CorfuRuntime.getStreamID(fullyQualifiedTableName), streamTags);
             }
         }
+    }
 
+    private <K extends Message> void setupStreamsToDrop(Set<String> streamNamesToDrop,
+                                                        Class<K> valueSchema) throws Exception {
+
+        for (String streamToDrop : streamNamesToDrop) {
+            corfuStore.openTable(NAMESPACE, streamToDrop, StringKey.class, valueSchema, Metadata.class,
+                    TableOptions.fromProtoSchema(valueSchema));
+
+            String fullyQualifiedTableName = TableRegistry.getFullyQualifiedTableName(NAMESPACE, streamToDrop);
+            streamsToDrop.add(CorfuRuntime.getStreamID(fullyQualifiedTableName));
+        }
     }
 
     @Test
@@ -103,13 +111,18 @@ public class LogReplicationConfigManagerTest extends AbstractViewTest {
     }
 
     @Test
-    public void testConfigUpdate() {
+    public void testConfigUpdate() throws Exception {
         LogReplicationConfigManager configManager = new LogReplicationConfigManager(runtime);
         verifyExpectedConfigGenerated(configManager.getConfig());
+        // Open new tables and update the expected streams to replicate map, streams to drop and stream tags
+        setupStreamsToReplicateAndTagsMap(Collections.singleton(TABLE5), SampleSchema.ValueFieldTagOne.class);
+        setupStreamsToDrop(Collections.singleton(TABLE6), SampleSchema.Uuid.class);
         verifyExpectedConfigGenerated(configManager.getUpdatedConfig());
     }
 
     private void verifyExpectedConfigGenerated(LogReplicationConfig actualConfig) {
+        Assert.assertTrue(Objects.equals(streamsToReplicate, actualConfig.getStreamsToReplicate()));
+        Assert.assertTrue(Objects.equals(streamsToDrop, actualConfig.getStreamsToDrop()));
         Assert.assertTrue(Objects.equals(streamToTagsMap, actualConfig.getDataStreamToTagsMap()));
 
     }
