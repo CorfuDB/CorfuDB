b046b3739f86169f538fd4e39d137c918dfa0c02
Shama Hegde <shama358@gmail.com>
2023-02-09 17:08:10 -0800

    New TopologyDescriptor (#3510)
    +IPV6 conflicts Feb 13th

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/plugins/CorfuReplicationClusterManagerAdapter.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/plugins/CorfuReplicationClusterManagerAdapter.java
@@ -1,8 +1,9 @@
 package org.corfudb.infrastructure.logreplication.infrastructure.plugins;
 
 import org.corfudb.infrastructure.logreplication.infrastructure.CorfuReplicationDiscoveryService;
+import org.corfudb.infrastructure.logreplication.infrastructure.CorfuReplicationDiscoveryServiceAdapter;
 import org.corfudb.infrastructure.logreplication.infrastructure.LogReplicationDiscoveryServiceException;
-import org.corfudb.infrastructure.logreplication.proto.LogReplicationClusterInfo.TopologyConfigurationMsg;
+import org.corfudb.infrastructure.logreplication.infrastructure.TopologyDescriptor;
 import org.corfudb.runtime.LogReplication.LogReplicationSession;
 import org.corfudb.infrastructure.logreplication.proto.LogReplicationMetadata.ReplicationStatus;
 
@@ -21,12 +22,7 @@ public interface CorfuReplicationClusterManagerAdapter {
     /**
      * Register the discovery service
      */
-    void register(CorfuReplicationDiscoveryService corfuReplicationDiscoveryService);
-
-     /**
-     * Set the localEndpoint
-     */
-    void setLocalEndpoint(String endpoint);
+    void register(CorfuReplicationDiscoveryServiceAdapter corfuReplicationDiscoveryServiceAdapter);
 
     /**
      * Query the topology information.
@@ -34,12 +30,12 @@ public interface CorfuReplicationClusterManagerAdapter {
      *                  recent topology from the Cluster Manager/Topology Provider.
      * @return
      */
-    TopologyConfigurationMsg queryTopologyConfig(boolean useCached);
+    TopologyDescriptor queryTopologyConfig(boolean useCached);
 
     /**
      * Callback to update topology on cluster changes
      */
-    void updateTopologyConfig(TopologyConfigurationMsg newClusterConfig);
+    void updateTopologyConfig(TopologyDescriptor newClusterConfig);
 
     /**
      * Start cluster discovery against external topology provider
