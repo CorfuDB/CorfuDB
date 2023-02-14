e74f876605560de434d40de42c2753fac2fa83c2
xcchang <xchang@vmware.com>
2023-02-14 12:43:07 -0800

    LogReplicationConfigManager extension & client stream listener
    Implement Readers for LogicalGroup-Based Replication.
    
    Integration config service with logical group reader
    
    Logical group integration test template
    
    Fix for client config listener
    
    Cleanup LogReplicationLogicalGroupIT
    
    Added IT for group destination change with forced snapshot sync
    
    source new table IT + local write IT + log entry open table IT
    
    Delete group destination test + fix for log entry reader stream UUIDs
    
    Fix: client config listener re-subscribe exception handling
    
    Logical group Sink streaming IT
    
    Fix session manager refresh
    
    Upload all the logical group ITs
    
    Fix: correctly add merge-only streams tag
    
    Fix: DefaultClusterManager OP conflict
    
    Resolve rebase conflicts

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/send/logreader/LogicalGroupLogEntryReader.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/send/logreader/LogicalGroupLogEntryReader.java
@@ -2,10 +2,8 @@ package org.corfudb.infrastructure.logreplication.replication.send.logreader;
 
 import org.corfudb.infrastructure.logreplication.infrastructure.LogReplicationContext;
 import org.corfudb.runtime.CorfuRuntime;
-import org.corfudb.runtime.LogReplication.LogReplicationEntryMsg;
-import org.corfudb.runtime.exceptions.TrimmedException;
 import org.corfudb.runtime.LogReplication.LogReplicationSession;
-import java.util.UUID;
+
 
 /**
  * Log entry reader implementation for Logical Grouping Replication Model.
@@ -14,23 +12,10 @@ import java.util.UUID;
  * with the exception that it will read from a different transactional stream for log entry sync (one that is
  * specific for this model).
  */
-public class LogicalGroupLogEntryReader extends LogEntryReader {
+public class LogicalGroupLogEntryReader extends BaseLogEntryReader {
 
     public LogicalGroupLogEntryReader(CorfuRuntime runtime, LogReplicationSession session,
                                       LogReplicationContext replicationContext) {
-    }
-
-    @Override
-    public LogReplicationEntryMsg read(UUID logEntryRequestId) throws TrimmedException {
-        return null;
-    }
-
-    @Override
-    public void reset(long lastSentBaseSnapshotTimestamp, long lastAckedTimestamp) {
-    }
-
-    @Override
-    public StreamIteratorMetadata getCurrentProcessedEntryMetadata() {
-        return null;
+        super(runtime, session, replicationContext);
     }
 }
--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/send/logreader/LogicalGroupSnapshotReader.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/send/logreader/LogicalGroupSnapshotReader.java
@@ -3,23 +3,19 @@ package org.corfudb.infrastructure.logreplication.replication.send.logreader;
 import org.corfudb.infrastructure.logreplication.infrastructure.LogReplicationContext;
 import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.runtime.LogReplication.LogReplicationSession;
-import java.util.UUID;
 
 /**
  * Snapshot reader implementation for Logical Grouping Replication Model.
  */
-public class LogicalGroupSnapshotReader extends SnapshotReader {
+public class LogicalGroupSnapshotReader extends BaseSnapshotReader {
 
     public LogicalGroupSnapshotReader(CorfuRuntime runtime, LogReplicationSession session,
                                       LogReplicationContext replicationContext) {
+        super(runtime, session, replicationContext);
     }
 
     @Override
-    public SnapshotReadMessage read(UUID syncRequestId) {
-        return null;
-    }
-
-    @Override
-    public void reset(long ts) {
+    protected void refreshStreamsToReplicateSet() {
+        streams = replicationContext.getConfig(session).getStreamsToReplicate();
     }
 }
