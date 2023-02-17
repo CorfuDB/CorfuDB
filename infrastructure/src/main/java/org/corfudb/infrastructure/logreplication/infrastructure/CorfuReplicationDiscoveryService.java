87470856ce88e275fff5127f4bebe7d036d108d7
Pavel Zaytsev <zaytsev.pavel.a@gmail.com>
2023-02-17 12:26:11 -0800

    Test

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/CorfuReplicationDiscoveryService.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/CorfuReplicationDiscoveryService.java
@@ -38,8 +38,10 @@ import org.corfudb.utils.lock.states.LockState;
 import javax.annotation.Nonnull;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.time.Duration;
@@ -47,9 +49,11 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -76,6 +80,10 @@ public class CorfuReplicationDiscoveryService implements CorfuReplicationDiscove
      * Fraction of Lease Duration for Lease Renewal
      */
     private static final int RENEWAL_LEASE_FRACTION = 4;
+
+    private static final int CONFIG_RETRIES = 10;
+
+    private static final int CONFIG_RETRY_DURATION_SECONDS = 30;
 
     /**
      * Fraction of Lease Duration for Lease Monitoring
@@ -216,7 +224,7 @@ public class CorfuReplicationDiscoveryService implements CorfuReplicationDiscove
         try {
             log.info("Start Log Replication Discovery Service");
 
-            setLocalNodeId();
+            setupLocalNodeIdWithRetries();
             fetchTopology();
             processDiscoveredTopology(topologyDescriptor, true);
 
@@ -826,5 +834,40 @@ public class CorfuReplicationDiscoveryService implements CorfuReplicationDiscove
     @Override
     public Set<LogReplicationSession> getIncomingSessions() {
         return sessionManager.getIncomingSessions();
+    }
+
+    private Optional<String> getCorfuSaaSEndpoint() {
+        final String pluginConfigFilePath = serverContext.getPluginConfigFilePath();
+        if (pluginConfigFilePath != null) {
+            return extractEndpoint(pluginConfigFilePath);
+        }
+        log.warn("No plugin path found");
+        return Optional.empty();
+    }
+
+    private Optional<String> extractEndpoint(String path) {
+        try (InputStream input = new FileInputStream(path)) {
+            Properties prop = new Properties();
+            prop.load(input);
+            String endpoint = prop.getProperty("saas_endpoint");
+            return Optional.of(endpoint);
+        } catch (IOException e) {
+            log.warn("Error extracting saas endpoint");
+            return Optional.empty();
+        }
+    }
+
+    private void setupLocalNodeIdWithRetries() throws InterruptedException {
+        for (int i = 0; i < CONFIG_RETRIES; i++) {
+            try {
+                setLocalNodeId();
+            }
+            catch (IllegalStateException ise) {
+                TimeUnit.SECONDS.sleep(CONFIG_RETRY_DURATION_SECONDS);
+                continue;
+            }
+            return;
+        }
+        throw new RetryExhaustedException("Failed to fetch local node id within provided period");
     }
 }
