9809bce136c72b0e3bb2bbc1816cc562ef6767f9
Pankti Majmudar <pmajmudar@vmware.com>
2023-03-27 17:39:24 -0700

    Move LR protobufs shared with runtime from infrastructure to runtime. (#3571)
    Some protobufs and constants are required in the runtime package.
    Adding a dependency from runtime -> infrastructure will result in a circular
    dependency.  So move the required protobufs and constants in the runtime package.

--- test/src/test/java/org/corfudb/integration/SourceForwardingDataSender.java
+++ test/src/test/java/org/corfudb/integration/SourceForwardingDataSender.java
@@ -22,10 +22,12 @@ import org.corfudb.runtime.LogReplication;
 import org.corfudb.runtime.LogReplication.LogReplicationEntryMsg;
 import org.corfudb.runtime.LogReplication.LogReplicationEntryType;
 import org.corfudb.runtime.LogReplication.LogReplicationMetadataResponseMsg;
+import org.corfudb.runtime.LogReplicationUtils;
 import org.corfudb.runtime.collections.CorfuStore;
 import org.corfudb.runtime.collections.TableOptions;
 import org.corfudb.runtime.collections.TxnContext;
 import org.corfudb.runtime.LogReplication.LogReplicationSession;
+import org.corfudb.runtime.LogReplication.ReplicationStatus;
 import org.corfudb.runtime.view.Address;
 
 import java.util.List;
@@ -95,7 +97,7 @@ public class SourceForwardingDataSender extends AbstractIT implements DataSender
 
     private CorfuStore sinkCorfuStore;
 
-    private static final String REPLICATION_STATUS_TABLE = LogReplicationMetadataManager.REPLICATION_STATUS_TABLE_NAME;
+    private static final String REPLICATION_STATUS_TABLE = LogReplicationUtils.REPLICATION_STATUS_TABLE_NAME;
 
     private LogReplicationSession session = DefaultClusterConfig.getSessions().get(0);
 
@@ -126,9 +128,9 @@ public class SourceForwardingDataSender extends AbstractIT implements DataSender
         sinkCorfuStore.openTable(LogReplicationMetadataManager.NAMESPACE,
                 REPLICATION_STATUS_TABLE,
                 LogReplicationSession.class,
-                LogReplicationMetadata.ReplicationStatus.class,
+                ReplicationStatus.class,
                 null,
-                TableOptions.fromProtoSchema(LogReplicationMetadata.ReplicationStatus.class));
+                TableOptions.fromProtoSchema(ReplicationStatus.class));
     }
 
     @Override
@@ -327,8 +329,7 @@ public class SourceForwardingDataSender extends AbstractIT implements DataSender
 
     public void checkStatusOnSink(boolean expectedDataConsistent) {
         try (TxnContext txn = sinkCorfuStore.txn(LogReplicationMetadataManager.NAMESPACE)) {
-            LogReplicationMetadata.ReplicationStatus status = (LogReplicationMetadata.ReplicationStatus)
-                txn.getRecord(REPLICATION_STATUS_TABLE, session).getPayload();
+            ReplicationStatus status = (ReplicationStatus) txn.getRecord(REPLICATION_STATUS_TABLE, session).getPayload();
             assertThat(status.getSinkStatus().getDataConsistent()).isEqualTo(expectedDataConsistent);
         }
     }
