fffa0782aebe73ca6efe75a03b5c754923fce7da
Shama Hegde <shama358@gmail.com>
2023-04-27 16:40:40 -0700

    Connection model redesign (#3560)

--- test/src/test/java/org/corfudb/infrastructure/log/BoundedMapTest.java
+++ test/src/test/java/org/corfudb/infrastructure/log/BoundedMapTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SuppressWarnings("checkstyle:magicnumber")
 public class BoundedMapTest {
 
     @Test
