c887ad5a5633c3225678f7532b22e9a756541a13
Shama Hegde <shama358@gmail.com>
2023-05-10 20:52:09 -0700

    completing the previously incomplete comment

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/plugins/LogReplicationTransportSelectorPlugin.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/infrastructure/plugins/LogReplicationTransportSelectorPlugin.java
@@ -4,7 +4,8 @@ import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * Log replication allows for any transport layer to be plugged in.
- * This interface must be implemented to give the name of the property, in the plugin config file, which
+ * This interface must be implemented to provide the name of the property, in the plugin config file, which corresponds
+ * to the transport framework to be used for the local cluster.
  */
 public interface LogReplicationTransportSelectorPlugin {
 
