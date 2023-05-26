8110ecc01363d3679df097ccd25ddc4fb0de269c
zfrenette <66146392+zfrenette@users.noreply.github.com>
2023-05-26 14:02:10 -0400

    Update BoundedMap PreConditions

--- infrastructure/src/main/java/org/corfudb/infrastructure/log/BoundedMap.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/log/BoundedMap.java
@@ -18,7 +18,7 @@ public class BoundedMap {
     public BoundedMap(long offset, int size) {
         Preconditions.checkArgument(offset >= 0);
         Preconditions.checkArgument(size > 0);
-        Preconditions.checkArgument(Math.addExact(offset, size) < Integer.MAX_VALUE);
+        Math.addExact(offset, size);
         this.offset = offset;
         this.array = new AtomicLongArray(size);
         for (int idx = 0; idx < array.length(); idx++) {
