299df6b6450db38195d0e881cf73c6e2a026c153
Sundar Sridharan <sundar.sridharan@gmail.com>
2023-06-23 10:56:33 -0700

    Q lookalike logupdate test (#3660)
    * Design Spec for LR Routing Queue model (#3654)
    
    * Design Spec for LR Routing Queue modelw
    
    * Add more details on routing queues and stream tags
    
    * minor corrections
    
    * Can a logUpdate() be done to imitiate an enqueue() operation
    
    * Queue look alike POC test case
    
    * add comment on how to capture pre-commit hook
    
    * Add logUpdate() method for LR Routing Queue model (#3670)
    
    * logUpdateThatLooksLikeEnqueue
    
    * Address Sundar and Pankti's comments
    
    * Address Pankti's comment
    
    ---------
    
    Co-authored-by: LapHoi Un <ulaphoi@vmware.com>
    
    * Remove namespace validation for logUpdateEnqueue()
    
    * fixes to test the correct behavior properly
    
    ---------
    
    Co-authored-by: unlaphoi <67265290+unlaphoi@users.noreply.github.com>
    Co-authored-by: LapHoi Un <ulaphoi@vmware.com>

--- runtime/src/main/java/org/corfudb/runtime/LogReplicationUtils.java
+++ runtime/src/main/java/org/corfudb/runtime/LogReplicationUtils.java
@@ -47,6 +47,7 @@ import static org.corfudb.runtime.view.TableRegistry.CORFU_SYSTEM_NAMESPACE;
  */
 @Slf4j
 public final class LogReplicationUtils {
+
     public static final String LR_STATUS_STREAM_TAG = "lr_status";
 
     // Retain the old name from LR v1 to avoid polluting the registry table with stale entries.
@@ -67,11 +68,13 @@ public final class LogReplicationUtils {
     public static final String SNAPSHOT_SYNC_QUEUE_TAG_SENDER_PREFIX = "lrq_snapsync_";
 
     // Prefix of the name of queue as it will appear on the receiver after replicated.  The suffix will be the Sender
-    // (Source) cluster id
-    public static final String REPLICATED_QUEUE_NAME_PREFIX = "LRQ_Recv_";
+    // (Source) cluster id Receiving queues per client name.
+    public static final String REPLICATED_QUEUE_NAME = "LRQ_Recv-source_cluster_id";
 
     // Stream tag applied to the replicated queue on the receiver
-    public static final String REPLICATED_QUEUE_TAG = "lrq_recv";
+    public static final String REPLICATED_QUEUE_TAG = "lrq_recv-source_cluster_id";
+
+    public static final String SNAP_SYNC_START_END_Q_NAME = "LRQ_SNAPSHOT_START_END_MARKER";
 
     // ---- End RoutingQueue Model constants -------/
 
@@ -80,8 +83,12 @@ public final class LogReplicationUtils {
     public static final UUID lrFullSyncSendQId = CorfuRuntime.getStreamID(TableRegistry
             .getFullyQualifiedTableName(CORFU_SYSTEM_NAMESPACE, SNAPSHOT_SYNC_QUEUE_NAME_SENDER));
 
+    public static final UUID lrSnapStartEndQId = CorfuRuntime.getStreamID(TableRegistry
+            .getFullyQualifiedTableName(CORFU_SYSTEM_NAMESPACE, SNAP_SYNC_START_END_Q_NAME));
+
     public static boolean skipCheckpointFor(UUID streamId) {
-        return streamId.equals(lrLogEntrySendQId) || streamId.equals(lrFullSyncSendQId);
+        return streamId.equals(lrLogEntrySendQId) || streamId.equals(lrFullSyncSendQId)
+                || streamId.equals(lrSnapStartEndQId);
     }
 
     private LogReplicationUtils() { }
@@ -147,6 +154,108 @@ public final class LogReplicationUtils {
                             "have specified stream tag: %s", table.getFullyQualifiedTableName(), tagToValidate));
                 }
             }
+        }
+    }
+    private static long getRoutingQueueSubscriptionTimestamp(CorfuStore corfuStore, String namespace,
+                                                             LogReplicationRoutingQueueListener clientListener) {
+        Optional<Counter> mvoTrimCounter = MicroMeterUtils.counter("logreplication.subscribe.trim.count");
+        Optional<Counter> conflictCounter = MicroMeterUtils.counter("logreplication.subscribe.conflict.count");
+        Optional<Timer.Sample> subscribeTimer = MicroMeterUtils.startTimer();
+
+        Table<LogReplicationSession, ReplicationStatus, Message> replicationStatusTable =
+                openReplicationStatusTable(corfuStore);
+
+        try {
+            return IRetry.build(IntervalRetry.class, () -> {
+                try (TxnContext txnContext = corfuStore.txn(namespace)) {
+                    // The transaction is started in the client's namespace and the Replication Status table resides in the
+                    // system namespace.  Corfu Store does not validate the cross-namespace access as long as there are no
+                    // writes on the table in the different namespace.  This hack is required here as we want client full
+                    // sync to happen in the same transaction which checks the status of a snapshot sync so that there is no
+                    // window between the check and full sync.
+                    List<CorfuStoreEntry<LogReplicationSession, ReplicationStatus, Message>> entries =
+                            txnContext.executeQuery(replicationStatusTable,
+                                    entry -> entry.getKey().getSubscriber().getModel()
+                                            .equals(LogReplication.ReplicationModel.ROUTING_QUEUES) &&
+                                            Objects.equals(entry.getKey().getSubscriber().getClientName(),
+                                                    clientListener.getClientName()));
+
+                    CorfuStoreEntry<LogReplicationSession, ReplicationStatus, Message> entry = null;
+
+                    // It is possible that there is no entry for the routing queue Model in the status table in
+                    // the following scenarios:
+                    // 1. Request to subscribe is received before the Log Replication JVM or pod starts and
+                    // initializes the table
+                    // 2. Topology change which removes this cluster from the status table.
+                    // We cannot differentiate between the two cases here so continue with the right
+                    // behavior for the 1st case, i.e., subscribe and wait for the initial Snapshot Sync to get
+                    // triggered.  If we are here because of the second case, the listener will not get any updates
+                    // from LR and subscription will be a no-op.
+                    if (entries.isEmpty()) {
+                        log.info("No record for client {} and Logical Group Model found in the Status Table.  " +
+                                "Subscription could have been attempted before LR startup.  Subscribe the listener and" +
+                                "wait for initial Snapshot Sync", clientListener.getClientName());
+                    } else {
+                        // For a given replication model and client, any Sink node will have a single session.
+                        Preconditions.checkState(entries.size() == 1);
+                        entry = entries.get(0);
+                    }
+
+                    boolean snapshotSyncInProgress = false;
+
+                    if (entry == null || entry.getPayload().getSinkStatus().getDataConsistent()) {
+                        // No snapshot sync is in progress
+                        log.info("No Snapshot Sync is in progress.  Request the client to perform a full sync on its " +
+                                "tables.");
+                        Optional<Timer.Sample> clientFullSyncTimer = MicroMeterUtils.startTimer();
+                        long clientFullSyncStartTime = System.currentTimeMillis();
+                        clientListener.performFullSyncAndMerge(txnContext);
+                        long clientFullSyncEndTime = System.currentTimeMillis();
+                        MicroMeterUtils.time(clientFullSyncTimer, "logreplication.client.fullsync.duration");
+                        log.info("Client Full Sync and Merge took {} ms",
+                                clientFullSyncEndTime - clientFullSyncStartTime);
+                    } else {
+                        // Snapshot sync is in progress.  Subscribe without performing a full sync on the tables.
+                        log.info("Snapshot Sync is in progress.  Subscribing without performing a full sync on client" +
+                                " tables.");
+                        snapshotSyncInProgress = true;
+                    }
+                    txnContext.commit();
+
+                    // Subscribe from the snapshot timestamp of this transaction, i.e., log tail when the transaction started.
+                    // Subscribing from the commit address will result in missed updates which took place between the start
+                    // and end of the transaction because reads in a transaction observe updates only till the snapshot when it
+                    // started.
+                    long subscriptionTimestamp = txnContext.getTxnSequence();
+
+                    // Update the flags and variables on the listener based on whether snapshot sync was in progress.
+                    // This must be done only after the transaction commits.
+                    setRoutingQueueListenerParamsForSnapshotSync(clientListener, subscriptionTimestamp, snapshotSyncInProgress);
+
+                    return subscriptionTimestamp;
+                } catch (TransactionAbortedException tae) {
+                    if (tae.getCause() instanceof TrimmedException) {
+                        // If the snapshot version where this transaction started has been evicted from the JVM's MVO
+                        // cache, a trimmed exception is thrown and requires a retry at a later timestamp.
+                        incrementCount(mvoTrimCounter);
+                        log.warn("Snapshot no longer available in the cache.  Retrying.", tae);
+                    } else if (tae.getAbortCause() == AbortCause.CONFLICT) {
+                        // Concurrent updates to the client's tables
+                        incrementCount(conflictCounter);
+                        log.warn("Concurrent updates to client tables.  Retrying.", tae);
+                    } else {
+                        log.error("Unexpected type of Transaction Aborted Exception", tae);
+                    }
+                    throw new RetryNeededException();
+                } catch (Exception e) {
+                    log.error("Unexpected exception type hit", e);
+                    throw new RetryNeededException();
+                }
+            }).run();
+        } catch (InterruptedException e) {
+            throw new StreamingException(e, StreamingException.ExceptionCause.SUBSCRIBE_ERROR);
+        } finally {
+            MicroMeterUtils.time(subscribeTimer, "logreplication.subscribe.duration");
         }
     }
 
