adabeb7f2408c1826f7489dd5c79e22a3b24f6bc
xcchang <xchang@vmware.com>
2023-06-25 23:19:56 -0700

    Override isValidTransactionEntry for routing queue reader

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/send/logreader/RoutingQueuesLogEntryReader.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/send/logreader/RoutingQueuesLogEntryReader.java
@@ -2,22 +2,24 @@ package org.corfudb.infrastructure.logreplication.replication.send.logreader;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import com.google.common.base.Preconditions;
+import lombok.NonNull;
 import org.corfudb.infrastructure.logreplication.infrastructure.LogReplicationContext;
 import org.corfudb.protocols.logprotocol.OpaqueEntry;
 import org.corfudb.protocols.logprotocol.SMREntry;
 import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.runtime.LogReplication.LogReplicationSession;
+import org.corfudb.runtime.LogReplicationUtils;
 import org.corfudb.runtime.Queue.RoutingTableEntryMsg;
-import org.corfudb.runtime.view.TableRegistry;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
-import static org.corfudb.runtime.LogReplicationUtils.LOG_ENTRY_SYNC_QUEUE_NAME_SENDER;
 import static org.corfudb.runtime.LogReplicationUtils.REPLICATED_QUEUE_NAME_PREFIX;
-import static org.corfudb.runtime.view.TableRegistry.CORFU_SYSTEM_NAMESPACE;
 
 
 /**
@@ -35,10 +37,8 @@ public class RoutingQueuesLogEntryReader extends BaseLogEntryReader {
     }
 
     @Override
-    protected OpaqueEntry filterTransactionEntry(OpaqueEntry opaqueEntry) {
-        List<SMREntry> routingTableEntryMsgs = opaqueEntry.getEntries()
-            .get(CorfuRuntime.getStreamID(TableRegistry.getFullyQualifiedTableName(CORFU_SYSTEM_NAMESPACE,
-                    LOG_ENTRY_SYNC_QUEUE_NAME_SENDER)));
+    OpaqueEntry filterTransactionEntry(OpaqueEntry opaqueEntry) {
+        List<SMREntry> routingTableEntryMsgs = opaqueEntry.getEntries().get(LogReplicationUtils.lrLogEntrySendQId);
 
         List<SMREntry> filteredMsgs = new ArrayList<>();
 
@@ -56,5 +56,14 @@ public class RoutingQueuesLogEntryReader extends BaseLogEntryReader {
         opaqueEntryMap.put(CorfuRuntime.getStreamID(REPLICATED_QUEUE_NAME_PREFIX + session.getSourceClusterId()),
                 filteredMsgs);
         return new OpaqueEntry(opaqueEntry.getVersion(), opaqueEntryMap);
+    }
+
+    @Override
+    boolean isValidTransactionEntry(@NonNull OpaqueEntry entry) {
+        Set<UUID> txEntryStreamIds = new HashSet<>(entry.getEntries().keySet());
+        Preconditions.checkState(txEntryStreamIds.size() == 1, "Routing queue session's log" +
+                " entries should only come from the shared data queue for log entry sync");
+
+        return txEntryStreamIds.contains(LogReplicationUtils.lrLogEntrySendQId);
     }
 }
