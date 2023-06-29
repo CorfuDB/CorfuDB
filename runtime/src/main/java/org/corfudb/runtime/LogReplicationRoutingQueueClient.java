940158b869f8c0f09a38169cf06241ee2dc59d36
Shreay Patel <shreaypatel@yahoo.com>
2023-06-28 21:29:27 -0700

    Routing Queue Client Refactor (#3678)
    * Routing Queue Client Refactor
    
    ---------
    
    Co-authored-by: Shreay Patel <pshreay@vmware.com>

--- runtime/src/main/java/org/corfudb/runtime/LogReplicationRoutingQueueClient.java
+++ runtime/src/main/java/org/corfudb/runtime/LogReplicationRoutingQueueClient.java
@@ -1,21 +1,18 @@
 package org.corfudb.runtime;
 
-import org.corfudb.runtime.collections.LRMessageWithDestinations;
+import org.corfudb.runtime.Queue.RoutingTableEntryMsg;
+import org.corfudb.runtime.collections.TxnContext;
 
 import java.util.List;
 
 public interface LogReplicationRoutingQueueClient {
-
-    // TODO (V2): This field should be removed after the rpc stream is added for Sink side session creation.
-    String DEFAULT_ROUTING_QUEUE_CLIENT = "00000000-0000-0000-0000-0000000000002";
-
     /**
      * Transmits Delta Messages.
      * Must be invoked with the same transaction builder that was used for as actual data modification.
      * This method must be used if transaction has multiple messages. Repetitive calls in the same transaction to the
      * transmitDeltaMessages/transmitDeltaMessage won't be supported
      */
-    void transmitDeltaMessage(byte[] payload, List<String> destinations);
+    void transmitDeltaMessage(TxnContext txn, RoutingTableEntryMsg message);
 
-    void transmitDeltaMessages(List<LRMessageWithDestinations> messages);
+    void transmitDeltaMessages(TxnContext txn, List<RoutingTableEntryMsg> messages);
 }
