b400bc33755e599d4f3c634da168391876a62e2e
xcchang <xchang@vmware.com>
2023-07-12 20:53:51 -0700

    Fix client transmit methods

--- runtime/src/main/java/org/corfudb/runtime/LogReplicationRoutingQueueClient.java
+++ runtime/src/main/java/org/corfudb/runtime/LogReplicationRoutingQueueClient.java
@@ -1,8 +1,10 @@
 package org.corfudb.runtime;
 
 import org.corfudb.runtime.Queue.RoutingTableEntryMsg;
+import org.corfudb.runtime.collections.CorfuStore;
 import org.corfudb.runtime.collections.TxnContext;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public interface LogReplicationRoutingQueueClient {
@@ -12,7 +14,7 @@ public interface LogReplicationRoutingQueueClient {
      * This method must be used if transaction has multiple messages. Repetitive calls in the same transaction to the
      * transmitDeltaMessages/transmitDeltaMessage won't be supported
      */
-    void transmitDeltaMessage(TxnContext txn, RoutingTableEntryMsg message);
+    void transmitDeltaMessage(TxnContext txn, RoutingTableEntryMsg message, CorfuStore corfuStore) throws Exception;
 
-    void transmitDeltaMessages(TxnContext txn, List<RoutingTableEntryMsg> messages);
+    void transmitDeltaMessages(TxnContext txn, List<RoutingTableEntryMsg> messages, CorfuStore corfuStore) throws Exception;
 }
