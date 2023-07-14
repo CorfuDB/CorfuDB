07c95b0f57aaf7c0d286705d721d50093af10182
Sundar Sridharan <sundar.sridharan@gmail.com>
2023-07-13 17:02:45 -0700

    Full Sync Implementation for Routing Queues (#3705)
    * Full Sync Implementation for RoutingQ
    
    Remove DEMO_NAMESPACE
    Reuse SnapStartEndMarker
    
    * Fix compilation issue
    
    * Use a Start Marker in Snapshot Reader to determine the Base Snapshot Timestamp.
    
    ---------
    
    Co-authored-by: Sourav Kumar <souravk1@vmware.com>
    Co-authored-by: Pankti Majmudar <pmajmudar@vmware.com>

--- runtime/src/main/java/org/corfudb/runtime/LogReplicationUtils.java
+++ runtime/src/main/java/org/corfudb/runtime/LogReplicationUtils.java
@@ -38,8 +38,6 @@ import static org.corfudb.runtime.view.TableRegistry.CORFU_SYSTEM_NAMESPACE;
  */
 @Slf4j
 public final class LogReplicationUtils {
-    public static final String DEMO_NAMESPACE = "nsx";
-
 
     public static final String LR_STATUS_STREAM_TAG = "lr_status";
 
@@ -68,17 +66,21 @@ public final class LogReplicationUtils {
     // Stream tag applied to the replicated queue on the receiver
     public static final String REPLICATED_QUEUE_TAG = "lrq_recv";
 
-    public static final String SNAPSHOT_END_MARKER_TABLE_NAME = "SnapshotSyncEndMarker";
+    public static final String SNAP_SYNC_START_END_Q_NAME = "LRQ_SNAPSHOT_START_END_MARKER";
 
     // ---- End RoutingQueue Model constants -------/
 
     public static final UUID lrLogEntrySendQId = CorfuRuntime.getStreamID(TableRegistry
-            .getFullyQualifiedTableName(DEMO_NAMESPACE, LOG_ENTRY_SYNC_QUEUE_NAME_SENDER));
+            .getFullyQualifiedTableName(CORFU_SYSTEM_NAMESPACE, LOG_ENTRY_SYNC_QUEUE_NAME_SENDER));
     public static final UUID lrFullSyncSendQId = CorfuRuntime.getStreamID(TableRegistry
-            .getFullyQualifiedTableName(DEMO_NAMESPACE, SNAPSHOT_SYNC_QUEUE_NAME_SENDER));
+            .getFullyQualifiedTableName(CORFU_SYSTEM_NAMESPACE, SNAPSHOT_SYNC_QUEUE_NAME_SENDER));
+
+    public static final UUID lrSnapStartEndQId = CorfuRuntime.getStreamID(TableRegistry
+            .getFullyQualifiedTableName(CORFU_SYSTEM_NAMESPACE, SNAP_SYNC_START_END_Q_NAME));
 
     public static boolean skipCheckpointFor(UUID streamId) {
-        return streamId.equals(lrLogEntrySendQId) || streamId.equals(lrFullSyncSendQId);
+        return streamId.equals(lrLogEntrySendQId) || streamId.equals(lrFullSyncSendQId)
+                || streamId.equals(lrSnapStartEndQId);
     }
 
     private LogReplicationUtils() { }
