6ad07df5d3253e1b806dc5dd91e580d8f8c83786
Pankti Majmudar <pmajmudar@vmware.com>
2023-07-31 18:40:50 -0700

    Address Compilation Errors from log_replication_v2 merge.

--- test/src/test/java/org/corfudb/integration/LogReplicationIT.java
+++ test/src/test/java/org/corfudb/integration/LogReplicationIT.java
@@ -39,6 +39,7 @@ import org.corfudb.util.Utils;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
