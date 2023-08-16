f207a2afaa776d14a86bb67f1034aeb5f01456c3
Shama Hegde <shama358@gmail.com>
2023-08-16 11:14:31 +0530

    remove topologyId changes and address self TODO in sourceManager

--- runtime/src/main/java/org/corfudb/runtime/CorfuRuntime.java
+++ runtime/src/main/java/org/corfudb/runtime/CorfuRuntime.java
@@ -332,7 +332,7 @@ public class CorfuRuntime {
          * Used only by LR.
          * On LogData.getPayload(), after deserializing the data part, the data part should not be set to null.
          */
-        boolean nullifyDataOnGetPayload = true;
+        boolean retainSerializedDataInCache = true;
 
         // endregion
 
@@ -842,7 +842,7 @@ public class CorfuRuntime {
                 corfuRuntimeParameters.setCacheWrites(cacheWrites);
                 corfuRuntimeParameters.setClientName(clientName);
                 corfuRuntimeParameters.setCheckpointTriggerFreqMillis(checkpointTriggerFreqMillis);
-                corfuRuntimeParameters.setNullifyDataOnGetPayload(nullifyDataOnGetPayload);
+                corfuRuntimeParameters.setRetainSerializedDataInCache(nullifyDataOnGetPayload);
                 return corfuRuntimeParameters;
             }
         }
