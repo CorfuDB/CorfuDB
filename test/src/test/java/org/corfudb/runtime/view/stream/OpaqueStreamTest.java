647d0a8700d708628b2a4fb6327a28babda1b4cb
Shama Hegde <shama358@gmail.com>
2023-08-16 19:54:58 +0530

    address Anny's comments : rename + comments +test

--- test/src/test/java/org/corfudb/runtime/view/stream/OpaqueStreamTest.java
+++ test/src/test/java/org/corfudb/runtime/view/stream/OpaqueStreamTest.java
@@ -4,24 +4,34 @@ import com.google.common.reflect.TypeToken;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.corfudb.CustomSerializer;
+import org.corfudb.infrastructure.logreplication.proto.Sample;
 import org.corfudb.protocols.logprotocol.OpaqueEntry;
 import org.corfudb.protocols.service.CorfuProtocolLogReplication;
+import org.corfudb.protocols.wireprotocol.LogData;
 import org.corfudb.protocols.wireprotocol.Token;
 import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.runtime.MultiCheckpointWriter;
+import org.corfudb.runtime.collections.CorfuStore;
 import org.corfudb.runtime.collections.PersistentCorfuTable;
+import org.corfudb.runtime.collections.Table;
+import org.corfudb.runtime.collections.TableOptions;
+import org.corfudb.runtime.collections.TxnContext;
 import org.corfudb.runtime.exceptions.SerializerException;
 import org.corfudb.runtime.view.AbstractViewTest;
+import org.corfudb.runtime.view.TableRegistry;
 import org.corfudb.util.serializer.ISerializer;
 import org.corfudb.util.serializer.Serializers;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.corfudb.integration.LogReplicationAbstractIT.NAMESPACE;
 import static org.corfudb.protocols.service.CorfuProtocolLogReplication.extractOpaqueEntries;
 import static org.corfudb.protocols.service.CorfuProtocolLogReplication.generatePayload;
 
@@ -107,6 +117,69 @@ public class OpaqueStreamTest extends AbstractViewTest {
         OpaqueStream opaqueStream = new OpaqueStream(streamView);
         List<OpaqueEntry> entries = opaqueStream.streamUpTo(Integer.MAX_VALUE).collect(Collectors.toList());
         Assertions.assertThat(extractOpaqueEntries(generatePayload(entries)).size()).isEqualTo(entries.size());
+    }
+
+
+    /**
+     * 1. Create a runtime. By default "RetainSerializedDataInCache" is false.
+     * 2. Write 3 records, and deserialize them by calling LogData.getPayload().
+     * 3.Ensure that the data part is null and observe the opaqueStream skipping those entries.
+     * ------
+     * 4. Now set "RetainSerializedDataInCache" as true.
+     * 5. Write 3 more records, and deserialize them by calling LogData.getPayload()
+     * 6. The data part will not be not-null, and opaque stream will be able to read those entries.
+     */
+    @Test
+    public void deserializeDataAndGenerateOpaqueStream() throws Exception {
+        CorfuRuntime runtime = getDefaultRuntime();
+        CorfuStore store = new CorfuStore(runtime);
+        Table<Sample.StringKey, Sample.IntValueTag, Sample.Metadata> tableSource =
+                store.openTable(NAMESPACE, "test", Sample.StringKey.class,
+                        Sample.IntValueTag.class, Sample.Metadata.class,
+                        TableOptions.fromProtoSchema(Sample.IntValueTag.class));
+
+        final int entryCount = 3;
+        List<Long> addressWrittenTo = new ArrayList<>();
+        for (int i = 0; i < entryCount; i++) {
+            Sample.StringKey stringKey = Sample.StringKey.newBuilder().setKey(String.valueOf(i)).build();
+            Sample.IntValueTag IntValueTag = Sample.IntValueTag.newBuilder().setValue(i).build();
+            Sample.Metadata metadata = Sample.Metadata.newBuilder().setMetadata("Metadata_" + i).build();
+            try (TxnContext txn = store.txn(NAMESPACE)) {
+                txn.putRecord(tableSource, stringKey, IntValueTag, metadata);
+                addressWrittenTo.add(txn.commit().getSequence());
+            }
+        }
+
+        validateOpaqueStream(runtime, 0, addressWrittenTo);
+
+        runtime.getParameters().setRetainSerializedDataInCache(true);
+        validateOpaqueStream(runtime, entryCount, addressWrittenTo);
+    }
+
+    private void validateOpaqueStream(CorfuRuntime runtime, int expected, List<Long> addressWrittenTo) {
+        //invalidate the cache, so the data is read from the logUnit server
+        runtime.getAddressSpaceView().invalidateClientCache();
+
+        // read the previously written data
+        for(long address : addressWrittenTo) {
+            LogData logData = (LogData) runtime.getAddressSpaceView().read(address);
+            assertThat(logData.getData()).isNotEmpty();
+
+            // verify the "data" part after the logData is deserialized
+            logData.getPayload(runtime);
+            if(runtime.getParameters().isRetainSerializedDataInCache()) {
+                assertThat(logData.getData()).isNotEmpty();
+            } else {
+                assertThat(logData.getData()).isNullOrEmpty();
+            }
+        }
+
+        IStreamView streamView = runtime.getStreamsView().get(CorfuRuntime.getStreamID(TableRegistry.getFullyQualifiedTableName(NAMESPACE,
+                "test")));
+        OpaqueStream opaqueStream = new OpaqueStream(streamView);
+        List<OpaqueEntry> entries = opaqueStream.streamUpTo(Integer.MAX_VALUE).collect(Collectors.toList());
+
+        Assertions.assertThat(entries.size()).isEqualTo(expected);
     }
 
     @Test
