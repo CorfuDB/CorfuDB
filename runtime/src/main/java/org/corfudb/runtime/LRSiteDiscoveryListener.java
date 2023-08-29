d6344ad85a389a022d75ef195e8b87ce91fe359d
Pankti Majmudar <pmajmudar@vmware.com>
2023-08-29 13:13:11 -0700

    Support for a new client using ROUTING_QUEUES replication model. Fixes.

--- runtime/src/main/java/org/corfudb/runtime/LRSiteDiscoveryListener.java
+++ runtime/src/main/java/org/corfudb/runtime/LRSiteDiscoveryListener.java
@@ -3,11 +3,15 @@ package org.corfudb.runtime;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.runtime.collections.CorfuStore;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * This listener will notify when a new remote site for specific
@@ -15,47 +19,58 @@ import java.util.concurrent.TimeUnit;
  */
 @Slf4j
 public abstract class LRSiteDiscoveryListener {
-    private final Set<String> knownSites;
+
     protected final CorfuStore corfuStore;
     private final ScheduledExecutorService executorService;
     private final boolean executorServiceStartedByMe;
 
+    private final String clientName;
+
+    private final Map<String, Set<String>> clientToKnownSites = new HashMap<>();
+
     public LRSiteDiscoveryListener(CorfuStore corfuStore,
-                                   LogReplication.ReplicationModel replicationModel) {
+                                   LogReplication.ReplicationModel replicationModel, String clientName) {
         if (!replicationModel.equals(LogReplication.ReplicationModel.ROUTING_QUEUES)) {
             throw new UnsupportedOperationException("Only routing queue based clients are currently supported");
         }
         this.executorService = new ScheduledThreadPoolExecutor(1);
         this.executorServiceStartedByMe = true;
         this.corfuStore = corfuStore;
-        this.knownSites = new HashSet<>();
+        this.clientName = clientName;
+        clientToKnownSites.put(clientName, new HashSet<>());
         executorService.scheduleWithFixedDelay(this::pollForNewSites, 0, 10, TimeUnit.SECONDS);
     }
 
     public LRSiteDiscoveryListener(CorfuStore corfuStore,
                                    LogReplication.ReplicationModel replicationModel,
-                                   ScheduledExecutorService executorService) {
+                                   ScheduledExecutorService executorService, String clientName) {
         if (!replicationModel.equals(LogReplication.ReplicationModel.ROUTING_QUEUES)) {
             throw new UnsupportedOperationException("Only routing queue based clients are currently supported");
         }
         this.corfuStore = corfuStore;
-        this.knownSites = new HashSet<>();
         this.executorService = executorService;
         this.executorServiceStartedByMe = false;
+        this.clientName = clientName;
+        clientToKnownSites.put(clientName, new HashSet<>());
         executorService.scheduleWithFixedDelay(this::pollForNewSites, 0, 10, TimeUnit.SECONDS);
     }
 
     public void pollForNewSites() {
         corfuStore.getRuntime().getTableRegistry().listTables().forEach(tableName -> {
             if (tableName.getTableName().startsWith(LogReplicationUtils.REPLICATED_RECV_Q_PREFIX)) {
-                String siteId = tableName.getTableName().substring(LogReplicationUtils.REPLICATED_RECV_Q_PREFIX.length());
-                if (!knownSites.contains(siteId)) {
+                String siteId = StringUtils.substringBetween(tableName.getTableName(), "_");
+                String client = StringUtils.substringAfterLast(tableName.getTableName(), "_");
+                if (!Objects.equals(client, clientName)) {
+                    return;
+                }
+
+                if (!clientToKnownSites.get(client).contains(siteId)) {
                     try {
-                        log.info("Discovered new site {}", siteId);
+                        log.info("Discovered new site {} for client {}", siteId, client);
                         this.onNewSiteUp(siteId);
-                        knownSites.add(siteId);
+                        clientToKnownSites.get(client).add(siteId);
                     } catch (Exception e) {
-                        log.error("LRSiteDiscovery callback hit error on site {}, {}", siteId, e);
+                        log.error("LRSiteDiscovery callback hit error on site {}, client {}, {}", siteId, client, e);
                     }
                 }
             }
