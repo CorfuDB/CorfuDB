0a26e5c5181c587175c8f6a5d49911a019ed6311
Pankti Majmudar <pmajmudar@vmware.com>
2023-09-09 15:13:15 -0700

    Request for snapshot sync data only if timed out waiting for data. (#3771)
    * Request for snapshot sync data only if timed out waiting for data.
    
    * Remove a redundant TODO.

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/send/LogReplicationEventMetadata.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/replication/send/LogReplicationEventMetadata.java
@@ -1,6 +1,7 @@
 package org.corfudb.infrastructure.logreplication.replication.send;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
@@ -31,6 +32,9 @@ public class LogReplicationEventMetadata {
     private long lastTransferredBaseSnapshot;
 
     private boolean forceSnapshotSync = false;
+
+    @Setter
+    private boolean timeoutException = false;
 
     /**
      * Empty Metadata
@@ -97,5 +101,9 @@ public class LogReplicationEventMetadata {
     }
 
     public boolean isForcedSnapshotSync() { return this.forceSnapshotSync; }
+
+    public boolean isTimeoutException() {
+        return timeoutException;
+    }
 }
 
