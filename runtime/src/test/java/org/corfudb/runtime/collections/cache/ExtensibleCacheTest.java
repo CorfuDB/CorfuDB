893409644f17f9ba42a1279c2175a38a9ce78d75
Viacheslav <xrw.null@gmail.com>
2023-11-06 14:42:06 -0800

    Extensible corfu storage implementation. The main class is ExtensibleCache, provides a kv interface put/get and works as an embedded database like Sqlite. It uses DiskBackedCorfuTable which works on top of RocksDb.

--- runtime/src/test/java/org/corfudb/runtime/collections/cache/ExtensibleCacheTest.java
+++ runtime/src/test/java/org/corfudb/runtime/collections/cache/ExtensibleCacheTest.java
@@ -1,6 +1,8 @@
 package org.corfudb.runtime.collections.cache;
 
 import org.corfudb.runtime.object.PersistenceOptions;
+import org.corfudb.runtime.object.RocksDbStore.IndexMode;
+import org.corfudb.runtime.object.RocksDbStore.StoreMode;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -16,6 +18,8 @@ public class ExtensibleCacheTest {
     public void testAccess() throws Exception {
         PersistenceOptions persistenceOptions = PersistenceOptions.builder()
                 .dataPath(testTempDir.newFolder().toPath())
+                .storeMode(StoreMode.PERSISTENT)
+                .indexMode(IndexMode.NON_INDEX)
                 .build();
 
         ExtensibleCache<String, String> cache = new ExtensibleCache<>(persistenceOptions);
