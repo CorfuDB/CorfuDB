d2161dd8b02b45245ab4a69a72815cada292c432
Chetan Gudisagar <chetangudisagar@gmail.com>
2024-01-29 10:17:23 -0800

    Syntactical changes

--- test/src/test/java/org/corfudb/infrastructure/CorfuServerNodeTest.java
+++ test/src/test/java/org/corfudb/infrastructure/CorfuServerNodeTest.java
@@ -14,4 +14,17 @@ public class CorfuServerNodeTest {
         CorfuServerNode node = new CorfuServerNode(context);
         assertThatCode(node::close).doesNotThrowAnyException();
     }
+
+    // Test that restarting server channel and closing the new channels does not
+    // cause any failures
+    @Test
+    public void restartServerChannelTest() {
+        final int port = 9000;
+        ServerContext context = ServerContextBuilder.defaultContext(port);
+        CorfuServerNode node = new CorfuServerNode(context);
+
+        node.restartServerChannel();
+
+        assertThatCode(node::close).doesNotThrowAnyException();
+    }
 }
