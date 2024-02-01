6c4af55c435d2e826b30891785088d248f35b6b2
Maithem <Maithem@users.noreply.github.com>
2024-02-01 15:23:00 -0800

    Fault Detector/ClusterStateAggregator Bug
    This patch fixes a bug where incorrect aggregation of poll
    reports leads to unnecessary cluster configurations. Specifically,
    if the last poll fails to heartbeat one of the nodes, then
    NodeConnectivity::connectivity would be coalesced as failed
    despite succeeding in the previous polls.

--- infrastructure/src/test/java/org/corfudb/infrastructure/management/ClusterStateAggregatorTest.java
+++ infrastructure/src/test/java/org/corfudb/infrastructure/management/ClusterStateAggregatorTest.java
@@ -1,12 +1,20 @@
 package org.corfudb.infrastructure.management;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.corfudb.protocols.wireprotocol.ClusterState;
 import org.corfudb.protocols.wireprotocol.NodeState;
+import org.corfudb.protocols.wireprotocol.failuredetector.NodeRank;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.corfudb.infrastructure.NodeNames.A;
@@ -62,7 +70,6 @@ public class ClusterStateAggregatorTest {
                 NodeState.getUnavailableNodeState(C)
         );
 
-        final int epoch = 1;
         ClusterState clusterState3 = ClusterState.buildClusterState(
                 localEndpoint,
                 ImmutableList.of(),
@@ -82,7 +89,7 @@ public class ClusterStateAggregatorTest {
                 .build();
 
         //check [CONNECTED, CONNECTED, CONNECTED]
-        NodeState expectedLocalNodeState = clusterState3.getNode(localEndpoint).get();
+        NodeState expectedLocalNodeState = nodeState(localEndpoint, epoch, OK, OK, FAILED);
         NodeState localNodeState = aggregator.getAggregatedState().getNode(localEndpoint).get();
         assertThat(localNodeState.isConnected()).isTrue();
         assertThat(localNodeState).isEqualTo(expectedLocalNodeState);
@@ -98,5 +105,99 @@ public class ClusterStateAggregatorTest {
         NodeState nodeCState = aggregator.getAggregatedState().getNode(C).get();
         assertThat(nodeCState.isConnected()).isFalse();
         assertThat(nodeCState).isEqualTo(expectedNodeCState);
+    }
+
+    @Test
+    public void testAggregation() {
+
+        ClusterState cs1 = ClusterState.buildClusterState(
+                A,
+                ImmutableList.of(),
+                nodeState(A, epoch, OK, OK, OK),
+                nodeState(B, epoch, OK, OK, OK),
+                nodeState(C, epoch, OK, OK, OK)
+        );
+
+        ClusterState cs2 = ClusterState.buildClusterState(
+                A,
+                ImmutableList.of(),
+                nodeState(A, epoch, OK, OK, OK),
+                nodeState(B, epoch, OK, OK, OK),
+                nodeState(C, epoch, OK, OK, OK)
+        );
+
+        ClusterState cs3 = ClusterState.buildClusterState(
+                A,
+                ImmutableList.of(),
+                nodeState(A, epoch, OK, OK, FAILED),
+                nodeState(B, epoch, OK, OK, OK),
+                nodeState(C, epoch, OK, OK, OK)
+        );
+
+        PollReport pollReport1 = PollReport.builder()
+                .pingResponsiveServers(ImmutableList.of(A, B, C))
+                .wrongEpochs(ImmutableMap.of())
+                .clusterState(cs1)
+                .elapsedTime(Duration.ZERO)
+                .build();
+
+        PollReport pollReport2 = PollReport.builder()
+                .pingResponsiveServers(ImmutableList.of(A, B, C))
+                .wrongEpochs(ImmutableMap.of())
+                .clusterState(cs2)
+                .elapsedTime(Duration.ZERO)
+                .build();
+
+        PollReport pollReport3 = PollReport.builder()
+                .pingResponsiveServers(ImmutableList.of(A, B))
+                .wrongEpochs(ImmutableMap.of())
+                .clusterState(cs3)
+                .elapsedTime(Duration.ZERO)
+                .build();
+
+        List<PollReport> reports = new ArrayList<>();
+        reports.add(pollReport1);
+        reports.add(pollReport2);
+        reports.add(pollReport3);
+
+        PollReport pr = aggregatePollReports(epoch, reports, ImmutableList.<String>builder().build());
+
+        CompleteGraphAdvisor advisor = new CompleteGraphAdvisor();
+        Optional<NodeRank> ret = advisor.failedServer(pr.getClusterState());
+        assertThat(ret).isEmpty();
+    }
+
+    private PollReport aggregatePollReports(long epoch, List<PollReport> reports,
+                                            ImmutableList<String> layoutUnresponsiveNodes) {
+        Map<String, Long> wrongEpochsAggregated = new HashMap<>();
+
+        reports.forEach(report -> {
+            //Calculate wrong epochs
+            wrongEpochsAggregated.putAll(report.getWrongEpochs());
+            report.getReachableNodes().forEach(wrongEpochsAggregated::remove);
+        });
+
+        List<ClusterState> clusterStates = reports.stream()
+                .map(PollReport::getClusterState)
+                .collect(Collectors.toList());
+
+        ClusterStateAggregator aggregator = ClusterStateAggregator.builder()
+                .localEndpoint(A)
+                .clusterStates(clusterStates)
+                .unresponsiveNodes(layoutUnresponsiveNodes)
+                .build();
+
+        Duration totalElapsedTime = reports.stream()
+                .map(PollReport::getElapsedTime)
+                .reduce(Duration.ZERO, Duration::plus);
+
+        final ClusterState aggregatedClusterState = aggregator.getAggregatedState();
+        return PollReport.builder()
+                .pollEpoch(epoch)
+                .elapsedTime(totalElapsedTime)
+                .pingResponsiveServers(aggregatedClusterState.getPingResponsiveNodes())
+                .wrongEpochs(ImmutableMap.copyOf(wrongEpochsAggregated))
+                .clusterState(aggregatedClusterState)
+                .build();
     }
 }
