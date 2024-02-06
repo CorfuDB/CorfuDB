499e8c4d55fcd678d8bca1e4ce3a993059b19669
Viacheslav <314292+xnull@users.noreply.github.com>
2024-02-06 10:13:00 -0800

    Jvm metrics: safepoint decommission (#3885)

--- common/src/main/java/org/corfudb/common/metrics/micrometer/JVMMetrics.java
+++ common/src/main/java/org/corfudb/common/metrics/micrometer/JVMMetrics.java
@@ -1,31 +1,16 @@
 package org.corfudb.common.metrics.micrometer;
 
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-import lombok.Data;
-import lombok.Getter;
-import sun.management.HotspotRuntimeMBean;
-import sun.management.ManagementFactoryHelper;
 
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Provides JVM level metrics.
  */
 
 public final class JVMMetrics {
-
-    @Getter(lazy = true)
-    private final static HotspotRuntimeMBean runtimeMBean = ManagementFactoryHelper.getHotspotRuntimeMBean();
-
-    @Data
-    private static class SafePointStats {
-        long safepointTime;
-        long safepointCount;
-    }
 
     private static void subscribeThreadMetrics(MeterRegistry meterRegistry) {
         JvmThreadMetrics threadMetrics = new JvmThreadMetrics();
@@ -37,34 +22,12 @@ public final class JVMMetrics {
         memoryMetrics.bindTo(meterRegistry);
     }
 
-    private static void subscribeSafePointMetrics(MeterRegistry meterRegistry) {
-        SafePointStats safePointStats = new SafePointStats();
-        Gauge.builder("jvm.safe_point_time", safePointStats, data -> {
-            long current = getRuntimeMBean().getTotalSafepointTime();
-            long delta = current - data.safepointTime;
-            data.safepointTime = current;
-            return delta;
-        }).baseUnit(TimeUnit.MILLISECONDS.toString())
-                .strongReference(true)
-                .register(meterRegistry);
-
-        Gauge.builder("jvm.safe_point_count", safePointStats, data -> {
-            long current = getRuntimeMBean().getSafepointCount();
-            long delta = current - data.safepointCount;
-            data.safepointCount = current;
-            return delta;
-        }).baseUnit(TimeUnit.MILLISECONDS.toString())
-                .strongReference(true)
-                .register(meterRegistry);
-    }
-
     public static void register(Optional<MeterRegistry> metricsRegistry) {
 
         if (metricsRegistry.isPresent()) {
             final MeterRegistry meterRegistry = metricsRegistry.get();
             subscribeMemoryMetrics(meterRegistry);
             subscribeThreadMetrics(meterRegistry);
-            subscribeSafePointMetrics(meterRegistry);
         }
     }
 }
