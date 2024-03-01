7c67b085ab3a294d61dbb0c2056a4361f6221c3f
Shreay Patel <pshreay@vmware.com>
2024-03-01 00:21:45 -0800

    Address test failures

--- infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/transport/sample/GRPCLogReplicationClientChannelAdapter.java
+++ infrastructure/src/main/java/org/corfudb/infrastructure/logreplication/transport/sample/GRPCLogReplicationClientChannelAdapter.java
@@ -311,7 +311,7 @@ public class GRPCLogReplicationClientChannelAdapter extends IClientChannelAdapte
             log.info("Metadata request for session {}", session);
             if (sessionToBlockingStubMap.containsKey(session)) {
                 ResponseMsg response = sessionToBlockingStubMap.get(session)
-                        .withDeadlineAfter(180000, TimeUnit.MILLISECONDS)
+                        .withDeadlineAfter(5000, TimeUnit.MILLISECONDS)
                         .withWaitForReady()
                         .negotiate(request);
                 receive(response);
