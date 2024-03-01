00585ef50c815557ce5e771b362e63b2e9fec69a
Chetan Gudisagar <chetangudisagar@gmail.com>
2024-03-01 13:29:04 -0800

    Bring back UTs and other unintentional changes

--- common/src/main/java/org/corfudb/common/metrics/micrometer/JVMMetrics.java
+++ common/src/main/java/org/corfudb/common/metrics/micrometer/JVMMetrics.java
@@ -11,6 +11,7 @@ import java.util.Optional;
  */
 
 public final class JVMMetrics {
+
     private static void subscribeThreadMetrics(MeterRegistry meterRegistry) {
         JvmThreadMetrics threadMetrics = new JvmThreadMetrics();
         threadMetrics.bindTo(meterRegistry);
