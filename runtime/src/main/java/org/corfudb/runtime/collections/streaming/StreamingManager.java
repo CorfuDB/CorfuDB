e73b53bb34c18aff47f03899a012208224b3a810
Sundar Sridharan <sundar.sridharan@gmail.com>
2024-03-05 15:15:21 -0800

    Revert "Special LR Stream Listener across namespaces. (#3573)"
    This reverts commit 33f7733503df051723a4ad87a0dfda324a2e05d2.

--- runtime/src/main/java/org/corfudb/runtime/collections/streaming/StreamingManager.java
+++ runtime/src/main/java/org/corfudb/runtime/collections/streaming/StreamingManager.java
@@ -4,7 +4,6 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.protocols.wireprotocol.StreamAddressRange;
 import org.corfudb.runtime.CorfuRuntime;
-import org.corfudb.runtime.LogReplicationListener;
 import org.corfudb.runtime.collections.StreamListener;
 import org.corfudb.runtime.exceptions.StreamingException;
 import org.corfudb.runtime.exceptions.TrimmedException;
@@ -14,18 +13,11 @@ import org.corfudb.runtime.view.stream.StreamAddressSpace;
 import org.corfudb.util.Utils;
 
 import javax.annotation.Nonnull;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-
-import static org.corfudb.runtime.LogReplicationUtils.LR_STATUS_STREAM_TAG;
-import static org.corfudb.runtime.LogReplicationUtils.REPLICATION_STATUS_TABLE_NAME;
-import static org.corfudb.runtime.view.TableRegistry.CORFU_SYSTEM_NAMESPACE;
 
 /**
  * A streaming subscription manager that allows clients to listen on
@@ -96,31 +88,6 @@ public class StreamingManager {
         this.scheduler.addTask(streamListener, namespace, streamTag, tablesOfInterest, lastAddress, bufferSize);
     }
 
-    /**
-     * This subscription is exclusive to Log Replication(LR).  It is used for subscribing to transaction updates across
-     * LR's metadata table(ReplicationStatus) in the CorfuSystem namespace and application-specific tables containing
-     * 'streamTag' within the application namespace.
-     *
-     * @param streamListener   client listener for callback
-     * @param namespace       namespace of application tables on which updates should be received
-     * @param streamTag       only updates of application tables with the stream tag will be polled
-     * @param tablesOfInterest only updates from these tables will be returned
-     * @param lastAddress      last processed address, new notifications start from lastAddress + 1
-     */
-    public void subscribeLogReplicationListener(@Nonnull LogReplicationListener streamListener, @Nonnull String namespace,
-                                                @Nonnull String streamTag, @Nonnull List<String> tablesOfInterest,
-                                                long lastAddress, int bufferSize) {
-        Map<String, List<String>> nsToTableName = new HashMap<>();
-        nsToTableName.put(namespace, tablesOfInterest);
-        nsToTableName.put(CORFU_SYSTEM_NAMESPACE, Arrays.asList(REPLICATION_STATUS_TABLE_NAME));
-
-        Map<String, String> nsToStreamTags = new HashMap<>();
-        nsToStreamTags.put(namespace, streamTag);
-        nsToStreamTags.put(CORFU_SYSTEM_NAMESPACE, LR_STATUS_STREAM_TAG);
-        this.scheduler.addLRTask(streamListener, nsToStreamTags, nsToTableName, lastAddress,
-                bufferSize == 0 ? defaultBufferSize : bufferSize);
-    }
-
 
     // TODO(Maithem): this is obsolete, delta stream already detects this case, but can't be
     // removed until some tests that depend on this internal behavior are fixed
@@ -153,6 +120,7 @@ public class StreamingManager {
         // log warn message if it didnt exist?
         this.scheduler.removeTask(streamListener);
     }
+
 
     /**
      * Shutdown the streaming manager and clean up resources.
