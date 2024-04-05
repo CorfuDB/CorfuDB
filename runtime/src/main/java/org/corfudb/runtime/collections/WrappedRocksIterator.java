4b99eed85d839aeeb94d3aeaf9d165a44d080ae3
Vjeko Brajkovic <vjeko@brajkovic.org>
2024-04-05 13:59:24 -0700

    @rocksdb: Upgrade RocksDB JNI to 9.1.1
    This release brings support for batched implementation of multi-get
    which is leveraged by secondary indexes in disk-backed tables.

--- runtime/src/main/java/org/corfudb/runtime/collections/WrappedRocksIterator.java
+++ runtime/src/main/java/org/corfudb/runtime/collections/WrappedRocksIterator.java
@@ -4,6 +4,7 @@ import org.corfudb.runtime.exceptions.unrecoverable.UnrecoverableCorfuError;
 import org.rocksdb.RocksDBException;
 import org.rocksdb.RocksIterator;
 import org.rocksdb.RocksIteratorInterface;
+import org.rocksdb.Snapshot;
 
 import java.nio.ByteBuffer;
 
@@ -144,6 +145,11 @@ public class WrappedRocksIterator implements RocksIteratorInterface {
 
     @Override
     public void refresh(){
+        throw new UnsupportedOperationException("refresh");
+    }
+
+    @Override
+    public void refresh(Snapshot snapshot) throws RocksDBException {
         throw new UnsupportedOperationException("refresh");
     }
 
