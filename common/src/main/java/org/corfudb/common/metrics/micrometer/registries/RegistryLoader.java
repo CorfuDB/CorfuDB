77bb1cc3b3009d2ddf5a20d8992715eeca65b2db
Pavel Zaytsev <zaytsev.pavel.a@gmail.com>
2024-08-30 14:34:59 -0700

    Metric registry provider metadata (#4006)
    * Fix registry cleanup + metadata for provided registries
    
    * Fix loader
    
    * Common tags to provided registries

--- common/src/main/java/org/corfudb/common/metrics/micrometer/registries/RegistryLoader.java
+++ common/src/main/java/org/corfudb/common/metrics/micrometer/registries/RegistryLoader.java
@@ -1,15 +1,16 @@
 package org.corfudb.common.metrics.micrometer.registries;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.ServiceLoader;
+import java.util.stream.Collectors;
 
 /**
  * Lazily loads the registry providers.
  */
 public class RegistryLoader {
 
-    private final ServiceLoader<RegistryProvider> loader = ServiceLoader.load(RegistryProvider.class,
-            ClassLoader.getSystemClassLoader());
+    private final ServiceLoader<RegistryProvider> loader = ServiceLoader.load(RegistryProvider.class);
 
     public Iterator<RegistryProvider> getRegistries() {
         return loader.iterator();
