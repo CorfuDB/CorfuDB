ff20059175270474d906ddd847d86da766f783e4
Chetan Gudisagar <chetangudisagar@gmail.com>
2024-09-09 16:31:54 -0700

    Revert "Metric registry provider metadata (#4006) (#4011)" (#4013)
    This reverts commit 5732e8cdbd7c85cb31421a3b7a35649c07a9b676.

--- common/src/main/java/org/corfudb/common/metrics/micrometer/registries/RegistryProvider.java
+++ common/src/main/java/org/corfudb/common/metrics/micrometer/registries/RegistryProvider.java
@@ -2,15 +2,13 @@ package org.corfudb.common.metrics.micrometer.registries;
 
 import io.micrometer.core.instrument.MeterRegistry;
 
-import java.util.Map;
-
 /**
  * Clients who are willing to integrate their registries into Corfu metrics composite registry
  * must implement this interface.
  */
 public interface RegistryProvider {
 
-    MeterRegistry provideRegistry(Map<String, String> registryMetadata);
+    MeterRegistry provideRegistry();
 
     void close();
 
