5ad838f1ecf9388720d2e6b8726169e3f74c0214
Chetan Gudisagar <chetangudisagar@gmail.com>
2024-09-09 11:13:15 -0700

    Revert "Metric registry provider metadata (#4006) (#4011)"
    This reverts commit 5732e8cdbd7c85cb31421a3b7a35649c07a9b676.

--- common/src/main/java/org/corfudb/common/metrics/micrometer/registries/RegistryLoader.java
+++ common/src/main/java/org/corfudb/common/metrics/micrometer/registries/RegistryLoader.java
@@ -1,16 +1,15 @@
 package org.corfudb.common.metrics.micrometer.registries;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
 
 /**
  * Lazily loads the registry providers.
  */
 public class RegistryLoader {
 
-    private final ServiceLoader<RegistryProvider> loader = ServiceLoader.load(RegistryProvider.class);
+    private final ServiceLoader<RegistryProvider> loader = ServiceLoader.load(RegistryProvider.class,
+            ClassLoader.getSystemClassLoader());
 
     public Iterator<RegistryProvider> getRegistries() {
         return loader.iterator();
